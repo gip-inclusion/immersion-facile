d2c57df33a849157f043de7894da3bda030c24bf
Nicolas Wettstein <nicolas.wettstein@beta.gouv.fr>
2021-07-21 00:43:17 +1000

    fix broken test in JsonTodoRepository.integration.test

--- back/src/domain/todos/ports/Clock.ts
+++ back/src/domain/todos/ports/Clock.ts
@@ -9,7 +9,7 @@ export class RealClock implements Clock {
 }
 
 export class CustomClock implements Clock {
-  nextDate = new Date();
+  nextDate = new Date("2021-07-21T12:00:00.000Z");
 
   getNow() {
     return this.nextDate;
