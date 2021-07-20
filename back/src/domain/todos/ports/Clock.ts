341af3a7452d0411500d8f2ea764f504c2cef1e3
Nicolas Wettstein <nicolas.wettstein@beta.gouv.fr>
2021-07-21 01:25:27 +1000

    better error message + fix date creation

--- back/src/domain/todos/ports/Clock.ts
+++ back/src/domain/todos/ports/Clock.ts
@@ -9,7 +9,7 @@ export class RealClock implements Clock {
 }
 
 export class CustomClock implements Clock {
-  nextDate = new Date("2021-07-21T12:00:00.000Z");
+  nextDate = new Date("2021-07-21T10:00:00");
 
   getNow() {
     return this.nextDate;
