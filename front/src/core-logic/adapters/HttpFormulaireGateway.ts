7501f53615137f10993993d0dfbfac5f0ef48829
Jean-Francois Macresy <jean-francois.macresy@beta.gouv.fr>
2021-08-19 11:42:55 +0000

    Getting the Form ID and raising an alert

--- front/src/core-logic/adapters/HttpFormulaireGateway.ts
+++ front/src/core-logic/adapters/HttpFormulaireGateway.ts
@@ -1,12 +1,14 @@
 import axios from "axios";
 import { FormulaireGateway } from "src/core-logic/ports/formulaireGateway";
 import { formulairesRoute } from "src/shared/routes";
-import type { FormulaireDto } from "src/shared/FormulaireDto";
+import type { FormulaireDto, AddFormulaireResponseDto } from "src/shared/FormulaireDto";
 
 const prefix = "api";
 
 export class HttpFormulaireGateway implements FormulaireGateway {
-  public async add(formulaireDto: FormulaireDto): Promise<void> {
-    await axios.post(`/${prefix}/${formulairesRoute}`, formulaireDto);
+  public async add(formulaireDto: FormulaireDto): Promise<AddFormulaireResponseDto> {
+    const response = await axios.post(`/${prefix}/${formulairesRoute}`, formulaireDto);
+    console.log(response);
+    return response.data;
   }
 }
