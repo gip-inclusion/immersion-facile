60276ffaca556dd19cfdc9a091a5cdb5aadb111d
Jérôme Burkard <burkardjerome@gmail.com>
2021-09-29 20:15:35 +0200

    handle delete case for Business Contact

--- front/src/app/ImmersionOffer/BusinessContactList.tsx
+++ front/src/app/ImmersionOffer/BusinessContactList.tsx
@@ -2,11 +2,13 @@ import { useField } from "formik";
 import React from "react";
 import { ProfessionList } from "src/app/ImmersionOffer/ProfessionList";
 import { ButtonAdd } from "src/components/ButtonAdd";
+import { DeleteButton } from "src/components/DeleteButton";
 import { TextInput } from "src/components/form/TextInput";
 import {
   BusinessContactDto,
   ImmersionOfferDto,
 } from "src/shared/ImmersionOfferDto";
+import { removeAtIndex } from "../../../../back/src/shared/utils";
 
 const emptyContact: BusinessContactDto = {
   email: "",
@@ -21,10 +23,14 @@ export const BusinessContactList = () => {
   const name: keyof ImmersionOfferDto = "businessContacts";
   const [field, _, { setValue }] = useField<BusinessContactDto[]>({ name });
 
+  const onDelete = (index: number) => {
+    setValue(removeAtIndex(field.value, index));
+  };
+
   return (
     <div style={{ display: "flex", flexDirection: "column" }}>
       {field.value.map((_, index) => (
-        <BusinessContact index={index} key={index} />
+        <BusinessContact index={index} key={index} onDelete={onDelete} />
       ))}
       <ButtonAdd
         style={{ alignSelf: "center", margin: "30px auto" }}
@@ -38,16 +44,20 @@ export const BusinessContactList = () => {
 
 type BusinessContactProps = {
   index: number;
+  onDelete: (indexToDelete: number) => void;
 };
 
-const BusinessContact = ({ index }: BusinessContactProps) => {
+const BusinessContact = ({ index, onDelete }: BusinessContactProps) => {
   const parentFieldName: keyof ImmersionOfferDto = "businessContacts";
   const makeName = (name: keyof BusinessContactDto) =>
     `${parentFieldName}[${index}].${name}`;
 
   return (
     <div>
-      <h4>Détails du référent :</h4>
+      <div style={{ display: "flex", alignItems: "center", padding: "10px" }}>
+        <DeleteButton onClick={() => onDelete(index)} />
+        <h4 style={{ margin: 0 }}>Détails du référent :</h4>
+      </div>
       <TextInput label="Nom du référent *" name={makeName("lastName")} />
       <TextInput label="Prénom du référent *" name={makeName("firstName")} />
       <TextInput label="Fonction du référent *" name={makeName("job")} />
