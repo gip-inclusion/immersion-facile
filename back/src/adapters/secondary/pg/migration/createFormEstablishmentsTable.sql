018eabd42623bc4fb7c657c1463e551b4e303d23
Jérôme Burkard <burkardjerome@gmail.com>
2021-10-25 15:19:27 +0200

    fix naf and add configurable ports for PG and adminer in docker-compose

--- back/src/adapters/secondary/pg/migration/createFormEstablishmentsTable.sql
+++ back/src/adapters/secondary/pg/migration/createFormEstablishmentsTable.sql
@@ -3,7 +3,7 @@ CREATE TABLE public.form_establishments (
     siret char(14) NOT NULL,
     business_name varchar(255) NOT NULL,
     business_address varchar(255) NOT NULL,
-    naf jsonb NOT NULL,
+    naf jsonb,
     professions jsonb NOT NULL,
     business_contacts jsonb NOT NULL,
     preferred_contact_methods jsonb NOT NULL
