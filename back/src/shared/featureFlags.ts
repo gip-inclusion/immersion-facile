1ad2e79cb3bd5fc68cadc43231de0ab48ee54805
stkhapugin <stkhapugin@chromium.org>
2021-11-24 16:21:41 +0100

    feature flag

--- back/src/shared/featureFlags.ts
+++ back/src/shared/featureFlags.ts
@@ -5,6 +5,12 @@ export const getFeatureFlags = (processEnv: ProcessEnv) => {
 
   return {
     enableAdminUi: getBooleanVariable("ENABLE_ADMIN_UI"),
+    // Flag that enables the separate signatures by the company and the beneficiary.
+    // This introduces additional application states for collecting signatures of
+    // both the company and the beneficiary; as well as state transitions that permit
+    // the company and the beneficiary modify the application (revoking other
+    // party's signature, if present).
+    enableEnterpriseSignature: getBooleanVariable("ENABLE_ENTERPRISE_SIGNATURE"),
   };
 };
 
