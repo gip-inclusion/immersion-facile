06a112d346a5aa2c7cf78ce7abcb630f47ddacc5
stkhapugin <stkhapugin@chromium.org>
2021-12-14 09:21:33 +0100

    subj

--- front/src/app/ApplicationForm/createSuccessInfos.ts
+++ front/src/app/ApplicationForm/createSuccessInfos.ts
@@ -1,3 +1,7 @@
+import { ENV } from "src/environmentVariables";
+
+const { featureFlags } = ENV;
+
 export interface SuccessInfos {
   message: string;
   link: string | undefined;
@@ -6,10 +10,16 @@ export interface SuccessInfos {
 export const createSuccessInfos = (link: string | undefined): SuccessInfos => ({
   message: link
     ? "Vous pouvez acceder à votre demande avec le lien suivant :"
-    : `Merci d'avoir complété ce formulaire. Il va être transmis à votre conseiller
-      référent. Il vous informera par mail de la validation ou non de l'immersion.
-      Le tuteur qui vous encadrera pendant cette période recevra aussi la réponse.
-      Attention, ne démarrez jamais une immersion tant que vous n'avez pas reçu de
-      validation ! Bonne journée !`,
+    : defaultSuccessMessage(),
   link,
 });
+
+const defaultSuccessMessage = () =>
+  featureFlags.enableEnterpriseSignature
+    ? `Merci d'avoir complété cette demande d'immersion. Elle n'est plus modifiable. 
+Vous allez pouvoir désormais confirmer votre demande. Un mail vient de vous être envoyé.`
+    : `Merci d'avoir complété ce formulaire. Il va être transmis à votre conseiller
+référent. Il vous informera par mail de la validation ou non de l'immersion.
+Le tuteur qui vous encadrera pendant cette période recevra aussi la réponse.
+Attention, ne démarrez jamais une immersion tant que vous n'avez pas reçu de
+validation ! Bonne journée !`;
