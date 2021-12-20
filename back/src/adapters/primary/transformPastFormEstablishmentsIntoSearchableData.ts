bf70d4531098c18b4317413b6a6262a609d93570
Clément Moutet <clement.moutet@beta.gouv.fr>
2021-12-20 14:38:53 +0000

    Update script transform establishments so it only transforms establishments not yet present

--- back/src/adapters/primary/transformPastFormEstablishmentsIntoSearchableData.ts
+++ back/src/adapters/primary/transformPastFormEstablishmentsIntoSearchableData.ts
@@ -81,7 +81,13 @@ const transformPastFormEstablishmentsIntoSearchableData = async (
       new TestUuidGenerator(),
     );
   const AllIdsResult = await clientOrigin.query(
-    "SELECT id FROM public.form_establishments",
+    "WITH siretInFormEstablishment AS ( \
+    SELECT DISTINCT siret::text \
+      FROM   form_establishments\
+    ), \
+    siretFromFormInImmersionOffer AS (SELECT DISTINCT siret::text from immersion_offers where data_source = 'form') \
+    SELECT id FROM public.form_establishments WHERE siret IN \
+    ((SELECT siret FROM siretInFormEstablishment) EXCEPT (SELECT siret FROM siretFromFormInImmersionOffer))",
   );
   for (let pas = 0; pas < AllIdsResult.rows.length; pas++) {
     await transformFormEstablishmentIntoSearchData._execute(
