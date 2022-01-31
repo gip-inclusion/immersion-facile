01a4050461faadd09aca1d9bef2ef5c6f09b371e
Jérôme Burkard <burkardjerome@gmail.com>
2022-01-31 09:47:45 +0100

    migration to add address to agencies

--- back/src/adapters/secondary/pg/migration/alterAgenciesTable.sql
+++ back/src/adapters/secondary/pg/migration/alterAgenciesTable.sql
@@ -1,2 +1,5 @@
 ALTER TABLE public.agencies 
 ADD IF NOT EXISTS "position" public.geography(Point, 4326) NOT NULL DEFAULT public.st_geographyfromtext('POINT(0.00 0.00)'::text);
+
+ALTER TABLE public.agencies
+ADD IF NOT EXISTS "address" varchar(255) NULL;
