88c7c66bb0574d6494a7fb1d5c6a8d29f6708b90
Jérôme Burkard <burkardjerome@gmail.com>
2022-02-02 14:42:30 +0100

    add backup container

---
+++ backup/home/backup/BACKUP.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+ENV_TYPE=${ENV_TYPE:-develop};
+
+if [ "$ENV_TYPE" != "production" ];
+then
+	exit 0;
+fi
+
+DAY=`date +%A`
+NICE="/usr/bin/nice -n 15"
+
+LOCAL_DIR="/home/backup";
+DIST_DIR="/mnt/backup";
+FILENAME="docker_$DAY.tar.bz2";
+
+mkdir -p $DIST_DIR;
+
+$NICE tar --exclude=cache/* --exclude=*.log --exclude=*.gz --exclude=*.bz2 -C /home/docker/ -I pbzip2 -cf - . | tee $LOCAL_DIR/$FILENAME $DIST_DIR/$FILENAME >/dev/null;
