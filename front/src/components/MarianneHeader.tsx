6a08f1b3b120bb52849cbfe4216e364c9c0516fb
Jérôme Burkard <burkardjerome@gmail.com>
2022-02-04 10:18:59 +0100

    make logo clickable to go home

--- front/src/components/MarianneHeader.tsx
+++ front/src/components/MarianneHeader.tsx
@@ -1,16 +1,28 @@
 import React from "react";
+import { routes } from "src/app/routes";
 import immersionFacileLogo from "src/assets/Logo-immersion-facilitee-01-RVB-reflets-crop.svg";
 
+export const LinkHome: React.FC<{ className?: string }> = ({
+  children,
+  className,
+}) => (
+  <a {...routes.home().link} className={className}>
+    {children}
+  </a>
+);
+
 export const MarianneLogo = () => (
-  <div className="fr-header__brand-top">
-    <div className="fr-header__logo">
-      <p className="fr-logo">
-        République
-        <br />
-        Française
-      </p>
+  <LinkHome>
+    <div className="fr-header__brand-top">
+      <div className="fr-header__logo">
+        <p className="fr-logo">
+          République
+          <br />
+          Française
+        </p>
+      </div>
     </div>
-  </div>
+  </LinkHome>
 );
 
 export const MarianneHeader = () => (
@@ -24,11 +36,14 @@ export const MarianneHeader = () => (
         Faciliter la réalisation des immersions professionnelles
       </div>
     </div>
+
     <div
       className="flex flex-wrap justify-center"
       style={{ minWidth: "100px" }}
     >
-      <img src={immersionFacileLogo} alt="Logo Immersion-Facile" />
+      <LinkHome className="w-full h-full shadow-none">
+        <img src={immersionFacileLogo} alt="Logo Immersion-Facile" />
+      </LinkHome>
     </div>
   </section>
 );
