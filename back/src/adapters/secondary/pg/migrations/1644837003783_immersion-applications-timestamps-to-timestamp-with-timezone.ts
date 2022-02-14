732edae7fed29b1dde167da74ec776a8fddcb072
Romain Cambonie <romain.cambonie@gmail.com>
2022-02-14 14:00:54 +0100

    code review

--- back/src/adapters/secondary/pg/migrations/1644837003783_immersion-applications-timestamps-to-timestamp-with-timezone.ts
+++ back/src/adapters/secondary/pg/migrations/1644837003783_immersion-applications-timestamps-to-timestamp-with-timezone.ts
@@ -1,29 +1,57 @@
-/* eslint-disable @typescript-eslint/naming-convention */
-import {MigrationBuilder, AlterColumnOptions} from "node-pg-migrate";
+import { MigrationBuilder, AlterColumnOptions } from "node-pg-migrate";
 
 export const timestamptWithTimezone = (): AlterColumnOptions => ({
   type: "timestamptz",
   notNull: true,
-  allowNull: false,
 });
 
-export const timestamptWithTimezoneWithDefaultNow = (pgm: MigrationBuilder): AlterColumnOptions => ({
+export const timestamptWithTimezoneWithDefaultNow = (
+  pgm: MigrationBuilder,
+): AlterColumnOptions => ({
   ...timestamptWithTimezone(),
   default: pgm.func("now()"),
 });
 
 export async function up(pgm: MigrationBuilder): Promise<void> {
-  pgm.alterColumn("immersion_applications", "created_at", timestamptWithTimezoneWithDefaultNow(pgm));
-  pgm.alterColumn("immersion_applications", "updated_at", timestamptWithTimezoneWithDefaultNow(pgm));
-  pgm.alterColumn("immersion_applications", "date_start", timestamptWithTimezone());
-  pgm.alterColumn("immersion_applications", "date_end", timestamptWithTimezone());
-  pgm.alterColumn("immersion_applications", "date_submission", timestamptWithTimezone());
+  pgm.alterColumn(
+    "immersion_applications",
+    "created_at",
+    timestamptWithTimezoneWithDefaultNow(pgm),
+  );
+  pgm.alterColumn(
+    "immersion_applications",
+    "updated_at",
+    timestamptWithTimezoneWithDefaultNow(pgm),
+  );
+  pgm.alterColumn(
+    "immersion_applications",
+    "date_start",
+    timestamptWithTimezone(),
+  );
+  pgm.alterColumn(
+    "immersion_applications",
+    "date_end",
+    timestamptWithTimezone(),
+  );
+  pgm.alterColumn(
+    "immersion_applications",
+    "date_submission",
+    timestamptWithTimezone(),
+  );
 }
 
 export async function down(pgm: MigrationBuilder): Promise<void> {
-  pgm.alterColumn("immersion_applications", "created_at", { type: "timestamp" });
-  pgm.alterColumn("immersion_applications", "updated_at", { type: "timestamp" });
-  pgm.alterColumn("immersion_applications", "date_start", { type: "timestamp" });
+  pgm.alterColumn("immersion_applications", "created_at", {
+    type: "timestamp",
+  });
+  pgm.alterColumn("immersion_applications", "updated_at", {
+    type: "timestamp",
+  });
+  pgm.alterColumn("immersion_applications", "date_start", {
+    type: "timestamp",
+  });
   pgm.alterColumn("immersion_applications", "date_end", { type: "timestamp" });
-  pgm.alterColumn("immersion_applications", "date_submission", { type: "timestamp" });
+  pgm.alterColumn("immersion_applications", "date_submission", {
+    type: "timestamp",
+  });
 }
