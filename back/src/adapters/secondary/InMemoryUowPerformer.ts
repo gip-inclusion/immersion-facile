25e05ae28d4bfbf3a3dc0891ecf77093d7e8510d
Jérôme Burkard <burkardjerome@gmail.com>
2022-02-18 09:30:11 +0100

    remove console.log

--- back/src/adapters/secondary/InMemoryUowPerformer.ts
+++ back/src/adapters/secondary/InMemoryUowPerformer.ts
@@ -7,7 +7,6 @@ export class InMemoryUowPerformer implements UnitOfWorkPerformer {
   constructor(private uow: UnitOfWork) {}
 
   public async perform<T>(cb: (uow: UnitOfWork) => Promise<T>): Promise<T> {
-    console.log("IN UOW performer");
     return cb(this.uow);
   }
 }
