af27718e0710f26f99d1f01ce627dd57ef3df2c9
Raphaëlle Bertrand-Lalo <r.bertrand.lalo@gmail.com>
2022-02-24 14:48:52 +0100

    Add script to replay quarantined form events (if siret is correct)

---
+++ back/src/adapters/primary/freeQuarantinedFormEstablishmentsWithCorrectSiret.ts
@@ -0,0 +1,69 @@
+import { Pool } from "pg";
+import { random, sleep } from "../../shared/utils";
+import { createLogger } from "../../utils/logger";
+import { RealClock } from "../secondary/core/ClockImplementations";
+import {
+  defaultMaxBackoffPeriodMs,
+  defaultRetryDeadlineMs,
+  ExponentialBackoffRetryStrategy,
+} from "../secondary/core/ExponentialBackoffRetryStrategy";
+import { QpsRateLimiter } from "../secondary/core/QpsRateLimiter";
+import { HttpsSireneRepository } from "../secondary/HttpsSireneRepository";
+import { AppConfig } from "./appConfig";
+
+const maxQpsSireneApi = 0.25;
+
+const logger = createLogger(__filename);
+
+const clock = new RealClock();
+
+const config = AppConfig.createFromEnv();
+
+const freeQuarantinedFormEstablishmentsWithCorrectSiret = async () => {
+  logger.info(
+    "Starting to free quarantined form establishments with correct siret",
+  );
+
+  const dbUrl = config.pgImmersionDbUrl;
+  const pool = new Pool({
+    connectionString: dbUrl,
+  });
+  const client = await pool.connect();
+
+  const sireneRepository = new HttpsSireneRepository(
+    config.sireneHttpsConfig,
+    clock,
+    new QpsRateLimiter(maxQpsSireneApi, clock, sleep),
+    new ExponentialBackoffRetryStrategy(
+      defaultMaxBackoffPeriodMs,
+      defaultRetryDeadlineMs,
+      clock,
+      sleep,
+      random,
+    ),
+  );
+
+  const quarantinedFormEstablishmentAddedResult = await client.query(
+    "SELECT id, payload FROM outbox WHERE topic='FormEstablishmentAdded' AND was_quarantined LIMIT 20",
+  );
+  logger.info(
+    `Found ${quarantinedFormEstablishmentAddedResult.rowCount} events with topic 'FormEstablishmentAdded' in quarantined.`,
+  );
+  for (const { payload, id } of quarantinedFormEstablishmentAddedResult.rows) {
+    const siretIsCorrect = !!(await sireneRepository.get(payload.siret));
+    if (siretIsCorrect) {
+      await client.query(
+        "UPDATE outbox SET was_quarantined=false WHERE id=$1; ",
+        [id],
+      );
+      logger.info(`[OK] Event with id ${id} left quarantine !`);
+    } else
+      logger.info(
+        `[NOK] Event with id ${id} stays in quarantine, since siret ${payload.siret} seems incorrect.`,
+      );
+  }
+  client.release();
+  await pool.end();
+};
+
+freeQuarantinedFormEstablishmentsWithCorrectSiret();
