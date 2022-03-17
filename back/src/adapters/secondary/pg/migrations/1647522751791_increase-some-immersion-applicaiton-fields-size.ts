68f86bef73b35db7d4a1d9dabfaa5999d9c84ca7
Jérôme Burkard <burkardjerome@gmail.com>
2022-03-17 14:30:55 +0100

    change some immersion appliaction from varchar(255) to text

---
+++ back/src/adapters/secondary/pg/migrations/1647522751791_increase-some-immersion-applicaiton-fields-size.ts
@@ -0,0 +1,28 @@
+/* eslint-disable @typescript-eslint/naming-convention */
+import { MigrationBuilder } from "node-pg-migrate";
+
+export async function up(pgm: MigrationBuilder): Promise<void> {
+  pgm.alterColumn("immersion_applications", "immersion_objective", {
+    type: "text",
+  });
+  pgm.alterColumn("immersion_applications", "immersion_activities", {
+    type: "text",
+    notNull: true,
+  });
+  pgm.alterColumn("immersion_applications", "immersion_skills", {
+    type: "text",
+  });
+}
+
+export async function down(pgm: MigrationBuilder): Promise<void> {
+  pgm.alterColumn("immersion_applications", "immersion_objective", {
+    type: "varchar(255)",
+  });
+  pgm.alterColumn("immersion_applications", "immersion_activities", {
+    type: "varchar(255)",
+    notNull: true,
+  });
+  pgm.alterColumn("immersion_applications", "immersion_skills", {
+    type: "varchar(255)",
+  });
+}
