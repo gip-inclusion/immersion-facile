206c5e8d43b1ab39aa80c0dd0690a39d65bc2972
Romain Cambonie <romain.cambonie@gmail.com>
2022-04-01 11:34:24 +0200

    Revert "Rollback rome autocomplete in search immesion"
    This reverts commit 717848e1c76573c0d3c4aae2dc22eb9928f6ee8a.

--- front/src/app/Search/Search.tsx
+++ front/src/app/Search/Search.tsx
@@ -1,6 +1,7 @@
 import SearchIcon from "@mui/icons-material/Search";
 import { Form, Formik } from "formik";
 import { searchEpic } from "src/app/dependencies";
+import { RomeAutocomplete } from "src/app/Profession/RomeAutocomplete";
 import { OurAdvises } from "src/app/Search/OurAdvises";
 import { SearchResultPanel } from "src/app/Search/SearchResultPanel";
 import distanceSearchIcon from "/distance-search-icon.svg";
@@ -12,7 +13,6 @@ import { SearchButton } from "src/components/SearchButton";
 import { useObservable } from "src/useObservable";
 import { StaticDropdown } from "./Dropdown/StaticDropdown";
 import React from "react";
-import { AppellationAutocomplete } from "src/app/Profession/AppellationAutocomplete";
 
 interface SearchInput {
   rome?: string;
@@ -59,7 +59,7 @@ export const Search = () => {
               <Form>
                 <div className="gap-5 flex flex-col">
                   <div>
-                    <AppellationAutocomplete
+                    <RomeAutocomplete
                       title="Métier recherché"
                       setFormValue={(newValue) =>
                         setFieldValue("rome", newValue.romeCode)
