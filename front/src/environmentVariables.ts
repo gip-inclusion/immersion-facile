988c0badbf80d456b1e0a1f0f79ba28abc7da5ae
Romain Cambonie <romain.cambonie@gmail.com>
2022-04-08 19:26:18 +0200

    Added env_type key in ENV front constant

--- front/src/environmentVariables.ts
+++ front/src/environmentVariables.ts
@@ -1,9 +1,13 @@
 const gateway =
   import.meta.env.VITE_GATEWAY === "IN_MEMORY" ? "IN_MEMORY" : "HTTP";
 
+const env_type = import.meta.env.VITE_ENV_TYPE;
+
 console.info("Gateway is : ", gateway);
+console.info("Env type is : ", env_type);
 
 export const ENV = {
   dev: import.meta.env.DEV,
+  env_type,
   gateway,
 };
