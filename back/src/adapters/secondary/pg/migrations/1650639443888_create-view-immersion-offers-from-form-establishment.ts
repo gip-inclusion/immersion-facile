dda0a6b12daed3f16cf1bea0212e6da701b04d9d
Raphaëlle Bertrand-Lalo <r.bertrand.lalo@gmail.com>
2022-04-22 17:01:40 +0200

    Create view view_offers_from_form_establishment

---
+++ back/src/adapters/secondary/pg/migrations/1650639443888_create-view-immersion-offers-from-form-establishment.ts
@@ -0,0 +1,24 @@
+/* eslint-disable @typescript-eslint/naming-convention */
+import { MigrationBuilder } from "node-pg-migrate";
+
+export async function up(pgm: MigrationBuilder): Promise<void> {
+  pgm.createView(
+    "view_offers_from_form_establishment",
+    { replace: true },
+    `SELECT 
+      io.update_date, 
+      e.name, 
+      rome_label, 
+      appellation_label,
+      io.siret
+      FROM immersion_offers AS io
+      LEFT JOIN establishments AS e ON io.siret = e.siret
+      LEFT JOIN view_appellations_dto vad ON io.rome_appellation = vad.appellation_code
+      WHERE data_source = 'form'
+      ORDER BY (io.update_date, e.name)`,
+  );
+}
+
+export async function down(pgm: MigrationBuilder): Promise<void> {
+  pgm.dropView("view_offers_from_form_establishment");
+}
