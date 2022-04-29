5e7df8e6299adc04a07a1a711d81c8fd541ec558
Jérôme Burkard <burkardjerome@gmail.com>
2022-04-29 13:54:52 +0200

    fix sleep function

--- back/src/shared/utils.ts
+++ back/src/shared/utils.ts
@@ -4,27 +4,9 @@ export const stringOfNumbers = /^\+?[0-9]+$/;
 
 export type SleepFn = typeof sleep;
 
-// TODO WTF is that and how do we remove it from the solution
-/*
- * export const sleep = (ms: number): Promise<number> => {
- *  if (ms <= 0) {
- *    return Promise.resolve(0);
- *  }
- *  return new Promise((r) => setTimeout(r, ms));
- *};
- *
- * Suddenly give the following error on typecheck
- * error TS2345: Argument of type '(value: number | PromiseLike<number>) => void' is not assignable to parameter of type '(args: void) => void'.
- * Types of parameters 'value' and 'args' are incompatible.
- *   Type 'void' is not assignable to type 'number | PromiseLike<number>'.
- */
-export const sleep = (ms: number): Promise<number> => {
-  if (ms <= 0) {
-    return Promise.resolve(0);
-  }
-  // eslint-disable-next-line @typescript-eslint/no-empty-function
-  return new Promise((): NodeJS.Timeout => setTimeout(() => {}, ms));
-};
+// sleep function is use to simulate latency for demo and dev purpose
+export const sleep = (ms: number): Promise<number> =>
+  new Promise((resolve: any) => setTimeout(resolve, ms));
 
 export type RandomFn = typeof random;
 export const random = (max: number): number => Math.floor(Math.random() * max);
