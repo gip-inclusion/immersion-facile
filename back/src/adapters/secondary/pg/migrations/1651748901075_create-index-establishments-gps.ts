a542254532fb974b412c89111d847c8b84a0fdc1
Raphaëlle Bertrand-Lalo <r.bertrand.lalo@gmail.com>
2022-05-05 13:10:19 +0200

    Add index gps on establishments

---
+++ back/src/adapters/secondary/pg/migrations/1651748901075_create-index-establishments-gps.ts
@@ -0,0 +1,12 @@
+/* eslint-disable @typescript-eslint/naming-convention */
+import { MigrationBuilder } from "node-pg-migrate";
+
+export async function up(pgm: MigrationBuilder): Promise<void> {
+  await pgm.sql(
+    `CREATE INDEX establishments_gps ON establishments USING GIST (geography(gps));`,
+  );
+}
+
+export async function down(pgm: MigrationBuilder): Promise<void> {
+  pgm.dropIndex("establishments", "gps");
+}
