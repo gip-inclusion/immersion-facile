42cf79e183e82c901b89e3c4c810cc488b3ea770
Jérôme Burkard <burkardjerome@gmail.com>
2022-05-05 13:30:44 +0200

    fix siret fetching in establishment - wierd behaviero in zodValidate.ts...

--- front/src/uiComponents/form/zodValidate.ts
+++ front/src/uiComponents/form/zodValidate.ts
@@ -25,7 +25,10 @@ const createValidationError = (e: z.ZodError) => {
 export const toFormikValidationSchema = <T>(
   schema: z.ZodSchema<T>,
 ): { validate: (obj: T) => void } => ({
-  validate(obj: T) {
+  // super strange behavior: we need it to be async so that it correctly works...
+  // it broke the siret fetching in the establishment form when it was not...
+  // eslint-disable-next-line @typescript-eslint/require-await
+  async validate(obj: T): Promise<void> {
     try {
       schema.parse(obj);
     } catch (err: unknown) {
