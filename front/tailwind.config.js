dd4556753e5a53312723f8a9bb528b4d4e23f491
Benjamin Bohec <benjamin.bohec@beta.gouv.fr>
2022-06-14 16:37:17 +0200

    scan react design system for tailwind

--- front/tailwind.config.js
+++ front/tailwind.config.js
@@ -1,5 +1,9 @@
 module.exports = {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "./node_modules/react-design-system/src/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
     extend: {
       colors: {
