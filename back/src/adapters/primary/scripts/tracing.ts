de60c98befefa7648d87a36925331456e0e4df4b
Jérôme Burkard <burkardjerome@gmail.com>
2022-08-05 14:20:14 +0200

    WIP try with zipkin

--- back/src/adapters/primary/scripts/tracing.ts
+++ back/src/adapters/primary/scripts/tracing.ts
@@ -1,20 +1,21 @@
 import * as opentelemetry from "@opentelemetry/sdk-node";
 import { getNodeAutoInstrumentations } from "@opentelemetry/auto-instrumentations-node";
 import { diag, DiagConsoleLogger, DiagLogLevel } from "@opentelemetry/api";
-import { OTLPTraceExporter } from "@opentelemetry/exporter-trace-otlp-http";
+// import { OTLPTraceExporter } from "@opentelemetry/exporter-trace-otlp-http";
+import { ZipkinExporter } from "@opentelemetry/exporter-zipkin";
 
 // For troubleshooting, set the log level to DiagLogLevel.DEBUG
 diag.setLogger(new DiagConsoleLogger(), DiagLogLevel.INFO);
 
-const exporter = new OTLPTraceExporter({
-  // optional - url default value is http://localhost:4318/v1/traces
-  url: "http://localhost:4318/v1/traces",
-
-  // optional - collection of custom headers to be sent with each request, empty by default
-  headers: {},
-});
+// const exporter = new OTLPTraceExporter({
+//   // optional - url default value is http://localhost:4318/v1/traces
+//   url: "http://localhost:4318/v1/traces",
+//
+//   // optional - collection of custom headers to be sent with each request, empty by default
+//   headers: {},
+// });
 
 export const tracingSdk = new opentelemetry.NodeSDK({
-  traceExporter: exporter,
+  traceExporter: new ZipkinExporter(),
   instrumentations: [getNodeAutoInstrumentations()],
 });
