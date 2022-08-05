24ed52b5a229daa3381521fc39e929b6c61c8bc4
Jérôme Burkard <jerome.burkard@beta.gouv.fr>
2022-08-05 11:03:44 +0200

    add opentelemetry exporter

--- back/src/adapters/primary/scripts/tracing.ts
+++ back/src/adapters/primary/scripts/tracing.ts
@@ -1,11 +1,20 @@
 import * as opentelemetry from "@opentelemetry/sdk-node";
 import { getNodeAutoInstrumentations } from "@opentelemetry/auto-instrumentations-node";
 import { diag, DiagConsoleLogger, DiagLogLevel } from "@opentelemetry/api";
+import { OTLPTraceExporter } from "@opentelemetry/exporter-trace-otlp-http";
 
 // For troubleshooting, set the log level to DiagLogLevel.DEBUG
 diag.setLogger(new DiagConsoleLogger(), DiagLogLevel.INFO);
 
+const exporter = new OTLPTraceExporter({
+  // optional - url default value is http://localhost:4318/v1/traces
+  url: "http://localhost:4318/v1/traces",
+
+  // optional - collection of custom headers to be sent with each request, empty by default
+  headers: {},
+});
+
 export const tracingSdk = new opentelemetry.NodeSDK({
-  traceExporter: new opentelemetry.tracing.ConsoleSpanExporter(),
+  traceExporter: exporter,
   instrumentations: [getNodeAutoInstrumentations()],
 });
