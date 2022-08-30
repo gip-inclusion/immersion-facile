10c5a5d80f4e6eeda893bca69810aca6d31d2178
Benjamin Bohec <bbohec.pro@gmail.com>
2022-08-30 12:51:45 +0200

    remove app css & better DSFR css priority

--- front/src/app/main.tsx
+++ front/src/app/main.tsx
@@ -3,8 +3,8 @@ import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import { App } from "src/app/App";
 import { store } from "src/app/config/dependencies";
-import "@gouvfr/dsfr/dist/css/dsfr.min.css";
 import "src/assets/index.css";
+import "@gouvfr/dsfr/dist/css/dsfr.min.css";
 import { RouteProvider } from "./routing/routes";
 
 ReactDOM.render(
