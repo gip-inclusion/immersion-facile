2a5897f302e61fc61d8c87579082952ba8ec990a
Enguerran Weiss <enguerranweiss@gmail.com>
2022-09-02 14:49:41 +0200

    add crisp-react, add .env on front/

--- front/src/app/App.tsx
+++ front/src/app/App.tsx
@@ -1,9 +1,10 @@
 import React, { useEffect } from "react";
 import { useDispatch } from "react-redux";
+import Crisp from "crisp-react";
 import { useFetchFeatureFlags } from "src/app/utils/useFeatureFlags";
 import { adminSlice } from "src/core-logic/domain/admin/admin.slice";
 import { Router } from "./routing/Router";
-
+const { VITE_CRISP_WEBSITE_ID } = import.meta.env;
 const useCheckIfUserIsAdmin = () => {
   const dispatch = useDispatch();
   useEffect(() => {
@@ -14,10 +15,10 @@ const useCheckIfUserIsAdmin = () => {
 export const App = () => {
   useFetchFeatureFlags();
   useCheckIfUserIsAdmin();
-
   return (
     <>
       <Router />
+      <Crisp crispWebsiteId={VITE_CRISP_WEBSITE_ID} />
     </>
   );
 };
