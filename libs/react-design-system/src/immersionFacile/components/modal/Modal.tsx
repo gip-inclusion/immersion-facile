d012d080f664befc156e473219f081e2b9b62ec4
Enguerran Weiss <enguerranweiss@gmail.com>
2022-09-08 11:26:11 +0200

    add onRequestClose on modal

--- libs/react-design-system/src/immersionFacile/components/modal/Modal.tsx
+++ libs/react-design-system/src/immersionFacile/components/modal/Modal.tsx
@@ -1,4 +1,4 @@
-import React, { cloneElement, Children, useEffect } from "react";
+import React, { cloneElement, Children } from "react";
 
 import ReactDOM from "react-dom";
 import classNames from "classnames";
@@ -38,7 +38,7 @@ export const ModalDialog = ({
   const colSizes = { sm: 4, lg: 8, md: 6 };
   const _colSize = size ? colSizes[size] : null;
   const _className = classNames(
-    "fr-modal fr-container--fluid fr-container-md",
+    "fr-modal",
 
     {
       "fr-modal--opened": isOpen,
@@ -63,33 +63,12 @@ export const ModalDialog = ({
       React.isValidElement(child) && child.props.__TYPE === "ModalClose",
   );
 
-  useEffect(
-    () => () => {
-      document.body.style.overflow = "";
-    },
-    [],
-  );
-
   const handleAnimatedUnmount = () => {
     // handleModal(false);
     setTimeout(() => {
       if (focusBackTo) (focusBackTo as HTMLElement).focus();
       hide();
     }, MODAL_ANIMATION_TIME);
-  };
-
-  useEffect(() => {
-    if (isOpen) {
-      document.addEventListener("keydown", handleAllKeyDown);
-      return () => document.removeEventListener("keydown", handleAllKeyDown);
-    }
-  }, [isOpen]);
-
-  const handleAllKeyDown = (e: KeyboardEvent | React.KeyboardEvent) => {
-    if (e.key === "Escape") {
-      hide();
-      e.preventDefault();
-    }
   };
 
   let closeComponent;
@@ -102,12 +81,15 @@ export const ModalDialog = ({
       <ModalClose hide={handleAnimatedUnmount} />
     ) : null;
   }
+
   const component = (
     <Modal
       aria-labelledby="fr-modal-title-modal"
       className={_className}
       overlayClassName={"fr-container-md"}
       isOpen={isOpen}
+      onRequestClose={hide}
+      shouldCloseOnOverlayClick={true}
     >
       <div className="fr-grid-row fr-grid-row--center">
         <div className="fr-modal__body fr-col-md-6">
@@ -120,20 +102,6 @@ export const ModalDialog = ({
         </div>
       </div>
     </Modal>
-    // <div className="fr-container fr-container--fluid fr-container-md fr-col-md-6">
-    //   <div className="fr-grid-row fr-grid-row--center closing-overlay">
-    //     <div className={`fr-col-12 fr-col-md-${colSize}`}>
-    //       <div className="fr-modal__body">
-    //         <div className="fr-modal__header">{closeComponent}</div>
-    //         <div className="fr-modal__content">
-    //           {title}
-    //           {content}
-    //         </div>
-    //         {footer}
-    //       </div>
-    //     </div>
-    //   </div>
-    // </div>
   );
   return ReactDOM.createPortal(component, document.body);
 };
