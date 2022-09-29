8eb4e283d9c3503e21075fc30932b063b7b5604c
Enguerran Weiss <enguerranweiss@gmail.com>
2022-09-29 10:40:48 +0200

    fix establishment form horizontal scroll

--- front/src/app/pages/Establishment/components/EstablishmentFormikForm.tsx
+++ front/src/app/pages/Establishment/components/EstablishmentFormikForm.tsx
@@ -48,161 +48,165 @@ export const EstablishmentFormikForm = ({
   }
 
   return (
-    <div
-      className="fr-grid-row fr-grid-row--center fr-grid-row--gutters"
-      style={{ marginTop: "25px" }}
-    >
-      <Formik
-        enableReinitialize={true}
-        initialValues={initialValues}
-        validationSchema={toFormikValidationSchema(formEstablishmentSchema)}
-        onSubmit={async (data, { setSubmitting }) => {
-          try {
-            setIsSuccess(false);
-            setSubmitError(null);
-
-            formEstablishmentSchema.parse(data);
-
-            await saveForm(data);
-
-            setIsSuccess(true);
-            setSubmitError(null);
-          } catch (e: any) {
-            //eslint-disable-next-line no-console
-            console.log("onSubmit", e);
-            setIsSuccess(false);
-            setSubmitError(e);
-          } finally {
-            setSubmitting(false);
-          }
-        }}
+    <div className="fr-container">
+      <div
+        className="fr-grid-row fr-grid-row--center fr-grid-row--gutters"
+        style={{ marginTop: "25px" }}
       >
-        {({ isSubmitting, submitCount, errors, values }) => (
-          <div
-            style={{ margin: "5px 12px", maxWidth: "600px" }}
-            className="flex flex-col  gap-1"
-          >
-            <p>
-              Bienvenue sur l'espace de référencement des entreprises
-              volontaires pour l'accueil des immersions professionnelles.
-            </p>
+        <Formik
+          enableReinitialize={true}
+          initialValues={initialValues}
+          validationSchema={toFormikValidationSchema(formEstablishmentSchema)}
+          onSubmit={async (data, { setSubmitting }) => {
+            try {
+              setIsSuccess(false);
+              setSubmitError(null);
 
-            <p>
-              En référençant votre entreprise, vous rejoignez la communauté{" "}
-              <a
-                href={"https://lesentreprises-sengagent.gouv.fr/"}
-                target={"_blank"}
-              >
-                « Les entreprises s'engagent »
-              </a>
-              .
-            </p>
-            <p>
-              Ce formulaire vous permet d'indiquer les métiers de votre
-              établissement ouverts aux immersions. Si votre entreprise comprend
-              plusieurs établissements, il convient de renseigner un formulaire
-              pour chaque établissement (Siret différent).
-            </p>
-            <Form>
-              <div className="flex flex-col gap-10 mt-4">
-                <div>
-                  <span className="flex-col gap-1 block text-lg font-semibold">
-                    Votre établissement
-                  </span>
-                  {children}
-                  <BoolCheckboxGroup
-                    {...getLabelAndName("isEngagedEnterprise")}
-                    description=""
-                    descriptionLink=""
-                    disabled={false}
-                  />
-                  <TextInput {...getLabelAndName("website")} />
-                  <TextInput {...getLabelAndName("additionalInformation")} />
-                </div>
-                <AppellationList
-                  name="appellations"
-                  title={`${fieldsToLabel["appellations"]} *`}
-                />
-                <BusinessContact />
-              </div>
-              {isEditing && (
-                <>
-                  <BoolCheckboxGroup
-                    name="isSearchable"
-                    label={`L'entreprise est-elle recherchable par les utilisateurs ? ${
-                      values.isSearchable
-                        ? "(décochez la case si vous ne  voulez pas être visible sur la recherche)."
-                        : "(cochez la case si vous voulez être visible sur la recherche)."
-                    } Vous pourrez réactiver la visibilité à tout moment`}
-                  />
-                  <p>
-                    Vous pouvez demander la suppression définitive de votre
-                    entreprise{" "}
-                    <a
-                      href={"#"}
-                      onClick={(event) => {
-                        event.preventDefault();
-                        window.open(mailtoHref(initialValues.siret));
-                      }}
-                    >
-                      en cliquant ici
-                    </a>
-                  </p>
-                  <p>
-                    Si vous avez besoin d'aide, envoyez-nous un email: <br />
-                    contact@immersion-facile.beta.gouv.fr
-                  </p>
-                </>
-              )}
+              formEstablishmentSchema.parse(data);
 
-              {submitCount !== 0 && Object.values(errors).length > 0 && (
-                <div style={{ color: "red" }}>
-                  Veuillez corriger les champs erronés :
-                  <ul>
-                    {(Object.keys(errors) as FieldsWithLabel[]).map((field) => {
-                      const err = errors[field];
-                      return typeof err === "string" ? (
-                        <li key={field}>
-                          {fieldsToLabel[field] || field}: {err}
-                        </li>
-                      ) : null;
-                    })}
-                  </ul>
-                </div>
-              )}
-              <br />
-              {submitError && (
-                <>
-                  <Notification
-                    type="error"
-                    title="Veuillez nous excuser. Un problème est survenu qui a compromis l'enregistrement de vos informations. "
-                  >
-                    {errorMessage}
-                  </Notification>
-                  <br />
-                </>
-              )}
-              {isSuccess && (
-                <Notification type="success" title="Succès de l'envoi">
-                  Succès. Nous avons bien enregistré les informations concernant
-                  votre entreprise.
-                </Notification>
-              )}
-              {!isSuccess && (
-                <button
-                  className="fr-btn fr-fi-checkbox-circle-line fr-btn--icon-left"
-                  type="submit"
-                  disabled={isSubmitting}
+              await saveForm(data);
+
+              setIsSuccess(true);
+              setSubmitError(null);
+            } catch (e: any) {
+              //eslint-disable-next-line no-console
+              console.log("onSubmit", e);
+              setIsSuccess(false);
+              setSubmitError(e);
+            } finally {
+              setSubmitting(false);
+            }
+          }}
+        >
+          {({ isSubmitting, submitCount, errors, values }) => (
+            <div
+              style={{ margin: "5px 12px", maxWidth: "600px" }}
+              className="flex flex-col  gap-1"
+            >
+              <p>
+                Bienvenue sur l'espace de référencement des entreprises
+                volontaires pour l'accueil des immersions professionnelles.
+              </p>
+
+              <p>
+                En référençant votre entreprise, vous rejoignez la communauté{" "}
+                <a
+                  href={"https://lesentreprises-sengagent.gouv.fr/"}
+                  target={"_blank"}
                 >
-                  Enregistrer mes informations
-                </button>
-              )}
-            </Form>
-            <br />
-            <br />
-          </div>
-        )}
-      </Formik>
+                  « Les entreprises s'engagent »
+                </a>
+                .
+              </p>
+              <p>
+                Ce formulaire vous permet d'indiquer les métiers de votre
+                établissement ouverts aux immersions. Si votre entreprise
+                comprend plusieurs établissements, il convient de renseigner un
+                formulaire pour chaque établissement (Siret différent).
+              </p>
+              <Form>
+                <div className="flex flex-col gap-10 mt-4">
+                  <div>
+                    <span className="flex-col gap-1 block text-lg font-semibold">
+                      Votre établissement
+                    </span>
+                    {children}
+                    <BoolCheckboxGroup
+                      {...getLabelAndName("isEngagedEnterprise")}
+                      description=""
+                      descriptionLink=""
+                      disabled={false}
+                    />
+                    <TextInput {...getLabelAndName("website")} />
+                    <TextInput {...getLabelAndName("additionalInformation")} />
+                  </div>
+                  <AppellationList
+                    name="appellations"
+                    title={`${fieldsToLabel["appellations"]} *`}
+                  />
+                  <BusinessContact />
+                </div>
+                {isEditing && (
+                  <>
+                    <BoolCheckboxGroup
+                      name="isSearchable"
+                      label={`L'entreprise est-elle recherchable par les utilisateurs ? ${
+                        values.isSearchable
+                          ? "(décochez la case si vous ne  voulez pas être visible sur la recherche)."
+                          : "(cochez la case si vous voulez être visible sur la recherche)."
+                      } Vous pourrez réactiver la visibilité à tout moment`}
+                    />
+                    <p>
+                      Vous pouvez demander la suppression définitive de votre
+                      entreprise{" "}
+                      <a
+                        href={"#"}
+                        onClick={(event) => {
+                          event.preventDefault();
+                          window.open(mailtoHref(initialValues.siret));
+                        }}
+                      >
+                        en cliquant ici
+                      </a>
+                    </p>
+                    <p>
+                      Si vous avez besoin d'aide, envoyez-nous un email: <br />
+                      contact@immersion-facile.beta.gouv.fr
+                    </p>
+                  </>
+                )}
+
+                {submitCount !== 0 && Object.values(errors).length > 0 && (
+                  <div style={{ color: "red" }}>
+                    Veuillez corriger les champs erronés :
+                    <ul>
+                      {(Object.keys(errors) as FieldsWithLabel[]).map(
+                        (field) => {
+                          const err = errors[field];
+                          return typeof err === "string" ? (
+                            <li key={field}>
+                              {fieldsToLabel[field] || field}: {err}
+                            </li>
+                          ) : null;
+                        },
+                      )}
+                    </ul>
+                  </div>
+                )}
+                <br />
+                {submitError && (
+                  <>
+                    <Notification
+                      type="error"
+                      title="Veuillez nous excuser. Un problème est survenu qui a compromis l'enregistrement de vos informations. "
+                    >
+                      {errorMessage}
+                    </Notification>
+                    <br />
+                  </>
+                )}
+                {isSuccess && (
+                  <Notification type="success" title="Succès de l'envoi">
+                    Succès. Nous avons bien enregistré les informations
+                    concernant votre entreprise.
+                  </Notification>
+                )}
+                {!isSuccess && (
+                  <button
+                    className="fr-btn fr-fi-checkbox-circle-line fr-btn--icon-left"
+                    type="submit"
+                    disabled={isSubmitting}
+                  >
+                    Enregistrer mes informations
+                  </button>
+                )}
+              </Form>
+              <br />
+              <br />
+            </div>
+          )}
+        </Formik>
+      </div>
     </div>
   );
 };
