dcbea8e53692fe3c6fbdf3eda40a1608bbf8c7ac
Enguerran Weiss <enguerranweiss@gmail.com>
2022-10-04 10:12:53 +0200

    move eslintignore to /

---
+++ front/vite.config.ts
@@ -0,0 +1,38 @@
+import react from "@vitejs/plugin-react";
+import { resolve } from "path";
+import { defineConfig } from "vite";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  publicDir: "src/assets",
+  plugins: [react()],
+  resolve: {
+    alias: {
+      src: resolve(__dirname, "src"),
+    },
+  },
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+      },
+    },
+    sourcemap: true,
+  },
+  optimizeDeps: {
+    include: ["react/jsx-runtime"],
+  },
+  server: {
+    proxy: {
+      "/api": {
+        target: "http://localhost:1234/",
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ""),
+      },
+    },
+    port: 3000,
+  },
+  preview: {
+    port: 5000,
+  },
+});
