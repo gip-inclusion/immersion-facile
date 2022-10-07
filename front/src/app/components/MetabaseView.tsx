e9320e196b0591c2b56f7e7b1205d85998789ddf
Enguerran Weiss <enguerranweiss@gmail.com>
2022-10-07 15:51:22 +0200

    add button to Metabase view

--- front/src/app/components/MetabaseView.tsx
+++ front/src/app/components/MetabaseView.tsx
@@ -2,6 +2,16 @@ import React from "react";
 import { DsfrTitle } from "react-design-system";
 import { AbsoluteUrl } from "shared";
 
+const TitleButton = ({ url }: { url: AbsoluteUrl }) => (
+  <a
+    href={url}
+    target="_blank"
+    className="fr-btn fr-btn--tertiary fr-btn--sm fr-ml-auto"
+  >
+    Ouvrir en plein écran
+  </a>
+);
+
 export const MetabaseView = ({
   title,
   url,
@@ -11,7 +21,12 @@ export const MetabaseView = ({
 }) =>
   url ? (
     <div>
-      <DsfrTitle level={5} text={title} />
+      <DsfrTitle
+        level={5}
+        text={title}
+        action={<TitleButton url={url} />}
+        className={"flex"}
+      />
       <iframe src={url} frameBorder="0" width="100%" height="800"></iframe>
     </div>
   ) : (
--- libs/react-design-system/src/immersionFacile/components/text/DsfrTitle.tsx
+++ libs/react-design-system/src/immersionFacile/components/text/DsfrTitle.tsx
@@ -4,7 +4,15 @@ type DsfrTitleProps = {
   text: string;
   level: 1 | 2 | 3 | 4 | 5 | 6;
   className?: string;
+  action?: JSX.Element;
 };
-export const DsfrTitle = ({ text, level = 1, className }: DsfrTitleProps) => (
-  <div className={`fr-h${level} mb-2 ${className}`}>{text}</div>
+export const DsfrTitle = ({
+  text,
+  level = 1,
+  className = "",
+  action,
+}: DsfrTitleProps) => (
+  <div className={`fr-h${level} fr-mb-2w ${className}`}>
+    {text} {action}
+  </div>
 );
