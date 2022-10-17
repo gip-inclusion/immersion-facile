d785a84747e27168d8acfd0c4913e87ca98b9c31
Benjamin Bohec <bbohec.pro@gmail.com>
2022-10-17 12:23:32 +0200

    radio button before tutor fields

--- front/src/app/pages/Convention/ConventionFields/Sections/Establishment/EstablishmentFormSection.tsx
+++ front/src/app/pages/Convention/ConventionFields/Sections/Establishment/EstablishmentFormSection.tsx
@@ -1,6 +1,9 @@
 import React from "react";
 import { FederatedIdentity } from "shared";
+import { RadioGroup } from "src/app/components/RadioGroup";
 import { ShareActions } from "src/app/pages/Convention/ConventionFields/ShareActions";
+import { isEstablishmentTutorIsEstablishmentRepresentativeHook } from "src/hooks/convention.hooks";
+import { useSiretFetcher } from "src/hooks/siret.hooks";
 import { FormSectionTitle } from "src/uiComponents/FormSectionTitle";
 import { useConventionTextsFromFormikContext } from "../../../texts/textSetup";
 import { EstablishementTutorFields } from "./EstablishementTutorFields";
@@ -17,19 +20,41 @@ export const EstablishmentFormSection = ({
   federatedIdentity,
 }: EstablishmentFormSectionParams): JSX.Element => {
   const t = useConventionTextsFromFormikContext();
+  const {
+    isEstablishmentTutorIsEstablishmentRepresentativeValue,
+    setIsEstablishmentTutorIsEstablishmentRepresentative,
+  } = isEstablishmentTutorIsEstablishmentRepresentativeHook();
+  const { isFetchingSiret } = useSiretFetcher({
+    shouldFetchEvenIfAlreadySaved: true,
+  });
   return (
     <>
       <FormSectionTitle>
         {t.sectionTitles.establishment}
         <ShareActions
-          isFrozen={isFrozen}
+          isFrozen={isFrozen || isFetchingSiret}
           federatedIdentity={federatedIdentity}
         />
       </FormSectionTitle>
       <h4>{t.establishment.subtitle}</h4>
-      <EstablishmentBusinessFields disabled={isFrozen} />
+      <EstablishmentBusinessFields disabled={isFrozen || isFetchingSiret} />
+      <RadioGroup
+        id="is-establishmentRepresentative"
+        disabled={isFrozen || isFetchingSiret}
+        currentValue={isEstablishmentTutorIsEstablishmentRepresentativeValue}
+        setCurrentValue={setIsEstablishmentTutorIsEstablishmentRepresentative}
+        groupLabel={`${t.establishment.isEstablishmentTutorIsEstablishmentRepresentative} *`}
+        options={[
+          { label: t.yes, value: true },
+          { label: t.no, value: false },
+        ]}
+      />
       <EstablishementTutorFields disabled={isFrozen} />
-      <EstablishmentRepresentativeFields disabled={isFrozen} />
+      {!isEstablishmentTutorIsEstablishmentRepresentativeValue && (
+        <EstablishmentRepresentativeFields
+          disabled={isFrozen || isFetchingSiret}
+        />
+      )}
     </>
   );
 };
--- front/src/app/pages/Convention/ConventionFields/Sections/Establishment/EstablishmentRepresentativeFields.tsx
+++ front/src/app/pages/Convention/ConventionFields/Sections/Establishment/EstablishmentRepresentativeFields.tsx
@@ -1,8 +1,5 @@
 import React from "react";
 import { getConventionFieldName } from "shared";
-import { RadioGroup } from "src/app/components/RadioGroup";
-import { isEstablishmentTutorIsEstablishmentRepresentativeHook } from "src/hooks/convention.hooks";
-import { useSiretFetcher } from "src/hooks/siret.hooks";
 import { TextInput } from "src/uiComponents/form/TextInput";
 import { useConventionTextsFromFormikContext } from "../../../texts/textSetup";
 
@@ -13,84 +10,62 @@ type EstablishmentRepresentativeFieldsProperties = {
 export const EstablishmentRepresentativeFields = ({
   disabled,
 }: EstablishmentRepresentativeFieldsProperties): JSX.Element => {
-  const { isFetchingSiret } = useSiretFetcher({
-    shouldFetchEvenIfAlreadySaved: true,
-  });
-  const {
-    isEstablishmentTutorIsEstablishmentRepresentativeValue,
-    setIsEstablishmentTutorIsEstablishmentRepresentative,
-  } = isEstablishmentTutorIsEstablishmentRepresentativeHook();
   const t = useConventionTextsFromFormikContext();
   return (
     <>
-      <RadioGroup
-        id="is-establishmentRepresentative"
+      <TextInput
+        label={`${t.establishment.establishmentRepresentative.firstName.label} *`}
+        name={getConventionFieldName(
+          "signatories.establishmentRepresentative.firstName",
+        )}
+        type="text"
+        placeholder=""
+        description={
+          t.establishment.establishmentRepresentative.firstName.description
+        }
         disabled={disabled}
-        currentValue={isEstablishmentTutorIsEstablishmentRepresentativeValue}
-        setCurrentValue={setIsEstablishmentTutorIsEstablishmentRepresentative}
-        groupLabel={`${t.establishment.isEstablishmentTutorIsEstablishmentRepresentative} *`}
-        options={[
-          { label: t.yes, value: true },
-          { label: t.no, value: false },
-        ]}
       />
-      {!isEstablishmentTutorIsEstablishmentRepresentativeValue && (
-        <>
-          <TextInput
-            label={`${t.establishment.establishmentRepresentative.firstName.label} *`}
-            name={getConventionFieldName(
-              "signatories.establishmentRepresentative.firstName",
-            )}
-            type="text"
-            placeholder=""
-            description={
-              t.establishment.establishmentRepresentative.firstName.description
-            }
-            disabled={disabled || isFetchingSiret}
-          />
-          <TextInput
-            label={`${t.establishment.establishmentRepresentative.lastName.label} *`}
-            name={getConventionFieldName(
-              "signatories.establishmentRepresentative.lastName",
-            )}
-            type="text"
-            placeholder=""
-            description={
-              t.establishment.establishmentRepresentative.lastName.description
-            }
-            disabled={disabled || isFetchingSiret}
-          />
-          <TextInput
-            label={`${t.establishment.establishmentRepresentative.phone.label} *`}
-            name={getConventionFieldName(
-              "signatories.establishmentRepresentative.phone",
-            )}
-            type="tel"
-            placeholder={
-              t.establishment.establishmentRepresentative.phone.placeholder
-            }
-            description={
-              t.establishment.establishmentRepresentative.phone.description
-            }
-            disabled={disabled || isFetchingSiret}
-          />
-          <TextInput
-            label={`${t.establishment.establishmentRepresentative.email.label} *`}
-            name={getConventionFieldName(
-              "signatories.establishmentRepresentative.email",
-            )}
-            type="email"
-            placeholder={
-              t.establishment.establishmentRepresentative.email.placeholder
-            }
-            description={
-              t.establishment.establishmentRepresentative.email.description
-            }
-            disabled={disabled || isFetchingSiret}
-            className="!mb-1"
-          />
-        </>
-      )}
+      <TextInput
+        label={`${t.establishment.establishmentRepresentative.lastName.label} *`}
+        name={getConventionFieldName(
+          "signatories.establishmentRepresentative.lastName",
+        )}
+        type="text"
+        placeholder=""
+        description={
+          t.establishment.establishmentRepresentative.lastName.description
+        }
+        disabled={disabled}
+      />
+      <TextInput
+        label={`${t.establishment.establishmentRepresentative.phone.label} *`}
+        name={getConventionFieldName(
+          "signatories.establishmentRepresentative.phone",
+        )}
+        type="tel"
+        placeholder={
+          t.establishment.establishmentRepresentative.phone.placeholder
+        }
+        description={
+          t.establishment.establishmentRepresentative.phone.description
+        }
+        disabled={disabled}
+      />
+      <TextInput
+        label={`${t.establishment.establishmentRepresentative.email.label} *`}
+        name={getConventionFieldName(
+          "signatories.establishmentRepresentative.email",
+        )}
+        type="email"
+        placeholder={
+          t.establishment.establishmentRepresentative.email.placeholder
+        }
+        description={
+          t.establishment.establishmentRepresentative.email.description
+        }
+        disabled={disabled}
+        className="!mb-1"
+      />
     </>
   );
 };
