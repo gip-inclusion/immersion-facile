2f498bc9b92f2023e5a1b1bc563b23374a36dded
Benjamin Bohec <bbohec.pro@gmail.com>
2022-10-21 15:59:19 +0200

    clean

--- front/src/app/pages/immersionAssessment/components/ImmersionAssessmentForm.tsx
+++ front/src/app/pages/immersionAssessment/components/ImmersionAssessmentForm.tsx
@@ -28,21 +28,10 @@ export const ImmersionAssessmentForm = ({
   convention,
   jwt,
 }: ImmersionAssessmentFormProperties): JSX.Element => {
-  const getName = (name: keyof ImmersionAssessmentDto) => name;
-  const labels: Record<AssessmentStatus, string> = {
-    FINISHED: "Le bénéficiaire a suivi son immersion jusqu'à la fin",
-    ABANDONED: "Le bénéficiaire a abandonné l'immersion",
-  };
   const { createAssessment } = useImmersionAssessment(jwt);
   const assessmentError = useAppSelector(immersionAssessmentErrorSelector);
   const assessmentStatus = useAppSelector(immersionAssessmentStatusSelector);
   const isDisabled = assessmentStatus !== "Idle" || assessmentError !== null;
-  const downloadFullImmersionAssessmentPdf = () => {
-    window.open(
-      "https://immersion.cellar-c2.services.clever-cloud.com/bilan-immersion-professionnelle-inscriptible.pdf",
-      "_blank",
-    );
-  };
   return (
     <Formik
       initialValues={identity<ImmersionAssessmentDto>({
@@ -107,5 +96,17 @@ export const ImmersionAssessmentForm = ({
         </Form>
       )}
     </Formik>
+  );
+};
+
+const getName = (name: keyof ImmersionAssessmentDto) => name;
+const labels: Record<AssessmentStatus, string> = {
+  FINISHED: "Le bénéficiaire a suivi son immersion jusqu'à la fin",
+  ABANDONED: "Le bénéficiaire a abandonné l'immersion",
+};
+const downloadFullImmersionAssessmentPdf = () => {
+  window.open(
+    "https://immersion.cellar-c2.services.clever-cloud.com/bilan-immersion-professionnelle-inscriptible.pdf",
+    "_blank",
   );
 };
