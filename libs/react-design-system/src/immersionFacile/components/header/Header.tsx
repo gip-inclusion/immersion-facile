b01a4ad9e3a053a84f83fd8826f478702c6c1e94
Enguerran Weiss <enguerranweiss@gmail.com>
2022-11-18 15:10:36 +0100

    added mobile menu

--- libs/react-design-system/src/immersionFacile/components/header/Header.tsx
+++ libs/react-design-system/src/immersionFacile/components/header/Header.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { NavLink, NavWrapper, TabLinks } from "../tabLinks";
 
 export type Tool = {
@@ -9,8 +9,8 @@ export type Tool = {
 
 const navWrapperOptions: NavWrapper = {
   role: "navigation",
-  id: "main-menu",
-  className: "fr-nav fr-nav--main fr-container",
+  id: "modal-header__menu-inner",
+  className: "fr-header__menu",
   ariaLabel: "Menu principal",
 };
 
@@ -44,41 +44,74 @@ export const Header = ({
   immersionLogo,
   navLinks,
   sticky,
-}: ImmersionPureHeaderProps) => (
-  <header className={`fr-header`} style={sticky ? getStickyStyles() : {}}>
-    <div className="fr-header__body">
-      <div className="fr-container">
-        <div className="fr-header__body-row">
-          <div className="fr-header__brand fr-enlarge-link">
-            <div className="fr-header__brand-top">
-              {marianneLogo}
-              {immersionLogo && (
-                <div className="fr-header__operator">{immersionLogo}</div>
-              )}
-            </div>
-            <div className="fr-header__service">
-              <p className="fr-header__service-title">Immersion Facilitée</p>
-              <p className="fr-header__service-tagline">
-                Faciliter la réalisation des immersions professionnelles
-              </p>
-            </div>
-          </div>
-          {tools && tools?.length > 0 && (
-            <div className="fr-header__tools">
-              <div className="fr-header__tools-links">
-                <ul className="fr-nav fr-nav--right">
-                  {tools.map((tool, index) => (
-                    <Tool tool={tool} key={index} />
-                  ))}
-                </ul>
+}: ImmersionPureHeaderProps) => {
+  const [isOpened, setIsOpened] = useState(false);
+  const onMenuMobileToggleClick = () => {
+    setIsOpened((openState) => !openState);
+  };
+  return (
+    <header className={`fr-header`} style={sticky ? getStickyStyles() : {}}>
+      <div className="fr-header__body">
+        <div className="fr-container">
+          <div className="fr-header__body-row">
+            <div className="fr-header__brand fr-enlarge-link">
+              <div className="fr-header__brand-top">
+                {marianneLogo}
+                {immersionLogo && (
+                  <div className="fr-header__operator">{immersionLogo}</div>
+                )}
+                <div className="fr-header__navbar">
+                  <button
+                    aria-controls="modal-header__menu"
+                    aria-haspopup="menu"
+                    title="Afficher le menu"
+                    className="fr-btn--menu fr-btn"
+                    onClick={onMenuMobileToggleClick}
+                  >
+                    Menu
+                  </button>
+                </div>
+              </div>
+              <div className="fr-header__service">
+                <p className="fr-header__service-title">Immersion Facilitée</p>
+                <p className="fr-header__service-tagline">
+                  Faciliter la réalisation des immersions professionnelles
+                </p>
               </div>
             </div>
-          )}
+            {tools && tools?.length > 0 && (
+              <div className="fr-header__tools">
+                <div className="fr-header__tools-links">
+                  <ul className="fr-nav fr-nav--right">
+                    {tools.map((tool, index) => (
+                      <Tool tool={tool} key={index} />
+                    ))}
+                  </ul>
+                </div>
+              </div>
+            )}
+          </div>
         </div>
       </div>
-    </div>
-    {navLinks && navLinks.length > 0 && (
-      <TabLinks navLinks={navLinks} navWrapper={navWrapperOptions} />
-    )}
-  </header>
-);
+      {navLinks && navLinks.length > 0 && (
+        <div
+          id="modal-header__menu"
+          className={`fr-header__menu fr-modal ${
+            isOpened ? "fr-modal--opened" : ""
+          }`}
+        >
+          <div className="fr-container">
+            <button
+              aria-controls="modal-header__menu"
+              className="fr-btn fr-btn--close"
+              onClick={onMenuMobileToggleClick}
+            >
+              Fermer
+            </button>
+            <TabLinks navLinks={navLinks} navWrapper={navWrapperOptions} />
+          </div>
+        </div>
+      )}
+    </header>
+  );
+};
