17839a675569f56db182498db9168b6b7a908995
Enguerran Weiss <enguerranweiss@gmail.com>
2022-12-08 15:48:22 +0100

    add target blank on email buttons

--- libs/html-templates/src/components/email/button.ts
+++ libs/html-templates/src/components/email/button.ts
@@ -6,7 +6,7 @@ export const renderButton = (params?: {
   `<table>
     <tr>
       <td align="center" width="600" style="padding: 20px; padding-top: 10px; padding-bottom: 30px;">
-        <a style="text-decoration: none; display: inline-block;padding: 10px 20px; background-color: #000091; color: #fff; text-align: center;" href="${params.url}">${params.label}</a>
+        <a style="text-decoration: none; display: inline-block;padding: 10px 20px; background-color: #000091; color: #fff; text-align: center;" href="${params.url}" target="_blank">${params.label}</a>
       </td>
     </tr>
   </table>`;
