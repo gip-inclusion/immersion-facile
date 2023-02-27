9c0d870ab81239f5c3a006db5973562972a7e7cf
clement-duport <clement.duport@beta.gouv.fr>
2023-02-27 15:14:48 +0100

    update migration to have not searchable establishment max contact per week to zero

--- back/src/adapters/secondary/pg/migrations/1677492719994_move-max-contact-per-week-from-contact-table-to-establisment-table.ts
+++ back/src/adapters/secondary/pg/migrations/1677492719994_move-max-contact-per-week-from-contact-table-to-establisment-table.ts
@@ -4,15 +4,27 @@ const contactTableName = "immersion_contacts";
 const maxContactsPerWeekCol = "max_contacts_per_week";
 const establishmentTableName = "establishments";
 const formEstablishmentsTableName = "form_establishments";
+const defaultMaxContactsPerWeek = 10;
 
 export async function up(pgm: MigrationBuilder): Promise<void> {
   pgm.dropColumn(contactTableName, "max_contact_per_week");
   pgm.addColumn(establishmentTableName, {
-    [maxContactsPerWeekCol]: { type: "int4", notNull: false },
+    [maxContactsPerWeekCol]: {
+      type: "int4",
+      notNull: true,
+      default: defaultMaxContactsPerWeek,
+    },
   });
   pgm.addColumn(formEstablishmentsTableName, {
-    [maxContactsPerWeekCol]: { type: "int4", notNull: false },
+    [maxContactsPerWeekCol]: {
+      type: "int4",
+      notNull: true,
+      default: defaultMaxContactsPerWeek,
+    },
   });
+  pgm.sql(
+    "UPDATE establishments SET max_contacts_per_week=0 WHERE is_searchable=false",
+  );
 }
 
 export async function down(pgm: MigrationBuilder): Promise<void> {
