0b6bd8c11189020ed63b96df5823f2ea8c6022af
Jérôme Burkard <burkardjerome@gmail.com>
2023-03-07 12:13:54 +0100

    use github actions to depoly to scalingo

--- back/transpile-backend-and-prepare-for-prod.sh
+++ back/transpile-backend-and-prepare-for-prod.sh
@@ -10,7 +10,7 @@ echo "Coping package.json of back, shared, and libs for prod..."
 cp -v ../package.json build/package.json
 cp -v ../pnpm-lock.yaml build/pnpm-lock.yaml
 cp -v ../pnpm-workspace.yaml build/pnpm-workspace.yaml
-cp -v ../.env build/back/.env
+#cp -v ../.env build/back/.env
 cp -v -R ./src/adapters/secondary/pg/staticData build/back/src/adapters/secondary/pg/staticData
 
 # back package.json
@@ -22,15 +22,26 @@ cp -v ../libs/http-client/package.json build/libs/http-client/package.json
 cp -v ../libs/html-templates/package.json build/libs/html-templates/package.json
 
 # remove precommit from root package.json
-sed -i "" -e "/prepare/d" build/package.json
+sed -i "/prepare/d" build/package.json
+
+#For mac
+#sed -i "" -e "/prepare/d" build/package.json
 
 # change dependencies to use js instead of ts
-sed -i "" -e "/\"types\": \"src\/index.ts\"/d" build/libs/http-client/package.json
-sed -i "" -e "s/\"main\": \"src\/index.ts\"/\"main\": \"src\/index.js\"/" build/libs/http-client/package.json
-sed -i "" -e "/\"types\": \"src\/index.ts\"/d" build/libs/html-templates/package.json
-sed -i "" -e "s/\"main\": \"src\/index.ts\"/\"main\": \"src\/index.js\"/"  build/libs/html-templates/package.json
-sed -i "" -e "/\"types\": \"src\/index.ts\"/d" build/shared/package.json
-sed -i "" -e "s/\"main\": \"src\/index.ts\"/\"main\": \"src\/index.js\"/"  build/shared/package.json
+sed -i "/\"types\": \"src\/index.ts\"/d" build/libs/http-client/package.json
+sed -i "s/\"main\": \"src\/index.ts\"/\"main\": \"src\/index.js\"/" build/libs/http-client/package.json
+sed -i "/\"types\": \"src\/index.ts\"/d" build/libs/html-templates/package.json
+sed -i "s/\"main\": \"src\/index.ts\"/\"main\": \"src\/index.js\"/"  build/libs/html-templates/package.json
+sed -i "/\"types\": \"src\/index.ts\"/d" build/shared/package.json
+sed -i "s/\"main\": \"src\/index.ts\"/\"main\": \"src\/index.js\"/"  build/shared/package.json
+
+# For mac
+#sed -i "" -e "/\"types\": \"src\/index.ts\"/d" build/libs/http-client/package.json
+#sed -i "" -e "s/\"main\": \"src\/index.ts\"/\"main\": \"src\/index.js\"/" build/libs/http-client/package.json
+#sed -i "" -e "/\"types\": \"src\/index.ts\"/d" build/libs/html-templates/package.json
+#sed -i "" -e "s/\"main\": \"src\/index.ts\"/\"main\": \"src\/index.js\"/"  build/libs/html-templates/package.json
+#sed -i "" -e "/\"types\": \"src\/index.ts\"/d" build/shared/package.json
+#sed -i "" -e "s/\"main\": \"src\/index.ts\"/\"main\": \"src\/index.js\"/"  build/shared/package.json
 
 echo "web: cd back && pnpm prod-tsc\npostdeploy: cd back && pnpm migrate-prod" > build/Procfile
 echo "https://github.com/unfold/heroku-buildpack-pnpm" > build/.buildpacks
