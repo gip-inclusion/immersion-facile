b3b623b719db5c02899c604ca1621ed43ce03f01
Enguerran Weiss <enguerranweiss@gmail.com>
2023-03-09 15:50:30 +0100

    mv front/public to public folder, updated ignore files

--- front/vite.config.ts
+++ front/vite.config.ts
@@ -4,7 +4,7 @@ import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  publicDir: "src/public",
+  publicDir: "public",
   plugins: [react()],
   resolve: {
     alias: {
