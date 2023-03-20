7556e396d48bd359d7d45107b5235656d17ac498
Enguerran Weiss <enguerranweiss@gmail.com>
2023-03-20 18:05:43 +0100

    add missing files

---
+++ libs/react-design-system/src/immersionFacile/components/page-header/PageHeader.styles.ts
@@ -0,0 +1,17 @@
+import "./PageHeader.scss";
+
+export default {
+  root: "im-page-header",
+  inner: "im-page-header__inner",
+  default: "im-page-header--default",
+  candidate: "im-page-header--candidate",
+  establishment: "im-page-header--establishment",
+  agency: "im-page-header--agency",
+  title: "im-page-header__title",
+  titleCentered: "im-page-header__title--centered",
+  patterns: "im-page-header__patterns",
+  pattern: "im-page-header__pattern",
+  pattern0: "im-page-header__pattern--0",
+  pattern1: "im-page-header__pattern--1",
+  pattern2: "im-page-header__pattern--2",
+};
