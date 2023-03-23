01e1fa4c57f9e174c6740dda898900b63bec6fa0
Enguerran Weiss <enguerranweiss@gmail.com>
2023-03-23 09:06:47 +0100

    add common form values as separate files

---
+++ front/src/app/contents/forms/common/values.ts
@@ -0,0 +1,14 @@
+export const booleanSelectOptions = [
+  {
+    label: "Oui",
+    nativeInputProps: {
+      value: 1,
+    },
+  },
+  {
+    label: "Non",
+    nativeInputProps: {
+      value: 0,
+    },
+  },
+];
