af23cfbdfafd613d6688c0daa8b0bdf7f13bb296
Jérôme Burkard <burkardjerome@gmail.com>
2023-03-24 16:49:47 +0100

    put back scalingo related fields in a new folder instead of generating at buildtime

--- back/transpile-backend-and-prepare-for-prod.sh
+++ back/transpile-backend-and-prepare-for-prod.sh
@@ -21,46 +21,41 @@ cp -v ../shared/package.json build/shared/package.json
 cp -v ../libs/http-client/package.json build/libs/http-client/package.json
 cp -v ../libs/html-templates/package.json build/libs/html-templates/package.json
 
+
+# <----------- For linux (and CI)
 # remove precommit from root package.json
-#sed -i "/prepare/d" build/package.json
-
-#For mac
-sed -i "" -e "/prepare/d" build/package.json
-
-## change dependencies to use js instead of ts
-#sed -i "/\"types\": \"src\/index.ts\"/d" build/libs/http-client/package.json
-#sed -i "s/\"main\": \"src\/index.ts\"/\"main\": \"src\/index.js\"/" build/libs/http-client/package.json
-#sed -i "/\"types\": \"src\/index.ts\"/d" build/libs/html-templates/package.json
-#sed -i "s/\"main\": \"src\/index.ts\"/\"main\": \"src\/index.js\"/"  build/libs/html-templates/package.json
-#sed -i "/\"types\": \"src\/index.ts\"/d" build/shared/package.json
-#sed -i "s/\"main\": \"src\/index.ts\"/\"main\": \"src\/index.js\"/"  build/shared/package.json
-## change ts-node scripts to node scripts
-#sed -i "s/\"ts-node /\"node /g"  build/back/package.json
-#sed -i -r "s/(\"node .*)(\.ts)/\1\.js/g"  build/back/package.json
-## change migration script from ts source files to js
-#sed -i "s/node_modules\/node-pg-migrate\/bin\/node-pg-migrate -j ts/node_modules\/node-pg-migrate\/bin\/node-pg-migrate/"  build/back/package.json
-
-# For mac
-sed -i "" -e "/\"types\": \"src\/index.ts\"/d" build/libs/http-client/package.json
-sed -i "" -e "s/\"main\": \"src\/index.ts\"/\"main\": \"src\/index.js\"/" build/libs/http-client/package.json
-sed -i "" -e "/\"types\": \"src\/index.ts\"/d" build/libs/html-templates/package.json
-sed -i "" -e "s/\"main\": \"src\/index.ts\"/\"main\": \"src\/index.js\"/"  build/libs/html-templates/package.json
-sed -i "" -e "/\"types\": \"src\/index.ts\"/d" build/shared/package.json
-sed -i "" -e "s/\"main\": \"src\/index.ts\"/\"main\": \"src\/index.js\"/"  build/shared/package.json
+sed -i "/prepare/d" build/package.json
+# change dependencies to use js instead of ts
+sed -i "/\"types\": \"src\/index.ts\"/d" build/libs/http-client/package.json
+sed -i "s/\"main\": \"src\/index.ts\"/\"main\": \"src\/index.js\"/" build/libs/http-client/package.json
+sed -i "/\"types\": \"src\/index.ts\"/d" build/libs/html-templates/package.json
+sed -i "s/\"main\": \"src\/index.ts\"/\"main\": \"src\/index.js\"/"  build/libs/html-templates/package.json
+sed -i "/\"types\": \"src\/index.ts\"/d" build/shared/package.json
+sed -i "s/\"main\": \"src\/index.ts\"/\"main\": \"src\/index.js\"/"  build/shared/package.json
 # change ts-node scripts to node scripts
-sed -i "" -e "s/\"ts-node /\"node /g"  build/back/package.json
-sed -i "" -E "s/(\"node .*)(\.ts)/\1\.js/g"  build/back/package.json
+sed -i "s/\"ts-node /\"node /g"  build/back/package.json
+sed -i -r "s/(\"node .*)(\.ts)/\1\.js/g"  build/back/package.json
 # change migration script from ts source files to js
-sed -i "" -e "s/node_modules\/node-pg-migrate\/bin\/node-pg-migrate -j ts/node_modules\/node-pg-migrate\/bin\/node-pg-migrate/"  build/back/package.json
+sed -i "s/node_modules\/node-pg-migrate\/bin\/node-pg-migrate -j ts/node_modules\/node-pg-migrate\/bin\/node-pg-migrate/"  build/back/package.json
+# End for linux -------->
 
-echo "web: cd back && pnpm prod-tsc\npostdeploy: cd back && pnpm migrate up" > build/Procfile
-echo "https://github.com/unfold/heroku-buildpack-pnpm" > build/.buildpacks
-echo '{"jobs": [
-      {
-             "command": "*/10 * * * * pnpm back run trigger-refresh-materialized-views",
-             "size": "M"
-           }
-         ]}' > build/cron.json
+## <--------  For mac
+#sed -i "" -e "/prepare/d" build/package.json
+# change dependencies to use js instead of ts
+#sed -i "" -e "/\"types\": \"src\/index.ts\"/d" build/libs/http-client/package.json
+#sed -i "" -e "s/\"main\": \"src\/index.ts\"/\"main\": \"src\/index.js\"/" build/libs/http-client/package.json
+#sed -i "" -e "/\"types\": \"src\/index.ts\"/d" build/libs/html-templates/package.json
+#sed -i "" -e "s/\"main\": \"src\/index.ts\"/\"main\": \"src\/index.js\"/"  build/libs/html-templates/package.json
+#sed -i "" -e "/\"types\": \"src\/index.ts\"/d" build/shared/package.json
+#sed -i "" -e "s/\"main\": \"src\/index.ts\"/\"main\": \"src\/index.js\"/"  build/shared/package.json
+## change ts-node scripts to node scripts
+#sed -i "" -e "s/\"ts-node /\"node /g"  build/back/package.json
+#sed -i "" -E "s/(\"node .*)(\.ts)/\1\.js/g"  build/back/package.json
+## change migration script from ts source files to js
+#sed -i "" -e "s/node_modules\/node-pg-migrate\/bin\/node-pg-migrate -j ts/node_modules\/node-pg-migrate\/bin\/node-pg-migrate/"  build/back/package.json
+## End for mac -------->
+
+cp -r -v scalingo/. build/
 
 echo "Making tar.gz from transpiled code"
 
