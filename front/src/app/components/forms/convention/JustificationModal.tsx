048f58d28b7d3603b515a0d2e7a9d234dafff218
clement-duport <clement.duport@beta.gouv.fr>
2023-03-29 12:38:43 +0200

    replace formik by react hook form in justification modal

--- front/src/app/components/forms/convention/JustificationModal.tsx
+++ front/src/app/components/forms/convention/JustificationModal.tsx
@@ -1,4 +1,4 @@
-import { Form, Formik } from "formik";
+import { useForm, SubmitHandler } from "react-hook-form";
 import React from "react";
 import {
   ModalClose,
@@ -13,11 +13,11 @@ import {
   WithStatusJustification,
   withStatusJustificationSchema,
 } from "shared";
-import { TextInput } from "src/app/components/forms/commons/TextInput";
-import { toFormikValidationSchema } from "src/app/components/forms/commons/zodValidate";
+import { Input } from "@codegouvfr/react-dsfr/Input";
 import { fr } from "@codegouvfr/react-dsfr";
 import { ButtonsGroup } from "@codegouvfr/react-dsfr/ButtonsGroup";
 import { Alert } from "@codegouvfr/react-dsfr/Alert";
+import { zodResolver } from "@hookform/resolvers/zod";
 
 type JustificationModalProps = {
   title: React.ReactNode;
@@ -35,7 +35,16 @@ export const JustificationModal = ({
   newStatus,
 }: JustificationModalProps) => {
   const closeModal = () => setIsOpen(false);
-  const name: keyof WithStatusJustification = "statusJustification";
+
+  const { register, handleSubmit } = useForm<WithStatusJustification>({
+    resolver: zodResolver(withStatusJustificationSchema),
+    mode: "onTouched",
+    defaultValues: { statusJustification: "" },
+  });
+  const onFormSubmit: SubmitHandler<WithStatusJustification> = (values) => {
+    onSubmit({ ...values, status: newStatus });
+    closeModal();
+  };
 
   return (
     <ModalDialog isOpen={isOpen} hide={closeModal}>
@@ -70,48 +79,39 @@ export const JustificationModal = ({
             description="Vous souhaitez annuler une convention qui a déjà été validée. Veuillez indiquer votre nom et prénom afin de garantir un suivi des annulations de convention."
           />
         )}
-        <Formik
-          initialValues={{ statusJustification: "" }}
-          validationSchema={toFormikValidationSchema(
-            withStatusJustificationSchema,
-          )}
-          onSubmit={(values) => {
-            onSubmit({ ...values, status: newStatus });
-            closeModal();
-          }}
-        >
-          <Form>
-            <TextInput
-              multiline={true}
-              label={inputLabelByStatus[newStatus]}
-              name={name}
-            />
-            <ButtonsGroup
-              alignment="center"
-              inlineLayoutWhen="always"
-              buttons={[
-                {
-                  type: "button",
-                  priority: "secondary",
-                  onClick: closeModal,
-                  nativeButtonProps: {
-                    id: domElementIds.manageConvention
-                      .justificationModalCancelButton,
-                  },
-                  children: "Annuler",
+        <form onSubmit={handleSubmit(onFormSubmit)}>
+          <Input
+            textArea
+            label={inputLabelByStatus[newStatus]}
+            nativeTextAreaProps={{
+              ...register("statusJustification"),
+            }}
+          />
+          <ButtonsGroup
+            alignment="center"
+            inlineLayoutWhen="always"
+            buttons={[
+              {
+                type: "button",
+                priority: "secondary",
+                onClick: closeModal,
+                nativeButtonProps: {
+                  id: domElementIds.manageConvention
+                    .justificationModalCancelButton,
                 },
-                {
-                  type: "submit",
-                  nativeButtonProps: {
-                    id: domElementIds.manageConvention
-                      .justificationModalSubmitButton,
-                  },
-                  children: confirmByStatus[newStatus],
+                children: "Annuler",
+              },
+              {
+                type: "submit",
+                nativeButtonProps: {
+                  id: domElementIds.manageConvention
+                    .justificationModalSubmitButton,
                 },
-              ]}
-            />
-          </Form>
-        </Formik>
+                children: confirmByStatus[newStatus],
+              },
+            ]}
+          />
+        </form>
       </ModalContent>
     </ModalDialog>
   );
