528486e314c9e1474ccfc9626cbeeec16067085b
Enguerran Weiss <enguerranweiss@gmail.com>
2023-04-18 17:31:28 +0200

    add ts-pattern, conditional rendering on AgencyDashboardPage with patterns

--- front/src/app/components/forms/register-agencies/RegisterAgenciesForm.tsx
+++ front/src/app/components/forms/register-agencies/RegisterAgenciesForm.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import { useForm } from "react-hook-form";
 import { useDispatch } from "react-redux";
 import { fr } from "@codegouvfr/react-dsfr";
-import { Alert } from "@codegouvfr/react-dsfr/Alert";
 import { Button } from "@codegouvfr/react-dsfr/Button";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { keys } from "ramda";
@@ -10,8 +9,6 @@ import { z } from "zod";
 import { agencyIdAndNameSchema, AgencyOption, toDotNotation } from "shared";
 import { ErrorNotifications } from "react-design-system";
 import { formErrorsToFlatErrors } from "src/app/hooks/formContents.hooks";
-import { useAppSelector } from "src/app/hooks/reduxHooks";
-import { inclusionConnectedSelectors } from "src/core-logic/domain/inclusionConnected/inclusionConnected.selectors";
 import { inclusionConnectedSlice } from "src/core-logic/domain/inclusionConnected/inclusionConnected.slice";
 import { MultipleAgencyInput } from "./MultipleAgencyInput";
 
@@ -32,57 +29,45 @@ export const RegisterAgenciesForm = () => {
       resolver: zodResolver(registerAgenciesFormSchema),
     });
   const dispatch = useDispatch();
-  const feedback = useAppSelector(inclusionConnectedSelectors.feedback);
   return (
     <>
-      {feedback.kind !== "agencyRegistrationSuccess" && (
-        <>
-          <p>
-            C'est votre première connexion sur Immersion Facilitée avec votre
-            compte Inclusion Connect, choisissez l'organisme auquel nous devons
-            vous associer.
-          </p>
-          <form
-            onSubmit={handleSubmit((values) =>
-              dispatch(
-                inclusionConnectedSlice.actions.registerAgenciesRequested({
-                  agencies: values.agencies.map((agency) => agency.id),
-                }),
+      <p>
+        C'est votre première connexion sur Immersion Facilitée avec votre compte
+        Inclusion Connect, choisissez l'organisme auquel nous devons vous
+        associer.
+      </p>
+      <form
+        onSubmit={handleSubmit((values) =>
+          dispatch(
+            inclusionConnectedSlice.actions.registerAgenciesRequested({
+              agencies: values.agencies.map((agency) => agency.id),
+            }),
+          ),
+        )}
+      >
+        <MultipleAgencyInput
+          initialAgencies={watch("agencies")}
+          label="Organisme(s) au(x)quel(s) vous êtes rattaché(s)"
+          onAgencyAdd={(agency) => {
+            setValue("agencies", [...getValues("agencies"), agency]);
+          }}
+          onAgencyDelete={(agency) => {
+            setValue(
+              "agencies",
+              getValues("agencies").filter(
+                (agencyOption) => agencyOption.id !== agency.id,
               ),
-            )}
-          >
-            <MultipleAgencyInput
-              initialAgencies={watch("agencies")}
-              label="Organisme(s) au(x)quel(s) vous êtes rattaché(s)"
-              onAgencyAdd={(agency) => {
-                setValue("agencies", [...getValues("agencies"), agency]);
-              }}
-              onAgencyDelete={(agency) => {
-                setValue(
-                  "agencies",
-                  getValues("agencies").filter(
-                    (agencyOption) => agencyOption.id !== agency.id,
-                  ),
-                );
-              }}
-            />
-            <ErrorNotifications
-              errors={toDotNotation(formErrorsToFlatErrors(formState.errors))}
-              visible={keys(formState.errors).length > 0}
-            />
-            <div className={fr.cx("fr-mt-2w")}>
-              <Button>Demander à être relié à ces organismes</Button>
-            </div>
-          </form>
-        </>
-      )}
-      {feedback.kind === "agencyRegistrationSuccess" && (
-        <Alert
-          severity="success"
-          title="Bravo !"
-          description="Votre demande de première connexion a bien été reçue. Vous recevrez un email de confirmation dès qu'elle aura  été acceptée par nos équipes (2-7 jours ouvrés)."
+            );
+          }}
         />
-      )}
+        <ErrorNotifications
+          errors={toDotNotation(formErrorsToFlatErrors(formState.errors))}
+          visible={keys(formState.errors).length > 0}
+        />
+        <div className={fr.cx("fr-mt-2w")}>
+          <Button>Demander à être relié à ces organismes</Button>
+        </div>
+      </form>
     </>
   );
 };
--- front/src/app/pages/AgencyDashboardPage.tsx
+++ front/src/app/pages/AgencyDashboardPage.tsx
@@ -1,11 +1,15 @@
 import React, { useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { fr } from "@codegouvfr/react-dsfr";
+import { Alert } from "@codegouvfr/react-dsfr/Alert";
 import { Button } from "@codegouvfr/react-dsfr/Button";
+import { all } from "ramda";
+import { match, P } from "ts-pattern";
+import { AgencyRight } from "shared";
+import { Loader } from "react-design-system";
 import { MetabaseView } from "src/app/components/MetabaseView";
 import { SubmitFeedbackNotification } from "src/app/components/SubmitFeedbackNotification";
 import { useAppSelector } from "src/app/hooks/reduxHooks";
-import { authSelectors } from "src/core-logic/domain/auth/auth.selectors";
 import { authSlice } from "src/core-logic/domain/auth/auth.slice";
 import { inclusionConnectedSelectors } from "src/core-logic/domain/inclusionConnected/inclusionConnected.selectors";
 import { inclusionConnectedSlice } from "src/core-logic/domain/inclusionConnected/inclusionConnected.slice";
@@ -13,28 +17,16 @@ import { RegisterAgenciesForm } from "../components/forms/register-agencies/Regi
 
 export const AgencyDashboardPage = () => {
   // the Layout (Header, Footer...) is given by InclusionConnectedPrivateRoute (higher order component)
-  const inclusionConnectToken = useAppSelector(
-    authSelectors.inclusionConnectToken,
-  );
   const currentUser = useAppSelector(inclusionConnectedSelectors.currentUser);
   const feedback = useAppSelector(inclusionConnectedSelectors.feedback);
-  const shouldShowDashboard =
-    currentUser?.agencyRights &&
-    currentUser?.agencyRights.length > 0 &&
-    currentUser.dashboardUrl;
+  const isLoading = useAppSelector(inclusionConnectedSelectors.isLoading);
+
   const dispatch = useDispatch();
 
   useEffect(() => {
     dispatch(inclusionConnectedSlice.actions.currentUserFetchRequested());
   }, []);
 
-  if (!inclusionConnectToken)
-    return (
-      <p>
-        No inclusion token, should not happen as we are in
-        InclusionConnectedPrivateRoute
-      </p>
-    );
   return (
     <>
       <div className={fr.cx("fr-grid-row")}>
@@ -51,22 +43,95 @@ export const AgencyDashboardPage = () => {
           </Button>
         </div>
       </div>
-      {shouldShowDashboard ? (
-        <MetabaseView
-          title="Tableau de bord agence"
-          url={currentUser?.dashboardUrl}
-        />
-      ) : (
-        <RegisterAgenciesForm />
-      )}
+      {isLoading && <Loader />}
+      {match({ currentUser, feedback })
+        .with(
+          {
+            currentUser: null,
+            feedback: {
+              kind: "idle",
+            },
+          },
+          () => <Loader />,
+        )
+        .with(
+          {
+            feedback: {
+              kind: "agencyRegistrationSuccess",
+            },
+          },
+          () => (
+            <Alert
+              severity="success"
+              title="Bravo !"
+              description="Votre demande de première connexion a bien été reçue. Vous recevrez un email de confirmation dès qu'elle aura  été acceptée par nos équipes (2-7 jours ouvrés)."
+            />
+          ),
+        )
+        .with(
+          {
+            currentUser: {
+              dashboardUrl: P.select(
+                P.when((dashboardUrl) => dashboardUrl !== undefined),
+              ),
+            },
+          },
+          (dashboardUrl) => (
+            <MetabaseView title="Tableau de bord agence" url={dashboardUrl} />
+          ),
+        )
+        .with(
+          {
+            currentUser: {
+              agencyRights: [],
+            },
+          },
+          () => <RegisterAgenciesForm />,
+        )
+        .with(
+          {
+            currentUser: {
+              agencyRights: P.when(
+                all(
+                  (agencyRight: AgencyRight) => agencyRight.role === "toReview",
+                ),
+              ),
+            },
+          },
+          () => (
+            <Alert
+              severity="info"
+              title="En attente de validation"
+              description="Votre demande d'accès à l'outil est en cours de validation par l'administration. Vous recevrez un email dès que votre accès sera validé."
+            />
+          ),
+        )
+        .with(
+          {
+            currentUser: {
+              agencyRights: P.when(
+                all(
+                  (agencyRight: AgencyRight) => agencyRight.role === "toReview",
+                ),
+              ),
+            },
+          },
+          () => (
+            <Alert
+              severity="info"
+              title="En attente de validation"
+              description="Votre demande d'accès à l'outil est en cours de validation par l'administration. Vous recevrez un email dès que votre accès sera validé."
+            />
+          ),
+        )
+        .run()}
 
-      {feedback.kind === "errored" &&
-        !feedback.errorMessage.includes("No agency found for user with ID") && (
-          <SubmitFeedbackNotification
-            submitFeedback={feedback}
-            messageByKind={{ errored: "pas utilisé" }}
-          />
-        )}
+      {feedback.kind === "errored" && (
+        <SubmitFeedbackNotification
+          submitFeedback={feedback}
+          messageByKind={{ errored: "pas utilisé" }}
+        />
+      )}
     </>
   );
 };
