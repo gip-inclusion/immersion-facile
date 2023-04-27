9e19e49e61cb6d27d6faf0514d96dda6512f9dae
Benjamin Bohec <bbohec.pro@gmail.com>
2023-04-27 13:10:42 +0200

    NanoIdShortLinkGenerator adapter implementation

--- back/src/adapters/secondary/shortLinkGenerator/NanoIdShortLinkGenerator.ts
+++ back/src/adapters/secondary/shortLinkGenerator/NanoIdShortLinkGenerator.ts
@@ -1,9 +1,11 @@
+import { nanoid } from "nanoid";
 import { ShortLinkGenerator } from "../../../domain/core/ports/ShortLinkGenerator";
 import { ShortLinkId } from "../../../domain/core/ports/ShortLinkQuery";
 
+//2023-04-27: NANO ID used with version ^3.0.0 instead of latest (^4.0.0) due to ESM module errors with jest
 export class NanoIdShortLinkGenerator implements ShortLinkGenerator {
-  generate(): ShortLinkId {
-    // TODO
-    throw new Error("Method not implemented.");
+  public generate(): ShortLinkId {
+    return nanoid(this.idSize);
   }
+  public readonly idSize = 36;
 }
---
+++ back/src/adapters/secondary/shortLinkGenerator/shortLinkGenerator.manual.test.ts
@@ -0,0 +1,25 @@
+import { ShortLinkId } from "../../../domain/core/ports/ShortLinkQuery";
+import { NanoIdShortLinkGenerator } from "./NanoIdShortLinkGenerator";
+
+describe("shortLinkGenerator manual test", () => {
+  const shortLinkQty = 100000;
+  const shortLinkIds: ShortLinkId[] = [];
+  const shortLinkGenerator = new NanoIdShortLinkGenerator();
+
+  it(`generate ${shortLinkQty} ids`, () => {
+    for (let index = 0; index < shortLinkQty; index++) {
+      shortLinkIds.push(shortLinkGenerator.generate());
+    }
+    expect(shortLinkIds.length === shortLinkQty).toBeTruthy();
+  });
+  describe("make sure there is no collision on large shortLink Id generation", () => {
+    it(`make sure there is not collision for ${shortLinkQty} shortLinkIds`, () => {
+      expect(new Set(shortLinkIds).size === shortLinkQty).toBeTruthy();
+    });
+    it(`confirm each shortLinkIds have unique size of ${shortLinkGenerator.idSize}`, () => {
+      const sizes = new Set(shortLinkIds.map((record) => record.length));
+      expect(sizes.size === 1).toBeTruthy();
+      expect(sizes.has(shortLinkGenerator.idSize)).toBeTruthy();
+    });
+  });
+});
