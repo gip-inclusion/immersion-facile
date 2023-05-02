71d65b9b0890339f6e56046707a656f2cf2b1415
Enguerran Weiss <enguerranweiss@gmail.com>
2023-05-02 17:38:01 +0200

    ShareLinkByEmail w/ react-dsfr Modal

--- front/src/app/components/forms/convention/ShareLinkByEmail.tsx
+++ front/src/app/components/forms/convention/ShareLinkByEmail.tsx
@@ -1,56 +1,68 @@
 import React, { useState } from "react";
+import { createPortal } from "react-dom";
 import { useFormContext } from "react-hook-form";
+import { createModal } from "@codegouvfr/react-dsfr/Modal";
 import EmailOutlinedIcon from "@mui/icons-material/EmailOutlined";
 import { IconButton, Tooltip } from "@mui/material";
 import Alert from "@mui/material/Alert";
 import Snackbar from "@mui/material/Snackbar";
 import { ConventionReadDto } from "shared";
-import {
-  ElementModalContainer,
-  useElementContainerModal,
-} from "src/app/components/forms/commons/FormModal/ElementModalContainer";
 import { useConventionTexts } from "src/app/contents/forms/convention/textSetup";
 import { ShareForm } from "./ShareForm";
 
 const iconColor = "#3458a2";
 
+const { ShareLinkModal, openShareLinkModal, closeShareLinkModal } = createModal(
+  {
+    isOpenedByDefault: false,
+    name: "shareLink",
+  },
+);
+
 export const ShareLinkByEmail = () => {
   const { getValues } = useFormContext<ConventionReadDto>();
   const t = useConventionTexts(getValues().internshipKind);
-  const { modalState, dispatch } = useElementContainerModal();
   const [emailSent, setEmailSent] = useState<boolean | null>(null);
   const shareLinkByEmail = t.shareLinkByMail.share;
-
+  const getConventionFormData = () => ({
+    establishmentRepresentativeEmail:
+      getValues().signatories.establishmentRepresentative.email,
+    firstName: getValues().signatories.beneficiary.firstName,
+    lastName: getValues().signatories.beneficiary.lastName,
+    internshipKind: getValues().internshipKind,
+  });
+  const [_isModalOpened, setIsModalOpened] = useState(false);
   return (
     <>
       <Tooltip title={shareLinkByEmail}>
-        <IconButton onClick={() => dispatch({ type: "CLICKED_OPEN" })}>
+        <IconButton
+          onClick={() => {
+            openShareLinkModal();
+            setIsModalOpened(true);
+          }}
+        >
           <EmailOutlinedIcon sx={{ color: iconColor }} />
         </IconButton>
       </Tooltip>
-      <ElementModalContainer
-        modalState={modalState}
-        dispatch={dispatch}
-        title={shareLinkByEmail}
-      >
-        <ShareForm
-          onSuccess={() => {
-            dispatch({ type: "CLICKED_CLOSE" });
-            setEmailSent(true);
-          }}
-          onError={() => {
-            dispatch({ type: "CLICKED_CLOSE" });
-            setEmailSent(false);
-          }}
-          conventionFormData={{
-            establishmentRepresentativeEmail:
-              getValues().signatories.establishmentRepresentative.email,
-            firstName: getValues().signatories.beneficiary.firstName,
-            lastName: getValues().signatories.beneficiary.lastName,
-            internshipKind: getValues().internshipKind,
-          }}
-        />
-      </ElementModalContainer>
+      {createPortal(
+        <ShareLinkModal title={shareLinkByEmail}>
+          <ShareForm
+            onSuccess={() => {
+              closeShareLinkModal();
+              setEmailSent(true);
+              setIsModalOpened(false);
+            }}
+            onError={() => {
+              closeShareLinkModal();
+              setEmailSent(false);
+              setIsModalOpened(false);
+            }}
+            conventionFormData={getConventionFormData()}
+          />
+        </ShareLinkModal>,
+        document.body,
+      )}
+
       {emailSent != null && (
         <Snackbar
           open={true}
