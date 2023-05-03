5148ff8235f7c0f0f4fd84d048965b95a77994ff
clement-duport <clement.duport@beta.gouv.fr>
2023-05-03 15:34:49 +0200

    Co-authored-by: Enguerran Weiss <enguerranws@users.noreply.github.com>

--- libs/react-design-system/src/immersionFacile/components/submit-confirmation-section/SubmitConfirmationSection.styles.ts
+++ libs/react-design-system/src/immersionFacile/components/submit-confirmation-section/SubmitConfirmationSection.styles.ts
@@ -1,9 +1,8 @@
-import './SubmitConfirmationSection.scss';
-  
-    export default {
-      root: 'im-submit-confirmation-section',
-      title: 'im-submit-confirmation-section__title',
-description: 'im-submit-confirmation-section__description',
-copyContent: 'im-submit-confirmation-section__copy-content'
-    }
-  
+import "./SubmitConfirmationSection.scss";
+
+export default {
+  root: "im-submit-confirmation-section",
+  title: "im-submit-confirmation-section__title",
+  description: "im-submit-confirmation-section__description",
+  copyContent: "im-submit-confirmation-section__copy-content",
+};
