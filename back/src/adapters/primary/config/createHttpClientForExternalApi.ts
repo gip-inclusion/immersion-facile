93cc11bb2a2cfa614023673e431d6a5553bd17d9
Jérôme Burkard <burkardjerome@gmail.com>
2023-05-03 16:37:23 +0200

    add axios interceptors to log all external API calls

---
+++ back/src/adapters/primary/config/createHttpClientForExternalApi.ts
@@ -0,0 +1,67 @@
+import axios, { AxiosInstance, AxiosResponse } from "axios";
+import { calculateDurationInSecondsFrom } from "shared";
+import { configureHttpClient, createAxiosHandlerCreator } from "http-client";
+import { createLogger } from "../../../utils/logger";
+
+const logger = createLogger(__filename);
+
+const getRequestInfos = (
+  response?: AxiosResponse,
+  options: { logRequestBody?: boolean; logResponseBody?: boolean } = {},
+) => {
+  if (!response) {
+    return { error: "error.response is not defined, cannot extract infos" };
+  }
+  const requestedAt = (response.config as any).metadata.requestedAt;
+  const durationInSeconds = calculateDurationInSecondsFrom(
+    new Date(requestedAt),
+  );
+
+  return {
+    durationInSeconds,
+    host: response.request?.host,
+    httpStatus: response.status,
+    method: response.config?.method,
+    url: response.config?.url,
+    ...(options.logResponseBody ? { responseBody: response.data } : {}),
+    ...(options.logRequestBody ? { requestBody: response.config?.data } : {}),
+  };
+};
+
+export const configureCreateHttpClientForExternalApi = (
+  axiosInstance: AxiosInstance = axios.create({
+    timeout: 10_000,
+  }),
+) => {
+  axiosInstance.interceptors.request.use((request) => {
+    (request as any).metadata = { requestedAt: new Date().toISOString() };
+    return request;
+  });
+
+  axiosInstance.interceptors.response.use(
+    (response) => {
+      logger.info({
+        _title: "External Api Call",
+        status: "success",
+        ...getRequestInfos(response),
+      });
+
+      return response;
+    },
+    (error) => {
+      logger.error({
+        _title: "External Api Call",
+        status: "errored",
+        message: error.message,
+        ...getRequestInfos(error.response, {
+          logRequestBody: true,
+          logResponseBody: true,
+        }),
+      });
+
+      return Promise.reject(error);
+    },
+  );
+
+  return configureHttpClient(createAxiosHandlerCreator(axiosInstance));
+};
