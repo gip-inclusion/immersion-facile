ff19477cfa3395098d034698fbb7ed5f912aa3be
clement-duport <clement.duport@beta.gouv.fr>
2023-05-03 15:33:45 +0200

    fix review

--- libs/react-design-system/src/immersionFacile/components/submit-confirmation-section/SubmitConfirmationSection.styles.ts
+++ libs/react-design-system/src/immersionFacile/components/submit-confirmation-section/SubmitConfirmationSection.styles.ts
@@ -1,8 +1,9 @@
-import "./SubmitConfirmationSection.scss";
-
-export default {
-  root: "im-submit-confirmation-section",
-  text: "im-submit-confirmation-section__text",
-  copy: "im-submit-confirmation-section__copy",
-  title: "im-submit-confirmation-section__title",
-};
+import './SubmitConfirmationSection.scss';
+  
+    export default {
+      root: 'im-submit-confirmation-section',
+      title: 'im-submit-confirmation-section__title',
+description: 'im-submit-confirmation-section__description',
+copyContent: 'im-submit-confirmation-section__copy-content'
+    }
+  
