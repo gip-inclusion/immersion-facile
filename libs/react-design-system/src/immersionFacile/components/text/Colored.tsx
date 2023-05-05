a95a2ebe1ec7ccacc762a15b39fef9f1346cfdf2
Enguerran Weiss <enguerranweiss@gmail.com>
2023-05-05 10:29:33 +0200

    removed unused components

--- libs/react-design-system/src/immersionFacile/components/text/Colored.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-export type ColoredProps = { red?: boolean; children: string };
-export const Colored = ({ red, children }: ColoredProps) => {
-  const textColor = red ? "text-immersionRed-dark" : "text-immersionBlue-dark";
-  return <span className={`font-bold ${textColor}`}>{children}</span>;
-};
