13bb38faed26481b8c749f449f26345c0131f8de
Enguerran Weiss <enguerranweiss@gmail.com>
2023-05-05 10:29:33 +0200

    removed unused components

--- libs/react-design-system/src/immersionFacile/components/text/index.ts
+++ libs/react-design-system/src/immersionFacile/components/text/index.ts
@@ -1,7 +1,4 @@
 import { storybookPrefix } from "../../storyPrefixes";
 
-export * from "./Colored";
 export * from "./DsfrTitle";
-export * from "./SubTitle";
-export * from "./Title";
 export const textPrefix = storybookPrefix("Text");
