c9159cb7426fd3d3ca13694eb15b259d67748ada
Enguerran Weiss <enguerranweiss@gmail.com>
2023-05-05 10:29:33 +0200

    removed unused components

--- front/src/app/components/search/ContactModalContent.tsx
+++ front/src/app/components/search/ContactModalContent.tsx
@@ -1,4 +1,4 @@
-import React, { ReactNode } from "react";
+import React from "react";
 import { fr } from "@codegouvfr/react-dsfr";
 import {
   ContactMethod,
@@ -6,7 +6,6 @@ import {
   SearchImmersionResultDto,
   SiretDto,
 } from "shared";
-import { SubTitle } from "react-design-system";
 import { ContactByEmail } from "./ContactByEmail";
 import { ContactByPhone } from "./ContactByPhone";
 
@@ -49,10 +48,10 @@ const AdvisesForContact = ({
   data: SearchImmersionResultDto | undefined;
 }) => (
   <div>
-    <Paragraph>
+    <p className={fr.cx("fr-mb-2w")}>
       Cette entreprise peut recruter sur ce métier et être intéressée pour vous
       recevoir en immersion. Tentez votre chance en la contactant !
-    </Paragraph>
+    </p>
     <ul className={fr.cx("fr-btns-group", "fr-mt-3w")}>
       <li>
         {data?.website && (
@@ -82,14 +81,14 @@ const AdvisesForContact = ({
       Nos conseils pour cette première prise de contact&nbsp;!{" "}
     </h2>
 
-    <SubTitle>Comment présenter votre demande ? </SubTitle>
-    <Paragraph>
-      Soyez <Bold>direct, concret et courtois</Bold>. Présentez-vous, présentez
-      simplement votre projet et l’objectif que vous recherchez en effectuant
-      une immersion.
-    </Paragraph>
-    <Paragraph>
-      <Bold>Par exemple : </Bold>
+    <h3 className={fr.cx("fr-h6")}>Comment présenter votre demande ? </h3>
+    <p className={fr.cx("fr-mb-2w")}>
+      Soyez <strong>direct, concret et courtois</strong>. Présentez-vous,
+      présentez simplement votre projet et l’objectif que vous recherchez en
+      effectuant une immersion.
+    </p>
+    <p className={fr.cx("fr-mb-2w")}>
+      <strong>Par exemple : </strong>
       <span>
         “Je souhaite devenir mécanicien auto et je voudrais découvrir comment ce
         métier se pratique dans un garage comme le vôtre. Ca me permettra de
@@ -97,72 +96,66 @@ const AdvisesForContact = ({
         présentera le métier pourra aussi vérifier si ce métier est fait pour
         moi.”
       </span>
-    </Paragraph>
-    <Paragraph>
+    </p>
+    <p className={fr.cx("fr-mb-2w")}>
       Vous pouvez indiquer à votre interlocutrice ou interlocuteur que{" "}
-      <Bold>
+      <strong>
         cette immersion sera encadrée par une convention signée par l'organisme
         qui vous suit.
-      </Bold>
-    </Paragraph>
-    <Paragraph>
+      </strong>
+    </p>
+    <p className={fr.cx("fr-mb-2w")}>
       Indiquez lui le moment où vous aimeriez faire une immersion et pourquoi
       vous voulez la faire à cette date.
-    </Paragraph>
-    <Paragraph>
-      <Bold>Par exemple : </Bold>
+    </p>
+    <p className={fr.cx("fr-mb-2w")}>
+      <strong>Par exemple : </strong>
       <span>
         “il faudrait que je fasse une immersion avant de m’inscrire à une
         formation. “
       </span>
-    </Paragraph>
-    <Paragraph>
-      Indiquez également le <Bold>nombre de jours</Bold> que vous aimeriez faire
-      en immersion si vous le savez déjà.
-    </Paragraph>
-    <Paragraph>
-      Concluez en lui demandant <Bold>un rendez-vous</Bold> pour qu’il/elle se
-      rende compte du sérieux de votre projet.
-    </Paragraph>
+    </p>
+    <p className={fr.cx("fr-mb-2w")}>
+      Indiquez également le <strong>nombre de jours</strong> que vous aimeriez
+      faire en immersion si vous le savez déjà.
+    </p>
+    <p className={fr.cx("fr-mb-2w")}>
+      Concluez en lui demandant <strong>un rendez-vous</strong> pour qu’il/elle
+      se rende compte du sérieux de votre projet.
+    </p>
 
-    <SubTitle>Comment expliquer simplement ce qu’est une immersion ?</SubTitle>
-    <Paragraph>
+    <h3 className={fr.cx("fr-h6")}>
+      Comment expliquer simplement ce qu’est une immersion ?
+    </h3>
+    <p className={fr.cx("fr-mb-2w")}>
       C’est un stage d’observation, strictement encadré d’un point de vue
       juridique. Vous conservez votre statut et êtes couvert par votre Pôle
       emploi,votre Mission Locale ou le Conseil départemental (en fonction de
       votre situation).
-    </Paragraph>
-    <Paragraph>
+    </p>
+    <p className={fr.cx("fr-mb-2w")}>
       Le rôle de celui qui vous accueillera est de vous présenter le métier et
       de vérifier avec vous que ce métier vous convient en vous faisant des
       retours les plus objectifs possibles. Pendant la durée de votre présence,
       vous pouvez aider les salariés en donnant un coup de main mais vous n’êtes
       pas là pour remplacer un collègue absent.
-    </Paragraph>
+    </p>
 
-    <SubTitle>Quelle est la durée d’une immersion ?</SubTitle>
-    <Paragraph>
+    <h3 className={fr.cx("fr-h6")}>Quelle est la durée d’une immersion ?</h3>
+    <p className={fr.cx("fr-mb-2w")}>
       Les immersions se font le plus souvent pendant une semaine ou deux.{" "}
-      <Bold>Il n’est pas possible de dépasser un mois</Bold>. Il est possible de
-      faire une immersion de seulement un ou deux jours mais vous ne découvrirez
-      pas parfaitement un métier.
-    </Paragraph>
+      <strong>Il n’est pas possible de dépasser un mois</strong>. Il est
+      possible de faire une immersion de seulement un ou deux jours mais vous ne
+      découvrirez pas parfaitement un métier.
+    </p>
 
-    <SubTitle>Bon à savoir ! </SubTitle>
-    <Paragraph>
-      <Bold>Il n’est pas nécessaire d’apporter votre CV</Bold>. Vous êtes là
+    <h3 className={fr.cx("fr-h6")}>Bon à savoir ! </h3>
+    <p className={fr.cx("fr-mb-2w")}>
+      <strong>Il n’est pas nécessaire d’apporter votre CV</strong>. Vous êtes là
       pour demander à découvrir un métier et c’est ce projet qui est important,
       pas vos expériences professionnelles ni votre formation !
-    </Paragraph>
+    </p>
   </div>
-);
-
-const Paragraph = ({ children }: { children: ReactNode }) => (
-  <p className={fr.cx("fr-mb-2w")}>{children}</p>
-);
-
-const Bold = ({ children }: { children: string }) => (
-  <strong>{children}</strong>
 );
 
 export const getMapsLink = (
