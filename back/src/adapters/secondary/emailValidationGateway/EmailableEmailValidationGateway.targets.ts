2e0941e63b1dbf5b4f4e79a7509f03cbe604d912
Benjamin Bohec <bbohec.pro@gmail.com>
2023-05-23 16:32:49 +0200

    enum ZodError with data context

--- back/src/adapters/secondary/emailValidationGateway/EmailableEmailValidationGateway.targets.ts
+++ back/src/adapters/secondary/emailValidationGateway/EmailableEmailValidationGateway.targets.ts
@@ -1,3 +1,4 @@
+import { ZodErrorMap } from "zod";
 import { createTarget, createTargets } from "http-client";
 import {
   emailableEmailValidationStatusSchema,
@@ -5,11 +6,25 @@ import {
 } from "./EmailableEmailValidationGateway.schemas";
 
 export type EmailableValidationTargets = typeof emailableValidationTargets;
+
 export const emailableValidationTargets = createTargets({
   validateEmail: createTarget({
     method: "GET",
     url: "https://api.emailable.com/v1/verify",
     validateQueryParams: emailableValidationTargetsQueryParamsSchema.parse,
-    validateResponseBody: emailableEmailValidationStatusSchema.parse,
+    validateResponseBody: (data) =>
+      emailableEmailValidationStatusSchema.parse(data, {
+        errorMap,
+      }),
   }),
 });
+
+const errorMap: ZodErrorMap = (issue, ctx) =>
+  issue.code === "invalid_enum_value"
+    ? {
+        message: [
+          ctx.defaultError,
+          `But value '${JSON.stringify(ctx.data)}' provided.`,
+        ].join(" "),
+      }
+    : { message: ctx.defaultError };
