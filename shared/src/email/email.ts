e1afe0e20b79b77f1983bdce8a2fd40368f9c8b1
Jérôme Burkard <burkardjerome@gmail.com>
2023-05-25 18:11:51 +0200

    fixes after review

--- shared/src/email/email.ts
+++ shared/src/email/email.ts
@@ -23,11 +23,11 @@ type KeysOfUnion<T> = T extends T ? keyof T : never;
 // https://stackoverflow.com/questions/49401866/all-possible-keys-of-an-union-type
 export type EmailVariables = KeysOfUnion<TemplatedEmail["params"]>;
 
-type EmailTemplateByName = typeof emailTemplatesByName;
+export type EmailTemplatesByName = typeof emailTemplatesByName;
 
 export type TemplatedEmail = ValueOf<{
-  [TemplateName in keyof EmailTemplateByName]: GenericTemplatedEmail<
+  [TemplateName in keyof EmailTemplatesByName]: GenericTemplatedEmail<
     TemplateName,
-    Parameters<EmailTemplateByName[TemplateName]["createEmailVariables"]>[0]
+    Parameters<EmailTemplatesByName[TemplateName]["createEmailVariables"]>[0]
   >;
 }>;
