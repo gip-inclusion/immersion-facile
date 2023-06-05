758988ca52ce68a6bb06aa92de5297d778d421ae
Benjamin Bohec <bbohec.pro@gmail.com>
2023-06-05 15:56:49 +0200

    PeAccessTokenGateway with Bottleneck

--- back/src/adapters/secondary/immersionOffer/PoleEmploiAccessTokenGateway.ts
+++ back/src/adapters/secondary/immersionOffer/PoleEmploiAccessTokenGateway.ts
@@ -1,10 +1,10 @@
+import Bottleneck from "bottleneck";
 import { secondsToMilliseconds } from "date-fns";
 import querystring from "querystring";
 import {
   AccessTokenGateway,
   GetAccessTokenResponse,
 } from "../../../domain/core/ports/AccessTokenGateway";
-import { RateLimiter } from "../../../domain/core/ports/RateLimiter";
 import {
   RetryableError,
   RetryStrategy,
@@ -18,31 +18,33 @@ import { createLogger } from "../../../utils/logger";
 import { AccessTokenConfig } from "../../primary/config/appConfig";
 
 const logger = createLogger(__filename);
+
+const poleEmploiAccessTokenMaxRequestsPerSeconds = 3;
+
 export class PoleEmploiAccessTokenGateway implements AccessTokenGateway {
   public constructor(
     private readonly accessTokenConfig: AccessTokenConfig,
-    private readonly rateLimiter: RateLimiter,
     private readonly retryStrategy: RetryStrategy,
   ) {}
 
   public async getAccessToken(scope: string): Promise<GetAccessTokenResponse> {
-    const dataAccessToken = querystring.stringify({
-      grant_type: "client_credentials",
-      client_id: this.accessTokenConfig.clientId,
-      client_secret: this.accessTokenConfig.clientSecret,
-      scope,
-    });
-    const headers = {
-      "Content-Type": "application/x-www-form-urlencoded",
-    };
-
     return this.retryStrategy.apply(async () => {
       try {
-        const response = await this.rateLimiter.whenReady(() =>
+        const response = await this.limiter.schedule(() =>
           createAxiosInstance(logger).post(
             `${this.accessTokenConfig.peEnterpriseUrl}/connexion/oauth2/access_token?realm=%2Fpartenaire`,
-            dataAccessToken,
-            { headers, timeout: secondsToMilliseconds(10) },
+            querystring.stringify({
+              grant_type: "client_credentials",
+              client_id: this.accessTokenConfig.clientId,
+              client_secret: this.accessTokenConfig.clientSecret,
+              scope,
+            }),
+            {
+              headers: {
+                "Content-Type": "application/x-www-form-urlencoded",
+              },
+              timeout: secondsToMilliseconds(10),
+            },
           ),
         );
         return response.data;
@@ -53,4 +55,10 @@ export class PoleEmploiAccessTokenGateway implements AccessTokenGateway {
       }
     });
   }
+
+  private limiter = new Bottleneck({
+    reservoir: poleEmploiAccessTokenMaxRequestsPerSeconds,
+    reservoirRefreshInterval: 1000, // number of ms
+    reservoirRefreshAmount: poleEmploiAccessTokenMaxRequestsPerSeconds,
+  });
 }
