afeecd4df45e8494664c38ddf1045526a0c02163
Enguerran Weiss <enguerranweiss@gmail.com>
2023-06-13 09:01:15 +0200

    config vars in cypress.config.ts

---
+++ cypress/cypress/utils/log.ts
@@ -0,0 +1,6 @@
+export const disableUrlLogging = () => {
+  cy.on("log:added", () => {
+    const logs = window.top.document.querySelectorAll(".command-name-new-url");
+    logs.forEach((log) => log.classList.add("hidden"));
+  });
+};
