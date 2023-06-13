0e42538f3bcb3f2bd221c31f1cdc182d8afaa7f0
clement-duport <clement.duport@beta.gouv.fr>
2023-06-13 13:47:43 +0200

    fix migration

--- back/src/adapters/secondary/pg/migrations/1686299027505_add-column-potential-beneficiary-phone-immersion-obect-and-link-to-discussion-table.ts
+++ back/src/adapters/secondary/pg/migrations/1686299027505_add-column-potential-beneficiary-phone-immersion-obect-and-link-to-discussion-table.ts
@@ -1,6 +1,11 @@
 import { MigrationBuilder } from "node-pg-migrate";
 
 export async function up(pgm: MigrationBuilder): Promise<void> {
+  pgm.createType("immersion_objectives", [
+    "Confirmer un projet professionnel",
+    "Découvrir un métier ou un secteur d'activité",
+    "Initier une démarche de recrutement",
+  ]);
   pgm.addColumns("discussions", {
     potential_beneficiary_phone: {
       type: "text",
@@ -26,4 +31,5 @@ export async function down(pgm: MigrationBuilder): Promise<void> {
     "immersion_objective",
     "potential_beneficiary_resume_link",
   ]);
+  pgm.dropType("immersion_objectives");
 }
