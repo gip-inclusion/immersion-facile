cb1e4a0a0593e2c176c2dedef9f4c5c8cefd004a
celineung <celine.ung.pro@gmail.com>
2023-06-27 10:34:11 +0200

    fix upsert in PgConventionsToSyncRepository

--- back/src/adapters/secondary/pg/PgConventionsToSyncRepository.ts
+++ back/src/adapters/secondary/pg/PgConventionsToSyncRepository.ts
@@ -29,38 +29,19 @@ export class PgConventionsToSyncRepository
   }
 
   async save(conventionToSync: ConventionToSync): Promise<void> {
-    await this.client.query(
-      `
-          INSERT INTO ${conventionsToSyncTableName} (id,
-                                                     status,
-                                                     process_date,
-                                                     reason)
-          VALUES ($1, $2, $3, $4)
-      `,
-      [
-        conventionToSync.id,
-        conventionToSync.status,
-        conventionToSync.status !== "TO_PROCESS"
-          ? conventionToSync.processDate
-          : null,
-        conventionToSync.status === "ERROR" ||
-        conventionToSync.status === "SKIP"
-          ? conventionToSync.reason
-          : null,
-      ],
-    );
+    return (await isConventionToSyncAlreadyExists(
+      this.client,
+      conventionToSync.id,
+    ))
+      ? updateConventionToSync(this.client, conventionToSync)
+      : insertConventionToSync(this.client, conventionToSync);
   }
 
   async getById(id: ConventionId): Promise<ConventionToSync | undefined> {
-    const queryResult = await this.client.query<PgConventionToSync>(
-      `
-          SELECT id, status, process_date, reason
-          FROM ${conventionsToSyncTableName}
-          WHERE id = $1
-      `,
-      [id],
+    const pgConventionToSync = await selectConventionToSyncById(
+      this.client,
+      id,
     );
-    const pgConventionToSync = queryResult.rows.at(0);
     return pgConventionToSync
       ? pgResultToConventionToSync(pgConventionToSync)
       : undefined;
@@ -83,4 +64,84 @@ function pgResultToConventionToSync(
     processDate: pgConventionToSync.process_date ?? undefined,
     reason: pgConventionToSync.reason ?? undefined,
   } as ConventionToSync;
+}
+
+async function isConventionToSyncAlreadyExists(
+  client: PoolClient,
+  conventionId: ConventionId,
+): Promise<boolean> {
+  return (
+    await client.query(
+      `SELECT EXISTS(SELECT 1
+                     FROM ${conventionsToSyncTableName}
+                     WHERE id = $1)`,
+      [conventionId],
+    )
+  ).rows.at(0).exists;
+}
+
+async function updateConventionToSync(
+  client: PoolClient,
+  conventionToSync: ConventionToSync,
+): Promise<void> {
+  await client.query(
+    `
+        UPDATE ${conventionsToSyncTableName}
+        SET status       = $2,
+            process_date = $3,
+            reason       = $4
+        WHERE id = $1
+    `,
+    [
+      conventionToSync.id,
+      conventionToSync.status,
+      conventionToSync.status !== "TO_PROCESS"
+        ? conventionToSync.processDate
+        : null,
+      conventionToSync.status === "ERROR" || conventionToSync.status === "SKIP"
+        ? conventionToSync.reason
+        : null,
+    ],
+  );
+}
+
+async function insertConventionToSync(
+  client: PoolClient,
+  conventionToSync: ConventionToSync,
+): Promise<void> {
+  await client.query(
+    `
+        INSERT INTO ${conventionsToSyncTableName} (id,
+                                                   status,
+                                                   process_date,
+                                                   reason)
+        VALUES ($1, $2, $3, $4)
+    `,
+    [
+      conventionToSync.id,
+      conventionToSync.status,
+      conventionToSync.status !== "TO_PROCESS"
+        ? conventionToSync.processDate
+        : null,
+      conventionToSync.status === "ERROR" || conventionToSync.status === "SKIP"
+        ? conventionToSync.reason
+        : null,
+    ],
+  );
+}
+
+async function selectConventionToSyncById(
+  client: PoolClient,
+  conventionId: ConventionId,
+): Promise<PgConventionToSync | undefined> {
+  return (
+    await client.query<PgConventionToSync>(
+      `
+        SELECT id, status, process_date, reason
+        FROM ${conventionsToSyncTableName}
+        WHERE id = $1
+    `,
+      [conventionId],
+    )
+  ).rows.at(0);
 }
