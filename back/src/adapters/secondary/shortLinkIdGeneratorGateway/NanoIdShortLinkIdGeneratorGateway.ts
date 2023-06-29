5b5b46702cb37c9c46049673afc61a133ef2d5e4
Benjamin Bohec <bbohec.pro@gmail.com>
2023-06-29 11:15:13 +0200

    log on generate shortlink for troubleshooting

--- back/src/adapters/secondary/shortLinkIdGeneratorGateway/NanoIdShortLinkIdGeneratorGateway.ts
+++ back/src/adapters/secondary/shortLinkIdGeneratorGateway/NanoIdShortLinkIdGeneratorGateway.ts
@@ -1,13 +1,23 @@
 import { nanoid } from "nanoid";
 import { ShortLinkIdGeneratorGateway } from "../../../domain/core/ports/ShortLinkIdGeneratorGateway";
 import { ShortLinkId } from "../../../domain/core/ports/ShortLinkQuery";
+import { createLogger } from "../../../utils/logger";
+
+const logger = createLogger(__filename);
 
 //2023-04-27: NANO ID used with version ^3.0.0 instead of latest (^4.0.0) due to ESM module errors with jest
 export class NanoIdShortLinkIdGeneratorGateway
   implements ShortLinkIdGeneratorGateway
 {
   public generate(): ShortLinkId {
-    return nanoid(this.idSize);
+    const id = nanoid(this.idSize);
+    logger.info(
+      {
+        shortlinkId: id,
+      },
+      "shortlink id generated",
+    );
+    return id;
   }
   public readonly idSize = 36;
 }
