5194b08b98b78498f9dd9c897d5f9312ee90dea9
clement-duport <clement.duport@beta.gouv.fr>
2023-07-04 11:13:25 +0200

    create search immersion request public V1 schema and dto

---
+++ back/src/adapters/primary/routers/DtoAndSchemas/v1/input/SearchImmersionRequestPublicV1.schema.ts
@@ -0,0 +1,22 @@
+import { z } from "zod";
+import {
+  appellationSchema,
+  romeCodeSchema,
+  siretSchema,
+  zToBoolean,
+  zToNumber,
+} from "shared";
+import { SearchImmersionRequestPublicV1 } from "./SearchImmersionRequestPublicV1dto";
+
+export const searchImmersionRequestPublicV1Schema: z.Schema<SearchImmersionRequestPublicV1> =
+  z.object({
+    rome: romeCodeSchema.optional(),
+    appellationCode: appellationSchema.optional(),
+    siret: siretSchema.optional(),
+    latitude: zToNumber,
+    longitude: zToNumber,
+    distance_km: zToNumber.positive("'distance_km' doit être > 0").max(100),
+    voluntaryToImmersion: zToBoolean.optional(),
+    place: z.string().optional(),
+    sortedBy: z.enum(["distance", "date"]).optional(),
+  });
