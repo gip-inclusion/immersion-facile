d3f3b37db09b108a44a3c3702df6910d8ce9321b
Benjamin Bohec <bbohec.pro@gmail.com>
2023-07-05 19:04:55 +0200

    clean addFormEstablishment e2e

--- back/src/adapters/primary/routers/createEstablishment/addFormEstablishment.e2e.test.ts
+++ back/src/adapters/primary/routers/createEstablishment/addFormEstablishment.e2e.test.ts
@@ -2,16 +2,18 @@ import { SuperTest, Test } from "supertest";
 import {
   defaultValidFormEstablishment,
   establishmentTargets,
-  FormEstablishmentDto,
+  expectToEqual,
   FormEstablishmentDtoBuilder,
 } from "shared";
 import { avenueChampsElysees } from "../../../../_testBuilders/addressDtos";
 import {
   buildTestApp,
-  TestAppAndDeps,
+  InMemoryGateways,
 } from "../../../../_testBuilders/buildTestApp";
 import { validApiConsumerJwtPayload } from "../../../../_testBuilders/jwtTestHelper";
 import { processEventsForEmailToBeSent } from "../../../../_testBuilders/processEventsForEmailToBeSent";
+import { GenerateApiConsumerJwt } from "../../../../domain/auth/jwt";
+import { BasicEventCrawler } from "../../../secondary/core/EventCrawlerImplementations";
 import { TEST_OPEN_ESTABLISHMENT_1 } from "../../../secondary/siret/InMemorySiretGateway";
 import { InMemoryUnitOfWork } from "../../config/uowConfig";
 import { FormEstablishmentDtoPublicV0 } from "../DtoAndSchemas/v0/input/FormEstablishmentPublicV0.dto";
@@ -20,9 +22,13 @@ import { FormEstablishmentDtoPublicV1 } from "../DtoAndSchemas/v1/input/FormEsta
 describe("Add form establishment", () => {
   let request: SuperTest<Test>;
   let inMemoryUow: InMemoryUnitOfWork;
+  let generateApiConsumerJwt: GenerateApiConsumerJwt;
+  let gateways: InMemoryGateways;
+  let eventCrawler: BasicEventCrawler;
 
   beforeEach(async () => {
-    ({ request, inMemoryUow } = await buildTestApp());
+    ({ request, inMemoryUow, generateApiConsumerJwt, gateways, eventCrawler } =
+      await buildTestApp());
   });
 
   describe("Route to post form establishments from front (hence, without API key)", () => {
@@ -39,17 +45,14 @@ describe("Add form establishment", () => {
         .post(establishmentTargets.addFormEstablishment.url)
         .send(formEstablishment);
 
-      expect(response.status).toBe(200);
-      expect(response.body).toBe("");
-
-      const inRepo = await inMemoryUow.formEstablishmentRepository.getAll();
-      expect(inRepo).toEqual([formEstablishment]);
+      expectToEqual(response.status, 200);
+      expectToEqual(response.body, "");
+      expectToEqual(await inMemoryUow.formEstablishmentRepository.getAll(), [
+        formEstablishment,
+      ]);
     });
 
     it("Check if email notification has been sent and published after FormEstablishment added", async () => {
-      const { eventCrawler, gateways, request, inMemoryUow }: TestAppAndDeps =
-        await buildTestApp();
-
       gateways.addressApi.setAddressAndPosition([
         {
           position: {
@@ -68,31 +71,25 @@ describe("Add form establishment", () => {
       inMemoryUow.romeRepository.appellations =
         defaultValidFormEstablishment.appellations;
 
-      const formEstablishment: FormEstablishmentDto =
-        FormEstablishmentDtoBuilder.valid()
-          .withSiret(TEST_OPEN_ESTABLISHMENT_1.siret)
-          .build();
-      const formEstablishmentWithBusinessContact: FormEstablishmentDto = {
-        ...formEstablishment,
-        businessContact: {
-          ...formEstablishment.businessContact,
-          email: "tiredofthismess@seriously.com",
-        },
-      };
+      const email = "tiredofthismess@seriously.com";
 
       const response = await request
         .post(establishmentTargets.addFormEstablishment.url)
-        .send(formEstablishmentWithBusinessContact);
+        .send(
+          FormEstablishmentDtoBuilder.valid()
+            .withSiret(TEST_OPEN_ESTABLISHMENT_1.siret)
+            .withBusinessContactEmail(email)
+            .build(),
+        );
 
       expect(response.status).toBe(200);
 
       await processEventsForEmailToBeSent(eventCrawler);
 
-      const sentEmails = gateways.notification.getSentEmails();
-      expect(sentEmails).toHaveLength(1);
-      expect(sentEmails.map((e) => e.recipients)).toEqual([
-        ["tiredofthismess@seriously.com"],
-      ]);
+      expectToEqual(
+        gateways.notification.getSentEmails().map((e) => e.recipients),
+        [[email]],
+      );
     });
   });
 
@@ -101,59 +98,53 @@ describe("Add form establishment", () => {
     describe("v0", () => {
       // we don't want to use variables from src/routes.ts so that we can check if contract breaks
       it("forbids access to route if no api consumer", async () => {
-        const { request } = await buildTestApp();
-
         const response = await request.post(`/immersion-offers`).send({});
 
         expect(response.status).toBe(403);
       });
 
       it("support adding establishment from known api consumer", async () => {
-        const { request, generateApiConsumerJwt: generateApiJwt } =
-          await buildTestApp();
-
-        const formEstablishmentDtoPublicV0: FormEstablishmentDtoPublicV0 = {
-          businessAddress: "1 Rue du Moulin 12345 Quelque Part",
-          businessContacts: [
-            {
-              email: "amil@mail.com",
-              firstName: "Esteban",
-              lastName: "Ocon",
-              phone: "+33012345678",
-              job: "a job",
-            },
-          ],
-          preferredContactMethods: ["EMAIL"],
-          naf: { code: "A", nomenclature: "nomenclature code A" },
-          businessName: "Mon entreprise",
-          businessNameCustomized: "Ma belle enseigne du quartier",
-          isEngagedEnterprise: false,
-          siret: TEST_OPEN_ESTABLISHMENT_1.siret,
-          professions: [
-            {
-              romeCodeMetier: "A1111",
-              romeCodeAppellation: "11111",
-              description: "Boulangerie",
-            },
-            {
-              romeCodeMetier: "B9112",
-              romeCodeAppellation: "22222",
-              description: "Patissier",
-            },
-            {
-              romeCodeMetier: "D1103",
-              romeCodeAppellation: undefined,
-              description: "Boucherie",
-            },
-          ],
-        };
-
-        const jwt = generateApiJwt(validApiConsumerJwtPayload);
-
         const response = await request
           .post(`/immersion-offers`)
-          .set("Authorization", jwt)
-          .send(formEstablishmentDtoPublicV0);
+          .set(
+            "Authorization",
+            generateApiConsumerJwt(validApiConsumerJwtPayload),
+          )
+          .send({
+            businessAddress: "1 Rue du Moulin 12345 Quelque Part",
+            businessContacts: [
+              {
+                email: "amil@mail.com",
+                firstName: "Esteban",
+                lastName: "Ocon",
+                phone: "+33012345678",
+                job: "a job",
+              },
+            ],
+            preferredContactMethods: ["EMAIL"],
+            naf: { code: "A", nomenclature: "nomenclature code A" },
+            businessName: "Mon entreprise",
+            businessNameCustomized: "Ma belle enseigne du quartier",
+            isEngagedEnterprise: false,
+            siret: TEST_OPEN_ESTABLISHMENT_1.siret,
+            professions: [
+              {
+                romeCodeMetier: "A1111",
+                romeCodeAppellation: "11111",
+                description: "Boulangerie",
+              },
+              {
+                romeCodeMetier: "B9112",
+                romeCodeAppellation: "22222",
+                description: "Patissier",
+              },
+              {
+                romeCodeMetier: "D1103",
+                romeCodeAppellation: undefined,
+                description: "Boucherie",
+              },
+            ],
+          } satisfies FormEstablishmentDtoPublicV0);
 
         expect(response.body).toBe("");
         expect(response.status).toBe(200);
@@ -163,7 +154,6 @@ describe("Add form establishment", () => {
     describe("v1", () => {
       const consumerv1FormEstablishmentsRoute = `/v1/form-establishments`;
       it("forbids access to route if no api consumer", async () => {
-        const { request } = await buildTestApp();
         const response = await request
           .post(consumerv1FormEstablishmentsRoute)
           .send({});
@@ -172,22 +162,19 @@ describe("Add form establishment", () => {
       });
 
       it("forbids access to route if invalid jwt", async () => {
-        const { request } = await buildTestApp();
         const response = await request
           .post(consumerv1FormEstablishmentsRoute)
           .set("Authorization", "jwt-invalid")
           .send({});
+
         expect(response.body).toEqual({ error: "forbidden: incorrect Jwt" });
         expect(response.status).toBe(401);
       });
 
       it("forbids adding establishment from unauthorized api consumer", async () => {
-        const { request, generateApiConsumerJwt: generateApiJwt } =
-          await buildTestApp();
-        const jwt = generateApiJwt({ id: "my-unknown-id" });
         const response = await request
           .post(consumerv1FormEstablishmentsRoute)
-          .set("Authorization", jwt)
+          .set("Authorization", generateApiConsumerJwt({ id: "my-unknown-id" }))
           .send({});
 
         expect(response.body).toEqual({
@@ -197,13 +184,14 @@ describe("Add form establishment", () => {
       });
 
       it("forbids access to route if id is unauthorized", async () => {
-        const { request, generateApiConsumerJwt: generateApiJwt } =
-          await buildTestApp();
-        const jwt = generateApiJwt({ id: "my-unauthorized-id" });
         const response = await request
           .post(consumerv1FormEstablishmentsRoute)
-          .set("Authorization", jwt)
+          .set(
+            "Authorization",
+            generateApiConsumerJwt({ id: "my-unauthorized-id" }),
+          )
           .send({});
+
         expect(response.body).toEqual({
           error: "forbidden: unauthorised consumer Id",
         });
@@ -211,42 +199,36 @@ describe("Add form establishment", () => {
       });
 
       it("forbids access to route if token has expired", async () => {
-        const {
-          request,
-          generateApiConsumerJwt: generateApiJwt,
-          gateways,
-        } = await buildTestApp();
         gateways.timeGateway.setNextDate(new Date());
-        const jwt = generateApiJwt({ id: "my-outdated-id" });
+
         const response = await request
           .post(consumerv1FormEstablishmentsRoute)
-          .set("Authorization", jwt)
+          .set(
+            "Authorization",
+            generateApiConsumerJwt({ id: "my-outdated-id" }),
+          )
           .send({});
-        expect(response.body).toEqual({
+
+        expectToEqual(response.body, {
           error: "forbidden: expired token",
         });
         expect(response.status).toBe(403);
       });
 
       it("support adding establishment from known api consumer (for exemple Un Jeune Une Solution)", async () => {
-        const { request, generateApiConsumerJwt: generateApiJwt } =
-          await buildTestApp();
-        const formEstablishmentDto = FormEstablishmentDtoBuilder.valid()
-          .withSiret(TEST_OPEN_ESTABLISHMENT_1.siret)
-          .build();
-
-        const formEstablishmentDtoPublicV1: FormEstablishmentDtoPublicV1 = {
-          ...formEstablishmentDto,
-          isSearchable: true,
-          businessAddress: avenueChampsElysees,
-        };
-
-        const jwt = generateApiJwt(validApiConsumerJwtPayload);
-
         const response = await request
           .post(consumerv1FormEstablishmentsRoute)
-          .set("Authorization", jwt)
-          .send(formEstablishmentDtoPublicV1);
+          .set(
+            "Authorization",
+            generateApiConsumerJwt(validApiConsumerJwtPayload),
+          )
+          .send({
+            ...FormEstablishmentDtoBuilder.valid()
+              .withSiret(TEST_OPEN_ESTABLISHMENT_1.siret)
+              .build(),
+            isSearchable: true,
+            businessAddress: avenueChampsElysees,
+          } satisfies FormEstablishmentDtoPublicV1);
 
         expect(response.status).toBe(200);
       });
