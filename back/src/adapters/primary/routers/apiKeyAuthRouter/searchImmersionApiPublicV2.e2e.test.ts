46378b4ac9e65c980e24147c65bd593cddbb8fc2
clement-duport <clement.duport@beta.gouv.fr>
2023-07-05 09:14:04 +0200

    update api key auth router v2 and add test

---
+++ back/src/adapters/primary/routers/apiKeyAuthRouter/searchImmersionApiPublicV2.e2e.test.ts
@@ -0,0 +1,143 @@
+import { SuperTest, Test } from "supertest";
+import {
+  avenueChampsElysees,
+  avenueChampsElyseesDto,
+} from "../../../../_testBuilders/addressDtos";
+import { buildTestApp } from "../../../../_testBuilders/buildTestApp";
+import { EstablishmentAggregateBuilder } from "../../../../_testBuilders/EstablishmentAggregateBuilder";
+import {
+  defaultNafCode,
+  EstablishmentEntityBuilder,
+} from "../../../../_testBuilders/EstablishmentEntityBuilder";
+import { ImmersionOfferEntityV2Builder } from "../../../../_testBuilders/ImmersionOfferEntityV2Builder";
+import { GenerateApiConsumerJwt } from "../../../../domain/auth/jwt";
+import { InMemoryEstablishmentAggregateRepository } from "../../../secondary/immersionOffer/InMemoryEstablishmentAggregateRepository";
+import { SearchImmersionResultPublicV2 } from "../DtoAndSchemas/v2/output/SearchImmersionResultPublicV2.dto";
+
+describe("search-immersion route", () => {
+  let request: SuperTest<Test>;
+  let establishmentAggregateRepository: InMemoryEstablishmentAggregateRepository;
+
+  let generateApiJwt: GenerateApiConsumerJwt;
+
+  beforeEach(async () => {
+    const {
+      request: testAppRequest,
+      generateApiJwt: testAppGenerateApiJwt,
+      inMemoryUow,
+    } = await buildTestApp();
+    request = testAppRequest;
+    establishmentAggregateRepository =
+      inMemoryUow.establishmentAggregateRepository;
+    generateApiJwt = testAppGenerateApiJwt;
+  });
+
+  describe(`v2 - /v2/immersion-offers`, () => {
+    describe("verify consumer is authenticated and authorized", () => {
+      it("rejects unauthenticated requests", async () => {
+        await request
+          .get(
+            `/v2/immersion-offers?rome=XXXXX&distanceKm=30&longitude=2.34999&latitude=48.8531&sortedBy=distance`,
+          )
+          .expect(401, { error: "forbidden: unauthenticated" });
+      });
+      it("rejects unauthorized consumer", async () => {
+        await request
+          .get(
+            `/v2/immersion-offers?rome=XXXXX&distanceKm=30&longitude=2.34999&latitude=48.8531&sortedBy=distance`,
+          )
+          .set("Authorization", generateApiJwt({ id: "my-unauthorized-id" }))
+          .expect(403, {
+            error: "forbidden: unauthorised consumer Id",
+          });
+      });
+    });
+    describe("authenficated consumer", () => {
+      it("with given rome and position", async () => {
+        const immersionOffer = new ImmersionOfferEntityV2Builder()
+          .withRomeCode("M1808")
+          .withAppellationCode("11704")
+          .build();
+        // Prepare
+        await establishmentAggregateRepository.insertEstablishmentAggregates([
+          new EstablishmentAggregateBuilder()
+            .withImmersionOffers([immersionOffer])
+            .withEstablishment(
+              new EstablishmentEntityBuilder()
+                .withPosition({
+                  lat: 48.8531,
+                  lon: 2.34999,
+                })
+                .withWebsite("www.jobs.fr")
+                .build(),
+            )
+            .build(),
+        ]);
+
+        // Act and assert
+        const expectedResult: SearchImmersionResultPublicV2[] = [
+          {
+            address: avenueChampsElysees,
+            naf: defaultNafCode,
+            nafLabel: "test_naf_label",
+            name: "Company inside repository",
+            website: "www.jobs.fr",
+            additionalInformation: "",
+            rome: "M1808",
+            romeLabel: "test_rome_label",
+            appellationLabels: [immersionOffer.appellationLabel],
+            siret: "78000403200019",
+            voluntaryToImmersion: true,
+            contactMode: "EMAIL",
+            numberOfEmployeeRange: "10-19",
+            distance_m: 719436,
+            position: { lat: 43.8666, lon: 8.3333 },
+            city: avenueChampsElyseesDto.city,
+          },
+        ];
+        const response = await request
+          .get(
+            `/v2/immersion-offers?appellationCode=11704&distanceKm=30&longitude=2.34999&latitude=48.8531&sortedBy=distance&address=5%20rue%20des%20champs%20elysees%2044000%20Nantes`,
+          )
+          .set("Authorization", generateApiJwt({ id: "my-authorized-id" }));
+        expect(response.body).toEqual(expectedResult);
+        expect(response.status).toBe(200);
+      });
+
+      it("accept address with only city", async () => {
+        const response = await request
+          .get(
+            `/v2/immersion-offers?rome=A1000&distanceKm=30&longitude=2.34999&latitude=48.8531&sortedBy=distance&address=Lyon`,
+          )
+          .set("Authorization", generateApiJwt({ id: "my-authorized-id" }));
+        expect(response.status).toBe(200);
+      });
+      it("with no specified appellation code", async () => {
+        await request
+          .get(
+            `/v2/immersion-offers?distanceKm=30&longitude=2.34999&latitude=48.8531&sortedBy=distance`,
+          )
+          .set("Authorization", generateApiJwt({ id: "my-authorized-id" }))
+          .expect(200, []);
+      });
+
+      it("with filter voluntaryToImmersion", async () => {
+        await request
+          .get(
+            `/v2/immersion-offers?distanceKm=30&longitude=2.34999&latitude=48.8531&voluntaryToImmersion=true&sortedBy=distance`,
+          )
+          .set("Authorization", generateApiJwt({ id: "my-authorized-id" }))
+          .expect(200, []);
+      });
+    });
+
+    it("rejects invalid requests with error code 400", async () => {
+      await request
+        .get(
+          `/v2/immersion-offers?rome=XXXXX&distanceKm=30&longitude=2.34999&latitude=48.8531&sortedBy=distance`,
+        )
+        .set("Authorization", generateApiJwt({ id: "my-authorized-id" }))
+        .expect(400, /Code ROME incorrect/);
+    });
+  });
+});
---
+++ back/src/domain/immersionOffer/useCases/GetImmersionOfferBySiretAndAppellationCode.e2e.test.ts
@@ -0,0 +1,121 @@
+import { SuperTest, Test } from "supertest";
+import { AppellationAndRomeDto } from "shared";
+import {
+  rueSaintHonore,
+  rueSaintHonoreDto,
+} from "../../../_testBuilders/addressDtos";
+import { AppConfigBuilder } from "../../../_testBuilders/AppConfigBuilder";
+import { buildTestApp } from "../../../_testBuilders/buildTestApp";
+import { ContactEntityBuilder } from "../../../_testBuilders/ContactEntityBuilder";
+import { EstablishmentAggregateBuilder } from "../../../_testBuilders/EstablishmentAggregateBuilder";
+import {
+  defaultNafCode,
+  EstablishmentEntityBuilder,
+} from "../../../_testBuilders/EstablishmentEntityBuilder";
+import { ImmersionOfferEntityV2Builder } from "../../../_testBuilders/ImmersionOfferEntityV2Builder";
+import { InMemoryUnitOfWork } from "../../../adapters/primary/config/uowConfig";
+import { SearchImmersionResultPublicV2 } from "../../../adapters/primary/routers/DtoAndSchemas/v2/output/SearchImmersionResultPublicV2.dto";
+import {
+  TEST_NAF_LABEL,
+  TEST_POSITION,
+  TEST_ROME_LABEL,
+} from "../../../adapters/secondary/immersionOffer/InMemoryEstablishmentAggregateRepository";
+import { validAuthorizedApiKeyId } from "../../../adapters/secondary/InMemoryApiConsumerRepository";
+import { GenerateApiConsumerJwt } from "../../auth/jwt";
+
+const styliste: AppellationAndRomeDto = {
+  romeCode: "B1805",
+  romeLabel: "Stylisme",
+  appellationCode: "19540",
+  appellationLabel: "Styliste",
+};
+const immersionOfferSiret = "78000403200019";
+
+describe(`Route to get ImmersionSearchResultDto by siret and rome - /v2/immersion-offers/:siret/:appellationCode`, () => {
+  let request: SuperTest<Test>;
+  let inMemoryUow: InMemoryUnitOfWork;
+  let generateApiJwt: GenerateApiConsumerJwt;
+  let authToken: string;
+
+  beforeEach(async () => {
+    const config = new AppConfigBuilder()
+      .withRepositories("IN_MEMORY")
+      .withAuthorizedApiKeyIds([validAuthorizedApiKeyId])
+      .build();
+    ({ request, inMemoryUow, generateApiJwt } = await buildTestApp(config));
+    authToken = generateApiJwt({
+      id: validAuthorizedApiKeyId,
+    });
+
+    await inMemoryUow.establishmentAggregateRepository.insertEstablishmentAggregates(
+      [
+        new EstablishmentAggregateBuilder()
+          .withEstablishment(
+            new EstablishmentEntityBuilder()
+              .withSiret(immersionOfferSiret)
+              .withPosition(TEST_POSITION)
+              .withNumberOfEmployeeRange("10-19")
+              .withAddress(rueSaintHonoreDto)
+              .build(),
+          )
+          .withContact(
+            new ContactEntityBuilder().withContactMethod("EMAIL").build(),
+          )
+          .withImmersionOffers([
+            new ImmersionOfferEntityV2Builder()
+              .withRomeCode(styliste.romeCode)
+              .withAppellationCode(styliste.appellationCode)
+              .withAppellationLabel(styliste.appellationLabel)
+              .build(),
+          ])
+          .build(),
+      ],
+    );
+  });
+
+  it("rejects unauthenticated requests", async () => {
+    await request
+      .get(
+        `/v2/immersion-offers/${immersionOfferSiret}/${styliste.appellationCode}`,
+      )
+      .expect(401);
+  });
+
+  it("accepts valid authenticated requests", async () => {
+    // /!\ Those fields come from Builder (should probably not.)
+    const expectedResult: SearchImmersionResultPublicV2 = {
+      rome: styliste.romeCode,
+      naf: defaultNafCode,
+      siret: "78000403200019",
+      name: "Company inside repository",
+      voluntaryToImmersion: true,
+      position: TEST_POSITION,
+      numberOfEmployeeRange: "10-19",
+      address: rueSaintHonore,
+      contactMode: "EMAIL",
+      romeLabel: TEST_ROME_LABEL,
+      appellationLabels: [styliste.appellationLabel],
+      nafLabel: TEST_NAF_LABEL,
+      city: rueSaintHonoreDto.city,
+    };
+
+    const response = await request
+      .get(
+        `/v2/immersion-offers/${immersionOfferSiret}/${styliste.appellationCode}`,
+      )
+      .set("Authorization", authToken);
+
+    expect(response.body).toEqual(expectedResult);
+    expect(response.status).toBe(200);
+  });
+  it("returns 404 if no offer can be found with such siret & appelation code", async () => {
+    const siretNotInDB = "11000403200019";
+    await request
+      .get(`/v2/immersion-offers/${siretNotInDB}/${styliste.appellationCode}`)
+      .set("Authorization", authToken)
+      .expect(
+        404,
+        `{"errors":"No offer found for siret ${siretNotInDB} and appellation code ${styliste.appellationCode}"}`,
+      );
+  });
+});
