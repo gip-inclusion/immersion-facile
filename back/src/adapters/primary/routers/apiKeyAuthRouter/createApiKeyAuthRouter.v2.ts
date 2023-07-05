fe5e4b7d9a52334283f7062991987cc80d9d7a78
clement-duport <clement.duport@beta.gouv.fr>
2023-07-05 09:14:04 +0200

    update api key auth router v2 and add test

--- back/src/adapters/primary/routers/apiKeyAuthRouter/createApiKeyAuthRouter.v2.ts
+++ back/src/adapters/primary/routers/apiKeyAuthRouter/createApiKeyAuthRouter.v2.ts
@@ -1,5 +1,10 @@
 import { Router } from "express";
-import { contactEstablishmentRoute, pipeWithValue } from "shared";
+import {
+  contactEstablishmentRoute,
+  immersionOffersRoute,
+  pipeWithValue,
+  SiretAndAppellationDto,
+} from "shared";
 import { createLogger } from "../../../../utils/logger";
 import type { AppDependencies } from "../../config/createAppDependencies";
 import {
@@ -9,6 +14,8 @@ import {
 import { sendHttpResponse } from "../../helpers/sendHttpResponse";
 import { contactEstablishmentPublicV2ToDomain } from "../DtoAndSchemas/v2/input/ContactEstablishmentPublicV2.dto";
 import { contactEstablishmentPublicV2Schema } from "../DtoAndSchemas/v2/input/ContactEstablishmentPublicV2.schema";
+import { searchImmersionRequestPublicV2ToDomain } from "../DtoAndSchemas/v2/input/SearchImmersionRequestPublicV2.dto";
+import { domainToSearchImmersionResultPublicV2 } from "../DtoAndSchemas/v2/output/SearchImmersionResultPublicV2.dto";
 
 const logger = createLogger(__filename);
 
@@ -16,6 +23,39 @@ export const createApiKeyAuthRouterV2 = (deps: AppDependencies) => {
   const publicV2Router = Router({ mergeParams: true });
 
   publicV2Router.use(deps.apiKeyAuthMiddleware);
+
+  publicV2Router.route(`/${immersionOffersRoute}`).get(async (req, res) =>
+    sendHttpResponse(req, res, async () => {
+      const searchImmersionRequest = searchImmersionRequestPublicV2ToDomain(
+        req.query as any,
+      );
+      const searchImmersionResultDtos =
+        await deps.useCases.searchImmersion.execute(
+          searchImmersionRequest,
+          req.apiConsumer,
+        );
+      return searchImmersionResultDtos.map(
+        domainToSearchImmersionResultPublicV2,
+      );
+    }),
+  );
+
+  publicV2Router
+    .route(`/${immersionOffersRoute}/:siret/:appellationCode`)
+    .get(async (req, res) =>
+      sendHttpResponse(req, res, async () => {
+        if (!req.apiConsumer?.isAuthorized) throw new ForbiddenError();
+        return domainToSearchImmersionResultPublicV2(
+          await deps.useCases.getImmersionOfferBySiretAndAppellationCode.execute(
+            {
+              siret: req.params.siret,
+              appellationCode: req.params.appellationCode,
+            } as SiretAndAppellationDto,
+            req.apiConsumer,
+          ),
+        );
+      }),
+    );
 
   publicV2Router.route(`/${contactEstablishmentRoute}`).post(async (req, res) =>
     sendHttpResponse(req, res, () => {
