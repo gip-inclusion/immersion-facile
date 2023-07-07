21ab02a1cc0180eb268bd4d4f23200d0724eeeb7
Jérôme Burkard <burkardjerome@gmail.com>
2023-07-07 15:13:33 +0200

    check for constructor type on promise rejection

--- shared/src/test.helpers.ts
+++ shared/src/test.helpers.ts
@@ -16,6 +16,7 @@ export const expectPromiseToFailWithError = async <T extends Error>(
   promise: Promise<unknown>,
   expectedError: T,
 ) => {
+  await expect(promise).rejects.toBeInstanceOf(expectedError.constructor);
   await expect(promise).rejects.toThrowError(expectedError);
 };
 
