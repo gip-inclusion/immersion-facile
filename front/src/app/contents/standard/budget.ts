139946383e6d09bdce8c0d55ea5788eb568684ac
Enguerran Weiss <enguerranweiss@gmail.com>
2023-07-10 17:49:10 +0200

    add budget contents

--- front/src/app/contents/standard/budget.ts
+++ front/src/app/contents/standard/budget.ts
@@ -1,25 +1,59 @@
 export default {
-  title: "Mentions légales",
-  content: `<strong>Éditeur</strong>
-  Ce site est édité au sein Groupement d'Intérêt Public Plateforme de l'inclusion :
-  127 rue de grenelle, 75007 Paris 7
-  
-  <strong>Directeur de la publication</strong>
-  Monsieur Arnaud Denoix, directeur du GIP Plateforme de l'inclusion.
-  
-  <strong>Hébergement du site</strong>
-  Ce site est hébergé par : OVH 
-  2 rue Kellermann 59100 Roubaix
-  
-  <strong>Téléphone</strong> : 09 72 10 10 07
+  title: "Budget",
+  content: `<strong>Présentation</strong>
+  Le Budget de la startup Immersion Facilitée est présenté en comité d’investissement 2 fois par an.
 
-  <strong>Accessibilité</strong>
-  La conformité aux normes d’accessibilité numérique est un objectif ultérieur mais nous tâchons de rendre ce site accessible à toutes et à tous.
-  <strong>Signaler un dysfonctionnement</strong>
-  Si vous rencontrez un défaut d’accessibilité vous empêchant d’accéder à un contenu ou une fonctionnalité du site, merci de nous en faire part. Si vous n’obtenez pas de réponse rapide de notre part, vous êtes en droit de faire parvenir vos doléances ou une demande de saisine au Défenseur des droits.
-  <strong>En savoir plus </strong>
-  Pour en savoir plus sur la politique d’accessibilité numérique de l’État : http://references.modernisation.gouv.fr/accessibilite-numerique
+  Il doit permettre au service Immersion Facilitée d’atteindre ses objectifs, en terme de volume d’immersions réalisées, d’entreprises inscrites sur le service, ou encore de satisfaction utilisateurs.
+
+  L’essentiel des dépenses se concentre sur les frais de personnels. 16 personnes travaillent pour la startup Immersion Facilitée.
   
-  <strong>Sécurité</strong>
-  Le site est protégé par un certificat électronique, matérialisé pour la grande majorité des navigateurs par un cadenas. Cette protection participe à la confidentialité des échanges. En aucun cas les services associés à la plateforme ne seront à l’origine d’envoi de courriels pour demander la saisie d’informations personnelles.`,
+  <strong>Répartition des dépenses pour la période d’avril à septembre</strong>
+  <table>
+    <tr>
+      <th>Poste</th>
+      <th>Budget (en milliers d’euros)</th>
+    </tr>
+    <tr>
+      <td>Développeurs</td>
+      <td>328,4</td>
+    </tr>
+    <tr>
+      <td>Chargés de déploiement</td>
+      <td>92,6</td>
+    </tr>
+    <tr>
+      <td>Designers</td>
+      <td>230,2</td>
+    </tr>
+    <tr>
+      <td>Chargés de produit</td>
+      <td>127,8</td>
+    </tr>
+    <tr>
+      <td>Frais (fonctionnement, communication…)</td>
+      <td>15</td>
+    </tr>
+    <tr>
+      <td>Total</td>
+      <td>794,1</td>
+    </tr>
+  </table>
+
+  <strong>Évolution du budget depuis 2021</strong>
+  Si vous rencontrez un défaut d’accessibilité vous empêchant d’accéder à un contenu ou une fonctionnalité du site, merci de nous en faire part. Si vous n’obtenez pas de réponse rapide de notre part, vous êtes en droit de faire parvenir vos doléances ou une demande de saisine au Défenseur des droits.
+  
+  <strong>Performance budgétaire</strong>
+  Le budget s’apprécie au regard de la capacité de la startup à acquérir plus d’utilisateurs, d’usagers, d’entreprises et de prescripteurs.  
+  
+  <strong>- Utilisateurs</strong>
+  Utilisateur : visiteur effectuant une action sur le site (recherche d’entreprise, mise en relation…)
+  
+  <strong>- Usagers</strong>
+  Usager : candidat démarrant une demande d’immersion
+  
+  <strong>- Entreprises</strong>
+  Entreprise : entreprise accueillante inscrite sur le site Immersion Facilitée
+  
+  <strong>- Prescripteurs</strong>
+  Prescripteur : structure prescrivant des Immersions sur le site Immersion Facilitée.`,
 };
