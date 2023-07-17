69e734a1fb042d477914fae8fc43f4d707e47778
Jérôme Burkard <burkardjerome@gmail.com>
2023-07-17 08:38:51 +0200

    upgrade shared routes and take advantage of the new options

--- front/src/app/pages/openApiDoc/OpenApiDocPage.tsx
+++ front/src/app/pages/openApiDoc/OpenApiDocPage.tsx
@@ -1,29 +1,27 @@
-import React, { useEffect } from "react";
-import { useDispatch } from "react-redux";
+import React from "react";
+// import { useDispatch } from "react-redux";
 import SwaggerUI from "swagger-ui-react";
-import { Loader, MainWrapper } from "react-design-system";
+import { openApiDocTargets } from "../../../../../shared/src";
+import { MainWrapper } from "react-design-system";
 import { HeaderFooterLayout } from "src/app/components/layout/HeaderFooterLayout";
-import { useAppSelector } from "src/app/hooks/reduxHooks";
-import { openApiDocSelectors } from "src/core-logic/domain/openApiDoc/openApiDoc.selectors";
-import { openApiDocSlice } from "src/core-logic/domain/openApiDoc/openApiDoc.slice";
-import "./open-api-doc-page.scss";
+// import { useAppSelector } from "src/app/hooks/reduxHooks";
+// import { openApiDocSelectors } from "src/core-logic/domain/openApiDoc/openApiDoc.selectors";
+// import { openApiDocSlice } from "src/core-logic/domain/openApiDoc/openApiDoc.slice";
 import "swagger-ui-react/swagger-ui.css";
 
-export const OpenApiDocPage = (): React.ReactElement => {
-  const openApiSpec = useAppSelector(openApiDocSelectors.openApiDoc);
-  const dispatch = useDispatch();
-  const isLoading = useAppSelector(openApiDocSelectors.isLoading);
+export const OpenApiDocPage = (): React.ReactElement => (
+  // const openApiSpec = useAppSelector(openApiDocSelectors.openApiDoc);
+  // const dispatch = useDispatch();
+  // const isLoading = useAppSelector(openApiDocSelectors.isLoading);
 
-  useEffect(() => {
-    dispatch(openApiDocSlice.actions.fetchOpenApiDocRequested());
-  }, []);
+  // useEffect(() => {
+  //   dispatch(openApiDocSlice.actions.fetchOpenApiDocRequested());
+  // }, []);
 
-  return (
-    <HeaderFooterLayout>
-      <MainWrapper layout="default" vSpacing={0}>
-        {isLoading && <Loader />}
-        <SwaggerUI spec={openApiSpec} />
-      </MainWrapper>
-    </HeaderFooterLayout>
-  );
-};
+  <HeaderFooterLayout>
+    <MainWrapper layout="default" vSpacing={0}>
+      {/* {isLoading && <Loader />} */}
+      <SwaggerUI url={`/api${openApiDocTargets.getOpenApiDoc.url}`} />
+    </MainWrapper>
+  </HeaderFooterLayout>
+);
