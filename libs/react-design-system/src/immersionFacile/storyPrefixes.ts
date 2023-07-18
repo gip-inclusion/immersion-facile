c8a3a2632d33f2c4ca6e5053b72521f40b7d98c1
Jérôme Burkard <burkardjerome@gmail.com>
2023-07-18 10:46:45 +0200

    fix typo

--- libs/react-design-system/src/immersionFacile/storyPrefixes.ts
+++ libs/react-design-system/src/immersionFacile/storyPrefixes.ts
@@ -1,4 +1,4 @@
-const immersionFacilePrefix = "Immersion Facilité/";
+const immersionFacilePrefix = "Immersion Facilitée/";
 const componentPrefix = `${immersionFacilePrefix}Components/`;
 export const storybookPrefix = (componentType: string) =>
   componentPrefix + componentType + "/";
