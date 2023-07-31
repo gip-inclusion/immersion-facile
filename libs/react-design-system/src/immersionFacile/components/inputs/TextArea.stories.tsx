415f11f3c52b8362d776e6d287c9afa898e85ef8
celineung <celine.ung.pro@gmail.com>
2023-07-31 15:02:33 +0200

    udpate TextArea story

--- libs/react-design-system/src/immersionFacile/components/inputs/TextArea.stories.tsx
+++ libs/react-design-system/src/immersionFacile/components/inputs/TextArea.stories.tsx
@@ -1,22 +1,32 @@
-import React from "react";
-import { ArgTypes, ComponentMeta, ComponentStory } from "@storybook/react";
+import { ArgTypes, Meta, StoryObj } from "@storybook/react";
 import { TextArea, TextAreaProperties } from "./TextArea";
 
 const Component = TextArea;
+type Story = StoryObj<typeof Component>;
 const argTypes: Partial<ArgTypes<TextAreaProperties>> | undefined = {};
+
+const componentDescription = `
+\`\`\`tsx  
+import { TextArea } from "react-design-system";
+\`\`\`
+`;
 
 export default {
   title: "TextArea",
   component: Component,
   argTypes,
-} as ComponentMeta<typeof Component>;
+  parameters: {
+    docs: {
+      description: {
+        component: componentDescription,
+      },
+    },
+  },
+} as Meta<typeof Component>;
 
-const componentStory: ComponentStory<typeof Component> = (args) => (
-  <Component {...args} />
-);
-
-export const Default = componentStory.bind({});
-Default.args = {
-  name: "Default",
-  value: "Default Value",
+export const Default: Story = {
+  args: {
+    name: "Default",
+    value: "Default Value",
+  },
 };
