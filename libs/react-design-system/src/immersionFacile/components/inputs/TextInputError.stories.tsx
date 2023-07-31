529fbcc8f6fe94c2ba15aafb73d7be39195d80b6
celineung <celine.ung.pro@gmail.com>
2023-07-31 15:05:26 +0200

    update TextInputError's story

--- libs/react-design-system/src/immersionFacile/components/inputs/TextInputError.stories.tsx
+++ libs/react-design-system/src/immersionFacile/components/inputs/TextInputError.stories.tsx
@@ -1,21 +1,31 @@
-import React from "react";
-import { ArgTypes, ComponentMeta, ComponentStory } from "@storybook/react";
+import { ArgTypes, Meta, StoryObj } from "@storybook/react";
 import { TextInputError, TextInputErrorProperties } from "./TextInputError";
 
 const Component = TextInputError;
+type Story = StoryObj<typeof Component>;
 const argTypes: Partial<ArgTypes<TextInputErrorProperties>> | undefined = {};
+
+const componentDescription = `
+\`\`\`tsx  
+import { TextInputError } from "react-design-system";
+\`\`\`
+`;
 
 export default {
   title: "TextInputError",
   component: Component,
   argTypes,
-} as ComponentMeta<typeof Component>;
+  parameters: {
+    docs: {
+      description: {
+        component: componentDescription,
+      },
+    },
+  },
+} as Meta<typeof Component>;
 
-const componentStory: ComponentStory<typeof Component> = (args) => (
-  <Component {...args} />
-);
-
-export const Default = componentStory.bind({});
-Default.args = {
-  errorMessage: "Error message.",
+export const Default: Story = {
+  args: {
+    errorMessage: "Error message.",
+  },
 };
