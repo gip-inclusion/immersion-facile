af3dd3c44e3b804fdbe53a5462d154acac68497b
celineung <celine.ung.pro@gmail.com>
2023-08-02 12:12:50 +0200

    add story for OverFooter

--- libs/react-design-system/src/immersionFacile/components/over-footer/OverFooter.stories.tsx
+++ libs/react-design-system/src/immersionFacile/components/over-footer/OverFooter.stories.tsx
@@ -1,44 +1,56 @@
-import React from "react";
-import { ArgTypes, ComponentMeta, ComponentStory } from "@storybook/react";
-import { OverFooter, OverFooterProps } from "./OverFooter";
+import type { ArgTypes, Meta, StoryObj } from "@storybook/react";
+import { OverFooter, OverFooterCols, OverFooterProps } from "./OverFooter";
 
 const Component = OverFooter;
+type Story = StoryObj<typeof Component>;
 const argTypes: Partial<ArgTypes<OverFooterProps>> | undefined = {};
+
+const componentDescription = `
+\`\`\`tsx  
+import { OverFooter } from "react-design-system";
+\`\`\`
+`;
+
+const overFooterCols: OverFooterCols = [
+  {
+    title: "Le centre d'aide",
+    subtitle:
+      "Consultez notre FAQ, trouvez les réponses aux questions les plus fréquentes et contactez-nous si vous n'avez pas trouvé de réponse",
+    iconTitle: "fr-icon-questionnaire-fill",
+    link: {
+      label: "Accédez à notre FAQ",
+      url: "https://aide.immersion-facile.beta.gouv.fr/fr/",
+    },
+    id: "faq-link",
+  },
+  {
+    title: "Rejoignez la communauté",
+    subtitle:
+      "Rejoignez la communauté d'Immersion Facilitée et suivez nos actualités.",
+    iconTitle: "fr-icon-links-fill",
+    link: {
+      label: "Rejoignez-nous sur Linkedin",
+      url: "https://www.linkedin.com/company/l-immersion-facilitee/",
+    },
+    id: "linkedin-link",
+  },
+];
 
 export default {
   title: "OverFooter",
   component: Component,
   argTypes,
-} as ComponentMeta<typeof Component>;
-
-const componentStory: ComponentStory<typeof Component> = (args) => (
-  <Component {...args} />
-);
-
-export const OverFooterMock = componentStory.bind({});
-OverFooterMock.args = {
-  cols: [
-    {
-      title: "Rejoignez la communauté",
-      subtitle:
-        "Rejoignez la communauté d'Immersion Facilitée et suivez nos actualités",
-      iconTitle: "fr-icon-links-fill",
-      link: {
-        label: "Rejoignez-nous sur Linkedin",
-        url: "https://www.linkedin.com/company/l-immersion-facilitee/",
+  parameters: {
+    docs: {
+      description: {
+        component: componentDescription,
       },
-      id: "im-over-footer__link-1",
     },
-    {
-      title: "Le centre de support",
-      subtitle:
-        "Consultez notre centre d'aide (FAQ) pour trouvez les réponses aux principales questions demandées",
-      iconTitle: "fr-icon-questionnaire-fill",
-      link: {
-        label: "Accédez à notre FAQ",
-        url: "https://aide.immersion-facile.beta.gouv.fr/fr/",
-      },
-      id: "im-over-footer__link-2",
-    },
-  ],
+  },
+} as Meta<typeof Component>;
+
+export const Default: Story = {
+  args: {
+    cols: overFooterCols,
+  },
 };
