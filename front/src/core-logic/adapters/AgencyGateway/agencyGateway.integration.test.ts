f5093f2f6d9b952edf08e5adf53fbdcc5576c030
celineung <celine.ung.pro@gmail.com>
2023-08-08 14:58:01 +0200

    remove old todos

--- front/src/core-logic/adapters/AgencyGateway/agencyGateway.integration.test.ts
+++ front/src/core-logic/adapters/AgencyGateway/agencyGateway.integration.test.ts
@@ -3,13 +3,7 @@ import { AgencyId } from "shared";
 import { InMemoryAgencyGateway } from "src/core-logic/adapters/AgencyGateway/InMemoryAgencyGateway";
 import { AgencyGateway } from "src/core-logic/ports/AgencyGateway";
 
-const adapters: AgencyGateway[] = [
-  new InMemoryAgencyGateway(),
-  // TODO Propose default configuration to target local / docker backend when running integration tests
-  // front/jest.config.js =>  testEnvironment: "node" => "jsdom" ? ref: https://stackoverflow.com/questions/51957139/jest-tests-run-in-bash-but-generate-referenceerror-xmlhttprequest-is-not-defin
-  //new HttpAgencyGateway()
-  // option to override HttpGateways (hostname / http https) => target another fqdn
-];
+const adapters: AgencyGateway[] = [new InMemoryAgencyGateway()];
 
 describe("Agency Gateway - integration tests", () => {
   adapters.forEach((adapter: AgencyGateway) => {
