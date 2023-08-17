64ad2a533ddda87e39d7c1b4ffff226b2a0f2b2a
Benjamin Bohec <bbohec.pro@gmail.com>
2023-08-17 10:36:43 +0200

    skip lbb result when lbb establishment deleted

--- back/src/adapters/secondary/InMemoryDeletedEstablishmentRepository.ts
+++ back/src/adapters/secondary/InMemoryDeletedEstablishmentRepository.ts
@@ -1,3 +1,5 @@
+import { prop } from "ramda";
+import { SiretDto } from "shared";
 import {
   DeletedEstablishementDto,
   DeletedEstablishmentRepository,
@@ -8,8 +10,20 @@ export class InMemoryDeletedEstablishmentRepository
 {
   #deletedEstablishments: DeletedEstablishementDto[] = [];
 
+  public async isSiretsDeleted(siretsToCheck: SiretDto[]): Promise<SiretDto[]> {
+    return siretsToCheck.filter((siret) =>
+      this.#deletedEstablishments.map(prop("siret")).includes(siret),
+    );
+  }
+
   public get deletedEstablishments() {
     return this.#deletedEstablishments;
+  }
+
+  public set deletedEstablishments(
+    deletedEstablishments: DeletedEstablishementDto[],
+  ) {
+    this.#deletedEstablishments = deletedEstablishments;
   }
 
   public async save(
