b75191113484cd2a000613d262259016f1aa6428
clement-duport <clement.duport@beta.gouv.fr>
2023-08-17 16:00:07 +0200

    refactor justification modal content

---
+++ shared/src/role/role.utils.ts
@@ -0,0 +1,14 @@
+import { SignatoryRole } from "./role.dto";
+
+export const signatoryTitleByRole = (role: SignatoryRole): string => {
+  const strategy: Record<SignatoryRole, string> = {
+    beneficiary: "Le bénéficiaire",
+    "beneficiary-representative": "Le représentant légal du bénéficiaire",
+    "legal-representative": "Le représentant légal du bénéficiaire",
+    establishment: "Le représentant de l'entreprise",
+    "establishment-representative": "Le représentant de l'entreprise",
+    "beneficiary-current-employer":
+      "Le représentant de l'entreprise actuelle du candidat",
+  };
+  return strategy[role];
+};
