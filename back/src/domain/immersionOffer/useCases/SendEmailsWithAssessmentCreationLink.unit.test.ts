87f706858b70382c058aec5db12895e3b46c31d0
Benjamin Bohec <bbohec.pro@gmail.com>
2023-08-23 09:20:48 +0200

    review

--- back/src/domain/immersionOffer/useCases/SendEmailsWithAssessmentCreationLink.unit.test.ts
+++ back/src/domain/immersionOffer/useCases/SendEmailsWithAssessmentCreationLink.unit.test.ts
@@ -1,6 +1,9 @@
-import { ConventionDtoBuilder, expectToEqual } from "shared";
+import { ConventionDtoBuilder, ConventionId, expectToEqual } from "shared";
 import { fakeGenerateMagicLinkUrlFn } from "../../../_testBuilders/jwtTestHelper";
-import { createInMemoryUow } from "../../../adapters/primary/config/uowConfig";
+import {
+  createInMemoryUow,
+  InMemoryUnitOfWork,
+} from "../../../adapters/primary/config/uowConfig";
 import { CustomTimeGateway } from "../../../adapters/secondary/core/TimeGateway/CustomTimeGateway";
 import { UuidV4Generator } from "../../../adapters/secondary/core/UuidGeneratorImplementations";
 import { InMemoryUowPerformer } from "../../../adapters/secondary/InMemoryUowPerformer";
@@ -9,79 +12,58 @@ import { makeCreateNewEvent } from "../../../domain/core/eventBus/EventBus";
 import { DomainEvent } from "../../../domain/core/eventBus/events";
 import { SendEmailsWithAssessmentCreationLink } from "../../../domain/immersionOffer/useCases/SendEmailsWithAssessmentCreationLink";
 
-const prepareUseCase = () => {
-  const uow = createInMemoryUow();
-  const outboxQueries = uow.outboxQueries;
-  const outboxRepository = uow.outboxRepository;
-  const conventionRepository = uow.conventionRepository;
+describe("SendEmailWithImmersionAssessmentCreationLink", () => {
+  const id: ConventionId = "immersion-ending-tomorrow-id";
 
-  const timeGateway = new CustomTimeGateway();
-  const notificationGateway = new InMemoryNotificationGateway();
-  const uuidGenerator = new UuidV4Generator();
-  const createNewEvent = makeCreateNewEvent({
-    timeGateway,
-    uuidGenerator,
+  let uow: InMemoryUnitOfWork;
+  let sendEmailWithAssessmentCreationLink: SendEmailsWithAssessmentCreationLink;
+  let timeGateway: CustomTimeGateway;
+  let notificationGateway: InMemoryNotificationGateway;
+
+  beforeEach(() => {
+    uow = createInMemoryUow();
+    timeGateway = new CustomTimeGateway();
+    notificationGateway = new InMemoryNotificationGateway();
+    sendEmailWithAssessmentCreationLink =
+      new SendEmailsWithAssessmentCreationLink(
+        new InMemoryUowPerformer(uow),
+        notificationGateway,
+        timeGateway,
+        fakeGenerateMagicLinkUrlFn,
+        makeCreateNewEvent({
+          timeGateway,
+          uuidGenerator: new UuidV4Generator(),
+        }),
+      );
   });
 
-  const sendEmailWithAssessmentCreationLink =
-    new SendEmailsWithAssessmentCreationLink(
-      new InMemoryUowPerformer(uow),
-      notificationGateway,
-      timeGateway,
-      fakeGenerateMagicLinkUrlFn,
-      createNewEvent,
-    );
-
-  return {
-    sendEmailWithAssessmentCreationLink,
-    outboxQueries,
-    outboxRepository,
-    notificationGateway,
-    conventionRepository,
-    timeGateway,
-    agencyRepository: uow.agencyRepository,
-  };
-};
-
-describe("SendEmailWithImmersionAssessmentCreationLink", () => {
   it("Sends an email to immersions ending tomorrow", async () => {
-    // Prepare
-    const {
-      sendEmailWithAssessmentCreationLink,
-      outboxRepository,
-      notificationGateway,
-      conventionRepository,
-      timeGateway,
-      agencyRepository,
-    } = prepareUseCase();
-
-    timeGateway.setNextDate(new Date("2021-05-15T08:00:00.000Z"));
-    const expectedAgency = agencyRepository.agencies[0];
+    // Arrange
+    const expectedAgency = uow.agencyRepository.agencies[0];
     const immersionApplicationEndingTomorrow = new ConventionDtoBuilder()
       .withAgencyId(expectedAgency.id)
       .withDateStart("2021-05-13T10:00:00.000Z")
       .withDateEnd("2021-05-16T10:00:00.000Z")
-      .withId("immersion-ending-tommorow-id")
+      .withId(id)
       .withEstablishmentTutorFirstName("Tom")
       .withEstablishmentTutorLastName("Cruise")
       .validated()
       .build();
-
     const immersionApplicationEndingYesterday = new ConventionDtoBuilder()
       .withAgencyId(expectedAgency.id)
       .withDateEnd("2021-05-14T10:00:00.000Z")
       .validated()
       .build();
 
-    await conventionRepository.save(immersionApplicationEndingTomorrow);
-    await conventionRepository.save(immersionApplicationEndingYesterday);
+    await uow.conventionRepository.save(immersionApplicationEndingTomorrow);
+    await uow.conventionRepository.save(immersionApplicationEndingYesterday);
+    timeGateway.setNextDate(new Date("2021-05-15T08:00:00.000Z"));
 
     // Act
     await sendEmailWithAssessmentCreationLink.execute();
 
     // Assert
-    const sentEmails = notificationGateway.getSentEmails();
-    expectToEqual(sentEmails, [
+    expectToEqual(notificationGateway.getSentEmails(), [
       {
         kind: "CREATE_IMMERSION_ASSESSMENT",
         recipients: [
@@ -98,45 +80,36 @@ describe("SendEmailWithImmersionAssessmentCreationLink", () => {
           conventionId: immersionApplicationEndingTomorrow.id,
           establishmentTutorName: "Tom Cruise",
           internshipKind: immersionApplicationEndingTomorrow.internshipKind,
-          immersionAssessmentCreationLink: `http://fake-magic-link/bilan-immersion/immersion-ending-tommorow-id/establishment-tutor/2021-05-15T08:00:00.000Z/establishment@example.com`,
+          immersionAssessmentCreationLink: `http://fake-magic-link/bilan-immersion/${id}/establishment-tutor/2021-05-15T08:00:00.000Z/establishment@example.com`,
         },
       },
     ]);
-    expect(outboxRepository.events).toHaveLength(1);
-    expect(outboxRepository.events[0].payload).toMatchObject({
-      id: "immersion-ending-tommorow-id",
+    expect(uow.outboxRepository.events).toHaveLength(1);
+    expect(uow.outboxRepository.events[0].payload).toMatchObject({
+      id,
     });
   });
 
   it("Does not send an email to immersions having already received one", async () => {
-    // Prepare
-    const {
-      sendEmailWithAssessmentCreationLink,
-      outboxRepository,
-      notificationGateway,
-      conventionRepository,
-      timeGateway,
-    } = prepareUseCase();
-
-    timeGateway.setNextDate(new Date("2021-05-15T08:00:00.000Z"));
-
+    // Arrange
     const immersionApplicationEndingTomorrow = new ConventionDtoBuilder()
       .withDateEnd("2021-05-16T10:00:00.000Z")
       .validated()
-      .withId("immersion-ending-tommorow-id")
+      .withId(id)
       .build();
-    await conventionRepository.save(immersionApplicationEndingTomorrow);
-    await outboxRepository.save({
+    await uow.conventionRepository.save(immersionApplicationEndingTomorrow);
+    await uow.outboxRepository.save({
       topic: "EmailWithLinkToCreateAssessmentSent",
       payload: { id: immersionApplicationEndingTomorrow.id },
     } as DomainEvent);
+
+    timeGateway.setNextDate(new Date("2021-05-15T08:00:00.000Z"));
 
     // Act
     await sendEmailWithAssessmentCreationLink.execute();
 
     // Assert
-    const sentEmails = notificationGateway.getSentEmails();
-    expect(sentEmails).toHaveLength(0);
-    expect(outboxRepository.events).toHaveLength(1);
+    expect(notificationGateway.getSentEmails()).toHaveLength(0);
+    expect(uow.outboxRepository.events).toHaveLength(1);
   });
 });
