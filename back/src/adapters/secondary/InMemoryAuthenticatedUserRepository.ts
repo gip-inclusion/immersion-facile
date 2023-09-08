d7cbfbab17a4733c3b0b373822adfa3f1b8b1cd3
celineung <celine.ung.pro@gmail.com>
2023-09-08 17:41:49 +0200

    fix lint warning issues in InMemoryAuthenticatedUserRepository

--- back/src/adapters/secondary/InMemoryAuthenticatedUserRepository.ts
+++ back/src/adapters/secondary/InMemoryAuthenticatedUserRepository.ts
@@ -1,33 +1,29 @@
 import { values } from "ramda";
-import { AuthenticatedUser, AuthenticatedUserId } from "shared";
+import { AuthenticatedUser } from "shared";
 import { AuthenticatedUserRepository } from "../../domain/generic/OAuth/ports/AuthenticatedUserRepositiory";
 
 export class InMemoryAuthenticatedUserRepository
   implements AuthenticatedUserRepository
 {
-  private usersById: Record<string, AuthenticatedUser> = {};
+  #usersById: Record<string, AuthenticatedUser> = {};
 
-  async findByEmail(email: string): Promise<AuthenticatedUser | undefined> {
+  public async findByEmail(
+    email: string,
+  ): Promise<AuthenticatedUser | undefined> {
     return this.users.find((user) => user.email === email);
   }
 
-  async findById(
-    id: AuthenticatedUserId,
-  ): Promise<AuthenticatedUser | undefined> {
-    return this.usersById[id];
-  }
-
-  async save(user: AuthenticatedUser): Promise<void> {
-    this.usersById[user.id] = user;
+  public async save(user: AuthenticatedUser): Promise<void> {
+    this.#usersById[user.id] = user;
   }
 
   // for test purpose
   public get users(): AuthenticatedUser[] {
-    return values(this.usersById);
+    return values(this.#usersById);
   }
 
   public set users(users: AuthenticatedUser[]) {
-    this.usersById = users.reduce(
+    this.#usersById = users.reduce(
       (acc, user) => ({
         ...acc,
         [user.id]: user,
