62b2c3a7c31dba56b3911d08c617f925afcdf8a6
celineung <celine.ung.pro@gmail.com>
2023-09-08 17:13:09 +0200

    fix lint warning issues in CustomTimeGateway

--- back/src/adapters/secondary/core/TimeGateway/CustomTimeGateway.ts
+++ back/src/adapters/secondary/core/TimeGateway/CustomTimeGateway.ts
@@ -5,19 +5,19 @@ import { TimeGateway } from "../../../../domain/core/ports/TimeGateway";
 export class CustomTimeGateway implements TimeGateway {
   constructor(private _nextDate = new Date("2021-09-01T10:10:00.000Z")) {}
 
-  advanceByMs(ms: number) {
+  public advanceByMs(ms: number) {
     this._nextDate = addMilliseconds(this._nextDate, ms);
   }
 
-  now() {
+  public now() {
     return this._nextDate;
   }
 
-  setNextDate(date: Date) {
+  public setNextDate(date: Date) {
     this._nextDate = date;
   }
 
-  setNextDateStr(dateStr: DateIsoString) {
+  public setNextDateStr(dateStr: DateIsoString) {
     this.setNextDate(new Date(dateStr));
   }
 }
