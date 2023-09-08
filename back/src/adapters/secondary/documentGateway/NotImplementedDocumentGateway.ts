56debfd9f5fcce05f3c596c389ccaf8c8b2c958c
celineung <celine.ung.pro@gmail.com>
2023-09-08 17:12:17 +0200

    fix lint warning issues in NotImplementedDocumentGateway

--- back/src/adapters/secondary/documentGateway/NotImplementedDocumentGateway.ts
+++ back/src/adapters/secondary/documentGateway/NotImplementedDocumentGateway.ts
@@ -6,13 +6,13 @@ const logger = createLogger(__filename);
 
 export class NotImplementedDocumentGateway implements DocumentGateway {
   // Not used for now... probably never will be but needed for compilation
-  getFileUrl(file: StoredFile): string {
+  public getFileUrl(file: StoredFile): string {
     logger.warn("Not implemented");
     return file.id;
   }
 
   // eslint-disable-next-line @typescript-eslint/require-await
-  async put(_file: StoredFile): Promise<void> {
+  public async put(_file: StoredFile): Promise<void> {
     logger.warn("Not implemented");
   }
 }
