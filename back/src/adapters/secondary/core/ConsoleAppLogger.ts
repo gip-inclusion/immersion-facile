f6680dea04fff2a2856fbbcb2ac00cc316e7f376
celineung <celine.ung.pro@gmail.com>
2023-09-08 17:22:05 +0200

    fix lint warning issues in ConsoleAppLogger

--- back/src/adapters/secondary/core/ConsoleAppLogger.ts
+++ back/src/adapters/secondary/core/ConsoleAppLogger.ts
@@ -2,19 +2,19 @@
 import { AppLogger } from "../../../domain/core/ports/AppLogger";
 
 export class ConsoleAppLogger implements AppLogger {
-  debug(...messages: any[]): void {
+  public debug(...messages: any[]): void {
     console.debug(...messages);
   }
 
-  error(...messages: any[]): void {
+  public error(...messages: any[]): void {
     console.error(...messages);
   }
 
-  info(...messages: any[]): void {
+  public info(...messages: any[]): void {
     console.log(...messages);
   }
 
-  warn(...messages: any[]): void {
+  public warn(...messages: any[]): void {
     console.warn(...messages);
   }
 }
