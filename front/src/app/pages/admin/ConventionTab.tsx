fd67629fd131ce3a3b1b2dbc96e51771b6cc3372
Jérôme Burkard <burkardjerome@gmail.com>
2023-09-12 17:06:27 +0200

    clean convention tab

--- front/src/app/pages/admin/ConventionTab.tsx
+++ front/src/app/pages/admin/ConventionTab.tsx
@@ -1,25 +1,15 @@
 import React from "react";
 import { Alert } from "@codegouvfr/react-dsfr/Alert";
-import { ConventionDtoBuilder, ConventionReadDto } from "shared";
-import { RenewConventionForm } from "src/app/components/admin/conventions/ConventionManageActions";
 import { MetabaseView } from "src/app/components/MetabaseView";
 import { ManageConventionFormSection } from "src/app/pages/admin/ManageConventionFormSection";
 import { useAdminDashboard } from "src/app/pages/admin/useAdminDashboard";
 
 export const ConventionTab = () => {
   const { url, error } = useAdminDashboard({ name: "conventions" });
-  const convention: ConventionReadDto = {
-    ...new ConventionDtoBuilder().build(),
-    agencyDepartment: "75",
-    agencyId: "123456789",
-    agencyName: "Agence",
-  };
   return error ? (
     <Alert severity="error" title="Erreur" description={error} />
   ) : (
     <>
-      <RenewConventionForm convention={convention} />
-
       <ManageConventionFormSection
         routeNameToRedirectTo={"manageConventionAdmin"}
       />
