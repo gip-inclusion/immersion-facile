5c49984c93f60262be504eb05119117ed02289df
LionelB <lionel@lumographe.fr>
2023-09-28 10:58:20 +0200

    feat: add department shape in public_departement_region
    this will ensure to localize precisely searches_mades
    
    ```sql
    SELECT appellation_code, address, department_code
    FROM searches_made
    LEFT JOIN public_department_region
    ON ST_Intersects(
    	public_department_region.shape,
    	searches_made.gps::geometry
    )
    ```

---
+++ back/src/adapters/secondary/pg/migrations/1695885238870_add-department-shape.ts
@@ -0,0 +1,24 @@
+import { MigrationBuilder } from "node-pg-migrate";
+import departments from "../staticData/departements-avec-outre-mer.json";
+const tableName = "public_department_region";
+
+export async function up(pgm: MigrationBuilder): Promise<void> {
+  pgm.addColumns(tableName, {
+    shape: {
+      type: "geometry",
+      notNull: false,
+    },
+  });
+  for (const department of departments.features)
+    pgm.sql(
+      `
+      UPDATE ${tableName}
+      SET shape = ST_GeomFromGeoJSON('${JSON.stringify(department.geometry)}')
+      WHERE department_code = '${department.properties.code}'
+  `,
+    );
+}
+
+export async function down(pgm: MigrationBuilder): Promise<void> {
+  pgm.dropColumn(tableName, "shape");
+}
