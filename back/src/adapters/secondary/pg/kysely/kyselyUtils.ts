a5fa3916366123715cd71d319b479d3f81f49ab2
celineung <celine.ung.pro@gmail.com>
2023-10-02 12:40:10 +0200

    add kysely utils + interfaces

---
+++ back/src/adapters/secondary/pg/kysely/kyselyUtils.ts
@@ -0,0 +1,14 @@
+import { CompiledQuery, Kysely, PostgresDialect } from "kysely";
+import { Pool, QueryResultRow } from "pg";
+import { Database } from "./model/database";
+
+export const executeKyselyRawSqlQuery = <T extends QueryResultRow>(
+  transaction: Kysely<Database>,
+  sqlQuery: string,
+  values?: any[],
+) => transaction.executeQuery<T>(CompiledQuery.raw(sqlQuery, values));
+
+export const makeKyselyDb = (pool: Pool): Kysely<Database> =>
+  new Kysely<Database>({
+    dialect: new PostgresDialect({ pool }),
+  });
