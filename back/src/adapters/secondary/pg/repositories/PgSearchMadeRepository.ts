a19861a2a28c241f762a118de048c8b7be00ac41
celineung <celine.ung.pro@gmail.com>
2023-10-02 14:20:30 +0200

    use kysely on PgSearchMadeRepository

--- back/src/adapters/secondary/pg/repositories/PgSearchMadeRepository.ts
+++ back/src/adapters/secondary/pg/repositories/PgSearchMadeRepository.ts
@@ -1,12 +1,13 @@
-import { PoolClient } from "pg";
 import { SearchMadeEntity } from "../../../../domain/offer/entities/SearchMadeEntity";
 import { SearchMadeRepository } from "../../../../domain/offer/ports/SearchMadeRepository";
+import { executeKyselyRawSqlQuery, KyselyDb } from "../kysely/kyselyUtils";
 
 export class PgSearchMadeRepository implements SearchMadeRepository {
-  constructor(private client: PoolClient) {}
+  constructor(private transaction: KyselyDb) {}
 
   public async insertSearchMade(searchMade: SearchMadeEntity) {
-    await this.client.query(
+    await executeKyselyRawSqlQuery(
+      this.transaction,
       `INSERT INTO searches_made (
          id, ROME, lat, lon, distance, needsToBeSearched, gps, voluntary_to_immersion, api_consumer_name, sorted_by, address, appellation_code, number_of_results
        ) VALUES ($1, $2, $3, $4, $5, $6, ST_GeographyFromText($7), $8, $9, $10, $11, $12, $13)`,
