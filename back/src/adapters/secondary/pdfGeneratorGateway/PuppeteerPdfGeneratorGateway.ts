3c30b90adca490a182be3c6f5702cb985213fd1e
Benjamin Bohec <bbohec.pro@gmail.com>
2023-10-03 15:05:18 +0200

    make html to pdf working for dev
    Co-authored-by: Enguerran Weiss <enguerranws@users.noreply.github.com>
    Co-authored-by: Celine Ung <celineung@users.noreply.github.com>

--- back/src/adapters/secondary/pdfGeneratorGateway/PuppeteerPdfGeneratorGateway.ts
+++ back/src/adapters/secondary/pdfGeneratorGateway/PuppeteerPdfGeneratorGateway.ts
@@ -1,14 +1,24 @@
+import Bottleneck from "bottleneck";
 import { unlinkSync } from "fs";
 import { launch } from "puppeteer";
 import { UuidGenerator } from "../../../domain/core/ports/UuidGenerator";
 import { PdfGeneratorGateway } from "../../../domain/generic/htmlToPdf/PdfGeneratorGateway";
 
 export class PuppeteerPdfGeneratorGateway implements PdfGeneratorGateway {
+  #limiter = new Bottleneck({
+    maxConcurrent: 1,
+  });
+
   constructor(private readonly uuidGenerator: UuidGenerator) {}
 
   public async make(htmlContent: string): Promise<string> {
+    return this.#limiter.schedule(() => this.#executePuppeteerJob(htmlContent));
+  }
+
+  async #executePuppeteerJob(htmlContent: string) {
     const browser = await launch({
       headless: "new",
+      args: ["--no-sandbox"],
     });
 
     const page = await browser.newPage();
@@ -19,7 +29,12 @@ export class PuppeteerPdfGeneratorGateway implements PdfGeneratorGateway {
     const base64Pdf = (
       await page.pdf({
         path: fileName,
-        margin: { top: "80px", right: "50px", bottom: "80px", left: "50px" },
+        margin: {
+          top: "2.5cm",
+          right: "1.5cm",
+          bottom: "2.5cm",
+          left: "1.5cm",
+        },
         printBackground: true,
         format: "A4",
       })
