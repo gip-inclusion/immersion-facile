12bd322e17b4145fc24efdd8ce41927213e5ced7
celineung <celine.ung.pro@gmail.com>
2023-10-05 16:39:43 +0200

    add migration file to create new table searches-made-appellation-code

---
+++ back/src/adapters/secondary/pg/migrations/1696514659179_create-table-searches-made-appellation-code.ts
@@ -0,0 +1,49 @@
+/* eslint-disable @typescript-eslint/naming-convention */
+import { MigrationBuilder } from "node-pg-migrate";
+
+const newTableName = "searches_made__appellation_code";
+
+export async function up(pgm: MigrationBuilder): Promise<void> {
+  pgm.createTable(newTableName, {
+    search_made_id: {
+      type: "uuid",
+      notNull: true,
+      references: { name: "searches_made" },
+      onDelete: "CASCADE",
+    },
+    appellation_code: {
+      type: "varchar(6)",
+      notNull: false,
+      default: null,
+    },
+  });
+  pgm.addConstraint(newTableName, `${newTableName}_unicity`, {
+    unique: ["search_made_id", "appellation_code"],
+  });
+  await moveAppellationCodeFromOldTable(pgm); // met environ 3m sur la db nightly
+  pgm.dropColumn("searches_made", "appellation_code");
+}
+
+export async function down(pgm: MigrationBuilder): Promise<void> {
+  pgm.addColumns("searches_made", {
+    appellation_code: { type: "varchar(6)", notNull: false, default: null },
+  });
+  await moveAppellationCodeFromNewTable(pgm); // met envrion 30m sur la db nightly
+  pgm.dropTable(newTableName);
+}
+
+const moveAppellationCodeFromOldTable = async (pgm: MigrationBuilder) => {
+  await pgm.sql(`
+  INSERT INTO ${newTableName}
+      (SELECT id, appellation_code FROM searches_made)
+  `);
+};
+
+const moveAppellationCodeFromNewTable = async (pgm: MigrationBuilder) => {
+  await pgm.sql(`
+  UPDATE searches_made
+      SET id = ${newTableName}.search_made_id, appellation_code = ${newTableName}.appellation_code
+  FROM ${newTableName}
+  WHERE searches_made.id = ${newTableName}.search_made_id
+  `);
+};
