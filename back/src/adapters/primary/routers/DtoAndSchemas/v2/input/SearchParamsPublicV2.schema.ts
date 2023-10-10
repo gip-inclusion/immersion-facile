435887a2f89e60bd257417ab652e0cc3d2101f9b
Jérôme Burkard <burkardjerome@gmail.com>
2023-10-10 17:52:53 +0200

    remove useless siret from search v2 params

--- back/src/adapters/primary/routers/DtoAndSchemas/v2/input/SearchParamsPublicV2.schema.ts
+++ back/src/adapters/primary/routers/DtoAndSchemas/v2/input/SearchParamsPublicV2.schema.ts
@@ -1,16 +1,10 @@
 import { z } from "zod";
-import {
-  appellationCodesSchema,
-  siretSchema,
-  zToBoolean,
-  zToNumber,
-} from "shared";
+import { appellationCodesSchema, zToBoolean, zToNumber } from "shared";
 import { SearchParamsPublicV2 } from "./SearchParamsPublicV2.dto";
 
 export const searchParamsPublicV2Schema: z.Schema<SearchParamsPublicV2> =
   z.object({
     appellationCodes: appellationCodesSchema.optional(),
-    siret: siretSchema.optional(),
     latitude: zToNumber,
     longitude: zToNumber,
     distanceKm: zToNumber.positive("'distance_km' doit être > 0").max(100),
