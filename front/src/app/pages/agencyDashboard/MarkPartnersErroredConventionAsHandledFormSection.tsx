5f7eb7e409eaa755854467f60e8bfdc3541084e9
Jérôme Burkard <burkardjerome@gmail.com>
2023-11-03 14:06:43 +0100

    change wording on button to mark convention error as treated

--- front/src/app/pages/agencyDashboard/MarkPartnersErroredConventionAsHandledFormSection.tsx
+++ front/src/app/pages/agencyDashboard/MarkPartnersErroredConventionAsHandledFormSection.tsx
@@ -55,7 +55,7 @@ export const MarkPartnersErroredConventionAsHandledFormSection = ({
             />
           </div>
           <Button
-            title="Marquer la convention comme traitée"
+            title="Marquer la convention comme saisie dans les applicatifs PE"
             disabled={!formState.isValid}
             className={fr.cx("fr-mt-2w")}
           >
