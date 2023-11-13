7d52c7f76e72ae06863a6e1cb751d043d9086cf8
celineung <celine.ung.pro@gmail.com>
2023-11-13 18:25:36 +0100

    add usecase to delete a subscription

---
+++ back/src/domain/broadcast/useCases/DeleteSubscription.unit.test.ts
@@ -0,0 +1,108 @@
+import { expectPromiseToFailWithError, expectToEqual } from "shared";
+import { ApiConsumerBuilder } from "../../../_testBuilders/ApiConsumerBuilder";
+import {
+  createInMemoryUow,
+  InMemoryUnitOfWork,
+} from "../../../adapters/primary/config/uowConfig";
+import {
+  ForbiddenError,
+  NotFoundError,
+} from "../../../adapters/primary/helpers/httpErrors";
+import { TestUuidGenerator } from "../../../adapters/secondary/core/UuidGeneratorImplementations";
+import { InMemoryUowPerformer } from "../../../adapters/secondary/InMemoryUowPerformer";
+import { DeleteSubscription } from "./DeleteSubscription";
+
+describe("DeleteSubscription", () => {
+  const uuidGenerator = new TestUuidGenerator();
+  let uow: InMemoryUnitOfWork;
+  let deleteSubscription: DeleteSubscription;
+
+  beforeEach(() => {
+    uow = createInMemoryUow();
+    const uowPerformer = new InMemoryUowPerformer(uow);
+
+    deleteSubscription = new DeleteSubscription(uowPerformer);
+  });
+
+  it("throws error when apiConsumer does not have the rights", async () => {
+    const now = new Date("2023-09-22");
+    const subscriptionId = uuidGenerator.new();
+    const apiConsumer = new ApiConsumerBuilder()
+      .withConventionRight({
+        kinds: ["WRITE"],
+        scope: { agencyIds: [] },
+        subscriptions: [
+          {
+            id: subscriptionId,
+            createdAt: now.toISOString(),
+            callbackHeaders: { authorization: "lol" },
+            callbackUrl: "https://www.lol.com",
+            subscribedEvent: "convention.updated",
+          },
+        ],
+      })
+      .build();
+    uow.apiConsumerRepository.consumers = [apiConsumer];
+
+    await expectPromiseToFailWithError(
+      deleteSubscription.execute(subscriptionId, apiConsumer),
+      new ForbiddenError(
+        `You do not have the "SUBSCRIPTION" kind associated to the "convention" right`,
+      ),
+    );
+  });
+
+  it("delete a webhook subscription", async () => {
+    const now = new Date("2023-09-22");
+    const subscriptionId = uuidGenerator.new();
+    const apiConsumer = new ApiConsumerBuilder()
+      .withConventionRight({
+        kinds: ["SUBSCRIPTION"],
+        scope: { agencyIds: ["yolo"] },
+        subscriptions: [
+          {
+            id: subscriptionId,
+            createdAt: now.toISOString(),
+            callbackHeaders: { authorization: "lol" },
+            callbackUrl: "https://www.lol.com",
+            subscribedEvent: "convention.updated",
+          },
+        ],
+      })
+      .build();
+    uow.apiConsumerRepository.consumers = [apiConsumer];
+
+    await deleteSubscription.execute(subscriptionId, apiConsumer);
+
+    const expectedConsumer = await uow.apiConsumerRepository.getById(
+      apiConsumer.id,
+    );
+    expectToEqual(expectedConsumer?.rights.convention.subscriptions, []);
+  });
+
+  it("throws 404 when subscription not found", async () => {
+    const now = new Date("2023-09-22");
+    const subscriptionId = "unexisting-id";
+    const apiConsumer = new ApiConsumerBuilder()
+      .withConventionRight({
+        kinds: ["SUBSCRIPTION"],
+        scope: { agencyIds: ["yolo"] },
+        subscriptions: [
+          {
+            id: uuidGenerator.new(),
+            createdAt: now.toISOString(),
+            callbackHeaders: { authorization: "lol" },
+            callbackUrl: "https://www.lol.com",
+            subscribedEvent: "convention.updated",
+          },
+        ],
+      })
+      .build();
+    uow.apiConsumerRepository.consumers = [apiConsumer];
+
+    await expectPromiseToFailWithError(
+      deleteSubscription.execute(subscriptionId, apiConsumer),
+      new NotFoundError(`subscription ${subscriptionId} not found`),
+    );
+  });
+});
