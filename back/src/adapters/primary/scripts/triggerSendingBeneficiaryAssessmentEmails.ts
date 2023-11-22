7006be02a3ec30740cd42a4a8970d8d04844722a
clement-duport <clement.duport@beta.gouv.fr>
2023-11-22 13:42:07 +0100

    add trigger sending beneficiary assesment emails script

---
+++ back/src/adapters/primary/scripts/triggerSendingBeneficiaryAssessmentEmails.ts
@@ -0,0 +1,73 @@
+import { Pool } from "pg";
+import { keys } from "ramda";
+import { makeCreateNewEvent } from "../../../domain/core/eventBus/EventBus";
+import { makeSaveNotificationAndRelatedEvent } from "../../../domain/generic/notifications/entities/Notification";
+import { SendBeneficiariesAssessmentsEmails } from "../../../domain/offer/useCases/SendBeneficiariesAssesmentsEmails";
+import { createLogger } from "../../../utils/logger";
+import { RealTimeGateway } from "../../secondary/core/TimeGateway/RealTimeGateway";
+import { UuidV4Generator } from "../../secondary/core/UuidGeneratorImplementations";
+import { AppConfig } from "../config/appConfig";
+import { createUowPerformer } from "../config/uowConfig";
+import { handleEndOfScriptNotification } from "./handleEndOfScriptNotification";
+
+const logger = createLogger(__filename);
+
+const config = AppConfig.createFromEnv();
+const sendBeneficiaryAssessmentEmailsScript = async () => {
+  logger.info("Starting to send Beneficiary assessment Emails ");
+
+  const dbUrl = config.pgImmersionDbUrl;
+  const pool = new Pool({
+    connectionString: dbUrl,
+  });
+  const timeGateway = new RealTimeGateway();
+
+  const { uowPerformer } = createUowPerformer(config, () => pool);
+
+  const uuidGenerator = new UuidV4Generator();
+
+  const saveNotificationAndRelatedEvent = makeSaveNotificationAndRelatedEvent(
+    uuidGenerator,
+    timeGateway,
+  );
+
+  const sendBeneficiariesAssesmentsEmails =
+    new SendBeneficiariesAssessmentsEmails(
+      uowPerformer,
+      saveNotificationAndRelatedEvent,
+      timeGateway,
+      makeCreateNewEvent({
+        timeGateway,
+        uuidGenerator: new UuidV4Generator(),
+      }),
+    );
+
+  return sendBeneficiariesAssesmentsEmails.execute();
+};
+
+/* eslint-disable @typescript-eslint/no-floating-promises */
+handleEndOfScriptNotification(
+  "sendBeneficiaryAssessmentEmailsScript",
+  config,
+  sendBeneficiaryAssessmentEmailsScript,
+  ({ numberOfImmersionEndingTomorrow, errors = {} }) => {
+    const failures = keys(errors);
+    const numberOfFailures = failures.length;
+    const numberOfSuccess = numberOfImmersionEndingTomorrow - numberOfFailures;
+
+    const errorsAsString = failures
+      .map(
+        (conventionId) =>
+          `For immersion ids ${conventionId} : ${errors[conventionId]} `,
+      )
+      .join("\n");
+
+    return [
+      `Total of immersion ending tomorrow : ${numberOfSuccess}`,
+      `Number of successfully sent Assessments : ${numberOfSuccess}`,
+      `Number of failures : ${numberOfFailures}`,
+      ...(numberOfFailures > 0 ? [`Failures : ${errorsAsString}`] : []),
+    ].join("\n");
+  },
+  logger,
+);
