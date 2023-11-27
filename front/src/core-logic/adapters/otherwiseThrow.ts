97234e41af1e2f3da91be8b9aee7edb4825af357
Jérôme Burkard <burkardjerome@gmail.com>
2023-11-27 13:10:41 +0100

    use ts-pattern to handle errors in httpAdminGateway

--- front/src/core-logic/adapters/otherwiseThrow.ts
+++ front/src/core-logic/adapters/otherwiseThrow.ts
@@ -1,3 +1,14 @@
+import { HttpResponse } from "shared-routes";
+
+export const logBodyAndThrow = <R extends HttpResponse<number, unknown>>({
+  body,
+}: R): never => {
+  const stringifiedBody = JSON.stringify(body, null, 2);
+  // eslint-disable-next-line no-console
+  console.error(stringifiedBody);
+  throw new Error(stringifiedBody);
+};
+
 export const otherwiseThrow = (unhandledResponse: never): never => {
   // eslint-disable-next-line no-console
   console.error(JSON.stringify(unhandledResponse, null, 2));
