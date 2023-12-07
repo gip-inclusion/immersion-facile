0802ab4531558b04d2e05960f8352489162bd823
clement-duport <clement.duport@beta.gouv.fr>
2023-12-07 14:07:59 +0100

    add review date column to convention table

---
+++ back/src/adapters/secondary/pg/migrations/1701942937132_add-review-date-column-in-conventions-table.ts
@@ -0,0 +1,31 @@
+/* eslint-disable @typescript-eslint/naming-convention */
+import { MigrationBuilder } from "node-pg-migrate";
+
+const conventionTable = "conventions";
+
+export async function up(pgm: MigrationBuilder): Promise<void> {
+  pgm.addColumn(conventionTable, {
+    review_date: {
+      type: "timestamptz",
+      notNull: false,
+    },
+  });
+
+  pgm.sql(`
+   WITH filtered_outbox AS (
+    SELECT
+    outbox.occurred_at AS review_occured_at,
+    (outbox.payload -> 'convention' ->> 'id'::text) AS convention_id
+    FROM outbox
+  WHERE outbox.topic = 'ConventionAcceptedByCounsellor'
+   )
+    UPDATE conventions AS c
+    SET review_date = filtered_outbox.review_occured_at
+    FROM filtered_outbox
+    WHERE filtered_outbox.convention_id = c.id::text
+   `);
+}
+
+export async function down(pgm: MigrationBuilder): Promise<void> {
+  pgm.dropColumn(conventionTable, "review_date");
+}
