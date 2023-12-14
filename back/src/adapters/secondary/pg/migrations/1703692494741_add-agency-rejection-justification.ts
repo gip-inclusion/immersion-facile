c198101cf242cf0e7367afa56437f6b12f8ef038
celineung <celine.ung.pro@gmail.com>
2023-12-14 15:27:16 +0100

    add migration file to add column rejection_justification in agencies table

---
+++ back/src/adapters/secondary/pg/migrations/1703692494741_add-agency-rejection-justification.ts
@@ -0,0 +1,12 @@
+/* eslint-disable @typescript-eslint/naming-convention */
+import { MigrationBuilder } from "node-pg-migrate";
+
+export async function up(pgm: MigrationBuilder): Promise<void> {
+  pgm.addColumn("agencies", {
+    rejection_justification: { type: "text" },
+  });
+}
+
+export async function down(pgm: MigrationBuilder): Promise<void> {
+  pgm.dropColumn("agencies", "rejection_justification");
+}
