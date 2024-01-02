102cbc1221c9fe6c037c5b2aca06161d0abdc8cd
celineung <celine.ung.pro@gmail.com>
2024-01-02 15:24:11 +0100

    check for conflicting agency in usecase when agency status update

--- back/src/domain/convention/ports/AgencyRepository.ts
+++ back/src/domain/convention/ports/AgencyRepository.ts
@@ -1,23 +1,32 @@
 import {
+  AddressDto,
   AgencyDto,
   AgencyId,
+  AgencyKind,
   GetAgenciesFilter,
   PartialAgencyDto,
 } from "shared";
 
 export interface AgencyRepository {
-  getBySafir(safirCode: string): Promise<AgencyDto | undefined>;
   insert: (agency: AgencyDto) => Promise<AgencyId | undefined>;
   update: (partialAgency: PartialAgencyDto) => Promise<void>;
   getByIds: (ids: AgencyId[]) => Promise<AgencyDto[]>;
-  getById: (ids: AgencyId) => Promise<AgencyDto | undefined>;
-  getAgenciesRelatedToAgency(id: AgencyId): Promise<AgencyDto[]>;
+  getById: (id: AgencyId) => Promise<AgencyDto | undefined>;
   getImmersionFacileAgencyId: () => Promise<AgencyId | undefined>;
   getAgencies: (props: {
     filters?: GetAgenciesFilter;
     limit?: number;
   }) => Promise<AgencyDto[]>;
   getAgencyWhereEmailMatches: (email: string) => Promise<AgencyDto | undefined>;
+
+  getBySafir(safirCode: string): Promise<AgencyDto | undefined>;
+
+  getAgenciesRelatedToAgency(id: AgencyId): Promise<AgencyDto[]>;
+  alreadyHasActiveAgencyWithSameAddressAndKind(params: {
+    address: AddressDto;
+    kind: AgencyKind;
+    idToIgnore: AgencyId;
+  }): Promise<boolean>;
 }
 
 export const someAgenciesMissingMessage = (agencyIds: AgencyId[]) =>
--- back/src/domain/convention/useCases/agencies/UpdateAgencyStatus.ts
+++ back/src/domain/convention/useCases/agencies/UpdateAgencyStatus.ts
@@ -1,4 +1,8 @@
 import { UpdateAgencyRequestDto, updateAgencyRequestSchema } from "shared";
+import {
+  ConflictError,
+  NotFoundError,
+} from "../../../../adapters/primary/helpers/httpErrors";
 import { CreateNewEvent } from "../../../core/eventBus/EventBus";
 import {
   UnitOfWork,
@@ -26,16 +30,31 @@ export class UpdateAgencyStatus extends TransactionalUseCase<
     { status, id }: UpdateAgencyRequestDto,
     uow: UnitOfWork,
   ): Promise<void> {
+    const existingAgency = await uow.agencyRepository.getById(id);
+    if (!existingAgency)
+      throw new NotFoundError(`No agency found with id ${id}`);
+
+    const hasSimilarAgency =
+      await uow.agencyRepository.alreadyHasActiveAgencyWithSameAddressAndKind({
+        address: existingAgency.address,
+        kind: existingAgency.kind,
+        idToIgnore: existingAgency.id,
+      });
+
+    if (hasSimilarAgency)
+      throw new ConflictError(
+        "An other agency exists with the same address and kind",
+      );
+
     if (status) await uow.agencyRepository.update({ id, status });
+
     if (status === "active") {
-      const [agency] = await uow.agencyRepository.getByIds([id]);
-      if (agency)
-        await uow.outboxRepository.save(
-          this.#createNewEvent({
-            topic: "AgencyActivated",
-            payload: { agency },
-          }),
-        );
+      await uow.outboxRepository.save(
+        this.#createNewEvent({
+          topic: "AgencyActivated",
+          payload: { agency: { ...existingAgency, status } },
+        }),
+      );
     }
   }
 }
