ec7ed251f90327d28886daff1600059a5a8f0b66
celineung <celine.ung.pro@gmail.com>
2024-01-05 11:20:40 +0100

    add tests for hightlightStringsFromMatches

--- front/src/app/components/forms/establishment/StringWithHighlights.tsx
+++ front/src/app/components/forms/establishment/StringWithHighlights.tsx
@@ -1,13 +1,10 @@
 import React, { Fragment } from "react";
 import { fr } from "@codegouvfr/react-dsfr";
-import { MatchRangeDto } from "shared";
+import {
+  hightlightStringsFromMatches,
+  SliceOfString,
+} from "src/app/components/forms/establishment/hightlightStringsFromMatches";
 import { Proposal } from "./Proposal";
-
-type SliceOfString = {
-  startIndexInclusive: number;
-  endIndexExclusive: number;
-  bolded: boolean;
-};
 
 export const StringWithHighlights = ({
   description,
@@ -16,7 +13,7 @@ export const StringWithHighlights = ({
   const slices: SliceOfString[] =
     matchRanges.length === 0
       ? onNoMatchRanges(description)
-      : onMatchRange(matchRanges, description);
+      : hightlightStringsFromMatches(matchRanges, description);
 
   return (
     <span>
@@ -48,42 +45,3 @@ const onNoMatchRanges = (description: string) => [
     bolded: false,
   },
 ];
-
-const onMatchRange = (matchRanges: MatchRangeDto[], description: string) =>
-  matchRanges.reduce(
-    (acc, { startIndexInclusive, endIndexExclusive }, index) => {
-      const isFirstMatch = acc.length === 0;
-      const unboldedRange = {
-        startIndexInclusive: isFirstMatch
-          ? 0
-          : acc[acc.length - 1].endIndexExclusive,
-        endIndexExclusive: startIndexInclusive,
-        bolded: false,
-      };
-
-      const boldedRange = {
-        startIndexInclusive,
-        endIndexExclusive,
-        bolded: true,
-      };
-
-      const isLastMatch = index === matchRanges.length - 1;
-      const addEndOfDescriptionIfLastMatch = isLastMatch
-        ? [
-            {
-              startIndexInclusive: endIndexExclusive,
-              endIndexExclusive: description.length,
-              bolded: false,
-            },
-          ]
-        : [];
-
-      return [
-        ...acc,
-        unboldedRange,
-        boldedRange,
-        ...addEndOfDescriptionIfLastMatch,
-      ];
-    },
-    [] as SliceOfString[],
-  );
