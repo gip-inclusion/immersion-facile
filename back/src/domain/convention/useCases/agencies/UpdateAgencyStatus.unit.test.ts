37583f6e7197338a83831fb4c6a703f762696e0b
Jérôme Burkard <burkardjerome@gmail.com>
2024-01-10 17:36:00 +0100

    make UpdateAgency usecases only if authenticated

--- back/src/domain/convention/useCases/agencies/UpdateAgencyStatus.unit.test.ts
+++ back/src/domain/convention/useCases/agencies/UpdateAgencyStatus.unit.test.ts
@@ -1,5 +1,6 @@
 import {
   AgencyDtoBuilder,
+  BackOfficeJwtPayload,
   expectPromiseToFailWithError,
   UpdateAgencyStatusParamsWithoutId,
 } from "shared";
@@ -10,6 +11,7 @@ import {
 import {
   ConflictError,
   NotFoundError,
+  UnauthorizedError,
 } from "../../../../adapters/primary/helpers/httpErrors";
 import { CustomTimeGateway } from "../../../../adapters/secondary/core/TimeGateway/CustomTimeGateway";
 import { TestUuidGenerator } from "../../../../adapters/secondary/core/UuidGeneratorImplementations";
@@ -19,6 +21,13 @@ import { UpdateAgencyStatus } from "./UpdateAgencyStatus";
 
 const nextDate = new Date("2022-01-01T10:00:00.000");
 const nextUuid = "event-uuid";
+const backofficeJwtPayload: BackOfficeJwtPayload = {
+  role: "backOffice",
+  iat: 0,
+  exp: 0,
+  sub: "backoffice-id",
+  version: 1,
+};
 
 describe("Update agency status", () => {
   let updateAgencyStatus: UpdateAgencyStatus;
@@ -57,10 +66,13 @@ describe("Update agency status", () => {
         uow.agencyRepository.setAgencies([existingAgency]);
 
         // Act
-        await updateAgencyStatus.execute({
-          id: existingAgency.id,
-          ...testParams,
-        });
+        await updateAgencyStatus.execute(
+          {
+            id: existingAgency.id,
+            ...testParams,
+          },
+          backofficeJwtPayload,
+        );
 
         // Assert
         expect(uow.agencyRepository.agencies[0].status).toBe(
@@ -83,15 +95,28 @@ describe("Update agency status", () => {
       .withStatus("active")
       .build();
 
+    it("returns 401 if no jwt payload", async () => {
+      await expectPromiseToFailWithError(
+        updateAgencyStatus.execute(
+          { id: existingAgency.id, status: "active" },
+          undefined,
+        ),
+        new UnauthorizedError(),
+      );
+    });
+
     it("returns 404 if agency not found", async () => {
       const agencyId = "not-found-id";
       await expectPromiseToFailWithError(
-        updateAgencyStatus.execute({ id: agencyId, status: "active" }),
+        updateAgencyStatus.execute(
+          { id: agencyId, status: "active" },
+          backofficeJwtPayload,
+        ),
         new NotFoundError(`No agency found with id ${agencyId}`),
       );
     });
 
-    it("returns HTTP 409 if attempt to update to another existing agency (with same address and kind, and a status 'active' or 'from-api-PE'", async () => {
+    it("returns HTTP 409 if attempt to update to another existing agency (with same address and kind, and a status 'active' or 'from-api-PE', (except if user is admin))", async () => {
       const agencyToUpdate = new AgencyDtoBuilder()
         .withId("agency-to-update-id")
         .withStatus("needsReview")
@@ -101,12 +126,23 @@ describe("Update agency status", () => {
 
       uow.agencyRepository.setAgencies([agencyToUpdate, existingAgency]);
 
+      // if user is not admin, conflict error
       await expectPromiseToFailWithError(
-        updateAgencyStatus.execute({ id: agencyToUpdate.id, status: "active" }),
+        updateAgencyStatus.execute(
+          { id: agencyToUpdate.id, status: "active" },
+          { role: "another-role" } as any,
+        ),
         new ConflictError(
           "Une autre agence du même type existe avec la même adresse",
         ),
       );
+
+      // if user is admin, no conflict error
+      const resultWithoutError = await updateAgencyStatus.execute(
+        { id: agencyToUpdate.id, status: "active" },
+        backofficeJwtPayload,
+      );
+      expect(resultWithoutError).toBeUndefined();
     });
   });
 });
