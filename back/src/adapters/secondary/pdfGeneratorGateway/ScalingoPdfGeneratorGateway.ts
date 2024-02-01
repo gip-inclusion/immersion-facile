820abcfaa039aae04836ae2da561dc2568d02ad1
Jérôme Burkard <burkardjerome@gmail.com>
2024-02-01 12:21:08 +0100

    use scalingo pdf generator instead of pupetteer

---
+++ back/src/adapters/secondary/pdfGeneratorGateway/ScalingoPdfGeneratorGateway.ts
@@ -0,0 +1,73 @@
+import { z } from "zod";
+import { AbsoluteUrl, withAuthorizationHeaders } from "shared";
+import { defineRoute, defineRoutes, HttpClient } from "shared-routes";
+import { UuidGenerator } from "../../../domain/core/ports/UuidGenerator";
+import { PdfGeneratorGateway } from "../../../domain/generic/htmlToPdf/PdfGeneratorGateway";
+import { createLogger } from "../../../utils/logger";
+
+const logger = createLogger(__filename);
+
+type ScalingoPdfGeneratorRoutes = ReturnType<
+  typeof makeScalingoPdfGeneratorRoutes
+>;
+export const makeScalingoPdfGeneratorRoutes = (baseUrl: AbsoluteUrl) => {
+  const url: AbsoluteUrl = `${baseUrl}/generate`;
+
+  return defineRoutes({
+    generate: defineRoute({
+      method: "post",
+      url,
+      ...withAuthorizationHeaders,
+      queryParamsSchema: z.object({
+        request_id: z.string().optional(),
+      }),
+      requestBodySchema: z.object({
+        htmlContent: z.string(),
+      }),
+      responses: {
+        200: z.string(),
+        401: z.string(),
+      },
+    }),
+  });
+};
+
+export class ScalingoPdfGeneratorGateway implements PdfGeneratorGateway {
+  #httpClient: HttpClient<ScalingoPdfGeneratorRoutes>;
+
+  #apiKey: string;
+
+  #uuidGenerator: UuidGenerator;
+
+  constructor(
+    httpClient: HttpClient<ScalingoPdfGeneratorRoutes>,
+    apiKey: string,
+    uuidGenerator: UuidGenerator,
+  ) {
+    this.#httpClient = httpClient;
+    this.#apiKey = apiKey;
+    this.#uuidGenerator = uuidGenerator;
+  }
+
+  public async make(htmlContent: string): Promise<string> {
+    const requestId = this.#uuidGenerator.new();
+    logger.info({ requestId }, "Generating pdf");
+
+    const response = await this.#httpClient.generate({
+      headers: { authorization: this.#apiKey },
+      body: { htmlContent },
+      queryParams: {
+        request_id: requestId,
+      },
+    });
+
+    if (response.status !== 200)
+      throw new Error(
+        `[requestId : ${requestId}] Pdf generation failed with status ${response.status} - ${response.body}`,
+      );
+
+    logger.info({ requestId }, "Pdf generated successfully");
+
+    return response.body;
+  }
+}
