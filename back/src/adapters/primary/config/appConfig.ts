c14599882f4649e17d52ab2eab495f7e78567146
Jérôme Burkard <burkardjerome@gmail.com>
2024-02-01 12:21:08 +0100

    use scalingo pdf generator instead of pupetteer

--- back/src/adapters/primary/config/appConfig.ts
+++ back/src/adapters/primary/config/appConfig.ts
@@ -338,10 +338,17 @@ export class AppConfig {
     return this.#throwIfNotDefinedOrDefault("PASS_EMPLOI_URL");
   }
 
+  public get pdfGenerator() {
+    return {
+      baseUrl: this.#throwIfNotAbsoluteUrl("PDF_GENERATOR_BASE_URL"),
+      apiKey: this.#throwIfNotDefinedOrDefault("PDF_GENERATOR_API_KEY"),
+    };
+  }
+
   public get pdfGeneratorGateway() {
     return this.#throwIfNotInArray({
       variableName: "PDF_GENERATOR_GATEWAY",
-      authorizedValues: ["IN_MEMORY", "PUPPETEER"],
+      authorizedValues: ["IN_MEMORY", "PUPPETEER", "SCALINGO"],
     });
   }
 
