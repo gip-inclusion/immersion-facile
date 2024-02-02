9393a7857e066521604f4ad406eba5d07940276e
Enguerran Weiss <enguerranweiss@gmail.com>
2024-02-02 12:45:52 +0100

    biome with front rules, applied

--- front/src/app/pages/StatsPage.tsx
+++ front/src/app/pages/StatsPage.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import { MainWrapper } from "react-design-system";
 import { MetabaseView } from "src/app/components/MetabaseView";
 import { HeaderFooterLayout } from "src/app/components/layout/HeaderFooterLayout";
-import { MetabaseView } from "src/app/components/MetabaseView";
 
 export const statsPageUrl =
   "https://metabase.immersion-facile.beta.gouv.fr/public/dashboard/93afb41e-949d-4677-aab3-95817f81223d";
