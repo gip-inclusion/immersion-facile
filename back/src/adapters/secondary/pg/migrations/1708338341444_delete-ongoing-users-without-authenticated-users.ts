9edef40419b220114db63f15406c3b25911ce128
celineung <celine.ung.pro@gmail.com>
2024-02-20 11:01:28 +0100

    delete ongoing_users with externalId but no authenticated_users

---
+++ back/src/adapters/secondary/pg/migrations/1708338341444_delete-ongoing-users-without-authenticated-users.ts
@@ -0,0 +1,19 @@
+/* eslint-disable @typescript-eslint/naming-convention */
+import { ColumnDefinitions, MigrationBuilder } from "node-pg-migrate";
+
+export const shorthands: ColumnDefinitions | undefined = undefined;
+
+export async function up(pgm: MigrationBuilder): Promise<void> {
+  pgm.sql(`
+      DELETE FROM ongoing_oauths
+      WHERE (ongoing_oauths.external_id, ongoing_oauths.created_at, ongoing_oauths.state, ongoing_oauths.nonce) in (
+          SELECT ongoing_oauths.external_id external_id, ongoing_oauths.created_at created_at, ongoing_oauths.state state, ongoing_oauths.nonce nonce
+          from ongoing_oauths
+                   LEFT JOIN authenticated_users on ongoing_oauths.user_id = authenticated_users.id
+          WHERE authenticated_users.id is null
+            and ongoing_oauths.external_id is not null
+      );
+  `);
+}
+
+export async function down(): Promise<void> {}
