a1ffcbe675f40e57de8f900abeb9555985ddf31b
celineung <celine.ung.pro@gmail.com>
2024-03-26 18:21:46 +0100

    display search info section

---
+++ front/src/app/components/search/SearchInfoSection.tsx
@@ -0,0 +1,38 @@
+import { fr } from "@codegouvfr/react-dsfr";
+import React from "react";
+import illustration from "src/assets/img/search.svg";
+
+export const SearchInfoSection = () => (
+  <section
+    className={fr.cx(
+      "fr-container",
+      "fr-grid-row",
+      "fr-grid-row--center",
+      "fr-pb-9w",
+    )}
+  >
+    <div className={fr.cx("fr-col-lg-8", "fr-col-12", "fr-pr-4w")}>
+      <h2>
+        Le saviez-vous ? En février 2024, il fallait envoyer en moyenne 3
+        candidatures pour obtenir 1 réponse !
+      </h2>
+      <p>
+        Les entreprises ne répondent pas à 100% des sollicitations, cela n’a
+        rien à voir avec vous, ne vous découragez pas ! Retrouvez{" "}
+        <a
+          href={
+            "https://aide.immersion-facile.beta.gouv.fr/fr/article/choisir-lobjet-et-rediger-un-email-de-motivation-pour-decrocher-une-immersion-xytzii/"
+          }
+          rel="noreferrer"
+          target="_blank"
+        >
+          nos conseils pour vous démarquer
+        </a>{" "}
+        lors de la première prise de contact.
+      </p>
+    </div>
+    <div className={fr.cx("fr-col-lg-4")}>
+      <img src={illustration} alt="" />
+    </div>
+  </section>
+);
