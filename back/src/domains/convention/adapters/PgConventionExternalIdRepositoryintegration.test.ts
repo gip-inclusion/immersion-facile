a280ce20f8838c89ca18f4e7598aa2f07681f211
Benjamin Bohec <bbohec.pro@gmail.com>
2024-04-05 15:49:12 +0200

    resolve flaky PG Repo tests

--- back/src/domains/convention/adapters/PgConventionExternalIdRepositoryintegration.test.ts
+++ back/src/domains/convention/adapters/PgConventionExternalIdRepositoryintegration.test.ts
@@ -1,16 +1,19 @@
 import { Pool, PoolClient } from "pg";
-import { ConventionDtoBuilder } from "shared";
+import { AgencyDtoBuilder, ConventionDtoBuilder } from "shared";
+import { v4 as uuid } from "uuid";
 import {
   executeKyselyRawSqlQuery,
   makeKyselyDb,
 } from "../../../config/pg/kysely/kyselyUtils";
 import { getTestPgPool } from "../../../config/pg/pgUtils";
+import { PgAgencyRepository } from "../../agency/adapters/PgAgencyRepository";
 import { PgConventionExternalIdRepository } from "./PgConventionExternalIdRepository";
 import { PgConventionRepository } from "./PgConventionRepository";
 
 describe("PgConventionExternalIdRepository", () => {
   let pgConventionExternalIdRepository: PgConventionExternalIdRepository;
   let pgConventionRepository: PgConventionRepository;
+  let pgAgencyRepository: PgAgencyRepository;
   let client: PoolClient;
   let pool: Pool;
 
@@ -30,27 +33,34 @@ describe("PgConventionExternalIdRepository", () => {
     pgConventionExternalIdRepository = new PgConventionExternalIdRepository(
       transaction,
     );
+    pgAgencyRepository = new PgAgencyRepository(transaction);
     await executeKyselyRawSqlQuery(
       transaction,
       "DELETE FROM convention_external_ids",
     );
     await executeKyselyRawSqlQuery(transaction, "DELETE FROM conventions");
+    await executeKyselyRawSqlQuery(
+      transaction,
+      "DELETE FROM agency_groups__agencies",
+    );
+    await executeKyselyRawSqlQuery(transaction, "DELETE FROM agencies");
   });
 
   describe("save", () => {
     it("saves a conventionId and creates a corresponding conventionExternalId, then gets it by id", async () => {
-      const conventionId = "11111111-1111-4111-1111-111111111111";
-      await pgConventionRepository.save(
-        new ConventionDtoBuilder().withId(conventionId).build(),
+      const convention = new ConventionDtoBuilder().withId(uuid()).build();
+      await pgAgencyRepository.insert(
+        new AgencyDtoBuilder().withId(convention.agencyId).build(),
       );
+      await pgConventionRepository.save(convention);
 
       expect(
-        await pgConventionExternalIdRepository.getByConventionId(conventionId),
+        await pgConventionExternalIdRepository.getByConventionId(convention.id),
       ).toBeUndefined();
 
-      await pgConventionExternalIdRepository.save(conventionId);
+      await pgConventionExternalIdRepository.save(convention.id);
       const externalId =
-        await pgConventionExternalIdRepository.getByConventionId(conventionId);
+        await pgConventionExternalIdRepository.getByConventionId(convention.id);
 
       expect(typeof externalId).toBe("string");
     });
