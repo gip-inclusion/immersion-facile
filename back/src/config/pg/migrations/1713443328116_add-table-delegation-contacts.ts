269a8fe50ccaa4bafe7071e0f7a20f0dfd726d50
celineung <celine.ung.pro@gmail.com>
2024-04-18 14:38:46 +0200

    add migration file to create delegaction_contacts table

---
+++ back/src/config/pg/migrations/1713443328116_add-table-delegation-contacts.ts
@@ -0,0 +1,17 @@
+/* eslint-disable @typescript-eslint/naming-convention */
+import { MigrationBuilder } from "node-pg-migrate";
+
+const tableName = "delegation_contacts";
+
+export async function up(pgm: MigrationBuilder): Promise<void> {
+  pgm.createTable(tableName, {
+    province: { type: "varchar(255)", primaryKey: true },
+    created_at: { type: "timestamp", default: pgm.func("now()") },
+    updated_at: { type: "timestamp", default: pgm.func("now()") },
+    email: { type: "varchar(255)", notNull: true },
+  });
+}
+
+export async function down(pgm: MigrationBuilder): Promise<void> {
+  pgm.dropTable(tableName);
+}
