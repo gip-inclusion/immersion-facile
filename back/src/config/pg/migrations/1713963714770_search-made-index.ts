f6dfeef52ef5b7f4b5b6bcd53b1fbd3e162d90d1
Benjamin Bohec <bbohec.pro@gmail.com>
2024-04-24 16:49:45 +0200

    add position & date indexes for searches_made

---
+++ back/src/config/pg/migrations/1713963714770_search-made-index.ts
@@ -0,0 +1,21 @@
+/* eslint-disable @typescript-eslint/naming-convention */
+import { MigrationBuilder } from "node-pg-migrate";
+
+const regionTableName = "public_department_region";
+const searchesMadeTableName = "searches_made";
+
+const gpsColumnName = "gps";
+const shapeColumnName = "shape";
+const updateDateColumnName = "update_date";
+
+export async function up(pgm: MigrationBuilder): Promise<void> {
+  pgm.createIndex(regionTableName, shapeColumnName, { method: "gist" });
+  pgm.createIndex(searchesMadeTableName, gpsColumnName, { method: "gist" });
+  pgm.createIndex(searchesMadeTableName, updateDateColumnName);
+}
+
+export async function down(pgm: MigrationBuilder): Promise<void> {
+  pgm.dropIndex(regionTableName, shapeColumnName);
+  pgm.dropIndex(searchesMadeTableName, gpsColumnName);
+  pgm.dropIndex(searchesMadeTableName, updateDateColumnName);
+}
