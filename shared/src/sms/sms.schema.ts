5e740e6d0068018849b8ac57a043f228eb9cfba4
clement-duport <clement.duport@beta.gouv.fr>
2024-05-15 12:49:16 +0200

    modify sms schema

--- shared/src/sms/sms.schema.ts
+++ shared/src/sms/sms.schema.ts
@@ -1,4 +1,6 @@
+import parseMobile from "libphonenumber-js/mobile";
 import { z } from "zod";
+import { phoneSchema } from "../phone.schema";
 import type { Phone, TemplatedSms } from "./smsTemplateByName";
 
 //Mobile number to send SMS with the country code - Limited on this gateway to allow only french mobile phones
@@ -6,10 +8,8 @@ export const smsRecipientPhoneSchema: z.Schema<Phone> = z
   .string()
   .superRefine((phone, ctx) => {
     if (
-      !(
-        isSupportedPhone(phone, franceMetropolitanPrefixes, 11) ||
-        isSupportedPhone(phone, franceDomPrefixes, 12)
-      )
+      phoneSchema.safeParse(phone).success &&
+      parseMobile(phone)?.getType() !== "MOBILE"
     )
       ctx.addIssue({
         message: `The phone number '${phone}' is not supported.`,
@@ -22,23 +22,3 @@ export const templatedSmsSchema = z.object({
   recipientPhone: smsRecipientPhoneSchema,
   params: z.any(),
 }) as z.Schema<TemplatedSms>;
-
-const isSupportedPhone = (
-  phone: string,
-  phoneStartsWithOptions: string[],
-  phoneLength: number,
-): boolean =>
-  phoneStartsWithOptions.some((option) => phone.startsWith(option)) &&
-  phone.length === phoneLength;
-
-const franceDomPrefixes = [
-  "590690",
-  "590691",
-  "594694",
-  "596696",
-  "596697",
-  "262639",
-  "262692",
-  "262693",
-];
-const franceMetropolitanPrefixes = ["336", "337"];
