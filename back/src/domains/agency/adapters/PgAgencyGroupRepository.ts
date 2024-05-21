d41f13894a82f2a48b137baede2d3dfa8d82a771
Benjamin Bohec <bbohec.pro@gmail.com>
2024-05-21 16:43:19 +0200

    WIP

--- back/src/domains/agency/adapters/PgAgencyGroupRepository.ts
+++ back/src/domains/agency/adapters/PgAgencyGroupRepository.ts
@@ -1,10 +1,15 @@
+import { SelectQueryBuilder } from "kysely";
 import { AgencyGroup, CodeSafir, DepartmentCode } from "shared";
+import { v4 as uuid } from "uuid";
 import {
   KyselyDb,
   cast,
   jsonBuildObject,
 } from "../../../config/pg/kysely/kyselyUtils";
+import { createLogger } from "../../../utils/logger";
 import { AgencyGroupRepository } from "../ports/AgencyGroupRepository";
+
+const logger = createLogger(__filename);
 
 export class PgAgencyGroupRepository implements AgencyGroupRepository {
   #transaction: KyselyDb;
@@ -14,34 +19,36 @@ export class PgAgencyGroupRepository implements AgencyGroupRepository {
   }
 
   async getByCodeSafir(codeSafir: CodeSafir): Promise<AgencyGroup | undefined> {
-    const result = await this.#transaction
-      .selectFrom("agency_groups as ag")
-      .select((qb) =>
-        jsonBuildObject({
-          id: qb.ref("ag.id"),
-          siret: qb.ref("ag.siret"),
-          codeSafir: qb.ref("ag.code_safir"),
-          name: qb.ref("ag.name"),
-          email: qb.ref("ag.email"),
-          kind: qb.ref("ag.kind"),
-          scope: qb.ref("ag.scope"),
-          departments: cast<DepartmentCode[]>(qb.ref("ag.departments")),
-          ccEmails: cast<string[] | null>(qb.ref("ag.cc_emails")),
-        }).as("agencyGroup"),
-      )
-      .where("ag.code_safir", "=", codeSafir)
-      .executeTakeFirst();
+    const result = await kyselyQueryDevOpsWrapper(
+      this.#transaction
+        .selectFrom("agency_groups as ag")
+        .select((qb) =>
+          jsonBuildObject({
+            id: qb.ref("ag.id"),
+            siret: qb.ref("ag.siret"),
+            codeSafir: qb.ref("ag.code_safir"),
+            name: qb.ref("ag.name"),
+            email: qb.ref("ag.email"),
+            kind: qb.ref("ag.kind"),
+            scope: qb.ref("ag.scope"),
+            departments: cast<DepartmentCode[]>(qb.ref("ag.departments")),
+            ccEmails: cast<string[] | null>(qb.ref("ag.cc_emails")),
+          }).as("agencyGroup"),
+        )
+        .where("ag.code_safir", "=", codeSafir),
+    )((builder) => builder.executeTakeFirst());
 
     const agencyGroupWithId = result?.agencyGroup;
     if (!agencyGroupWithId) return;
 
     const { id: agencyGroupId, ...agencyGroup } = agencyGroupWithId;
 
-    const agencyIds = await this.#transaction
-      .selectFrom("agency_groups__agencies as aga")
-      .select("aga.agency_id")
-      .where("aga.agency_group_id", "=", agencyGroupId)
-      .execute();
+    const agencyIds = await kyselyQueryDevOpsWrapper(
+      this.#transaction
+        .selectFrom("agency_groups__agencies as aga")
+        .select("aga.agency_id")
+        .where("aga.agency_group_id", "=", agencyGroupId),
+    )((builder) => builder.execute());
 
     return {
       ...agencyGroup,
@@ -49,3 +56,27 @@ export class PgAgencyGroupRepository implements AgencyGroupRepository {
     };
   }
 }
+
+const kyselyQueryDevOpsWrapper =
+  <DB, T extends keyof DB, O>(queryBuilder: SelectQueryBuilder<DB, T, O>) =>
+  <R>(
+    executionCallback: (
+      queryBuilder: SelectQueryBuilder<DB, T, O>,
+    ) => Promise<R>,
+  ) => {
+    const queryId = uuid();
+    logger.info(`Query ${queryId}`, "start");
+    return executionCallback(queryBuilder)
+      .then((result) => {
+        logger.info(`Query ${queryId}`, "end");
+        return result;
+      })
+      .catch((err) => {
+        //Logger et/ou notify discord ???
+        logger.error(`Query ${queryId}`, "end", {
+          sql: queryBuilder.compile().sql,
+          err,
+        });
+        throw err;
+      });
+  };
