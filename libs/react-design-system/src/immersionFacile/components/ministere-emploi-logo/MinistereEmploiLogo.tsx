ba8ff26a7983f552b60fa9bc305ead4e6bd99646
Enguerran Weiss <enguerranweiss@gmail.com>
2024-05-28 17:24:09 +0200

    removed border on footer logos

--- libs/react-design-system/src/immersionFacile/components/ministere-emploi-logo/MinistereEmploiLogo.tsx
+++ libs/react-design-system/src/immersionFacile/components/ministere-emploi-logo/MinistereEmploiLogo.tsx
@@ -1,12 +1,19 @@
 import { fr } from "@codegouvfr/react-dsfr";
 import React from "react";
 import { useStyles } from "tss-react/dsfr";
+import FooterStyles from "../footer/Footer.styles";
 import Styles from "./MinistereEmploiLogo.styles";
 
 export const MinistereEmploiLogo = () => {
   const { cx } = useStyles();
   return (
-    <p className={cx(fr.cx("fr-logo", "fr-footer__logo"), Styles.text)}>
+    <p
+      className={cx(
+        fr.cx("fr-logo", "fr-footer__logo"),
+        FooterStyles.logo,
+        Styles.text,
+      )}
+    >
       Ministère
       <br />
       du travail,
