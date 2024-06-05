ce7a926c4f9d7831e1c92c2248faaad92efd443c
Enguerran Weiss <enguerranweiss@gmail.com>
2024-06-05 16:32:44 +0200

    exclude feedback in includecheck

--- front/check_includes.sh
+++ front/check_includes.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-files=$(grep --include=\*.{ts,tsx} -rnw 'src' -e "import .* from .*back/.*" -e "from .*shared/.*" -e "from .*react-design-system/.*" -e "from .*/libs/.*")
+files=$(grep --include=\*.{ts,tsx} -rnw 'src' -e 'import .* from .*back/.*' -e 'import .* from .*shared/.*' -e 'import .* from .*react-design-system/.*' -e 'import .* from .*/libs/.*' | grep -v 'import .* from .*feedback.*')
 if [[ $files ]]; then 
   echo -e "\033[0;31mFound includes of back or detailled shared code in front!\033[0m";
   echo $files;
--- front/src/core-logic/domain/feedback/feedback.test.ts
+++ front/src/core-logic/domain/feedback/feedback.test.ts
@@ -10,15 +10,15 @@ import {
 import { createTestStore } from "src/core-logic/storeConfig/createTestStore";
 import { ReduxStore } from "src/core-logic/storeConfig/store";
 
-describe("Notifications", () => {
+describe("Feedbacks", () => {
   let store: ReduxStore;
 
   beforeEach(() => {
     ({ store } = createTestStore());
   });
 
-  describe("notifications slice extra reducers", () => {
-    it("stores notification with level success from other slice", () => {
+  describe("feedbacks slice extra reducers", () => {
+    it("stores feedback with level success from other slice", () => {
       expectToEqual(feedbacksSelectors.feedbacks(store.getState()), {});
 
       store.dispatch(
@@ -30,12 +30,12 @@ describe("Notifications", () => {
       expect(keys(feedbacksSelectors.feedbacks(store.getState()))).toHaveLength(
         1,
       );
-      expectNotificationStoreByTopicToEqual({
+      expectFeedbackStoreByTopicToEqual({
         topic: "api-consumer-global",
         kindAndLevel: "create.success",
       });
     });
-    it("stores notification with level error from other slice", () => {
+    it("stores feedback with level error from other slice", () => {
       expectToEqual(feedbacksSelectors.feedbacks(store.getState()), {});
 
       store.dispatch(
@@ -47,13 +47,13 @@ describe("Notifications", () => {
       expect(keys(feedbacksSelectors.feedbacks(store.getState()))).toHaveLength(
         1,
       );
-      expectNotificationStoreByTopicToEqual({
+      expectFeedbackStoreByTopicToEqual({
         topic: "api-consumer-global",
         kindAndLevel: "create.error",
       });
     });
   });
-  const expectNotificationStoreByTopicToEqual = ({
+  const expectFeedbackStoreByTopicToEqual = ({
     topic,
     kindAndLevel,
   }: { topic: FeedbackTopic; kindAndLevel: ActionKindAndLevel }) =>
