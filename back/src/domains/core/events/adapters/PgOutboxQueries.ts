615426fd722e65a22035cad4b1c455a63347e1d7
Benjamin Bohec <bbohec.pro@gmail.com>
2024-06-11 15:15:10 +0200

    getFailedEvents through kysely

--- back/src/domains/core/events/adapters/PgOutboxQueries.ts
+++ back/src/domains/core/events/adapters/PgOutboxQueries.ts
@@ -17,50 +17,85 @@ export class PgOutboxQueries implements OutboxQueries {
   public async getFailedEvents(params: { limit: number }): Promise<
     DomainEvent[]
   > {
-    const selectEventIdsWithFailure = `(
-      SELECT outbox.id
-      FROM outbox_failures
-      LEFT JOIN outbox_publications ON outbox_publications.id = outbox_failures.publication_id
-      LEFT JOIN outbox ON outbox.id = outbox_publications.event_id
-      WHERE outbox.was_quarantined = false
-      GROUP BY outbox.id
-    )`;
+    const results = await this.transaction
+      .with("ranked_publications", (eb) =>
+        eb
+          .selectFrom("outbox_publications")
+          .select(({ fn }) => [
+            "outbox_publications.id",
+            "outbox_publications.event_id",
+            "outbox_publications.published_at",
+            fn
+              .agg<number>("rank")
+              .over((ob) =>
+                ob
+                  .partitionBy("outbox_publications.event_id")
+                  .orderBy("outbox_publications.published_at", "desc"),
+              )
+              .as("outbox_rank"),
+          ])
+          .whereRef("event_id", "in", ({ selectFrom }) =>
+            selectFrom("outbox_failures")
+              .leftJoin(
+                "outbox_publications",
+                "outbox_publications.id",
+                "publication_id",
+              )
+              .leftJoin("outbox", "outbox.id", "event_id")
+              .select("outbox.id")
+              .where("was_quarantined", "=", false)
+              .groupBy("outbox.id"),
+          ),
+      )
+      .selectFrom("outbox")
+      .leftJoin(
+        "outbox_publications",
+        "outbox.id",
+        "outbox_publications.event_id",
+      )
+      .leftJoin(
+        "outbox_failures",
+        "outbox_publications.id",
+        "outbox_failures.publication_id",
+      )
+      .select([
+        "outbox.id as id",
+        "outbox.occurred_at",
+        "outbox.was_quarantined",
+        "outbox.topic",
+        "outbox.payload",
+        "outbox.status",
+        "outbox_publications.id as publication_id",
+        "outbox_publications.published_at",
+        "outbox_failures.subscription_id",
+        "error_message",
+      ])
+      .where("outbox.id", "in", ({ selectFrom }) =>
+        selectFrom("outbox_publications")
+          .select("event_id")
+          .leftJoin(
+            "outbox_failures",
+            "outbox_failures.publication_id",
+            "outbox_publications.id",
+          )
+          .where("outbox_publications.id", "in", ({ selectFrom }) =>
+            selectFrom("ranked_publications")
+              .select("ranked_publications.id")
+              .where("outbox_rank", "=", 1),
+          )
+          .where("outbox_failures.id", "is not", null)
+          .groupBy("outbox_publications.event_id"),
+      )
+      .orderBy([
+        "outbox.occurred_at",
+        "outbox_publications.published_at",
+        "outbox_failures.subscription_id",
+        "outbox_failures.id",
+      ])
+      .limit(params.limit)
+      .execute();
 
-    const selectLatestPublicationIds = `(
-      SELECT id FROM (
-        SELECT id, event_id, published_at,
-        RANK() OVER (PARTITION BY event_id ORDER BY published_at DESC) outbox_rank
-        FROM outbox_publications
-        WHERE event_id in ${selectEventIdsWithFailure}
-      ) as ranked_publications
-      WHERE outbox_rank = 1
-    )`;
-
-    const selectEventIdsStillFailing = `(
-      SELECT outbox_publications.event_id
-      FROM outbox_publications
-      LEFT JOIN outbox_failures ON outbox_failures.publication_id = outbox_publications.id
-      WHERE outbox_publications.id IN (select id from ${selectLatestPublicationIds} as latest_publications)
-        AND outbox_failures.id IS NOT null 
-      GROUP BY outbox_publications.event_id
-    )`;
-
-    const { rows } = await executeKyselyRawSqlQuery<StoredEventRow>(
-      this.transaction,
-      `
-     SELECT outbox.id as id, occurred_at, was_quarantined, topic, payload, status,
-       outbox_publications.id as publication_id, published_at,
-       subscription_id, error_message
-          FROM outbox
-     LEFT JOIN outbox_publications ON outbox.id = outbox_publications.event_id
-     LEFT JOIN outbox_failures ON outbox_failures.publication_id = outbox_publications.id
-          WHERE outbox.id IN ${selectEventIdsStillFailing}
-          ORDER BY outbox.occurred_at, outbox_publications.published_at, outbox_failures.subscription_id, outbox_failures.id ASC
-          LIMIT ${params.limit}
-      `,
-    );
-
-    return convertRowsToDomainEvents(rows);
+    return convertRowsToDomainEvents(results as StoredEventRow[]);
   }
 
   public async getEventsToPublish(params: { limit: number }): Promise<
