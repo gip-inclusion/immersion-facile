10693a15fbd21c8a1aa0b95e12e00c9530d3384c
celineung <celine.ung.pro@gmail.com>
2024-06-14 16:58:19 +0200

    use kysely builder in featureflagepository

--- back/src/domains/core/feature-flags/adapters/PgFeatureFlagRepository.ts
+++ back/src/domains/core/feature-flags/adapters/PgFeatureFlagRepository.ts
@@ -1,9 +1,6 @@
 import { keys } from "ramda";
 import { FeatureFlags, SetFeatureFlagParam, hasFeatureFlagValue } from "shared";
-import {
-  KyselyDb,
-  executeKyselyRawSqlQuery,
-} from "../../../../config/pg/kysely/kyselyUtils";
+import { KyselyDb } from "../../../../config/pg/kysely/kyselyUtils";
 import { FeatureFlagRepository } from "../ports/FeatureFlagRepository";
 
 const rawPgToFeatureFlags = (raw: any[]): FeatureFlags =>
@@ -25,43 +22,43 @@ export class PgFeatureFlagRepository implements FeatureFlagRepository {
   constructor(private transaction: KyselyDb) {}
 
   public async getAll(): Promise<FeatureFlags> {
-    const { rows } = await executeKyselyRawSqlQuery(
-      this.transaction,
-      "SELECT * FROM feature_flags",
-    );
+    const result = await this.transaction
+      .selectFrom("feature_flags")
+      .selectAll()
+      .execute();
 
-    return rawPgToFeatureFlags(rows);
+    return rawPgToFeatureFlags(result);
   }
 
   public async insertAll(featureFlags: FeatureFlags): Promise<void> {
-    await Promise.all(
-      keys(featureFlags).map(async (flagName) => {
-        const flag = featureFlags[flagName];
-        await executeKyselyRawSqlQuery(
-          this.transaction,
-          "INSERT INTO feature_flags (flag_name, is_active, kind, value) VALUES ($1, $2, $3, $4)",
-          [
-            flagName,
-            flag.isActive,
-            flag.kind,
-            hasFeatureFlagValue(flag) ? JSON.stringify(flag.value) : null,
-          ],
-        );
-      }),
-    );
+    await this.transaction
+      .insertInto("feature_flags")
+      .values(
+        keys(featureFlags).map((flagName) => {
+          const flag = featureFlags[flagName];
+          return {
+            flag_name: flagName,
+            is_active: flag.isActive,
+            kind: flag.kind,
+            value: hasFeatureFlagValue(flag)
+              ? JSON.stringify(flag.value)
+              : null,
+          };
+        }),
+      )
+      .execute();
   }
 
   public async update(params: SetFeatureFlagParam): Promise<void> {
-    await executeKyselyRawSqlQuery(
-      this.transaction,
-      "UPDATE feature_flags SET is_active = $1, value = $2 WHERE flag_name = $3",
-      [
-        params.featureFlag.isActive,
-        hasFeatureFlagValue(params.featureFlag)
+    await this.transaction
+      .updateTable("feature_flags")
+      .set({
+        is_active: params.featureFlag.isActive,
+        value: hasFeatureFlagValue(params.featureFlag)
           ? JSON.stringify(params.featureFlag.value)
           : null,
-        params.flagName,
-      ],
-    );
+      })
+      .where("flag_name", "=", params.flagName)
+      .execute();
   }
 }
