831e01c541cb84884bed4cab21f73a3bd2c42aa5
Benjamin Bohec <bbohec.pro@gmail.com>
2024-06-19 14:26:31 +0200

    DiagorienteAppellationsGateway exclude not valid appelation results

--- back/src/domains/core/rome/adapters/DiagorienteAppellationsGateway.manual.test.ts
+++ back/src/domains/core/rome/adapters/DiagorienteAppellationsGateway.manual.test.ts
@@ -36,112 +36,141 @@ describe("DiagorienteAppellationsGateway", () => {
     );
   });
 
-  it("returns an access token with client ID and secret", async () => {
-    const response = await appellationsGateway.getAccessToken();
-    expect(response).toMatchObject({
-      access_token: expect.any(String),
-      expires_in: expect.any(Number),
-      token_type: "Bearer",
+  describe("getAccessToken", () => {
+    it("returns an access token with client ID and secret", async () => {
+      const response = await appellationsGateway.getAccessToken();
+      expect(response).toMatchObject({
+        access_token: expect.any(String),
+        expires_in: expect.any(Number),
+        token_type: "Bearer",
+      });
+    });
+
+    it("should retrieve stored token, not requesting another", async () => {
+      const { access_token: originalToken } =
+        await appellationsGateway.getAccessToken();
+      const { access_token: newToken } =
+        await appellationsGateway.getAccessToken();
+      expect(originalToken).toEqual(newToken);
     });
   });
 
-  it("should retrieve stored token, not requesting another", async () => {
-    const { access_token: originalToken } =
-      await appellationsGateway.getAccessToken();
-    const { access_token: newToken } =
-      await appellationsGateway.getAccessToken();
-    expect(originalToken).toEqual(newToken);
+  describe("searchAppellations", () => {
+    const parallelCalls = 100;
+    it.each([
+      {
+        search: "Dév",
+        results: [
+          {
+            appellationLabel: "Nez",
+            appellationCode: "16989",
+          },
+          {
+            appellationLabel: "Dee-jay",
+            appellationCode: "13916",
+          },
+          {
+            appellationLabel: "Délaineur / Délaineuse",
+            appellationCode: "13923",
+          },
+          {
+            appellationLabel: "Déligneur / Déligneuse",
+            appellationCode: "13945",
+          },
+          {
+            appellationLabel: "Déménageur / Déménageuse",
+            appellationCode: "13947",
+          },
+        ],
+      },
+      {
+        search: "ux",
+        results: [
+          {
+            appellationLabel: "Mareyeur / Mareyeuse",
+            appellationCode: "16391",
+          },
+          {
+            appellationLabel: "Élagueur / Élagueuse",
+            appellationCode: "14608",
+          },
+          {
+            appellationLabel: "Scieur / Scieuse",
+            appellationCode: "19323",
+          },
+          {
+            appellationLabel: "Liégeur / Liégeuse",
+            appellationCode: "16185",
+          },
+          {
+            appellationLabel: "Licier / Licière",
+            appellationCode: "16183",
+          },
+        ],
+      },
+      {
+        search: "Secrétaire",
+        results: [
+          {
+            appellationLabel: "Secrétaire",
+            appellationCode: "19364",
+          },
+          {
+            appellationLabel: "Secrétaire bureautique",
+            appellationCode: "19368",
+          },
+          {
+            appellationLabel: "Secrétaire généraliste",
+            appellationCode: "19395",
+          },
+          {
+            appellationLabel: "Secrétaire administratif / administrative",
+            appellationCode: "19365",
+          },
+          {
+            appellationLabel: "Secrétaire juridique",
+            appellationCode: "19396",
+          },
+        ],
+      },
+      {
+        search: "Data analyst",
+        results: [
+          {
+            appellationCode: "38971",
+            appellationLabel: "Data analyst",
+          },
+          {
+            appellationCode: "38972",
+            appellationLabel: "Data scientist",
+          },
+          {
+            appellationCode: "38975",
+            appellationLabel: "Data manager",
+          },
+          {
+            appellationCode: "38970",
+            appellationLabel: "Data miner",
+          },
+          {
+            appellationCode: "15565",
+            appellationLabel: "Informaticien / Informaticienne analyste",
+          },
+        ],
+      },
+    ])(
+      `returns matching results for ${parallelCalls} parralel query with term $search`,
+      async ({ results, search }) => {
+        await Promise.all(
+          [...Array(parallelCalls)].map(async (_) => {
+            expectObjectsToMatch(
+              await appellationsGateway.searchAppellations(search),
+              results,
+            );
+          }),
+        );
+      },
+      parallelCalls * requestMinTime * 10 + 500,
+    );
   });
-
-  const parallelCalls = 100;
-  it.each([
-    {
-      search: "Dév",
-      results: [
-        {
-          appellationLabel: "Nez",
-          appellationCode: "16989",
-        },
-        {
-          appellationLabel: "Dee-jay",
-          appellationCode: "13916",
-        },
-        {
-          appellationLabel: "Délaineur / Délaineuse",
-          appellationCode: "13923",
-        },
-        {
-          appellationLabel: "Déligneur / Déligneuse",
-          appellationCode: "13945",
-        },
-        {
-          appellationLabel: "Déménageur / Déménageuse",
-          appellationCode: "13947",
-        },
-      ],
-    },
-    {
-      search: "ux",
-      results: [
-        {
-          appellationLabel: "Mareyeur / Mareyeuse",
-          appellationCode: "16391",
-        },
-        {
-          appellationLabel: "Élagueur / Élagueuse",
-          appellationCode: "14608",
-        },
-        {
-          appellationLabel: "Scieur / Scieuse",
-          appellationCode: "19323",
-        },
-        {
-          appellationLabel: "Liégeur / Liégeuse",
-          appellationCode: "16185",
-        },
-        {
-          appellationLabel: "Licier / Licière",
-          appellationCode: "16183",
-        },
-      ],
-    },
-    {
-      search: "Secrétaire",
-      results: [
-        {
-          appellationLabel: "Secrétaire",
-          appellationCode: "19364",
-        },
-        {
-          appellationLabel: "Secrétaire bureautique",
-          appellationCode: "19368",
-        },
-        {
-          appellationLabel: "Secrétaire généraliste",
-          appellationCode: "19395",
-        },
-        {
-          appellationLabel: "Secrétaire administratif / administrative",
-          appellationCode: "19365",
-        },
-        {
-          appellationLabel: "Secrétaire juridique",
-          appellationCode: "19396",
-        },
-      ],
-    },
-  ])(
-    `returns matching results for ${parallelCalls} parralel query with term $search`,
-    async ({ results, search }) => {
-      await Promise.all(
-        [...Array(parallelCalls)].map(async (_) => {
-          expectObjectsToMatch(
-            await appellationsGateway.searchAppellations(search),
-            results,
-          );
-        }),
-      );
-    },
-    parallelCalls * requestMinTime * 10,
-  );
 });
