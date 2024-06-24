e6dcf500cfb5b88b5675b7c959240b9cd497d366
Benjamin Bohec <bbohec.pro@gmail.com>
2024-06-24 16:17:23 +0200

    unwanted route todo

--- front/src/app/routes/routes.ts
+++ front/src/app/routes/routes.ts
@@ -139,6 +139,7 @@ export const { RouteProvider, useRoute, routes } = createRouter({
     { jwt: param.query.string },
     () => `/${frontRoutes.conventionToSign}`,
   ),
+  //TODO: Why this route still exist?
   debugPopulateDB: defineRoute(
     { count: param.path.number },
     (params) => `/debug/populate/${params.count}`,
