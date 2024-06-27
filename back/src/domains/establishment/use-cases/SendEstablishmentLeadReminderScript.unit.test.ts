ebb4aec1013a5f55090cf22c5f34ca30452c3125
Jérôme Burkard <burkardjerome@gmail.com>
2024-06-27 11:18:20 +0200

    add mtm_campaign to ESTABLISHEMENT_LEAD_REMINDER email

--- back/src/domains/establishment/use-cases/SendEstablishmentLeadReminderScript.unit.test.ts
+++ back/src/domains/establishment/use-cases/SendEstablishmentLeadReminderScript.unit.test.ts
@@ -188,7 +188,7 @@ describe("SendEstablishmentLeadReminder", () => {
       expect(
         await uow.shortLinkRepository.getById("addEstablishmentFormShortLink"),
       ).toBe(
-        "http://localhost/establishment?siret=12345678901235&bcLastName=Idol&bcFirstName=Billy&bcPhone=+33602010203&bcEmail=newestConvention.establishment@email.fr",
+        "http://localhost/establishment?siret=12345678901235&bcLastName=Idol&bcFirstName=Billy&bcPhone=+33602010203&bcEmail=newestConvention.establishment@email.fr&mtm_campaign=transactionnel-etablissement-rappel-inscription",
       );
 
       expect(
@@ -299,7 +299,7 @@ describe("SendEstablishmentLeadReminder", () => {
       expect(
         await uow.shortLinkRepository.getById("addEstablishmentFormShortLink"),
       ).toBe(
-        "http://localhost/establishment?siret=12345678901236&bcLastName=Idol&bcFirstName=Billy&bcPhone=+33602010203&bcEmail=establishment@example.com",
+        "http://localhost/establishment?siret=12345678901236&bcLastName=Idol&bcFirstName=Billy&bcPhone=+33602010203&bcEmail=establishment@example.com&mtm_campaign=transactionnel-etablissement-rappel-inscription",
       );
 
       expect(
