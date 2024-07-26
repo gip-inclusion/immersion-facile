7941eaedd52056a7329bffe87104997228d2bc48
Benjamin Bohec <bbohec.pro@gmail.com>
2024-07-26 14:30:30 +0200

    add index on discussions

---
+++ back/src/config/pg/migrations/1721994651652_add-index-on-discussions.ts
@@ -0,0 +1,17 @@
+/* eslint-disable @typescript-eslint/naming-convention */
+import { MigrationBuilder } from "node-pg-migrate";
+
+const tableName = "discussions";
+const columnNames = [
+  "establishment_contact_email",
+  "potential_beneficiary_email",
+  "appellation_code",
+];
+
+export async function up(pgm: MigrationBuilder): Promise<void> {
+  columnNames.forEach((columnName) => pgm.addIndex(tableName, columnName));
+}
+
+export async function down(pgm: MigrationBuilder): Promise<void> {
+  columnNames.forEach((columnName) => pgm.dropIndex(tableName, columnName));
+}
