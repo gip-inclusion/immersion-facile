610ec56901a00b205d150e9bafb16b6051eece09
Enguerran Weiss <enguerranweiss@gmail.com>
2024-08-01 15:33:11 +0200

    add html reporter to ci

--- playwright/playwright.config.ts
+++ playwright/playwright.config.ts
@@ -13,7 +13,7 @@ export default defineConfig({
   retries: process.env.CI ? 2 : 0,
   workers: 4,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: process.env.CI ? "github" : "html",
+  reporter: process.env.CI ? [["github"], ["html"]] : "html",
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
