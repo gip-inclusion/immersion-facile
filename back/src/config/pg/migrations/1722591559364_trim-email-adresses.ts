ac65ba062e6fefc898cab9dd1ea813e1b023e711
celineung <celine.ung.pro@gmail.com>
2024-08-02 11:57:16 +0200

    add migration file to trim emails

---
+++ back/src/config/pg/migrations/1722591559364_trim-email-adresses.ts
@@ -0,0 +1,31 @@
+/* eslint-disable @typescript-eslint/naming-convention */
+import { MigrationBuilder } from "node-pg-migrate";
+
+export async function up(pgm: MigrationBuilder): Promise<void> {
+  pgm.sql(`
+      UPDATE establishments_contacts
+      SET email = TRIM(email);
+  `);
+  pgm.sql(`
+      UPDATE public.actors
+      SET email = TRIM(email);
+  `);
+  pgm.sql(`
+      UPDATE users
+      SET email = TRIM(email);
+  `);
+  pgm.sql(`
+      UPDATE discussions
+      SET potential_beneficiary_email = TRIM(potential_beneficiary_email),
+          establishment_contact_email = TRIM(establishment_contact_email),
+          establishment_contact_copy_emails = (
+              SELECT CASE
+                         WHEN jsonb_array_length(establishment_contact_copy_emails) = 0 THEN '[]'::jsonb
+                         ELSE jsonb_agg(TRIM(value::text))
+                         END
+              FROM jsonb_array_elements(establishment_contact_copy_emails) AS elem(value)
+          )
+  `);
+}
+
+export async function down(): Promise<void> {}
