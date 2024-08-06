c7c35b1210d75264211eb6656af6b581c67194cf
celineung <celine.ung.pro@gmail.com>
2024-08-06 17:15:33 +0200

    fix emails in discussion and notifications_email_recipients tables

---
+++ back/src/config/pg/migrations/1722954848190_fix-notifications-and-discussions-emails.ts
@@ -0,0 +1,52 @@
+/* eslint-disable @typescript-eslint/naming-convention */
+import { MigrationBuilder } from "node-pg-migrate";
+
+export async function up(pgm: MigrationBuilder): Promise<void> {
+  // remove " and \ from emails in discussions table
+  pgm.sql(`
+      UPDATE discussions
+      SET establishment_contact_copy_emails = (
+            SELECT jsonb_agg(REPLACE(value::text, '\\"', '')::jsonb)
+             FROM jsonb_array_elements(establishment_contact_copy_emails) AS elems(value))
+      WHERE EXISTS (
+            SELECT 1
+            FROM
+                jsonb_array_elements(establishment_contact_copy_emails) AS elems(value)
+            WHERE
+                value::text LIKE '%\\"%');
+  `);
+
+  // get notifications id with troubles
+  const result = await pgm.db.query(`
+      select notifications_email_id
+    from notifications_email_recipients
+    where recipient_type = 'cc' and email ilike '"%'
+  `);
+
+  const formattedNotificationIds = result.rows
+    .map(({ notifications_email_id }) => `'${notifications_email_id}'`)
+    .join(",");
+
+  // update notifications and free outbox event in quarantine
+  if (formattedNotificationIds.length > 0) {
+    await pgm.db.query(
+      `
+      UPDATE notifications_email_recipients
+      SET email = REPLACE(email, '"', '')
+      WHERE notifications_email_id in (${formattedNotificationIds})
+      `,
+    );
+
+    await pgm.db.query(`
+      update outbox
+      set was_quarantined = false
+      where was_quarantined = true 
+        and occurred_at > '2024-08-06 12:00:00'
+        and topic = 'NotificationAdded'
+      `);
+  }
+}
+
+export async function down(): Promise<void> {
+  // nothing to do
+}
