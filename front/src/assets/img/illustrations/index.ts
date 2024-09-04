d47b3dd369c9bbdb0dd6c67ed3cbe0a58cec6c05
Enguerran Weiss <enguerranweiss@gmail.com>
2024-09-04 10:57:26 +0200

    illustrations as separate module, non-random illustrations

---
+++ front/src/assets/img/illustrations/index.ts
@@ -0,0 +1,6 @@
+import search1 from "./search-illustration-0.webp";
+import search2 from "./search-illustration-1.webp";
+import search3 from "./search-illustration-2.webp";
+import search4 from "./search-illustration-3.webp";
+
+export const searchIllustrations = [search1, search2, search3, search4];
