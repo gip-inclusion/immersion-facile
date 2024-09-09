9a6d8a989b7b66d3fd2f49b6d8d7ba092a6ee7a6
Enguerran Weiss <enguerranweiss@gmail.com>
2024-09-09 14:42:37 +0200

    admin tab with admin props (isVisible)

--- shared/src/admin/adminTabs.ts
+++ shared/src/admin/adminTabs.ts
@@ -7,5 +7,35 @@ export const adminTabsList = [
   "email-preview",
   "technical-options",
 ] as const;
+
+type AdminTabProps = {
+  isVisible: (env: "dev" | "staging" | "production" | "local") => boolean;
+};
+
+export const adminTabs: Record<AdminTab, AdminTabProps> = {
+  conventions: {
+    isVisible: () => true,
+  },
+  events: {
+    isVisible: () => true,
+  },
+  agencies: {
+    isVisible: () => true,
+  },
+  establishments: {
+    isVisible: () => true,
+  },
+  notifications: {
+    isVisible: (env: "dev" | "staging" | "production" | "local") =>
+      env !== "production",
+  },
+  "email-preview": {
+    isVisible: () => true,
+  },
+  "technical-options": {
+    isVisible: () => true,
+  },
+} as const;
+
 export type AdminTabList = typeof adminTabsList;
 export type AdminTab = AdminTabList[number];
