75838b62b2b69790a2a9ca3aacddd15d714c99a2
celineung <celine.ung.pro@gmail.com>
2024-09-09 14:22:16 +0200

    Script import Pro Connect external Ids

--- back/src/config/pg/kysely/kyselyUtils.ts
+++ back/src/config/pg/kysely/kyselyUtils.ts
@@ -83,3 +83,32 @@ export type KyselyDb = Kysely<Database>;
 
 export const falsyToNull = <T>(value: T | Falsy): T | null =>
   value ? value : null;
+
+//https://github.com/kysely-org/kysely/issues/839
+//https://old.kyse.link/?p=s&i=C0yoagEodj9vv4AxE3TH
+export function values<R extends Record<string, unknown>, A extends string>(
+  records: R[],
+  alias: A,
+) {
+  // Assume there's at least one record and all records
+  // have the same keys.
+  const keys = Object.keys(records[0]);
+
+  // Transform the records into a list of lists such as
+  // ($1, $2, $3), ($4, $5, $6)
+  const values = sql.join(
+    records.map((r) => sql`(${sql.join(keys.map((k) => r[k]))})`),
+  );
+
+  // Create the alias `v(id, v1, v2)` that specifies the table alias
+  // AND a name for each column.
+  const wrappedAlias = sql.ref(alias);
+  const wrappedColumns = sql.join(keys.map(sql.ref));
+  const aliasSql = sql`${wrappedAlias}(${wrappedColumns})`;
+
+  // Finally create a single `AliasedRawBuilder` instance of the
+  // whole thing. Note that we need to explicitly specify
+  // the alias type using `.as<A>` because we are using a
+  // raw sql snippet as the alias.
+  return sql<R>`(values ${values})`.as<A>(aliasSql);
+}
