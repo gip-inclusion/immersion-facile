a733767f5c2d5ea63acebb16c6bdb7c3ef987716
clement <clement.laumond@gmail.com>
2024-09-16 11:49:38 +0200

    add playwright test to access a search result

--- playwright/tests/search/search.spec.ts
+++ playwright/tests/search/search.spec.ts
@@ -5,17 +5,32 @@ import {
   fillAutocomplete,
 } from "../../utils/utils";
 
+const makeEmptySearch = async (page: Page) => {
+  await page.goto("/");
+  await page.click(`#${domElementIds.home.heroHeader.candidate}`);
+  await page.click(`#${domElementIds.homeCandidates.heroHeader.search}`);
+  await expect(page).toHaveURL(`/${frontRoutes.search}`);
+  await expectSearchSubmitButtonToBeEnabled(page);
+  await page.click(`#${domElementIds.search.searchSubmitButton}`);
+  await expectSearchToHaveResults(page);
+};
+
 test.describe.configure({ mode: "serial" });
 
 test.describe("Search", () => {
-  test("can access and search with empty fields", async ({ page }) => {
-    await page.goto("/");
-    await page.click(`#${domElementIds.home.heroHeader.candidate}`);
-    await page.click(`#${domElementIds.homeCandidates.heroHeader.search}`);
-    await expect(page).toHaveURL(`/${frontRoutes.search}`);
-    await expectSearchSubmitButtonToBeEnabled(page);
-    await page.click(`#${domElementIds.search.searchSubmitButton}`);
-    await expectSearchToHaveResults(page);
+  test("can search with empty fields", async ({ page }) => {
+    await makeEmptySearch(page);
+  });
+
+  test("can access a search result", async ({ page }) => {
+    await makeEmptySearch(page);
+    await page
+      .locator(`[id^=${domElementIds.search.searchResultButton}]`)
+      .first()
+      .click();
+    await page.getByRole("button", {
+      name: "Contacter l'entreprise",
+    });
   });
 
   test("can search with location fields and clear it", async ({ page }) => {
