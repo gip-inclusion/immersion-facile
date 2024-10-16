20730a63b92c2131fa285ae7024c53b40a40acc8
Benjamin Bohec <bbohec.pro@gmail.com>
2024-10-16 15:44:09 +0200

    repair usecase GetUsers

--- back/src/domains/inclusion-connected-users/use-cases/GetUsers.ts
+++ back/src/domains/inclusion-connected-users/use-cases/GetUsers.ts
@@ -2,9 +2,11 @@ import {
   GetUsersFilters,
   InclusionConnectedUser,
   UserInList,
+  UserWithAdminRights,
   getUsersFiltersSchema,
 } from "shared";
 import { createTransactionalUseCase } from "../../core/UseCase";
+import { UnitOfWork } from "../../core/unit-of-work/ports/UnitOfWork";
 import { throwIfNotAdmin } from "../helpers/throwIfIcUserNotBackofficeAdmin";
 
 export type GetUsers = ReturnType<typeof makeGetUsers>;
@@ -19,6 +21,20 @@ export const makeGetUsers = createTransactionalUseCase<
   },
   async ({ uow, currentUser, inputParams }) => {
     throwIfNotAdmin(currentUser);
-    return uow.userRepository.getUsers(inputParams);
+    const users = await uow.userRepository.getUsers(inputParams);
+    return Promise.all(users.map((user) => userToUserInList(uow, user)));
   },
 );
+
+const userToUserInList = async (
+  uow: UnitOfWork,
+  { isBackofficeAdmin: _, ...rest }: UserWithAdminRights,
+): Promise<UserInList> => {
+  const userAgencies = await uow.agencyRepository.getAgenciesRightsByUserId(
+    rest.id,
+  );
+  return {
+    ...rest,
+    numberOfAgencies: userAgencies.length,
+  };
+};
