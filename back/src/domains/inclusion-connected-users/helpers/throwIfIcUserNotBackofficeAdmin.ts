1eca4279854c24804219a04330dcfd43314216fa
Benjamin Bohec <bbohec.pro@gmail.com>
2024-10-16 12:18:36 +0200

    rework agency utilities

--- back/src/domains/inclusion-connected-users/helpers/throwIfIcUserNotBackofficeAdmin.ts
+++ back/src/domains/inclusion-connected-users/helpers/throwIfIcUserNotBackofficeAdmin.ts
@@ -1,11 +1,20 @@
+import { toPairs } from "ramda";
 import {
+  AgencyDto,
+  AgencyRole,
   InclusionConnectDomainJwtPayload,
   InclusionConnectedUser,
+  OAuthGatewayProvider,
   UserId,
   errors,
+  isTruthy,
 } from "shared";
-
+import {
+  AgencyUsersRights,
+  AgencyWithUsersRights,
+} from "../../agency/ports/AgencyRepository";
 import { oAuthProviderByFeatureFlags } from "../../core/authentication/inclusion-connect/port/OAuthGateway";
+import { UserWithAdminRights } from "../../core/authentication/inclusion-connect/port/UserRepository";
 import { UnitOfWork } from "../../core/unit-of-work/ports/UnitOfWork";
 
 export const throwIfIcUserNotBackofficeAdmin = async (
@@ -21,15 +30,70 @@ export const getIcUserOrThrow = async (
   uow: UnitOfWork,
   userId: UserId,
 ): Promise<InclusionConnectedUser> => {
-  const user = await uow.userRepository.getById(
-    userId,
-    oAuthProviderByFeatureFlags(await uow.featureFlagRepository.getAll()),
+  const provider = oAuthProviderByFeatureFlags(
+    await uow.featureFlagRepository.getAll(),
   );
+  const user = await uow.userRepository.getById(userId, provider);
   if (!user) throw errors.user.notFound({ userId });
-  return user;
+
+  const agencyRights =
+    await uow.agencyRepository.getAgenciesRightsByUserId(userId);
+  return {
+    ...user,
+    agencyRights: await Promise.all(
+      agencyRights.map(async ({ agency, isNotifiedByEmail, roles }) => ({
+        agency: await agencyWithUsersRightsToAgencyDto(uow, provider, agency),
+        isNotifiedByEmail,
+        roles,
+      })),
+    ),
+    dashboards: { agencies: {}, establishments: {} },
+  };
 };
 
-export const throwIfNotAdmin = (user: InclusionConnectedUser | undefined) => {
+export const throwIfNotAdmin = (user: InclusionConnectedUser) => {
   if (!user) throw errors.user.unauthorized();
   if (!user.isBackofficeAdmin) throw errors.user.forbidden({ userId: user.id });
+};
+
+const agencyWithUsersRightsToAgencyDto = async (
+  uow: UnitOfWork,
+  provider: OAuthGatewayProvider,
+  { usersRights, ...rest }: AgencyWithUsersRights,
+): Promise<AgencyDto> => {
+  const counsellorUsers = await usersWithRoleFromRights(
+    uow,
+    provider,
+    usersRights,
+    "counsellor",
+  );
+  const validatorUsers = await usersWithRoleFromRights(
+    uow,
+    provider,
+    usersRights,
+    "validator",
+  );
+
+  return {
+    ...rest,
+    counsellorEmails: counsellorUsers.map((user) => user.email),
+    validatorEmails: validatorUsers.map((user) => user.email),
+  };
+};
+
+const usersWithRoleFromRights = async (
+  uow: UnitOfWork,
+  provider: OAuthGatewayProvider,
+  usersRights: AgencyUsersRights,
+  role: AgencyRole,
+): Promise<UserWithAdminRights[]> => {
+  const userIdsWithRole = toPairs(usersRights).filter(([_, right]) =>
+    right.roles.includes(role),
+  );
+  const users = await Promise.all(
+    userIdsWithRole.map(([userId]) =>
+      uow.userRepository.getById(userId, provider),
+    ),
+  );
+  return users.filter(isTruthy);
 };
