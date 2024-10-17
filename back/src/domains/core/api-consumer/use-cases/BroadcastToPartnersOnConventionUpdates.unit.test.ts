5564555b5a26a61d6b1f7212b95a597c4501c059
Benjamin Bohec <bbohec.pro@gmail.com>
2024-10-17 15:35:35 +0200

    repair usecase BroadcastToPartnersOnConventionUpdates

--- back/src/domains/core/api-consumer/use-cases/BroadcastToPartnersOnConventionUpdates.unit.test.ts
+++ back/src/domains/core/api-consumer/use-cases/BroadcastToPartnersOnConventionUpdates.unit.test.ts
@@ -1,9 +1,12 @@
 import {
   AgencyDtoBuilder,
   ConventionDtoBuilder,
+  InclusionConnectedUserBuilder,
   SubscriptionParams,
   expectToEqual,
 } from "shared";
+import { v4 as uuid } from "uuid";
+import { toAgencyWithRights } from "../../../../utils/agency";
 import { BroadcastFeedback } from "../../saved-errors/ports/BroadcastFeedbacksRepository";
 import { CustomTimeGateway } from "../../time-gateway/adapters/CustomTimeGateway";
 import { InMemoryUowPerformer } from "../../unit-of-work/adapters/InMemoryUowPerformer";
@@ -19,87 +22,145 @@ import {
 import { SubscriberResponse } from "../ports/SubscribersGateway";
 import { BroadcastToPartnersOnConventionUpdates } from "./BroadcastToPartnersOnConventionUpdates";
 
-const agency1 = new AgencyDtoBuilder().withId("agency-1").build();
-const agency2 = new AgencyDtoBuilder().withId("agency-2").build();
-
-const convention1 = new ConventionDtoBuilder()
-  .withId("11111111-ee70-4c90-b3f4-668d492f7395")
-  .withAgencyId(agency1.id)
-  .build();
-
-const convention2 = new ConventionDtoBuilder()
-  .withId("22222222-ee70-4c90-b3f4-668d492f7395")
-  .withAgencyId(agency2.id)
-  .build();
-
-const subscriptionParams: SubscriptionParams = {
-  callbackHeaders: { authorization: "my-cb-auth-header" },
-  callbackUrl: "https://www.my-service.com/convention-updated",
-};
-
-const apiConsumer1 = new ApiConsumerBuilder()
-  .withId("my-api-consumer1")
-  .withConventionRight({
-    kinds: ["SUBSCRIPTION"],
-    scope: { agencyIds: [agency1.id] },
-    subscriptions: [
-      {
-        ...subscriptionParams,
-        subscribedEvent: "convention.updated",
-        createdAt: new Date().toISOString(),
-        id: "my-subscription-id",
-      },
-    ],
-  })
-  .build();
-
-const callbackParams2: SubscriptionParams = {
-  callbackHeaders: { authorization: "my-cb-auth-header" },
-  callbackUrl: "https://www.my-service.com/convention-updated",
-};
-
-const apiConsumer2 = new ApiConsumerBuilder()
-  .withId("my-api-consumer2")
-  .withConventionRight({
-    kinds: ["SUBSCRIPTION"],
-    scope: { agencyIds: [agency2.id] },
-    subscriptions: [
-      {
-        ...callbackParams2,
-        subscribedEvent: "convention.updated",
-        createdAt: new Date().toISOString(),
-        id: "my-subscription-id",
-      },
-    ],
-  })
-  .build();
-
-const apiConsumerWithoutSubscription = new ApiConsumerBuilder()
-  .withId("my-api-consumer-without-subscription")
-  .withConventionRight({
-    kinds: ["SUBSCRIPTION"],
-    scope: { agencyIds: [agency1.id, agency2.id] },
-    subscriptions: [],
-  })
-  .build();
-
-const apiConsumerNotAllowedToBeNotified = new ApiConsumerBuilder()
-  .withId("my-api-consumer-not-allowed")
-  .withConventionRight({
-    kinds: ["READ"],
-    scope: { agencyIds: [agency1.id] },
-    subscriptions: [
-      {
-        ...subscriptionParams,
-        subscribedEvent: "convention.updated",
-        createdAt: new Date().toISOString(),
-        id: "my-subscription-id",
-      },
-    ],
-  })
-  .build();
-
 describe("Broadcast to partners on updated convention", () => {
+  const counsellor1 = new InclusionConnectedUserBuilder()
+    .withId(uuid())
+    .withEmail("counsellor1@email.com")
+    .buildUser();
+  const counsellor2 = new InclusionConnectedUserBuilder()
+    .withId(uuid())
+    .withEmail("counsellor2@email.com")
+    .buildUser();
+  const counsellor3 = new InclusionConnectedUserBuilder()
+    .withId(uuid())
+    .withEmail("counsellor3@email.com")
+    .buildUser();
+  const validator1 = new InclusionConnectedUserBuilder()
+    .withId(uuid())
+    .withEmail("validator1@email.com")
+    .buildUser();
+  const validator2 = new InclusionConnectedUserBuilder()
+    .withId(uuid())
+    .withEmail("validator2@email.com")
+    .buildUser();
+
+  const agency1 = toAgencyWithRights(
+    new AgencyDtoBuilder()
+      .withCounsellorEmails([])
+      .withValidatorEmails([])
+      .withId("agency-1")
+      .build(),
+    {
+      [counsellor1.id]: { roles: ["counsellor"], isNotifiedByEmail: false },
+      [validator1.id]: { roles: ["validator"], isNotifiedByEmail: false },
+    },
+  );
+  const agency2 = toAgencyWithRights(
+    new AgencyDtoBuilder()
+      .withCounsellorEmails([])
+      .withValidatorEmails([])
+      .withId("agency-2")
+      .build(),
+    {
+      [counsellor2.id]: { roles: ["counsellor"], isNotifiedByEmail: false },
+      [validator2.id]: { roles: ["validator"], isNotifiedByEmail: false },
+    },
+  );
+
+  const agencyWithRefersTo = toAgencyWithRights(
+    new AgencyDtoBuilder()
+      .withCounsellorEmails([])
+      .withValidatorEmails([])
+      .withId("agency-with-refers-to")
+      .withKind("autre")
+      .withRefersToAgencyInfo({
+        refersToAgencyId: agency1.id,
+        refersToAgencyName: agency1.name,
+      })
+      .build(),
+    {
+      [counsellor3.id]: { roles: ["counsellor"], isNotifiedByEmail: false },
+      [validator1.id]: { roles: ["validator"], isNotifiedByEmail: false },
+    },
+  );
+
+  const convention1 = new ConventionDtoBuilder()
+    .withId("11111111-ee70-4c90-b3f4-668d492f7395")
+    .withAgencyId(agency1.id)
+    .build();
+
+  const convention2 = new ConventionDtoBuilder()
+    .withId("22222222-ee70-4c90-b3f4-668d492f7395")
+    .withAgencyId(agency2.id)
+    .build();
+
+  const subscriptionParams: SubscriptionParams = {
+    callbackHeaders: { authorization: "my-cb-auth-header" },
+    callbackUrl: "https://www.my-service.com/convention-updated",
+  };
+
+  const apiConsumer1 = new ApiConsumerBuilder()
+    .withId("my-api-consumer1")
+    .withConventionRight({
+      kinds: ["SUBSCRIPTION"],
+      scope: { agencyIds: [agency1.id] },
+      subscriptions: [
+        {
+          ...subscriptionParams,
+          subscribedEvent: "convention.updated",
+          createdAt: new Date().toISOString(),
+          id: "my-subscription-id",
+        },
+      ],
+    })
+    .build();
+
+  const callbackParams2: SubscriptionParams = {
+    callbackHeaders: { authorization: "my-cb-auth-header" },
+    callbackUrl: "https://www.my-service.com/convention-updated",
+  };
+
+  const apiConsumer2 = new ApiConsumerBuilder()
+    .withId("my-api-consumer2")
+    .withConventionRight({
+      kinds: ["SUBSCRIPTION"],
+      scope: { agencyIds: [agency2.id] },
+      subscriptions: [
+        {
+          ...callbackParams2,
+          subscribedEvent: "convention.updated",
+          createdAt: new Date().toISOString(),
+          id: "my-subscription-id",
+        },
+      ],
+    })
+    .build();
+
+  const apiConsumerWithoutSubscription = new ApiConsumerBuilder()
+    .withId("my-api-consumer-without-subscription")
+    .withConventionRight({
+      kinds: ["SUBSCRIPTION"],
+      scope: { agencyIds: [agency1.id, agency2.id] },
+      subscriptions: [],
+    })
+    .build();
+
+  const apiConsumerNotAllowedToBeNotified = new ApiConsumerBuilder()
+    .withId("my-api-consumer-not-allowed")
+    .withConventionRight({
+      kinds: ["READ"],
+      scope: { agencyIds: [agency1.id] },
+      subscriptions: [
+        {
+          ...subscriptionParams,
+          subscribedEvent: "convention.updated",
+          createdAt: new Date().toISOString(),
+          id: "my-subscription-id",
+        },
+      ],
+    })
+    .build();
+
   let uow: InMemoryUnitOfWork;
   let uowPerformer: InMemoryUowPerformer;
   let subscribersGateway: InMemorySubscribersGateway;
@@ -111,18 +172,23 @@ describe("Broadcast to partners on updated convention", () => {
     uowPerformer = new InMemoryUowPerformer(uow);
     subscribersGateway = new InMemorySubscribersGateway();
     timeGateway = new CustomTimeGateway();
-
     broadcastUpdatedConvention = new BroadcastToPartnersOnConventionUpdates(
       uowPerformer,
       subscribersGateway,
       timeGateway,
     );
+    uow.userRepository.users = [
+      counsellor1,
+      counsellor2,
+      counsellor3,
+      validator1,
+      validator2,
+    ];
+    uow.agencyRepository.setAgencies([agency1, agency2, agencyWithRefersTo]);
   });
 
-  it("broadcast updated convention", async () => {
-    uow.agencyRepository.setAgencies([agency1, agency2]);
+  it("broadcast updated convention to agency only", async () => {
     uow.conventionRepository.setConventions([convention1, convention2]);
-
     uow.apiConsumerRepository.consumers = [
       apiConsumer1,
       apiConsumer2,
@@ -143,8 +209,8 @@ describe("Broadcast to partners on updated convention", () => {
               agencyDepartment: agency1.address.departmentCode,
               agencyKind: agency1.kind,
               agencySiret: agency1.agencySiret,
-              agencyCounsellorEmails: agency1.counsellorEmails,
-              agencyValidatorEmails: agency1.validatorEmails,
+              agencyCounsellorEmails: [counsellor1.email],
+              agencyValidatorEmails: [validator1.email],
             },
           },
         },
@@ -168,8 +234,8 @@ describe("Broadcast to partners on updated convention", () => {
               agencyDepartment: agency2.address.departmentCode,
               agencyKind: agency2.kind,
               agencySiret: agency2.agencySiret,
-              agencyCounsellorEmails: agency2.counsellorEmails,
-              agencyValidatorEmails: agency2.validatorEmails,
+              agencyCounsellorEmails: [counsellor2.email],
+              agencyValidatorEmails: [validator2.email],
             },
           },
         },
@@ -184,9 +250,7 @@ describe("Broadcast to partners on updated convention", () => {
   });
 
   it("save webhook error", async () => {
-    uow.agencyRepository.setAgencies([agency1]);
     uow.conventionRepository.setConventions([convention1]);
-
     uow.apiConsumerRepository.consumers = [apiConsumer1];
 
     const now = new Date("2024-03-04T10:00:00Z");
@@ -234,9 +298,7 @@ describe("Broadcast to partners on updated convention", () => {
   });
 
   it("save feedback success", async () => {
-    uow.agencyRepository.setAgencies([agency1]);
     uow.conventionRepository.setConventions([convention1]);
-
     uow.apiConsumerRepository.consumers = [apiConsumer1];
 
     const now = new Date("2024-03-04T10:00:00Z");
@@ -313,9 +375,7 @@ describe("Broadcast to partners on updated convention", () => {
         })
         .build();
 
-    uow.agencyRepository.setAgencies([agency1, agencyWithRefersTo]);
     uow.conventionRepository.setConventions([conventionFromAgencyWithRefersTo]);
-
     uow.apiConsumerRepository.consumers = [
       apiConsumer1,
       apiConsumerWithSubscriptionOnAgencyWithReferesTo,
@@ -338,8 +398,8 @@ describe("Broadcast to partners on updated convention", () => {
               agencyDepartment: agencyWithRefersTo.address.departmentCode,
               agencyKind: agencyWithRefersTo.kind,
               agencySiret: agencyWithRefersTo.agencySiret,
-              agencyCounsellorEmails: agencyWithRefersTo.counsellorEmails,
-              agencyValidatorEmails: agencyWithRefersTo.validatorEmails,
+              agencyCounsellorEmails: [counsellor3.email],
+              agencyValidatorEmails: [validator1.email],
               agencyRefersTo: {
                 id: agency1.id,
                 kind: agency1.kind,
@@ -360,8 +420,8 @@ describe("Broadcast to partners on updated convention", () => {
               agencyDepartment: agencyWithRefersTo.address.departmentCode,
               agencyKind: agencyWithRefersTo.kind,
               agencySiret: agencyWithRefersTo.agencySiret,
-              agencyCounsellorEmails: agencyWithRefersTo.counsellorEmails,
-              agencyValidatorEmails: agencyWithRefersTo.validatorEmails,
+              agencyCounsellorEmails: [counsellor3.email],
+              agencyValidatorEmails: [validator1.email],
               agencyRefersTo: {
                 id: agency1.id,
                 kind: agency1.kind,
