64aa1fe5b5b3a0719fd252b00b78e2044dba1f8c
Jérôme Burkard <burkardjerome@gmail.com>
2024-10-22 15:12:10 +0200

    add slug to admin routes

--- shared/src/admin/adminTabs.ts
+++ shared/src/admin/adminTabs.ts
@@ -1,33 +1,42 @@
 import { keys } from "ramda";
 
 type AdminTabProps = {
+  slug: string;
   isVisible: (env: "dev" | "staging" | "production" | "local") => boolean;
 };
 
 export const adminTabs = {
   adminConventions: {
+    slug: "conventions",
     isVisible: () => true,
   },
   adminEvents: {
+    slug: "events",
     isVisible: () => true,
   },
   adminAgencies: {
+    slug: "agencies",
     isVisible: () => true,
   },
   adminUsers: {
+    slug: "users",
     isVisible: () => true,
   },
   adminEstablishments: {
+    slug: "establishments",
     isVisible: () => true,
   },
   adminNotifications: {
+    slug: "notifications",
     isVisible: (env: "dev" | "staging" | "production" | "local") =>
       env !== "production",
   },
   adminEmailPreview: {
+    slug: "email-preview",
     isVisible: () => true,
   },
   adminTechnicalOptions: {
+    slug: "technical-options",
     isVisible: () => true,
   },
 } satisfies Record<string, AdminTabProps>;
