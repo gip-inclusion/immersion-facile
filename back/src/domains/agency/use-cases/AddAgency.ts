db0b4363711429c6014b1466fb87a8620c5d306c
Benjamin Bohec <bbohec.pro@gmail.com>
2024-10-23 14:10:35 +0200

    repair usecase AddAgency

--- back/src/domains/agency/use-cases/AddAgency.ts
+++ back/src/domains/agency/use-cases/AddAgency.ts
@@ -1,48 +1,77 @@
+import { toPairs } from "ramda";
 import {
-  AgencyDto,
   AgencyId,
+  AgencyRole,
   CreateAgencyDto,
-  Email,
+  UserId,
   createAgencySchema,
   errors,
 } from "shared";
 import { TransactionalUseCase } from "../../core/UseCase";
+import { createOrGetUserIdByEmail } from "../../core/authentication/inclusion-connect/entities/user.helper";
 import { CreateNewEvent } from "../../core/events/ports/EventBus";
 import { SiretGateway } from "../../core/sirene/ports/SirenGateway";
+import { TimeGateway } from "../../core/time-gateway/ports/TimeGateway";
 import { UnitOfWork } from "../../core/unit-of-work/ports/UnitOfWork";
 import { UnitOfWorkPerformer } from "../../core/unit-of-work/ports/UnitOfWorkPerformer";
+import { UuidGenerator } from "../../core/uuid-generator/ports/UuidGenerator";
 import { throwConflictErrorOnSimilarAgencyFound } from "../entities/Agency";
+import {
+  AgencyUsersRights,
+  AgencyWithUsersRights,
+} from "../ports/AgencyRepository";
 
 export class AddAgency extends TransactionalUseCase<CreateAgencyDto, void> {
   protected inputSchema = createAgencySchema;
 
-  readonly #createNewEvent: CreateNewEvent;
-
   constructor(
     uowPerformer: UnitOfWorkPerformer,
-    createNewEvent: CreateNewEvent,
+    private createNewEvent: CreateNewEvent,
     private siretGateway: SiretGateway,
+    private timeGateway: TimeGateway,
+    private uuidGenerator: UuidGenerator,
   ) {
     super(uowPerformer);
-    this.#createNewEvent = createNewEvent;
   }
 
   protected async _execute(
-    params: CreateAgencyDto,
+    { validatorEmails, counsellorEmails, ...rest }: CreateAgencyDto,
     uow: UnitOfWork,
   ): Promise<void> {
-    const agency: AgencyDto = {
-      ...params,
-      validatorEmails: params.refersToAgencyId
-        ? await this.#getReferedAgencyValidatorEmails(
-            uow,
-            params.refersToAgencyId,
-          )
-        : params.validatorEmails,
+    const validatorUserIdForAgency = rest.refersToAgencyId
+      ? await this.#getReferedAgencyValidatorUserIds(uow, rest.refersToAgencyId)
+      : await Promise.all(
+          validatorEmails.map((email) =>
+            createOrGetUserIdByEmail(
+              uow,
+              this.timeGateway,
+              this.uuidGenerator,
+              email,
+            ),
+          ),
+        );
+
+    const counsellorUserIdForAgency = await Promise.all(
+      counsellorEmails.map((email) =>
+        createOrGetUserIdByEmail(
+          uow,
+          this.timeGateway,
+          this.uuidGenerator,
+          email,
+        ),
+      ),
+    );
+
+    const agency: AgencyWithUsersRights = {
+      ...rest,
       status: "needsReview",
-      questionnaireUrl: params.questionnaireUrl,
+      questionnaireUrl: rest.questionnaireUrl,
       codeSafir: null,
       rejectionJustification: null,
+      usersRights: this.#makeUserRights(
+        validatorUserIdForAgency,
+        counsellorUserIdForAgency,
+      ),
     };
 
     await throwConflictErrorOnSimilarAgencyFound({ uow, agency });
@@ -57,7 +86,7 @@ export class AddAgency extends TransactionalUseCase<CreateAgencyDto, void> {
     await Promise.all([
       uow.agencyRepository.insert(agency),
       uow.outboxRepository.save(
-        this.#createNewEvent({
+        this.createNewEvent({
           topic: "NewAgencyAdded",
           payload: { agencyId: agency.id, triggeredBy: null },
         }),
@@ -65,13 +94,52 @@ export class AddAgency extends TransactionalUseCase<CreateAgencyDto, void> {
     ]);
   }
 
-  async #getReferedAgencyValidatorEmails(
+  #makeUserRights(
+    validatorUserIdsForAgency: UserId[],
+    counsellorUserIdsForAgency: UserId[],
+  ): AgencyUsersRights {
+    const validatorsAndCounsellors = validatorUserIdsForAgency.filter((id) =>
+      counsellorUserIdsForAgency.includes(id),
+    );
+    const validators = validatorUserIdsForAgency.filter(
+      (id) => !validatorsAndCounsellors.includes(id),
+    );
+    const counsellors = counsellorUserIdsForAgency.filter(
+      (id) => !validatorsAndCounsellors.includes(id),
+    );
+    return {
+      ...buildAgencyUsersRights(validatorsAndCounsellors, [
+        "validator",
+        "counsellor",
+      ]),
+      ...buildAgencyUsersRights(validators, ["validator"]),
+      ...buildAgencyUsersRights(counsellors, ["counsellor"]),
+    };
+  }
+
+  async #getReferedAgencyValidatorUserIds(
     uow: UnitOfWork,
     refersToAgencyId: AgencyId,
-  ): Promise<Email[]> {
+  ): Promise<UserId[]> {
     const referedAgency = await uow.agencyRepository.getById(refersToAgencyId);
     if (!referedAgency)
       throw errors.agency.notFound({ agencyId: refersToAgencyId });
-    return referedAgency.validatorEmails;
+    return toPairs(referedAgency.usersRights)
+      .filter(([_, right]) => right?.roles.includes("validator"))
+      .map(([id]) => id);
   }
 }
+const buildAgencyUsersRights = (
+  userIdsBothValidatorAndCounsellor: UserId[],
+  roles: AgencyRole[],
+): AgencyUsersRights =>
+  userIdsBothValidatorAndCounsellor.reduce<AgencyUsersRights>(
+    (acc, id) => ({
+      ...acc,
+      [id]: {
+        roles,
+        isNotifiedByEmail: false,
+      },
+    }),
+    {},
+  );
