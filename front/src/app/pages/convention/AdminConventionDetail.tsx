931a7bb4eb6553436b11bba18ae2c3cc6461e90d
Jérôme Burkard <burkardjerome@gmail.com>
2024-10-23 13:35:14 +0200

    making manage convention admin sub part of admin routes (/admin/conventions/:conventionId)

---
+++ front/src/app/pages/convention/AdminConventionDetail.tsx
@@ -0,0 +1,32 @@
+import { Alert } from "@codegouvfr/react-dsfr/Alert";
+import React from "react";
+import { useAdminToken } from "src/app/hooks/jwt.hooks";
+import { routes } from "src/app/routes/routes";
+import { Route } from "type-route";
+import { ConventionManageContent } from "../../components/admin/conventions/ConventionManageContent";
+
+type ConventionManageAdminPageProps = {
+  route: Route<typeof routes.adminConventionDetail>;
+};
+
+export const AdminConventionDetail = ({
+  route,
+}: ConventionManageAdminPageProps) => {
+  const conventionId = route.params.conventionId;
+  const backOfficeJwt = useAdminToken();
+
+  // ... Récupérer le JWT convention ou bien { convention, fetchConventionError, submitFeedback, isLoading } à partir d'un admin qui a le conventionId
+
+  return backOfficeJwt ? (
+    <ConventionManageContent
+      jwtParams={{ jwt: backOfficeJwt, kind: "backoffice" }}
+      conventionId={conventionId}
+    />
+  ) : (
+    <Alert
+      severity="error"
+      title="Non autorisé"
+      description="Cette page est reservée aux administrateurs d'Immersion Facilitée."
+    />
+  );
+};
