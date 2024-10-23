363e5d646d40039b1f621056ace02eb1d1715c0c
Benjamin Bohec <bbohec.pro@gmail.com>
2024-10-23 14:01:55 +0200

    user utilities createOrGetUserIdByEmail

---
+++ back/src/domains/core/authentication/inclusion-connect/entities/user.helper.ts
@@ -0,0 +1,39 @@
+import { Email, OAuthGatewayProvider, UserId } from "shared";
+import { TimeGateway } from "../../../time-gateway/ports/TimeGateway";
+import { UnitOfWork } from "../../../unit-of-work/ports/UnitOfWork";
+import { UuidGenerator } from "../../../uuid-generator/ports/UuidGenerator";
+import { makeProvider } from "../port/OAuthGateway";
+
+export const createOrGetUserIdByEmail = async (
+  uow: UnitOfWork,
+  timeGateway: TimeGateway,
+  uuidGenerator: UuidGenerator,
+  email: Email,
+): Promise<UserId> => {
+  const provider = await makeProvider(uow);
+  const user = await uow.userRepository.findByEmail(email, provider);
+  return user
+    ? user.id
+    : createUser(uow, provider, uuidGenerator.new(), email, timeGateway.now());
+};
+
+const createUser = async (
+  uow: UnitOfWork,
+  provider: OAuthGatewayProvider,
+  userId: UserId,
+  email: Email,
+  createdAt: Date,
+): Promise<UserId> => {
+  await uow.userRepository.save(
+    {
+      id: userId,
+      email,
+      createdAt: createdAt.toISOString(),
+      externalId: null,
+      firstName: "Non fourni",
+      lastName: "Non fourni",
+    },
+    provider,
+  );
+  return userId;
+};
