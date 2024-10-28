d279ff659ed3f72c697ebaed71b0475c86f27436
Benjamin Bohec <bbohec.pro@gmail.com>
2024-10-28 17:37:18 +0100

    fixup UserRepository

--- back/src/domains/inclusion-connected-users/use-cases/GetUsers.ts
+++ back/src/domains/inclusion-connected-users/use-cases/GetUsers.ts
@@ -6,6 +6,7 @@ import {
   getUsersFiltersSchema,
 } from "shared";
 import { createTransactionalUseCase } from "../../core/UseCase";
+import { makeProvider } from "../../core/authentication/inclusion-connect/port/OAuthGateway";
 import { UnitOfWork } from "../../core/unit-of-work/ports/UnitOfWork";
 import { throwIfNotAdmin } from "../helpers/throwIfIcUserNotBackofficeAdmin";
 
@@ -21,7 +22,10 @@ export const makeGetUsers = createTransactionalUseCase<
   },
   async ({ uow, currentUser, inputParams }) => {
     throwIfNotAdmin(currentUser);
-    const users = await uow.userRepository.getUsers(inputParams);
+    const users = await uow.userRepository.getUsers(
+      inputParams,
+      await makeProvider(uow),
+    );
     return Promise.all(users.map((user) => userToUserInList(uow, user)));
   },
 );
