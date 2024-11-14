8789e01dd9a02402c831c4058d71a7e5ddc2c9d2
celineung <celine.ung.pro@gmail.com>
2024-11-14 14:07:41 +0100

    update storybook

--- libs/react-design-system/.storybook/main.js
+++ libs/react-design-system/.storybook/main.js
@@ -1,9 +1,10 @@
+import { dirname, join } from "path";
 module.exports = {
   stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
   addons: [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-interactions",
+    getAbsolutePath("@storybook/addon-links"),
+    getAbsolutePath("@storybook/addon-essentials"),
+    getAbsolutePath("@storybook/addon-interactions"),
     {
       name: "@storybook/addon-styling",
       options: {
@@ -12,10 +13,10 @@ module.exports = {
         },
       },
     },
-    "@storybook/addon-mdx-gfm",
+    getAbsolutePath("@storybook/addon-mdx-gfm"),
   ],
   framework: {
-    name: "@storybook/react-vite",
+    name: getAbsolutePath("@storybook/react-vite"),
     options: {},
   },
   docs: {
@@ -23,3 +24,7 @@ module.exports = {
   },
   staticDirs: ["../public"],
 };
+
+function getAbsolutePath(value) {
+  return dirname(require.resolve(join(value, "package.json")));
+}
