4af5138d85cf22daee1cf9df121cce456a236566
clement-duport <clement.duport@beta.gouv.fr>
2024-11-26 13:37:19 +0100

    update get inclusion conected users usecase to authorize agency admin

--- back/src/domains/inclusion-connected-users/use-cases/GetInclusionConnectedUsers.ts
+++ back/src/domains/inclusion-connected-users/use-cases/GetInclusionConnectedUsers.ts
@@ -1,12 +1,13 @@
 import {
   InclusionConnectedUser,
   WithUserFilters,
+  errors,
+  isWithAgencyId,
   withUserFiltersSchema,
 } from "shared";
 import { TransactionalUseCase } from "../../core/UseCase";
 import { UnitOfWork } from "../../core/unit-of-work/ports/UnitOfWork";
 import { getIcUsersByUserIds } from "../helpers/inclusionConnectedUser.helper";
-import { throwIfNotAdmin } from "../helpers/throwIfIcUserNotBackofficeAdmin";
 
 export class GetInclusionConnectedUsers extends TransactionalUseCase<
   WithUserFilters,
@@ -20,12 +21,35 @@ export class GetInclusionConnectedUsers extends TransactionalUseCase<
     uow: UnitOfWork,
     currentUser?: InclusionConnectedUser,
   ): Promise<InclusionConnectedUser[]> {
-    throwIfNotAdmin(currentUser);
+    throwIfNotAgencyAdminOrBackofficeAdmin(filters, currentUser);
+
     const userIds =
       await uow.agencyRepository.getUserIdWithAgencyRightsByFilters(filters);
+
     const icUsers = await getIcUsersByUserIds(uow, userIds);
+
     return icUsers.sort((a, b) =>
       a.lastName.toLowerCase() < b.lastName.toLowerCase() ? -1 : 1,
     );
   }
 }
+
+const throwIfNotAgencyAdminOrBackofficeAdmin = (
+  filters: WithUserFilters,
+  currentUser?: InclusionConnectedUser,
+): void => {
+  if (!currentUser) throw errors.user.unauthorized();
+  if (currentUser.isBackofficeAdmin) return;
+  if (!isWithAgencyId(filters))
+    throw errors.user.forbidden({ userId: currentUser.id });
+
+  const hasPermission = currentUser.agencyRights.some(
+    (agencyRight) =>
+      agencyRight.agency.id === filters.agencyId &&
+      agencyRight.roles.includes("agency-admin"),
+  );
+
+  if (!hasPermission) {
+    throw errors.user.forbidden({ userId: currentUser.id });
+  }
+};
