84f4f17f93c4e196708257c58a9b93584afde4f4
Benjamin Bohec <bbohec.pro@gmail.com>
2024-12-13 14:25:15 +0100

    send emails only to notified users of agency and also if refered to agency, includes notified admins of this agency

--- back/src/domains/convention/use-cases/SendEmailWhenAgencyIsRejected.unit.test.ts
+++ back/src/domains/convention/use-cases/SendEmailWhenAgencyIsRejected.unit.test.ts
@@ -19,17 +19,31 @@ import {
 import { UuidV4Generator } from "../../core/uuid-generator/adapters/UuidGeneratorImplementations";
 import { SendEmailWhenAgencyIsRejected } from "./SendEmailWhenAgencyIsRejected";
 
-describe("SendEmailWhenAgencyIsRejected", () => {
+describe("Feature - SendEmailWhenAgencyIsRejected", () => {
   const agency = AgencyDtoBuilder.create()
     .withName("just-rejected-agency")
     .withLogoUrl("https://logo.com")
     .withStatus("rejected")
     .withRejectionJustification("rejection justification")
     .build();
-  const validator = new InclusionConnectedUserBuilder()
-    .withId("validator")
-    .withEmail("validator@email.com")
-    .build();
+
+  const [
+    user1,
+    user2,
+    user3,
+    user4,
+    user5,
+    user6,
+    user7,
+    user8,
+    user9,
+    user10,
+  ] = [...Array(10).keys()].map((i) =>
+    new InclusionConnectedUserBuilder()
+      .withId(`user${i + 1}`)
+      .withEmail(`user${i + 1}@email.com`)
+      .build(),
+  );
 
   let uow: InMemoryUnitOfWork;
   let useCase: SendEmailWhenAgencyIsRejected;
@@ -48,74 +62,278 @@ describe("SendEmailWhenAgencyIsRejected", () => {
         new CustomTimeGateway(),
       ),
     );
+
+    uow.userRepository.users = [
+      user1,
+      user2,
+      user3,
+      user4,
+      user5,
+      user6,
+      user7,
+      user8,
+      user9,
+      user10,
+    ];
   });
 
-  it("Sends an email to validators with agency name", async () => {
-    // Prepare
-    uow.agencyRepository.agencies = [
-      toAgencyWithRights(agency, {
-        [validator.id]: { isNotifiedByEmail: true, roles: ["validator"] },
-      }),
-    ];
-    uow.userRepository.users = [validator];
-
-    // Act
-    await useCase.execute({ agencyId: agency.id });
-
-    // Assert
-    expectSavedNotificationsAndEvents({
-      emails: [
-        {
-          kind: "AGENCY_WAS_REJECTED",
-          recipients: [validator.email],
-          params: {
-            agencyName: agency.name,
-            // biome-ignore lint/style/noNonNullAssertion: <explanation>
-            rejectionJustification: agency.rejectionJustification!,
+  describe("With success feedback scenarios", () => {
+    it("If the agency doesn't refer to another agency then the feature sends a AGENCY_WAS_REJECTED notification to all agency's users that have notification enabled.", async () => {
+      // Prepare
+      uow.agencyRepository.agencies = [
+        toAgencyWithRights(agency, {
+          [user1.id]: {
+            isNotifiedByEmail: true,
+            roles: ["agency-admin"],
           },
-        },
-      ],
+          [user2.id]: {
+            isNotifiedByEmail: false,
+            roles: ["agency-admin"],
+          },
+          [user3.id]: {
+            isNotifiedByEmail: true,
+            roles: ["validator", "agency-admin"],
+          },
+          [user4.id]: {
+            isNotifiedByEmail: false,
+            roles: ["validator"],
+          },
+          [user5.id]: {
+            isNotifiedByEmail: true,
+            roles: ["counsellor"],
+          },
+          [user6.id]: {
+            isNotifiedByEmail: false,
+            roles: ["counsellor"],
+          },
+          [user7.id]: {
+            isNotifiedByEmail: true,
+            roles: ["agency-viewer"],
+          },
+        }),
+      ];
+
+      // Act
+      await useCase.execute({ agencyId: agency.id });
+
+      // Assert
+      expectSavedNotificationsAndEvents({
+        emails: [
+          {
+            kind: "AGENCY_WAS_REJECTED",
+            recipients: [user1.email, user3.email, user5.email, user7.email],
+            params: {
+              agencyName: agency.name,
+              // biome-ignore lint/style/noNonNullAssertion: <explanation>
+              rejectionJustification: agency.rejectionJustification!,
+            },
+          },
+        ],
+      });
+    });
+
+    it("If the agency A refers to agency B then the feature sends a AGENCY_WAS_REJECTED notification to all agency A's users that have notification enabled and agency B's admins that have notification enabled.", async () => {
+      // Prepare
+      const agencyWithRefersTo = AgencyDtoBuilder.create()
+        .withId("agencyWithRefersTo")
+        .withName("just-rejected-agency-refered-to-other-agency")
+        .withLogoUrl("https://logo-refers-to.com")
+        .withStatus("rejected")
+        .withRejectionJustification(
+          "rejection justification for agency with refers to",
+        )
+        .withRefersToAgencyInfo({
+          refersToAgencyId: agency.id,
+          refersToAgencyName: agency.name,
+        })
+        .build();
+
+      uow.agencyRepository.agencies = [
+        toAgencyWithRights(agency, {
+          [user1.id]: {
+            isNotifiedByEmail: true,
+            roles: ["agency-admin"],
+          },
+          [user2.id]: {
+            isNotifiedByEmail: false,
+            roles: ["agency-admin"],
+          },
+          [user3.id]: {
+            isNotifiedByEmail: true,
+            roles: ["counsellor"],
+          },
+        }),
+        toAgencyWithRights(agencyWithRefersTo, {
+          [user4.id]: {
+            isNotifiedByEmail: true,
+            roles: ["agency-admin"],
+          },
+          [user5.id]: {
+            isNotifiedByEmail: false,
+            roles: ["agency-admin"],
+          },
+          [user6.id]: {
+            isNotifiedByEmail: true,
+            roles: ["validator"],
+          },
+          [user7.id]: {
+            isNotifiedByEmail: false,
+            roles: ["validator"],
+          },
+          [user8.id]: {
+            isNotifiedByEmail: true,
+            roles: ["counsellor"],
+          },
+          [user9.id]: {
+            isNotifiedByEmail: false,
+            roles: ["counsellor"],
+          },
+          [user10.id]: {
+            isNotifiedByEmail: true,
+            roles: ["agency-viewer"],
+          },
+        }),
+      ];
+
+      // Act
+      await useCase.execute({ agencyId: agencyWithRefersTo.id });
+
+      // Assert
+      expectSavedNotificationsAndEvents({
+        emails: [
+          {
+            kind: "AGENCY_WAS_REJECTED",
+            recipients: [
+              user1.email,
+              user4.email,
+              user6.email,
+              user8.email,
+              user10.email,
+            ],
+            params: {
+              agencyName: agencyWithRefersTo.name,
+              rejectionJustification:
+                // biome-ignore lint/style/noNonNullAssertion: <explanation>
+                agencyWithRefersTo.rejectionJustification!,
+            },
+          },
+        ],
+      });
     });
   });
 
-  it("On missing agency", async () => {
-    // Prepare
-    uow.agencyRepository.agencies = [];
-    uow.userRepository.users = [validator];
+  describe("With error feedback scenarios", () => {
+    it("If the agency is missing then the feature raise a not found error.", async () => {
+      // Prepare
+      uow.agencyRepository.agencies = [];
 
-    // Act
-    await expectPromiseToFailWithError(
-      useCase.execute({ agencyId: agency.id }),
-      errors.agency.notFound({ agencyId: agency.id }),
-    );
+      // Act
+      await expectPromiseToFailWithError(
+        useCase.execute({ agencyId: agency.id }),
+        errors.agency.notFound({ agencyId: agency.id }),
+      );
 
-    // Assert
-    expectSavedNotificationsAndEvents({
-      emails: [],
+      // Assert
+      expectSavedNotificationsAndEvents({
+        emails: [],
+      });
     });
-  });
 
-  it("On agency not rejected", async () => {
-    // Prepare
-    uow.agencyRepository.agencies = [
-      toAgencyWithRights(
-        new AgencyDtoBuilder(agency).withRejectionJustification(null).build(),
-        {
-          [validator.id]: { isNotifiedByEmail: true, roles: ["validator"] },
-        },
-      ),
-    ];
-    uow.userRepository.users = [validator];
+    it("If the agency don't have a rejection justification then the feature raise a not rejected error.", async () => {
+      // Prepare
+      uow.agencyRepository.agencies = [
+        toAgencyWithRights(
+          new AgencyDtoBuilder(agency).withRejectionJustification(null).build(),
+          {
+            [user4.id]: {
+              isNotifiedByEmail: true,
+              roles: ["validator"],
+            },
+          },
+        ),
+      ];
 
-    // Act
-    await expectPromiseToFailWithError(
-      useCase.execute({ agencyId: agency.id }),
-      errors.agency.notRejected({ agencyId: agency.id }),
-    );
+      // Act
+      await expectPromiseToFailWithError(
+        useCase.execute({ agencyId: agency.id }),
+        errors.agency.notRejected({ agencyId: agency.id }),
+      );
 
-    // Assert
-    expectSavedNotificationsAndEvents({
-      emails: [],
+      // Assert
+      expectSavedNotificationsAndEvents({
+        emails: [],
+      });
+    });
+
+    it("If the agency have a rejection justification but don't have rejected status then the feature raise a bad agency status error.", async () => {
+      const agencyActiveWithJustification = new AgencyDtoBuilder(agency)
+        .withStatus("active")
+        .withRejectionJustification("justif")
+        .build();
+      // Prepare
+      uow.agencyRepository.agencies = [
+        toAgencyWithRights(agencyActiveWithJustification, {
+          [user4.id]: {
+            isNotifiedByEmail: true,
+            roles: ["validator"],
+          },
+          [user2.id]: {
+            isNotifiedByEmail: true,
+            roles: ["counsellor"],
+          },
+        }),
+      ];
+
+      uow.userRepository.users = [];
+
+      // Act
+      await expectPromiseToFailWithError(
+        useCase.execute({ agencyId: agencyActiveWithJustification.id }),
+        errors.agency.invalidStatus({
+          id: agencyActiveWithJustification.id,
+          actual: agencyActiveWithJustification.status,
+          expected: "rejected",
+        }),
+      );
+
+      // Assert
+      expectSavedNotificationsAndEvents({
+        emails: [],
+      });
+    });
+
+    it("If some users is missing then the feature raise a users not found error.", async () => {
+      // Prepare
+      uow.agencyRepository.agencies = [
+        toAgencyWithRights(
+          new AgencyDtoBuilder(agency)
+            .withRejectionJustification("justif")
+            .build(),
+          {
+            [user4.id]: {
+              isNotifiedByEmail: true,
+              roles: ["validator"],
+            },
+            [user2.id]: {
+              isNotifiedByEmail: true,
+              roles: ["counsellor"],
+            },
+          },
+        ),
+      ];
+
+      uow.userRepository.users = [];
+
+      // Act
+      await expectPromiseToFailWithError(
+        useCase.execute({ agencyId: agency.id }),
+        errors.users.notFound({ userIds: [user4.id, user2.id] }),
+      );
+
+      // Assert
+      expectSavedNotificationsAndEvents({
+        emails: [],
+      });
     });
   });
 });
