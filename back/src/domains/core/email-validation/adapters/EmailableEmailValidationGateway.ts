d31fd00cd446a12a15156fe7e72de4829f41b346
Jérôme Burkard <burkardjerome@gmail.com>
2024-12-16 17:52:51 +0100

    implement cache for emailable

--- back/src/domains/core/email-validation/adapters/EmailableEmailValidationGateway.ts
+++ back/src/domains/core/email-validation/adapters/EmailableEmailValidationGateway.ts
@@ -2,13 +2,19 @@ import Bottleneck from "bottleneck";
 import { ValidateEmailFeedback } from "shared";
 import { HttpClient } from "shared-routes";
 import { createLogger } from "../../../../utils/logger";
+import { WithCache } from "../../caching-gateway/port/WithCache";
 import { EmailValidationGetaway } from "../ports/EmailValidationGateway";
 import { EmailableApiKey } from "./EmailableEmailValidationGateway.dto";
-import { EmailableValidationRoutes } from "./EmailableEmailValidationGateway.routes";
+import {
+  EmailableValidationRoutes,
+  emailableValidationRoutes,
+} from "./EmailableEmailValidationGateway.routes";
 
 const logger = createLogger(__filename);
 
 const emailableVerifyEmailMaxRatePerSeconds = 25;
+
+const thirtyDaysInHours = 24 * 30;
 
 export class EmailableEmailValidationGateway implements EmailValidationGetaway {
   #limiter = new Bottleneck({
@@ -20,40 +26,54 @@ export class EmailableEmailValidationGateway implements EmailValidationGetaway {
   constructor(
     private readonly httpClient: HttpClient<EmailableValidationRoutes>,
     private readonly emailableApiKey: EmailableApiKey,
+    private readonly withCache: WithCache,
   ) {}
 
-  public async validateEmail(email: string): Promise<ValidateEmailFeedback> {
-    return this.#limiter.schedule(() =>
-      this.httpClient
-        .validateEmail({
-          queryParams: {
-            email,
-            api_key: this.emailableApiKey,
-          },
-        })
-        .then((response): ValidateEmailFeedback => {
-          if (response.status === 200)
-            return {
-              status: response.body.reason ?? "unexpected_error",
-              proposal: response.body.did_you_mean ?? null,
-            };
+  public async validateEmail(
+    emailInParams: string,
+  ): Promise<ValidateEmailFeedback> {
+    const sanitizedEmail = emailInParams.toLowerCase().trim();
 
-          return {
-            status: "service_unavailable",
-            proposal: null,
-          };
-        })
-        .catch((error): ValidateEmailFeedback => {
-          logger.error({
-            error,
-            message: `validateEmail => Error while calling emailable API : ${error.message}`,
-          });
+    return this.withCache({
+      logParams: {
+        partner: "emailable",
+        route: emailableValidationRoutes.validateEmail,
+      },
+      overrideCacheDurationInHours: thirtyDaysInHours,
+      getCacheKey: ({ email }) => `emailable_${email}`,
+      cb: (email) =>
+        this.#limiter.schedule(() =>
+          this.httpClient
+            .validateEmail({
+              queryParams: {
+                email,
+                api_key: this.emailableApiKey,
+              },
+            })
+            .then((response): ValidateEmailFeedback => {
+              if (response.status === 200)
+                return {
+                  status: response.body.reason ?? "unexpected_error",
+                  proposal: response.body.did_you_mean ?? null,
+                };
 
-          return {
-            status: "service_unavailable",
-            proposal: null,
-          };
-        }),
-    );
+              return {
+                status: "service_unavailable",
+                proposal: null,
+              };
+            })
+            .catch((error): ValidateEmailFeedback => {
+              logger.error({
+                error,
+                message: `validateEmail => Error while calling emailable API : ${error.message}`,
+              });
+
+              return {
+                status: "service_unavailable",
+                proposal: null,
+              };
+            }),
+        ),
+    })(sanitizedEmail);
   }
 }
