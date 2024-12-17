ab2ed7b42a885d9aa8fda323a0e9bcca1978d927
Jérôme Burkard <burkardjerome@gmail.com>
2024-12-17 15:37:32 +0100

    implement query to update establishments that do not answer

--- back/src/domains/establishment/adapters/PgEstablishmentAggregateRepository.sql.ts
+++ back/src/domains/establishment/adapters/PgEstablishmentAggregateRepository.sql.ts
@@ -190,3 +190,52 @@ export const updateAllEstablishmentScoresQuery = async (
     })
     .execute();
 };
+
+export const deactivateUnresponsiveEstablishmentsQuery = (db: KyselyDb) =>
+  db
+    .with("establishment_discussions", (qb) =>
+      qb
+        .selectFrom("discussions as d")
+        .innerJoin("exchanges as e", "d.id", "e.discussion_id")
+        .select([
+          "d.siret",
+          sql<number>`COUNT(DISTINCT d.id)`.as("total_discussions"),
+          sql<number>`COUNT(DISTINCT CASE WHEN e.sender = 'establishment'::exchange_role THEN d.id END)`.as(
+            "answered_discussions",
+          ),
+        ])
+        .groupBy("d.siret"),
+    )
+    .with("recent_conventions", (qb) =>
+      qb
+        .selectFrom("conventions as c")
+        .select("c.siret")
+        .where("c.status", "=", "ACCEPTED_BY_VALIDATOR")
+        .where("c.date_start", ">=", sql<Date>`NOW() - INTERVAL '5 months'`)
+        .groupBy("c.siret"),
+    )
+    .updateTable("establishments as e")
+    .set({
+      max_contacts_per_month: 0,
+      status: "DEACTIVATED_FOR_LACK_OF_RESPONSES",
+      status_updated_at: sql`NOW()`,
+    })
+    .where((eb) =>
+      eb.exists(
+        eb
+          .selectFrom("establishment_discussions as ed")
+          .whereRef("ed.siret", "=", "e.siret")
+          .where("ed.total_discussions", ">=", 50)
+          .where("ed.answered_discussions", "=", 0),
+      ),
+    )
+    .where((eb) =>
+      eb.not(
+        eb.exists(
+          eb
+            .selectFrom("recent_conventions as rc")
+            .whereRef("rc.siret", "=", "e.siret"),
+        ),
+      ),
+    )
+    .execute();
