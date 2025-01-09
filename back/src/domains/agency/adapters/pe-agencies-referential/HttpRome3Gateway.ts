903592c52f917e3adfa243e0f67a86ba93016a03
Jérôme Burkard <burkardjerome@gmail.com>
2025-01-09 11:33:49 +0100

    make HttpRome3Gateway work

--- back/src/domains/agency/adapters/pe-agencies-referential/HttpRome3Gateway.ts
+++ back/src/domains/agency/adapters/pe-agencies-referential/HttpRome3Gateway.ts
@@ -1,9 +1,4 @@
-import {
-  AbsoluteUrl,
-  AppellationDto,
-  RomeDto,
-  withAuthorizationHeaders,
-} from "shared";
+import { AbsoluteUrl, AppellationDto, withAuthorizationHeaders } from "shared";
 import { HttpClient, defineRoute, defineRoutes } from "shared-routes";
 import { z } from "zod";
 import { PoleEmploiGateway } from "../../../convention/ports/PoleEmploiGateway";
@@ -13,25 +8,16 @@ export type AppellationWithShortLabel = AppellationDto & {
   appellationLabelShort: string;
 };
 
-interface Rome4Gateway {
-  getAllRomes(): Promise<RomeDto[]>;
+interface Rome3Gateway {
   getAllAppellations(): Promise<AppellationWithShortLabel[]>;
 }
 
-type Rome4Routes = ReturnType<typeof makeRome4Routes>;
-export const makeRome4Routes = (peApiUrl: AbsoluteUrl) =>
+type Rome4Routes = ReturnType<typeof makeRome3Routes>;
+export const makeRome3Routes = (peApiUrl: AbsoluteUrl) =>
   defineRoutes({
-    getRomes: defineRoute({
-      method: "get",
-      url: `${peApiUrl}/partenaire/rome-metiers/v1/metiers/metier?champs=code,libelle`,
-      ...withAuthorizationHeaders,
-      responses: {
-        200: z.array(z.object({ code: z.string(), libelle: z.string() })),
-      },
-    }),
     getAppellations: defineRoute({
       method: "get",
-      url: `${peApiUrl}/partenaire/rome-metiers/v1/metiers/appellation?champs=code,libelle,libelleCourt,metier(code)`,
+      url: `${peApiUrl}/partenaire/rome/v1/appellation?champs=code,libelle,libelleCourt,metier(code)`,
       ...withAuthorizationHeaders,
       responses: {
         200: z.array(
@@ -48,7 +34,7 @@ export const makeRome4Routes = (peApiUrl: AbsoluteUrl) =>
     }),
   });
 
-export class HttpRome4Gateway implements Rome4Gateway {
+export class HttpRome3Gateway implements Rome3Gateway {
   #httpClient: HttpClient<Rome4Routes>;
   #poleEmploiGateway: PoleEmploiGateway;
   #poleEmploiClientId: string;
@@ -64,28 +50,14 @@ export class HttpRome4Gateway implements Rome4Gateway {
   }
 
   #getScope() {
-    return `application_${this.#poleEmploiClientId} api_rome-metiersv1 nomenclatureRome`;
-  }
-
-  public async getAllRomes(): Promise<RomeDto[]> {
-    const { access_token } = await this.#poleEmploiGateway.getAccessToken(
-      this.#getScope(),
-    );
-
-    return this.#httpClient
-      .getRomes({ headers: { authorization: `Bearer ${access_token}` } })
-      .then(({ body }) =>
-        body.map(({ libelle, code }) => ({
-          romeLabel: libelle,
-          romeCode: code,
-        })),
-      );
+    return `application_${this.#poleEmploiClientId} api_romev1 nomenclatureRome`;
   }
 
   public async getAllAppellations(): Promise<AppellationWithShortLabel[]> {
     const { access_token } = await this.#poleEmploiGateway.getAccessToken(
       this.#getScope(),
     );
+
     return this.#httpClient
       .getAppellations({ headers: { authorization: `Bearer ${access_token}` } })
       .then(({ body }) =>
@@ -95,6 +67,10 @@ export class HttpRome4Gateway implements Rome4Gateway {
           appellationLabel: libelle,
           appellationLabelShort: libelleCourt,
         })),
-      );
+      )
+      .catch((error) => {
+        console.error("Error getting appellations", error.response.data);
+        throw error;
+      });
   }
 }
