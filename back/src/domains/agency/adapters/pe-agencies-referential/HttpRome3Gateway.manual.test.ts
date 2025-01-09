fdab694cfc9176ea01461e53976265d709d6bc78
Jérôme Burkard <burkardjerome@gmail.com>
2025-01-09 11:33:49 +0100

    make HttpRome3Gateway work

--- back/src/domains/agency/adapters/pe-agencies-referential/HttpRome3Gateway.manual.test.ts
+++ back/src/domains/agency/adapters/pe-agencies-referential/HttpRome3Gateway.manual.test.ts
@@ -10,9 +10,9 @@ import { HttpPoleEmploiGateway } from "../../../convention/adapters/pole-emploi-
 import { InMemoryCachingGateway } from "../../../core/caching-gateway/adapters/InMemoryCachingGateway";
 import { noRetries } from "../../../core/retry-strategy/ports/RetryStrategy";
 import { RealTimeGateway } from "../../../core/time-gateway/adapters/RealTimeGateway";
-import { HttpRome4Gateway, makeRome4Routes } from "./HttpRome4Gateway";
+import { HttpRome3Gateway, makeRome3Routes } from "./HttpRome3Gateway";
 
-describe("HttpRome4Gateway", () => {
+describe("HttpRome3Gateway", () => {
   const config = AppConfig.createFromEnv();
 
   const cachingGateway = new InMemoryCachingGateway<AccessTokenResponse>(
@@ -28,32 +28,23 @@ describe("HttpRome4Gateway", () => {
     noRetries,
   );
 
-  const httpRome4Gateway = new HttpRome4Gateway(
-    createAxiosSharedClient(makeRome4Routes(config.peApiUrl), axios),
+  const httpRome3Gateway = new HttpRome3Gateway(
+    createAxiosSharedClient(makeRome3Routes(config.peApiUrl), axios),
     franceTravailGateway,
     config.poleEmploiClientId,
   );
 
-  it("fetches the updated list of romes", async () => {
-    const response = await httpRome4Gateway.getAllRomes();
-    expect(response.length).toBeGreaterThan(600);
-    expect(response.length).toBeLessThan(700);
-    expectToEqual(response[0], {
-      romeCode: "A1101",
-      romeLabel: "Conducteur / Conductrice d'engins agricoles",
-    });
-  });
-
-  it("fetches the updated list of appellations", async () => {
-    const response = await httpRome4Gateway.getAllAppellations();
+  it("fetches the updated list of appellations, with their rome code (all in ROME v3)", async () => {
+    const response = await httpRome3Gateway.getAllAppellations();
     expect(response.length).toBeGreaterThan(11_500);
     expect(response.length).toBeLessThan(15_000);
     expectToEqual(response[0], {
-      appellationCode: "10605",
-      appellationLabel: "Agent / Agente de service expédition marchandises",
+      appellationCode: "10320",
+      appellationLabel:
+        "Aérodynamicien / Aérodynamicienne en études, recherche et développement",
       appellationLabelShort:
-        "Agent / Agente de service expédition marchandises",
-      romeCode: "N1103",
+        "Aérodynamicien(ne) en études, recherche et développement",
+      romeCode: "H1206",
     });
   });
 });
