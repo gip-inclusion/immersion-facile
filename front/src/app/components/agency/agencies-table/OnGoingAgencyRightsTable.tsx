38cfd611a5a6202d45444cd666eb780994879fab
Benjamin Bohec <bbohec.pro@gmail.com>
2025-01-13 14:44:48 +0100

    NoActiveAgencyRights

--- front/src/app/components/agency/agencies-table/OnGoingAgencyRightsTable.tsx
+++ front/src/app/components/agency/agencies-table/OnGoingAgencyRightsTable.tsx
@@ -1,4 +1,3 @@
-import { fr } from "@codegouvfr/react-dsfr";
 import Table from "@codegouvfr/react-dsfr/Table";
 import { ReactNode } from "react";
 import { AgencyRight } from "shared";
@@ -9,18 +8,12 @@ import { AgencyLineCaracteristics } from "./agency-line/AgencyLineCaracteristics
 export const OnGoingAgencyRightsTable = ({
   agenciesWithToReviewRights,
 }: { agenciesWithToReviewRights: AgencyRight[] }) => (
-  <>
-    <h2 className={fr.cx("fr-h4")}>
-      Demandes d'accès en cours ({agenciesWithToReviewRights.length}{" "}
-      {agenciesWithToReviewRights.length === 1 ? "agence" : "agences"})
-    </h2>
-    <Table
-      headers={["Organisme", "Caractéristiques de l'agence", "Administrateurs"]}
-      data={agenciesWithToReviewRights
-        .sort((a, b) => a.agency.name.localeCompare(b.agency.name))
-        .map(makeAgencyWithToReviewRightLine())}
-    />
-  </>
+  <Table
+    headers={["Organisme", "Caractéristiques de l'agence", "Administrateurs"]}
+    data={agenciesWithToReviewRights
+      .sort((a, b) => a.agency.name.localeCompare(b.agency.name))
+      .map(makeAgencyWithToReviewRightLine())}
+  />
 );
 
 const makeAgencyWithToReviewRightLine =
