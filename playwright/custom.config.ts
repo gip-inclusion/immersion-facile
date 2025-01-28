8987875eb5c03c2241406698863c1ec0e6c2065c
Benjamin Bohec <bbohec.pro@gmail.com>
2025-01-28 15:34:14 +0100

    valid proconnect auth workflow

--- playwright/custom.config.ts
+++ playwright/custom.config.ts
@@ -1,6 +1,16 @@
+import { makeThrowIfNotDefinedOrDefault } from "shared";
+
+const throwIfNotDefinedOrDefault = makeThrowIfNotDefinedOrDefault(process.env);
 export const testConfig = {
   timeForDebounce: 600, // debounce time value * 2 for safety
   timeForEventCrawler: 2000, // event crawler time interval + 1s for safety
+  proConnect: {
+    baseUrl: "https://fca.integ01.dev-agentconnect.fr/api/v2",
+    username: throwIfNotDefinedOrDefault("PC_USERNAME"),
+    password: throwIfNotDefinedOrDefault("PC_PASSWORD"),
+    adminUsername: "admin+playwright@immersion-facile.beta.gouv.fr",
+    adminPassword: throwIfNotDefinedOrDefault("PC_ADMIN_PASSWORD"),
+  },
   inclusionConnect: {
     baseUrl: "https://recette.connect.inclusion.beta.gouv.fr",
     username: process.env.IC_USERNAME ?? "",
