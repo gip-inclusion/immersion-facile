2b248f3bcc3a8929f30d2120bdfa8d7e6a0150ca
Benjamin Bohec <bbohec.pro@gmail.com>
2025-02-05 15:51:36 +0100

    establishments rights in profile

---
+++ front/src/app/components/establishment/establishments-table/establishment-line/EstablishmentLineBusinessName.tsx
@@ -0,0 +1,21 @@
+import { fr } from "@codegouvfr/react-dsfr";
+import { ReactNode } from "react";
+import { WithEstablishmentData } from "shared";
+import { EstablishmentTag } from "../../EstablishmentTag";
+
+export const EstablishmentLineBusinessName = ({
+  withEstablishmentData,
+}: { withEstablishmentData: WithEstablishmentData }): ReactNode => (
+  <ul key={withEstablishmentData.siret} className={fr.cx("fr-raw-list")}>
+    <li>
+      <EstablishmentTag />
+    </li>
+    <li> {withEstablishmentData.businessName}</li>
+    <li>
+      {" "}
+      <span className={fr.cx("fr-hint-text")}>
+        {withEstablishmentData.siret}
+      </span>
+    </li>
+  </ul>
+);
