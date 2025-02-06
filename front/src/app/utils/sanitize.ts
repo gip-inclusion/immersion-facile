80e84848ab9ac90388b114658eda16500d350be8
Enguerran Weiss <enguerranweiss@gmail.com>
2025-02-06 11:47:17 +0100

    fixed xss on error pages content

---
+++ front/src/app/utils/sanitize.ts
@@ -0,0 +1,7 @@
+export const escapeHtml = (unsafe: string) =>
+  unsafe
+    .replace(/&/g, "&amp;")
+    .replace(/</g, "&lt;")
+    .replace(/>/g, "&gt;")
+    .replace(/"/g, "&quot;")
+    .replace(/'/g, "&#039;");
