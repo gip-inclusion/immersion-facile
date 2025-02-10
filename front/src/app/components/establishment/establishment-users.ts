e58b0fbe30b0a31e2d311dd1d18aa6ad1180e590
Benjamin Bohec <bbohec.pro@gmail.com>
2025-02-10 12:25:00 +0100

    review

--- front/src/app/components/establishment/establishment-users.ts
+++ front/src/app/components/establishment/establishment-users.ts
@@ -15,12 +15,12 @@ export const establishmentRoleToDisplay: Record<
     label: "Administrateur",
     className: "fr-badge--green-emeraude",
     description:
-      "Peut modifier les informations de l'entreprise, ajouter et supprimer des utilisateurs, modifier leur rôles, consulter les conventions et les mises en relation. Peut répondre aux mises en relation de l'entgreprise.",
+      "Peut modifier les informations de l'entreprise, ajouter et supprimer des utilisateurs, modifier leur rôles, consulter les conventions et les mises en relation. Peut répondre aux mises en relation de l'entreprise.",
   },
   "establishment-contact": {
     label: "Contact d'entreprise",
     className: "fr-badge--purple-glycine",
     description:
-      "Peut consulter des conventions et des mises en relation de l'entreprise. Peut répondre aux mises en relation de l'entgreprise.",
+      "Peut consulter des conventions et des mises en relation de l'entreprise. Peut répondre aux mises en relation de l'entreprise.",
   },
 };
