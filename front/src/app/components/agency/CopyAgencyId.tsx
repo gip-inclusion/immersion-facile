21c25bf731978c1cf704f43a62f470f111a5e2b0
celineung <celine.ung.pro@gmail.com>
2025-02-17 12:14:49 +0100

    fix copy button on agency page

--- front/src/app/components/agency/CopyAgencyId.tsx
+++ front/src/app/components/agency/CopyAgencyId.tsx
@@ -1,24 +1,19 @@
+import { fr } from "@codegouvfr/react-dsfr";
 import { Badge } from "@codegouvfr/react-dsfr/Badge";
-import { Button } from "@codegouvfr/react-dsfr/Button";
 import React from "react";
-import { useCopyButton } from "react-design-system";
+import { CopyButton } from "react-design-system";
 import { AgencyId } from "shared";
 
 export const CopyAgencyId = ({ agencyId }: { agencyId: AgencyId }) => {
-  const { copyButtonIsDisabled, copyButtonLabel, onCopyButtonClick } =
-    useCopyButton("Copier");
-
   return (
     <div>
       Id de l'agence : <Badge severity="success">{agencyId}</Badge>{" "}
-      <Button
-        type="button"
-        disabled={copyButtonIsDisabled}
-        iconId={"fr-icon-clipboard-fill"}
-        onClick={() => onCopyButtonClick(agencyId)}
-      >
-        {copyButtonLabel}
-      </Button>
+      <CopyButton
+        label="Copier"
+        textToCopy={agencyId}
+        withIcon={true}
+        className={fr.cx("fr-ml-1v")}
+      />
     </div>
   );
 };
