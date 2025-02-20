545ae2b14800494dcf4c6ba1d63340ef581d4526
Benjamin Bohec <bbohec.pro@gmail.com>
2025-02-20 14:57:34 +0100

    PgUowPerformer test FormEstablishmentAdded event replaced by NotificationAdded

--- back/src/domains/core/unit-of-work/adapters/PgUowPerformer.integration.test.ts
+++ back/src/domains/core/unit-of-work/adapters/PgUowPerformer.integration.test.ts
@@ -1,18 +1,19 @@
 import { Pool } from "pg";
-import { FormEstablishmentDtoBuilder } from "shared";
+import { Notification, expectPromiseToFailWithError } from "shared";
 import {
   KyselyDb,
   makeKyselyDb,
 } from "../../../../config/pg/kysely/kyselyUtils";
 import { getTestPgPool } from "../../../../config/pg/pgUtils";
-import { makeCreateNewEvent } from "../../events/ports/EventBus";
+import {
+  CreateNewEvent,
+  makeCreateNewEvent,
+} from "../../events/ports/EventBus";
 import { CustomTimeGateway } from "../../time-gateway/adapters/CustomTimeGateway";
 import { TestUuidGenerator } from "../../uuid-generator/adapters/UuidGeneratorImplementations";
 import { UnitOfWork } from "../ports/UnitOfWork";
 import { PgUowPerformer } from "./PgUowPerformer";
 import { createPgUow } from "./createPgUow";
-
-const someSiret = "12345678901234";
 
 describe("PgUowPerformer", () => {
   let pool: Pool;
@@ -32,7 +33,9 @@ describe("PgUowPerformer", () => {
   });
 
   beforeEach(async () => {
-    await db.deleteFrom("form_establishments").execute();
+    await db.deleteFrom("notifications_email_attachments").execute();
+    await db.deleteFrom("notifications_email_recipients").execute();
+    await db.deleteFrom("notifications_email").execute();
     await db.deleteFrom("outbox_failures").execute();
     await db.deleteFrom("outbox_publications").execute();
     await db.deleteFrom("outbox").execute();
@@ -44,51 +47,70 @@ describe("PgUowPerformer", () => {
 
   it("saves everything when all goes fine", async () => {
     uuidGenerator.setNextUuid("11111111-1111-1111-1111-111111111111");
-    await pgUowPerformer.perform(useCaseUnderTest);
-    await expectLengthOfRepos({ formEstablishmentLength: 1, outboxLength: 1 });
+
+    await pgUowPerformer.perform(useCaseUnderTest(createNewEvent));
+    await expectLengthOfRepos({
+      notificationLength: 1,
+      outboxLength: 1,
+      db,
+    });
   });
 
   it("keeps modifications atomic when something goes wrong", async () => {
-    uuidGenerator.setNextUuid("a failing uuid");
-    try {
-      await pgUowPerformer.perform(useCaseUnderTest);
-      expect("Should not be reached").toBe("");
-    } catch (error: any) {
-      expect(error.message).toBe(
-        'invalid input syntax for type uuid: "a failing uuid"',
-      );
-    }
+    const id = "a failing uuid";
+    uuidGenerator.setNextUuid(id);
 
-    await expectLengthOfRepos({ formEstablishmentLength: 0, outboxLength: 0 });
-  });
+    expectPromiseToFailWithError(
+      pgUowPerformer.perform(useCaseUnderTest(createNewEvent)),
+      new Error(`invalid input syntax for type uuid: "${id}"`),
+    );
 
-  const useCaseUnderTest = async (uow: UnitOfWork) => {
-    const formEstablishment = FormEstablishmentDtoBuilder.valid()
-      .withSiret(someSiret)
-      .build();
-
-    await uow.formEstablishmentRepository.create(formEstablishment);
-
-    const event = createNewEvent({
-      topic: "FormEstablishmentAdded",
-      payload: { formEstablishment, triggeredBy: null },
+    await expectLengthOfRepos({
+      notificationLength: 0,
+      outboxLength: 0,
+      db,
     });
-    await uow.outboxRepository.save(event);
-  };
+  });
+});
 
-  const expectLengthOfRepos = async ({
-    formEstablishmentLength,
-    outboxLength,
-  }: {
-    formEstablishmentLength: number;
-    outboxLength: number;
-  }) => {
-    expect(
-      await db.selectFrom("form_establishments").selectAll().execute(),
-    ).toHaveLength(formEstablishmentLength);
+const useCaseUnderTest =
+  (createNewEvent: CreateNewEvent) => async (uow: UnitOfWork) => {
+    const notification: Notification = {
+      id: "11111111-1111-1111-1111-111111111113",
+      createdAt: new Date().toISOString(),
+      kind: "email",
+      templatedContent: {
+        kind: "TEST_EMAIL",
+        params: { input1: "", input2: "", url: "http://" },
+        recipients: ["a@a.com"],
+      },
+      followedIds: {},
+    };
 
-    expect(await db.selectFrom("outbox").selectAll().execute()).toHaveLength(
-      outboxLength,
+    await uow.notificationRepository.save(notification);
+
+    await uow.outboxRepository.save(
+      createNewEvent({
+        topic: "NotificationAdded",
+        payload: { id: notification.id, kind: notification.kind },
+      }),
     );
   };
-});
+
+const expectLengthOfRepos = async ({
+  notificationLength,
+  outboxLength,
+  db,
+}: {
+  notificationLength: number;
+  outboxLength: number;
+  db: KyselyDb;
+}) => {
+  expect(
+    await db.selectFrom("notifications_email").selectAll().execute(),
+  ).toHaveLength(notificationLength);
+
+  expect(await db.selectFrom("outbox").selectAll().execute()).toHaveLength(
+    outboxLength,
+  );
+};
