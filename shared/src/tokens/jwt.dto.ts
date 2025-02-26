3830788b30b1e1d8f90b8a2a4444fb39a5a30ae0
Benjamin Bohec <bbohec.pro@gmail.com>
2025-02-26 11:33:36 +0100

    delete establishment jwt and jwt payload

--- shared/src/tokens/jwt.dto.ts
+++ shared/src/tokens/jwt.dto.ts
@@ -3,20 +3,17 @@ import { PayloadKey } from "./jwtPayload.dto";
 
 export type ApiConsumerJwt = Flavor<string, "ApiConsumerJwt">;
 export type ConventionJwt = Flavor<string, "ConventionJwt">;
-export type EstablishmentJwt = Flavor<string, "EstablishmentJwt">;
 export type InclusionConnectJwt = Flavor<string, "InclusionConnectJwt">;
 
 export type AppSupportedJwt =
   | ApiConsumerJwt
   | ConventionJwt
-  | EstablishmentJwt
   | InclusionConnectJwt;
 
 export type ConventionSupportedJwt = ConventionJwt | InclusionConnectJwt;
 
 export const currentJwtVersions: Record<PayloadKey, number> = {
   convention: 1,
-  establishment: 1,
   inclusion: 1,
   currentUser: 1, // useless, just for typecheck
 };
