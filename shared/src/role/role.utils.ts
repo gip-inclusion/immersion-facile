b649db6ceec1217c3f6b7adafd6acef356f13896
celineung <celine.ung.pro@gmail.com>
2025-02-26 12:24:05 +0100

    update signatoryTitleByRole

--- shared/src/role/role.utils.ts
+++ shared/src/role/role.utils.ts
@@ -1,9 +1,9 @@
 import { AgencyModifierRole, SignatoryRole } from "./role.dto";
 
 export const signatoryTitleByRole: Record<SignatoryRole, string> = {
-  beneficiary: "Le bénéficiaire",
-  "beneficiary-representative": "Le représentant légal du bénéficiaire",
-  "establishment-representative": "Le représentant de l'entreprise",
+  beneficiary: "bénéficiaire",
+  "beneficiary-representative": "représentant légal du bénéficiaire",
+  "establishment-representative": "représentant de l'entreprise",
   "beneficiary-current-employer":
     "Le représentant de l'entreprise actuelle du candidat",
 };
