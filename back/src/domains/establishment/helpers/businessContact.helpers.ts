c1c9f6e52eb4634630f0cef0d94f86275cc6d843
Benjamin Bohec <bbohec.pro@gmail.com>
2025-02-26 13:43:40 +0100

    helpers

--- back/src/domains/establishment/helpers/businessContact.helpers.ts
+++ back/src/domains/establishment/helpers/businessContact.helpers.ts
@@ -1,41 +1,80 @@
-import { type BusinessContactDto, errors } from "shared";
+import { type EstablishmentFormUserRights, errors } from "shared";
+import { type UserOnRepository } from "../../core/authentication/inclusion-connect/port/UserRepository";
 import type { UnitOfWork } from "../../core/unit-of-work/ports/UnitOfWork";
 import type {
   EstablishmentAdminRight,
   EstablishmentAggregate,
 } from "../entities/EstablishmentAggregate";
 
-export const businessContactFromEstablishmentAggregateAndUsers = async (
+export const establishmentFormUserRightsFromEstablishmentAggregateAndUsers =
+  async (
+    uow: UnitOfWork,
+    establishmentAggregate: EstablishmentAggregate,
+  ): Promise<EstablishmentFormUserRights> => {
+    const firstAdminRight = establishmentAggregate.userRights.find(
+      (right): right is EstablishmentAdminRight =>
+        right.role === "establishment-admin",
+    );
+    if (!firstAdminRight)
+      throw errors.establishment.adminNotFound({
+        siret: establishmentAggregate.establishment.siret,
+      });
+    const firstAdmin = await uow.userRepository.getById(firstAdminRight.userId);
+    if (!firstAdmin)
+      throw errors.establishment.adminNotFound({
+        siret: establishmentAggregate.establishment.siret,
+      });
+
+    const establishmentUsers = await uow.userRepository.getByIds(
+      establishmentAggregate.userRights.map(({ userId }) => userId),
+    );
+
+    return establishmentAggregate.userRights.map(
+      ({ role, userId, job, phone }) => {
+        const user = establishmentUsers.find((user) => user.id === userId);
+        if (!user) throw errors.user.notFound({ userId });
+        return role === "establishment-admin"
+          ? {
+              role,
+              email: user.email,
+              job,
+              phone,
+            }
+          : {
+              role,
+              email: user.email,
+              job,
+              phone,
+            };
+      },
+    );
+  };
+
+export const getDiscussionContactsFromAggregate = async (
   uow: UnitOfWork,
-  establishmentAggregate: EstablishmentAggregate,
-): Promise<BusinessContactDto> => {
-  const firstAdminRight = establishmentAggregate.userRights.find(
-    (right): right is EstablishmentAdminRight =>
-      right.role === "establishment-admin",
+  establishment: EstablishmentAggregate,
+): Promise<{
+  otherUsers: UserOnRepository[];
+  firstAdminUser: UserOnRepository;
+  firstAdminRight: EstablishmentAdminRight;
+}> => {
+  const firstAdminRight = establishment.userRights.find(
+    (right) => right.role === "establishment-admin",
   );
   if (!firstAdminRight)
     throw errors.establishment.adminNotFound({
-      siret: establishmentAggregate.establishment.siret,
-    });
-  const firstAdmin = await uow.userRepository.getById(firstAdminRight.userId);
-  if (!firstAdmin)
-    throw errors.establishment.adminNotFound({
-      siret: establishmentAggregate.establishment.siret,
+      siret: establishment.establishment.siret,
     });
 
-  const contacts = await uow.userRepository.getByIds(
-    establishmentAggregate.userRights
-      .filter((right) => right.role === "establishment-contact")
-      .map(({ userId }) => userId),
+  const firstAdminUser = await uow.userRepository.getById(
+    firstAdminRight.userId,
   );
-
-  return {
-    contactMethod: establishmentAggregate.establishment.contactMethod,
-    firstName: firstAdmin.firstName,
-    lastName: firstAdmin.lastName,
-    email: firstAdmin.email,
-    job: firstAdminRight.job,
-    phone: firstAdminRight.phone,
-    copyEmails: contacts.map(({ email }) => email),
-  };
+  if (!firstAdminUser)
+    throw errors.user.notFound({ userId: firstAdminRight.userId });
+  const otherUsers = await uow.userRepository.getByIds(
+    establishment.userRights
+      .map(({ userId }) => userId)
+      .filter((id) => id !== firstAdminUser.id),
+  );
+  return { otherUsers, firstAdminRight, firstAdminUser };
 };
--- back/src/domains/establishment/helpers/makeEstablishmentAggregate.ts
+++ back/src/domains/establishment/helpers/makeEstablishmentAggregate.ts
@@ -1,66 +1,106 @@
-import {
-  type FormEstablishmentDto,
-  type Location,
-  noContactPerMonth,
-} from "shared";
-import type { NafAndNumberOfEmpolyee } from "../../../utils/siret";
-import type { TimeGateway } from "../../core/time-gateway/ports/TimeGateway";
+import { FormEstablishmentDto, errors, noContactPerMonth } from "shared";
+import { rawAddressToLocation } from "../../../utils/address";
+import { NafAndNumberOfEmpolyee } from "../../../utils/siret";
+import { AddressGateway } from "../../core/address/ports/AddressGateway";
+import { createOrGetUserIdByEmail } from "../../core/authentication/inclusion-connect/entities/user.helper";
+import { makeProvider } from "../../core/authentication/inclusion-connect/port/OAuthGateway";
+import { TimeGateway } from "../../core/time-gateway/ports/TimeGateway";
+import { UnitOfWork } from "../../core/unit-of-work/ports/UnitOfWork";
+import { UuidGenerator } from "../../core/uuid-generator/ports/UuidGenerator";
 import type {
   EstablishmentAggregate,
   EstablishmentUserRight,
 } from "../entities/EstablishmentAggregate";
 import type { EstablishmentEntity } from "../entities/EstablishmentEntity";
 
-export const makeEstablishmentAggregate = ({
+export const makeEstablishmentAggregate = async ({
+  uow,
   timeGateway,
+  uuidGenerator,
+  addressGateway,
   formEstablishment,
   nafAndNumberOfEmployee,
-  addressesAndPosition,
-  score = 0,
-  userRights,
+  score,
   existingEntity,
 }: {
+  uow: UnitOfWork;
   timeGateway: TimeGateway;
+  addressGateway: AddressGateway;
+  uuidGenerator: UuidGenerator;
   formEstablishment: FormEstablishmentDto;
-  addressesAndPosition: Location[];
   nafAndNumberOfEmployee: NafAndNumberOfEmpolyee;
-  score?: number;
-  userRights: EstablishmentUserRight[];
+  score: number;
   existingEntity?: EstablishmentEntity;
-}): EstablishmentAggregate => ({
-  establishment: {
-    acquisitionCampaign: formEstablishment.acquisitionCampaign,
-    acquisitionKeyword: formEstablishment.acquisitionKeyword,
-    locations: addressesAndPosition,
-    additionalInformation: formEstablishment.additionalInformation,
-    createdAt: existingEntity ? existingEntity.createdAt : timeGateway.now(),
-    customizedName: formEstablishment.businessNameCustomized,
-    fitForDisabledWorkers: formEstablishment.fitForDisabledWorkers,
-    isCommited: formEstablishment.isEngagedEnterprise,
-    isOpen: true,
-    isMaxDiscussionsForPeriodReached:
-      formEstablishment.maxContactsPerMonth <= noContactPerMonth,
-    maxContactsPerMonth: formEstablishment.maxContactsPerMonth,
-    ...nafAndNumberOfEmployee,
-    name: formEstablishment.businessName,
-    siret: formEstablishment.siret,
-    sourceProvider: formEstablishment.source,
-    updatedAt: timeGateway.now(),
-    voluntaryToImmersion: true,
-    website: formEstablishment.website,
-    nextAvailabilityDate: formEstablishment.nextAvailabilityDate,
-    searchableBy: formEstablishment.searchableBy,
-    score,
-    contactMethod: formEstablishment.businessContact.contactMethod,
-  },
-  userRights: userRights,
-  offers: formEstablishment.appellations.map(
-    ({ appellationCode, appellationLabel, romeCode, romeLabel }) => ({
-      romeCode,
-      appellationCode,
-      appellationLabel,
-      romeLabel,
-      createdAt: timeGateway.now(),
-    }),
-  ),
-});
+}): Promise<EstablishmentAggregate> => {
+  const establishmentUsersIds = await Promise.all(
+    formEstablishment.userRights.map(({ email }) =>
+      createOrGetUserIdByEmail(uow, timeGateway, uuidGenerator, {
+        email,
+      }),
+    ),
+  );
+
+  const establishmentUsers = await uow.userRepository.getByIds(
+    establishmentUsersIds,
+    await makeProvider(uow),
+  );
+
+  const updatedUserRights: EstablishmentUserRight[] =
+    formEstablishment.userRights.map(({ email, role, job, phone }) => {
+      const user = establishmentUsers.find((user) => user.email === email);
+
+      if (!user) throw errors.user.notFoundByEmail({ email });
+      return role === "establishment-admin"
+        ? {
+            role,
+            userId: user.id,
+            job,
+            phone,
+          }
+        : { role, userId: user.id, job, phone };
+    });
+
+  const locations = await Promise.all(
+    formEstablishment.businessAddresses.map(async (address) =>
+      rawAddressToLocation(addressGateway, formEstablishment.siret, address),
+    ),
+  );
+
+  return {
+    establishment: {
+      acquisitionCampaign: formEstablishment.acquisitionCampaign,
+      acquisitionKeyword: formEstablishment.acquisitionKeyword,
+      locations,
+      additionalInformation: formEstablishment.additionalInformation,
+      createdAt: existingEntity ? existingEntity.createdAt : timeGateway.now(),
+      customizedName: formEstablishment.businessNameCustomized,
+      fitForDisabledWorkers: formEstablishment.fitForDisabledWorkers,
+      isCommited: formEstablishment.isEngagedEnterprise,
+      isOpen: true,
+      isMaxDiscussionsForPeriodReached:
+        formEstablishment.maxContactsPerMonth <= noContactPerMonth,
+      maxContactsPerMonth: formEstablishment.maxContactsPerMonth,
+      ...nafAndNumberOfEmployee,
+      name: formEstablishment.businessName,
+      siret: formEstablishment.siret,
+      sourceProvider: formEstablishment.source,
+      updatedAt: timeGateway.now(),
+      voluntaryToImmersion: true,
+      website: formEstablishment.website,
+      nextAvailabilityDate: formEstablishment.nextAvailabilityDate,
+      searchableBy: formEstablishment.searchableBy,
+      score,
+      contactMethod: formEstablishment.contactMethod,
+    },
+    userRights: updatedUserRights,
+    offers: formEstablishment.appellations.map(
+      ({ appellationCode, appellationLabel, romeCode, romeLabel }) => ({
+        romeCode,
+        appellationCode,
+        appellationLabel,
+        romeLabel,
+        createdAt: timeGateway.now(),
+      }),
+    ),
+  };
+};
