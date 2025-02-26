1c19e776ae38bd99c01ab1b364cfa7f8e6f4ac9b
celineung <celine.ung.pro@gmail.com>
2025-02-26 12:32:56 +0100

    add usecase RemindSignatories

--- shared/src/role/role.utils.ts
+++ shared/src/role/role.utils.ts
@@ -1,3 +1,4 @@
+import { Signatories } from "..";
 import { AgencyModifierRole, SignatoryRole } from "./role.dto";
 
 export const signatoryTitleByRole: Record<SignatoryRole, string> = {
@@ -5,7 +6,17 @@ export const signatoryTitleByRole: Record<SignatoryRole, string> = {
   "beneficiary-representative": "représentant légal du bénéficiaire",
   "establishment-representative": "représentant de l'entreprise",
   "beneficiary-current-employer":
-    "Le représentant de l'entreprise actuelle du candidat",
+    "représentant de l'entreprise actuelle du candidat",
+};
+
+export const conventionSignatoryRoleBySignatoryKey: Record<
+  SignatoryRole,
+  keyof Signatories
+> = {
+  beneficiary: "beneficiary",
+  "beneficiary-current-employer": "beneficiaryCurrentEmployer",
+  "beneficiary-representative": "beneficiaryRepresentative",
+  "establishment-representative": "establishmentRepresentative",
 };
 
 export const agencyModifierTitleByRole: Record<AgencyModifierRole, string> = {
