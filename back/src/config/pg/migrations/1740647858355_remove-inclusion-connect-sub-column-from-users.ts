07f5be94733c563a691e241c287e34489eaa531e
Jérôme Burkard <burkardjerome@gmail.com>
2025-02-27 10:21:17 +0100

    drop inclusion_connect_sub column from users

---
+++ back/src/config/pg/migrations/1740647858355_remove-inclusion-connect-sub-column-from-users.ts
@@ -0,0 +1,14 @@
+import { MigrationBuilder } from "node-pg-migrate";
+
+export async function up(pgm: MigrationBuilder): Promise<void> {
+  pgm.dropColumn("users", "inclusion_connect_sub");
+}
+
+export async function down(pgm: MigrationBuilder): Promise<void> {
+  pgm.addColumn("users", {
+    inclusion_connect_sub: {
+      type: "text",
+      notNull: false,
+    },
+  });
+}
