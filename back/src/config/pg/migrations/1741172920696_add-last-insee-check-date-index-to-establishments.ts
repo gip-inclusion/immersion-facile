3e2d03bc4e4fdb58fba0830fc297aafcde993d86
celineung <celine.ung.pro@gmail.com>
2025-03-05 12:18:46 +0100

    add index on establishments.last_insee_check_date

---
+++ back/src/config/pg/migrations/1741172920696_add-last-insee-check-date-index-to-establishments.ts
@@ -0,0 +1,14 @@
+/* eslint-disable @typescript-eslint/naming-convention */
+import { ColumnDefinitions, MigrationBuilder } from "node-pg-migrate";
+
+export const shorthands: ColumnDefinitions | undefined = undefined;
+
+export async function up(pgm: MigrationBuilder): Promise<void> {
+  pgm.addIndex("establishments", "last_insee_check_date", {
+    name: "establishments_last_insee_check_date_index",
+  });
+}
+
+export async function down(pgm: MigrationBuilder): Promise<void> {
+  pgm.dropIndex("establishments", "establishments_last_insee_check_date_index");
+}
