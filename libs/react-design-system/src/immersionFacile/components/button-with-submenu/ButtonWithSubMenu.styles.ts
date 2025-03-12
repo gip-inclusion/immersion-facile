a8e7f5f96f2d17e4cce0c5ec72ed5ccff9a5af7a
celineung <celine.ung.pro@gmail.com>
2025-03-12 16:09:43 +0100

    update ButtonWithSubMenu

--- libs/react-design-system/src/immersionFacile/components/button-with-submenu/ButtonWithSubMenu.styles.ts
+++ libs/react-design-system/src/immersionFacile/components/button-with-submenu/ButtonWithSubMenu.styles.ts
@@ -5,4 +5,5 @@ export default {
   menu: "im-button-with-sub-menu__menu",
   menuHidden: "im-button-with-sub-menu__menu--hidden",
   list: "im-button-with-sub-menu__list",
+  listElement: "im-button-with-sub-menu__list-element",
 };
