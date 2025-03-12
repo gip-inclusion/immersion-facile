8ca182967144d443088c64092174c17e351d71d2
celineung <celine.ung.pro@gmail.com>
2025-03-12 10:43:15 +0100

    use UploadFile component

--- front/src/app/components/UploadFile.tsx
+++ front/src/app/components/UploadFile.tsx
@@ -1,7 +1,8 @@
+import { fr } from "@codegouvfr/react-dsfr";
+import { Alert } from "@codegouvfr/react-dsfr/Alert";
 import { type ChangeEvent, type ReactNode, useState } from "react";
-import { File } from "react-design-system";
+import { CopyButton, File } from "react-design-system";
 import type { AbsoluteUrl } from "shared";
-import { domElementIds } from "shared";
 import { useAppSelector } from "src/app/hooks/reduxHooks";
 import { outOfReduxDependencies } from "src/config/dependencies";
 import { authSelectors } from "src/core-logic/domain/auth/auth.selectors";
@@ -10,44 +11,121 @@ interface UploadLogoProps {
   label: string;
   hint?: ReactNode;
   maxSize_Mo: number;
-  setFileUrl: (fileUrl: AbsoluteUrl) => void;
+  setUploadedFileUrl?: (fileUrl: AbsoluteUrl) => void;
+  initialFileUrl?: AbsoluteUrl;
+  id: string;
+  shouldDisplayFeedback: boolean;
 }
 
 export const UploadFile = ({
   maxSize_Mo,
-  setFileUrl,
+  setUploadedFileUrl,
   label,
   hint,
+  id,
+  shouldDisplayFeedback,
+  initialFileUrl,
 }: UploadLogoProps) => {
+  const copyButtonLabel = "Copier l'URL du fichier";
   const [error, setError] = useState<string>();
+  const [uploadError, setUploadError] = useState<string>();
+  const [file, setFile] = useState<File | null>(null);
+  const [fileUrl, setFileUrl] = useState<AbsoluteUrl | null>(null);
+  const [fileBase64, setFileBase64] = useState<string | ArrayBuffer | null>(
+    null,
+  );
   const connectedUserJwt = useAppSelector(authSelectors.inclusionConnectToken);
 
   if (!connectedUserJwt) return null;
-  return (
-    <File
-      onChange={async (e: ChangeEvent<HTMLInputElement>) => {
-        let file = null;
-        if (e.target.files?.length) {
-          file = e.target.files[0];
-        }
-        if (file && file.size > 1_000_000 * maxSize_Mo) {
-          setError(`Le fichier ne peut pas faire plus de ${maxSize_Mo} Mo`);
-          return;
-        }
-        setError(undefined);
-        if (!file) return;
 
-        const fileUrl =
-          await outOfReduxDependencies.technicalGateway.uploadFile(
-            file,
-            connectedUserJwt,
-          );
-        setFileUrl(fileUrl);
-      }}
-      label={label}
-      hint={hint}
-      errorMessage={error}
-      id={domElementIds.addAgency.uploadLogoInput}
-    />
+  const toBase64 = (file: File) => {
+    const reader = new FileReader();
+    reader.readAsDataURL(file);
+    reader.onload = () => setFileBase64(reader.result);
+    reader.onerror = () => setFileBase64(null);
+  };
+
+  const setUploadedFile = (file: File, fileUrl: AbsoluteUrl) => {
+    setFile(file);
+    toBase64(file);
+    setFileUrl(fileUrl);
+    setUploadedFileUrl ? setUploadedFileUrl(fileUrl) : null;
+  };
+
+  return (
+    <>
+      <File
+        onChange={async (e: ChangeEvent<HTMLInputElement>) => {
+          let file = null;
+          if (e.target.files?.length) {
+            file = e.target.files[0];
+          }
+          if (file && file.size > 1_000_000 * maxSize_Mo) {
+            setError(`Le fichier ne peut pas faire plus de ${maxSize_Mo} Mo`);
+            return;
+          }
+          setError(undefined);
+          if (!file) return;
+
+          try {
+            const fileUrl =
+              await outOfReduxDependencies.technicalGateway.uploadFile(
+                file,
+                connectedUserJwt,
+              );
+            setUploadedFile(file, fileUrl);
+          } catch (error: any) {
+            const errorMessage =
+              "message" in error
+                ? error.message
+                : "Une erreur est survenue lors de l'upload de fichier";
+            setUploadError(errorMessage);
+          }
+        }}
+        label={label}
+        hint={hint}
+        errorMessage={error}
+        id={id}
+      />
+      {((file?.type.startsWith("image/") && fileBase64) || initialFileUrl) && (
+        <div className={fr.cx("fr-col-3")}>
+          <figure
+            className={fr.cx("fr-content-media", "fr-m-0", "fr-mt-2w")}
+            aria-label="Aperçu de l'image"
+          >
+            <div className={fr.cx("fr-content-media__img")}>
+              {initialFileUrl && !fileBase64 ? (
+                <img src={initialFileUrl} alt="" />
+              ) : (
+                <img
+                  className={fr.cx("fr-responsive-img", "fr-ratio-16x9")}
+                  src={String(fileBase64)}
+                  alt=""
+                />
+              )}
+            </div>
+            <figcaption className={fr.cx("fr-content-media__caption")}>
+              Aperçu de l'image
+            </figcaption>
+          </figure>
+        </div>
+      )}
+      {shouldDisplayFeedback && (
+        <>
+          {uploadError && <Alert severity="error" title={uploadError} />}
+          {!uploadError && fileUrl && (
+            <>
+              <Alert
+                severity="success"
+                title="Fichier uploadé !"
+                description={`URL du fichier : ${fileUrl}`}
+                className={fr.cx("fr-mb-2w", "fr-mt-2w")}
+              />
+              <CopyButton textToCopy={fileUrl} label={copyButtonLabel} />
+            </>
+          )}
+        </>
+      )}
+    </>
   );
 };
--- front/src/app/pages/admin/technical-options-sections/UploadFileSection.tsx
+++ front/src/app/pages/admin/technical-options-sections/UploadFileSection.tsx
@@ -1,123 +1,24 @@
-import { fr } from "@codegouvfr/react-dsfr";
-import { Alert } from "@codegouvfr/react-dsfr/Alert";
-import Button from "@codegouvfr/react-dsfr/Button";
-import { useState } from "react";
-import { File } from "react-design-system";
-import { useCopyButton } from "react-design-system";
 import { domElementIds } from "shared";
+import { UploadFile } from "src/app/components/UploadFile";
 import { useAppSelector } from "src/app/hooks/reduxHooks";
-import { outOfReduxDependencies } from "src/config/dependencies";
 import { authSelectors } from "src/core-logic/domain/auth/auth.selectors";
 
 export const UploadFileSection = () => {
   const connectedUserJwt = useAppSelector(authSelectors.inclusionConnectToken);
-  const { onCopyButtonClick, copyButtonLabel, copyButtonIsDisabled } =
-    useCopyButton("Copier l'URL du fichier");
-  const [uploadedFileUrl, setUploadedFileUrl] = useState("");
-  const [error, setError] = useState<string>();
-  const [file, setFile] = useState<File | null>(null);
-  const [fileBase64, setFileBase64] = useState<string | ArrayBuffer | null>(
-    null,
-  );
   const label = "Télécharger un document sur clever";
   const maxSize_Mo = 10;
 
   if (!connectedUserJwt) return null;
 
-  const onUploadClick = async () => {
-    if (!file) return;
-    const fileUrl = await outOfReduxDependencies.technicalGateway.uploadFile(
-      file,
-      connectedUserJwt,
-    );
-    setUploadedFileUrl(fileUrl);
-  };
-  const toBase64 = (file: File) => {
-    const reader = new FileReader();
-    reader.readAsDataURL(file);
-    reader.onload = () => setFileBase64(reader.result);
-    reader.onerror = () => setFileBase64(null);
-  };
   return (
     <>
       <h4>Upload de fichier</h4>
-      <>
-        <File
-          onChange={(e) => {
-            let tempFile = null;
-            if (e.target.files?.length) {
-              tempFile = e.target.files[0];
-            }
-            if (tempFile && tempFile.size > 1_000_000 * maxSize_Mo) {
-              setError(`Le fichier ne peut pas faire plus de ${maxSize_Mo} Mo`);
-              return;
-            }
-            setError(undefined);
-            if (!tempFile) return;
-
-            setFile(tempFile);
-            toBase64(tempFile);
-          }}
-          label={label}
-          errorMessage={error}
-          id={domElementIds.addAgency.uploadLogoInput}
-        />
-        {file?.type.startsWith("image/") && fileBase64 && (
-          <div className={fr.cx("fr-col-3")}>
-            <figure
-              className={fr.cx("fr-content-media", "fr-m-0", "fr-mt-2w")}
-              aria-label="Aperçu de l'image"
-            >
-              <div className={fr.cx("fr-content-media__img")}>
-                <img
-                  className={fr.cx("fr-responsive-img", "fr-ratio-16x9")}
-                  src={String(fileBase64)}
-                  alt=""
-                />
-              </div>
-              <figcaption className={fr.cx("fr-content-media__caption")}>
-                Aperçu de l'image
-              </figcaption>
-            </figure>
-          </div>
-        )}
-
-        <Button
-          type="button"
-          onClick={onUploadClick}
-          className={fr.cx("fr-mt-2w")}
-          size="small"
-          priority="secondary"
-          disabled={!file}
-        >
-          Téléverser le fichier
-        </Button>
-      </>
-      {uploadedFileUrl && (
-        <>
-          <Alert
-            severity="success"
-            title="Fichier uploadé !"
-            description={`URL du fichier : ${uploadedFileUrl}`}
-            className={fr.cx("fr-mb-2w", "fr-mt-2w")}
-          />
-          <button
-            disabled={copyButtonIsDisabled}
-            onClick={() => onCopyButtonClick(uploadedFileUrl)}
-            className={fr.cx(
-              "fr-btn",
-              "fr-btn--sm",
-              "fr-icon-clipboard-fill",
-              "fr-btn--tertiary-no-outline",
-              "fr-btn--icon-left",
-              "fr-ml-1w",
-            )}
-            type="button"
-          >
-            {copyButtonLabel}
-          </button>
-        </>
-      )}
+      <UploadFile
+        id={domElementIds.addAgency.uploadLogoInput}
+        maxSize_Mo={maxSize_Mo}
+        label={label}
+        shouldDisplayFeedback={true}
+      />
     </>
   );
 };
