85cb45aad0912806fde66d4520487ac0740f55be
celineung <celine.ung.pro@gmail.com>
2025-03-12 14:33:42 +0100

    refacto after review

--- front/src/app/pages/admin/technical-options-sections/UploadFileSection.tsx
+++ front/src/app/pages/admin/technical-options-sections/UploadFileSection.tsx
@@ -1,14 +1,9 @@
 import { domElementIds } from "shared";
 import { UploadFile } from "src/app/components/UploadFile";
-import { useAppSelector } from "src/app/hooks/reduxHooks";
-import { authSelectors } from "src/core-logic/domain/auth/auth.selectors";
 
 export const UploadFileSection = () => {
-  const connectedUserJwt = useAppSelector(authSelectors.inclusionConnectToken);
   const label = "Télécharger un document sur clever";
   const maxSize_Mo = 10;
-
-  if (!connectedUserJwt) return null;
 
   return (
     <>
