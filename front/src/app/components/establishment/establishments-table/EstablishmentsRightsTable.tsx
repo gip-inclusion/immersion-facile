f9ac2361f22e7b1ec7034bf4432a9f565ba19f69
Benjamin Bohec <bbohec.pro@gmail.com>
2025-03-12 14:15:43 +0100

    fix afficher le nombre d'établissement dans la page Mon Profil

--- front/src/app/components/establishment/establishments-table/EstablishmentsRightsTable.tsx
+++ front/src/app/components/establishment/establishments-table/EstablishmentsRightsTable.tsx
@@ -16,7 +16,7 @@ export const EstablishmentsRightsTable = ({
     <h2 className={fr.cx("fr-h4")}>
       {withEstablishmentData.length === 1
         ? "Établissement rattaché au profil pour accueillir des candidats (1 établissement)"
-        : "Établissements rattachés au profil pour accueillir des candidats (${withEstablishmentData.length} établissements)"}
+        : `Établissements rattachés au profil pour accueillir des candidats (${withEstablishmentData.length} établissements)`}
     </h2>
 
     <Table
