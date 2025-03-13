4c577ae22363f399b397460643cd4734110ecad0
celineung <celine.ung.pro@gmail.com>
2025-03-13 09:38:37 +0100

    update wording

--- front/src/app/components/agency/UsersWithoutNameHint.tsx
+++ front/src/app/components/agency/UsersWithoutNameHint.tsx
@@ -8,7 +8,7 @@ export const UsersWithoutNameHint = () => (
     Pourquoi certains utilisateurs n'ont pas de nom ?
     <Tooltip
       type="click"
-      description="Certains utilisateurs n'ont pas de compte Inclusion Connect. Ils
+      description="Certains utilisateurs n'ont pas de compte ProConnect. Ils
             peuvent se créer un compte avec la même adresse email pour ajouter
             leurs infos et accéder à leur espace personnel."
       id={domElementIds.admin.agencyTab.editAgencyUserTooltip}
