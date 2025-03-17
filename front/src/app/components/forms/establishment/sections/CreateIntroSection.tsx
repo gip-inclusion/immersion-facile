43ab832ce1cacbf09c19b1218290599c7c55e35d
Benjamin Bohec <bbohec.pro@gmail.com>
2025-03-17 15:52:02 +0100

    allow establishment creation even if establishment already exist

--- front/src/app/components/forms/establishment/sections/CreateIntroSection.tsx
+++ front/src/app/components/forms/establishment/sections/CreateIntroSection.tsx
@@ -1,4 +1,5 @@
 import { fr } from "@codegouvfr/react-dsfr";
+import Alert from "@codegouvfr/react-dsfr/Alert";
 import { Button } from "@codegouvfr/react-dsfr/Button";
 import React from "react";
 import { domElementIds, toFormatedTextSiret } from "shared";
@@ -24,55 +25,37 @@ export const CreateIntroSection = ({
   if (!currentUser) throw frontErrors.generic.unauthorized();
 
   return (
-    <section className={fr.cx("fr-grid-row", "fr-grid-row--center")}>
+    <section className={fr.cx("fr-grid-row", "fr-grid-row--middle")}>
       <div className={fr.cx("fr-col-lg-6", "fr-col-12")}>
         <p>Bienvenue !</p>
+        <p>
+          Ce formulaire vous permet d’apparaître sur notre annuaire
+          d’entreprises accueillantes. Ainsi, les candidats pourront vous
+          trouver et vous contacter au rythme qui vous convient.
+        </p>
         {siretRawError ===
-        "Establishment with this siret is already in our DB" ? (
-          <>
-            <p>
-              Vous avez sélectionné le SIRET{" "}
-              <b>{toFormatedTextSiret(currentSiret)}</b> lors de la création de
-              votre compte sur ProConnect.
-            </p>
-            <p>
-              Un établissement est déjà référencé chez nous avec ce numéro de
-              SIRET.
-            </p>
-            <Button
-              priority="primary"
-              linkProps={{
-                ...routes.establishmentDashboard({ tab: "conventions" }).link,
-                id: domElementIds.establishment.create
-                  .navigateToEstablishmentDashboard,
-              }}
-            >
-              Accéder à mon espace
-            </Button>
-          </>
-        ) : (
-          <>
-            <p>
-              Ce formulaire vous permet d’apparaître sur notre annuaire
-              d’entreprises accueillantes. Ainsi, les candidats pourront vous
-              trouver et vous contacter au rythme qui vous convient.
-            </p>
-
-            <Button
-              iconId="fr-icon-arrow-right-line"
-              iconPosition="right"
-              onClick={() => onStepChange(1, [])}
-              id={domElementIds.establishment.create.startFormButton}
-            >
-              Commencer le référencement
-            </Button>
-            <p className={fr.cx("fr-mt-2w")}>
-              <a {...routes.conventionImmersion().link}>
-                Ou remplir une demande de convention sans me référencer
-              </a>
-            </p>
-          </>
+          "Establishment with this siret is already in our DB" && (
+          <Alert
+            className={fr.cx("fr-my-4w")}
+            severity="info"
+            small
+            title={`L'établissement ${toFormatedTextSiret(currentSiret)} est déjà référencé sur Immersion Facilitée`}
+            description={`Si vous souhaitez l'éditer, vous pouvez vous rendre sur votre tableau de bord entreprise. Si vous souhaitez ajouter un autre établissement, vous pouvez poursuivre en cliquant sur le bouton ci-dessous.`}
+          />
         )}
+        <Button
+          iconId="fr-icon-arrow-right-line"
+          iconPosition="right"
+          onClick={() => onStepChange(1, [])}
+          id={domElementIds.establishment.create.startFormButton}
+        >
+          Commencer le référencement
+        </Button>
+        <p className={fr.cx("fr-mt-2w")}>
+          <a {...routes.conventionImmersion().link}>
+            Ou remplir une demande de convention sans me référencer
+          </a>
+        </p>
       </div>
       <div
         className={fr.cx(
