7d4c125857a891e71188d76c544be4f4b9b9df21
Jérôme Burkard <burkardjerome@gmail.com>
2025-03-19 15:39:51 +0100

    setup sentry profiler

--- back/src/scripts/configureSentry.ts
+++ back/src/scripts/configureSentry.ts
@@ -1,4 +1,5 @@
 import * as Sentry from "@sentry/node";
+import { nodeProfilingIntegration } from "@sentry/profiling-node";
 
 import type { Environment } from "shared";
 import { version } from "./version";
@@ -6,13 +7,12 @@ import { version } from "./version";
 export const configureSentry = (envType: Environment) => {
   Sentry.init({
     dsn: "https://eb7a121cd835308163ca9966e5c82c98@o4508405260615680.ingest.de.sentry.io/4508999044038736",
-    integrations: [],
+    integrations: [nodeProfilingIntegration()],
     tracesSampleRate: 1.0,
     environment: envType,
     release: version,
+    profilesSampleRate: 0.01,
   });
 
   return Sentry;
 };
-
-export type SentryInstance = typeof Sentry;
