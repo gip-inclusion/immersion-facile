3fd7bee88ddfa18d7f7029349de5a2d2aa1e4a49
Jérôme Burkard <burkardjerome@gmail.com>
2025-03-20 11:41:39 +0100

    configure sentry trace to 1% only

--- back/src/scripts/configureSentry.ts
+++ back/src/scripts/configureSentry.ts
@@ -8,9 +8,9 @@ export const configureSentry = (envType: Environment) => {
   Sentry.init({
     dsn: "https://eb7a121cd835308163ca9966e5c82c98@o4508405260615680.ingest.de.sentry.io/4508999044038736",
     integrations: [nodeProfilingIntegration()],
-    tracesSampleRate: 1.0,
     environment: envType,
     release: version,
+    tracesSampleRate: 0.01,
     profilesSampleRate: 0.01,
   });
 
