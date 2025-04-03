80282d24ca7c0e3a169d189f477a764aabc01e8e
Enguerran Weiss <enguerranweiss@gmail.com>
2025-04-03 17:07:36 +0200

    added contents for each login page, renamed InclusionConnectedPrivateRoute, new layout for login page

--- front/src/app/routes/AgencyDashboardPrivateRoute.tsx
+++ front/src/app/routes/AgencyDashboardPrivateRoute.tsx
@@ -2,9 +2,9 @@ import type { ReactElement } from "react";
 import { PageHeader } from "react-design-system";
 import { Breadcrumbs } from "src/app/components/Breadcrumbs";
 import {
+  ConnectedPrivateRoute,
   type FrontAgencyDashboardRoute,
-  InclusionConnectedPrivateRoute,
-} from "src/app/routes/InclusionConnectedPrivateRoute";
+} from "src/app/routes/ConnectedPrivateRoute";
 
 type AgencyDashboardPrivateRouteProps = {
   route: FrontAgencyDashboardRoute;
@@ -15,7 +15,7 @@ export const AgencyDashboardPrivateRoute = ({
   route,
   children,
 }: AgencyDashboardPrivateRouteProps) => (
-  <InclusionConnectedPrivateRoute
+  <ConnectedPrivateRoute
     route={route}
     inclusionConnectConnexionPageHeader={
       <PageHeader
@@ -25,5 +25,5 @@ export const AgencyDashboardPrivateRoute = ({
     }
   >
     {children}
-  </InclusionConnectedPrivateRoute>
+  </ConnectedPrivateRoute>
 );
