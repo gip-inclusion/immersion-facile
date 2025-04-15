974c073925c5f0e611cb778026a6e38213ac40d1
Benjamin Bohec <bbohec.pro@gmail.com>
2025-04-15 16:46:47 +0200

    SearchResultContactSection as component

--- front/src/app/pages/search/SearchResultPage.tsx
+++ front/src/app/pages/search/SearchResultPage.tsx
@@ -22,21 +22,19 @@ import {
   makeSiretDescriptionLink,
 } from "shared";
 import { Feedback } from "src/app/components/feedback/Feedback";
-import { ContactByEmail } from "src/app/components/immersion-offer/ContactByEmail";
-import { ContactByPhone } from "src/app/components/immersion-offer/ContactByPhone";
-import { ContactInPerson } from "src/app/components/immersion-offer/ContactInPerson";
 import { HeaderFooterLayout } from "src/app/components/layout/HeaderFooterLayout";
 import {
   SearchMiniMap,
   getIconMarker,
 } from "src/app/components/search/SearchMiniMap";
+import { SearchResultContactSection } from "src/app/components/search/SearchResultContactSection";
 import { SearchResultLabels } from "src/app/components/search/SearchResultLabels";
 import { defaultPageMetaContents } from "src/app/contents/meta/metaContents";
 import { useAppSelector } from "src/app/hooks/reduxHooks";
 import { routes, useRoute } from "src/app/routes/routes";
 import { searchSelectors } from "src/core-logic/domain/search/search.selectors";
 import { searchSlice } from "src/core-logic/domain/search/search.slice";
-import { P, match } from "ts-pattern";
+import {} from "ts-pattern";
 import type { Route } from "type-route";
 
 const getFeedBackMessage = (contactMethod?: ContactMethod) => {
@@ -389,138 +387,11 @@ export const SearchResultPage = ({
                   />
                 </div>
               </div>
-
-              <div>
-                <div
-                  className={fr.cx("fr-card", "fr-p-4w", "fr-mt-8w")}
-                  ref={formContactRef}
-                >
-                  <h2 className={fr.cx("fr-h3", "fr-mb-2w")}>
-                    {currentSearchResult.voluntaryToImmersion
-                      ? "Contacter l'entreprise"
-                      : "Nos conseils pour cette première prise de contact ! "}
-                  </h2>
-
-                  {match(currentSearchResult.contactMode)
-                    .with("EMAIL", () => (
-                      <ContactByEmail
-                        appellations={currentSearchResult.appellations}
-                        onSubmitSuccess={onFormSubmitSuccess}
-                      />
-                    ))
-                    .with("PHONE", () => (
-                      <ContactByPhone
-                        appellations={currentSearchResult.appellations}
-                        onSubmitSuccess={onFormSubmitSuccess}
-                      />
-                    ))
-                    .with("IN_PERSON", () => (
-                      <ContactInPerson
-                        appellations={currentSearchResult.appellations}
-                        onSubmitSuccess={onFormSubmitSuccess}
-                      />
-                    ))
-                    .with(P.nullish, () => (
-                      <div>
-                        <h3 className={fr.cx("fr-h6")}>
-                          Comment présenter votre demande ?{" "}
-                        </h3>
-                        <p className={fr.cx("fr-mb-2w")}>
-                          Soyez <strong>direct, concret et courtois</strong>.
-                          Présentez-vous, présentez simplement votre projet et
-                          l’objectif que vous recherchez en effectuant une
-                          immersion.
-                        </p>
-                        <p className={fr.cx("fr-mb-2w")}>
-                          <strong>Par exemple : </strong>
-                          <span>
-                            “Je souhaite devenir mécanicien auto et je voudrais
-                            découvrir comment ce métier se pratique dans un
-                            garage comme le vôtre. Ca me permettra de vérifier
-                            que cela me plaît vraiment. La personne qui
-                            m’accueillera et me présentera le métier pourra
-                            aussi vérifier si ce métier est fait pour moi.”
-                          </span>
-                        </p>
-                        <p className={fr.cx("fr-mb-2w")}>
-                          Vous pouvez indiquer à votre interlocutrice ou
-                          interlocuteur que{" "}
-                          <strong>
-                            cette immersion sera encadrée par une convention
-                            signée par l'organisme qui vous suit.
-                          </strong>
-                        </p>
-                        <p className={fr.cx("fr-mb-2w")}>
-                          Indiquez lui le moment où vous aimeriez faire une
-                          immersion et pourquoi vous voulez la faire à cette
-                          date.
-                        </p>
-                        <p className={fr.cx("fr-mb-2w")}>
-                          <strong>Par exemple : </strong>
-                          <span>
-                            “il faudrait que je fasse une immersion avant de
-                            m’inscrire à une formation. “
-                          </span>
-                        </p>
-                        <p className={fr.cx("fr-mb-2w")}>
-                          Indiquez également le <strong>nombre de jours</strong>{" "}
-                          que vous aimeriez faire en immersion si vous le savez
-                          déjà.
-                        </p>
-                        <p className={fr.cx("fr-mb-2w")}>
-                          Concluez en lui demandant{" "}
-                          <strong>un rendez-vous</strong> pour qu’il/elle se
-                          rende compte du sérieux de votre projet.
-                        </p>
-
-                        <h3 className={fr.cx("fr-h6")}>
-                          Comment expliquer simplement ce qu’est une immersion ?
-                        </h3>
-                        <p className={fr.cx("fr-mb-2w")}>
-                          C’est un stage d’observation, strictement encadré d’un
-                          point de vue juridique. Vous conservez votre statut et
-                          êtes couvert par votre France Travail (anciennement
-                          Pôle emploi), votre Mission Locale ou le Conseil
-                          départemental (en fonction de votre situation).
-                        </p>
-                        <p className={fr.cx("fr-mb-2w")}>
-                          Le rôle de celui qui vous accueillera est de vous
-                          présenter le métier et de vérifier avec vous que ce
-                          métier vous convient en vous faisant des retours les
-                          plus objectifs possibles. Pendant la durée de votre
-                          présence, vous pouvez aider les salariés en donnant un
-                          coup de main mais vous n’êtes pas là pour remplacer un
-                          collègue absent.
-                        </p>
-
-                        <h3 className={fr.cx("fr-h6")}>
-                          Quelle est la durée d’une immersion ?
-                        </h3>
-                        <p className={fr.cx("fr-mb-2w")}>
-                          Les immersions se font le plus souvent pendant une
-                          semaine ou deux.{" "}
-                          <strong>
-                            Il n’est pas possible de dépasser un mois
-                          </strong>
-                          . Il est possible de faire une immersion de seulement
-                          un ou deux jours mais vous ne découvrirez pas
-                          parfaitement un métier.
-                        </p>
-
-                        <h3 className={fr.cx("fr-h6")}>Bon à savoir ! </h3>
-                        <p className={fr.cx("fr-mb-2w")}>
-                          <strong>
-                            Il n’est pas nécessaire d’apporter votre CV
-                          </strong>
-                          . Vous êtes là pour demander à découvrir un métier et
-                          c’est ce projet qui est important, pas vos expériences
-                          professionnelles ni votre formation !
-                        </p>
-                      </div>
-                    ))
-                    .exhaustive()}
-                </div>
-              </div>
+              <SearchResultContactSection
+                onFormSubmitSuccess={onFormSubmitSuccess}
+                formContactRef={formContactRef}
+                currentSearchResult={currentSearchResult}
+              />
             </>
           )}
           {showConfirmationMessage && (
