05e88453083c8b956f2e699205d79c1cfefc2e97
clement-duport <clement.duport@beta.gouv.fr>
2025-04-17 13:17:36 +0200

    fix review

--- shared/src/phone.schema.unit.test.ts
+++ shared/src/phone.schema.unit.test.ts
@@ -1,27 +1,24 @@
-import { type CountryCode, parsePhoneNumber } from "libphonenumber-js";
 import { isValidMobilePhone, phoneSchema } from "./phone.schema";
 
-describe("phoneschema", () => {
-  it.each<[string, CountryCode]>([
-    ["0600000000", "FR"],
-    ["0785689727", "FR"],
-    ["0639000001", "YT"],
-    ["0690000001", "GP"],
-    ["0691282545", "GP"],
-    ["0694000001", "GF"],
-    ["0696000001", "MQ"],
-    ["0692000001", "RE"],
-    ["0693000001", "RE"],
-    ["87123456", "PF"],
-    ["751234", "NC"],
-    ["723456", "WF"],
-    ["551234", "PM"],
+describe("phonesShema", () => {
+  it.each<[string, string]>([
+    ["0600000000", "+33600000000"], //FR
+    ["0785689727", "+33785689727"], //FR
+    ["0639000001", "+262639000001"], //YT
+    ["0690000001", "+590690000001"], //GP
+    ["0691282545", "+590691282545"], //GP
+    ["0694000001", "+594694000001"], //GF
+    ["0696000001", "+596696000001"], //MQ
+    ["0692000001", "+262692000001"], //RE
+    ["0693000001", "+262693000001"], //RE
+    ["87123456", "+68987123456"], //PF
+    ["751234", "+687751234"], //NC
+    ["723456", "+681723456"], //WF
+    ["551234", "+508551234"], //PM
   ])(
     "should be valid for mobile phone number %s %s",
-    async (phone, countryCode) => {
-      expect(phoneSchema.parse(phone)).toBe(
-        parsePhoneNumber(phone, countryCode).format("E.164"),
-      );
+    async (phone, expected) => {
+      expect(phoneSchema.parse(phone)).toBe(expected);
     },
   );
 
@@ -46,24 +43,19 @@ describe("phoneschema", () => {
     },
   );
 
-  it.each<[string, CountryCode]>([
-    ["01 00 00 00 00", "FR"],
-    ["05 55 68 97 27", "FR"],
-    ["0269 61 23 45", "YT"],
-    ["0590 27 58 43", "GP"],
-    ["0594 91 23 45", "GF"],
-    ["0596 81 23 45", "MQ"],
-    ["0262 61 23 45", "RE"],
-    ["40 12 34 56", "PF"],
-    ["24 56 78", "NC"],
-    ["72 34 56", "WF"],
-    ["41 23 56", "PM"],
-  ])(
-    "should be valid for fix phone number %s %s",
-    async (phone, countryCode) => {
-      expect(phoneSchema.parse(phone)).toBe(
-        parsePhoneNumber(phone, countryCode).format("E.164"),
-      );
-    },
-  );
+  it.each<[string, string]>([
+    ["01 00 00 00 00", "+33100000000"], //FR
+    ["05 55 68 97 27", "+33555689727"], //FR
+    ["0269 61 23 45", "+262269612345"], //YT
+    ["0590 27 58 43", "+590590275843"], //GP
+    ["0594 91 23 45", "+594594912345"], //GF
+    ["0596 81 23 45", "+596596812345"], //MQ
+    ["0262 61 23 45", "+262262612345"], //RE
+    ["40 12 34 56", "+68940123456"], //PF
+    ["24 56 78", "+687245678"], //NC
+    ["72 34 56", "+681723456"], //WF
+    ["41 23 56", "+508412356"], //PM
+  ])("should be valid for fix phone number %s %s", async (phone, expected) => {
+    expect(phoneSchema.parse(phone)).toBe(expected);
+  });
 });
