ad90d9120b04f12f114ec435115f31b546a7b573
celineung <celine.ung.pro@gmail.com>
2025-04-25 09:31:46 +0200

    fix event_status enum type creation in migration files

--- back/src/config/pg/migrations/1708954107350_add-in-process-to-type-event-status.ts
+++ back/src/config/pg/migrations/1708954107350_add-in-process-to-type-event-status.ts
@@ -1,7 +1,8 @@
 import type { MigrationBuilder } from "node-pg-migrate";
 
-export async function up(pgm: MigrationBuilder): Promise<void> {
-  pgm.addTypeValue("event_status", "in-process", { ifNotExists: true });
+export async function up(_pgm: MigrationBuilder): Promise<void> {
+  // content deleted to created whole enum type in a same transaction
+  // see PR #3323
 }
 
 export async function down(_pgm: MigrationBuilder): Promise<void> {
