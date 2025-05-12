49b25e6fe5323692c61f47b4ea74ee3add2eeac6
Jérôme Burkard <burkardjerome@gmail.com>
2025-05-12 13:27:19 +0200

    create function to generate the visual status of a discussion

--- shared/src/discussion/discussion.unit.test.ts
+++ shared/src/discussion/discussion.unit.test.ts
@@ -1,70 +1,197 @@
+import { subDays } from "date-fns";
 import { expectToEqual } from "../test.helpers";
 import { DiscussionBuilder } from "./DiscussionBuilder";
-import type { DiscussionReadDto } from "./discussion.dto";
+import { getDiscussionVisualStatus } from "./dicussion.helpers";
+import type {
+  DiscussionDto,
+  DiscussionReadDto,
+  DiscussionVisualStatus,
+  Exchange,
+  ExchangeRole,
+} from "./discussion.dto";
 import {
   discussionReadSchema,
   makeExchangeEmailSchema,
 } from "./discussion.schema";
 
-describe("Discussion schema", () => {
-  const discussionEmailIF = new DiscussionBuilder()
-    .withContactMode("EMAIL")
-    .withDiscussionKind("IF")
-    .build();
+describe("Discussions", () => {
+  describe("getDiscussionVisualStatus", () => {
+    type TestCase = {
+      message: string;
+      discussion: DiscussionDto;
+      expectedVisualStatus: DiscussionVisualStatus;
+    };
 
-  const discussionEmail1E1S = new DiscussionBuilder()
-    .withContactMode("EMAIL")
-    .withDiscussionKind("1_ELEVE_1_STAGE")
-    .build();
+    const createExchange = ({
+      sentAt,
+      sender,
+    }: { sentAt: Date; sender: ExchangeRole }): Exchange => ({
+      subject: "Some subject",
+      message: "Some message",
+      attachments: [],
+      sentAt: sentAt.toISOString(),
+      sender,
+      recipient:
+        sender === "potentialBeneficiary"
+          ? "establishment"
+          : "potentialBeneficiary",
+    });
 
-  const discussionPhoneIF = new DiscussionBuilder()
-    .withContactMode("PHONE")
-    .withDiscussionKind("IF")
-    .build();
+    const now = new Date("2025-05-12");
 
-  const discussionPhone1E1S = new DiscussionBuilder()
-    .withContactMode("PHONE")
-    .withDiscussionKind("1_ELEVE_1_STAGE")
-    .build();
+    const testCases: TestCase[] = [
+      {
+        message: "status is REJECTED",
+        expectedVisualStatus: "rejected",
+        discussion: new DiscussionBuilder().withStatus("REJECTED").build(),
+      },
+      {
+        message: "status is ACCEPTED",
+        expectedVisualStatus: "accepted",
+        discussion: new DiscussionBuilder().withStatus("ACCEPTED").build(),
+      },
+      {
+        message: "candidate has sent the first message without being answered",
+        expectedVisualStatus: "new",
+        discussion: new DiscussionBuilder()
+          .withStatus("PENDING")
+          .withExchanges([
+            createExchange({
+              sentAt: subDays(now, 1),
+              sender: "potentialBeneficiary",
+            }),
+          ])
+          .build(),
+      },
+      {
+        message:
+          "candidate has sent the last message without being answered (but it is not the first message)",
+        expectedVisualStatus: "needs-answer",
+        discussion: new DiscussionBuilder()
+          .withStatus("PENDING")
+          .withExchanges([
+            createExchange({
+              sentAt: subDays(now, 3),
+              sender: "potentialBeneficiary",
+            }),
+            createExchange({
+              sentAt: subDays(now, 2),
+              sender: "establishment",
+            }),
+            createExchange({
+              sentAt: subDays(now, 1),
+              sender: "potentialBeneficiary",
+            }),
+          ])
+          .build(),
+      },
+      {
+        message: "last message is sent by establishment",
+        expectedVisualStatus: "answered",
+        discussion: new DiscussionBuilder()
+          .withStatus("PENDING")
+          .withExchanges([
+            createExchange({
+              sentAt: subDays(now, 2),
+              sender: "potentialBeneficiary",
+            }),
+            createExchange({
+              sentAt: subDays(now, 1),
+              sender: "establishment",
+            }),
+          ])
+          .build(),
+      },
+      {
+        message:
+          "last message is from beneficiary and has had no answer for more than 15 days",
+        expectedVisualStatus: "needs-urgent-answer",
+        discussion: new DiscussionBuilder()
+          .withStatus("PENDING")
+          .withExchanges([
+            createExchange({
+              sentAt: subDays(now, 15),
+              sender: "potentialBeneficiary",
+            }),
+          ])
+          .build(),
+      },
+    ];
 
-  const discussionInPersonIF = new DiscussionBuilder()
-    .withContactMode("IN_PERSON")
-    .withDiscussionKind("IF")
-    .build();
+    it.each(testCases)(
+      "returns $expectedVisualStatus when $message",
+      ({ discussion, expectedVisualStatus }) => {
+        expectToEqual(
+          getDiscussionVisualStatus({ discussion, now }),
+          expectedVisualStatus,
+        );
+      },
+    );
+  });
 
-  const discussionInPerson1E1S = new DiscussionBuilder()
-    .withContactMode("IN_PERSON")
-    .withDiscussionKind("1_ELEVE_1_STAGE")
-    .build();
+  describe("Discussion schema", () => {
+    const discussionEmailIF = new DiscussionBuilder()
+      .withContactMode("EMAIL")
+      .withDiscussionKind("IF")
+      .build();
 
-  it.each([
-    discussionEmailIF,
-    discussionEmail1E1S,
-    discussionInPerson1E1S,
-    discussionInPersonIF,
-    discussionPhone1E1S,
-    discussionPhoneIF,
-  ])(
-    "Test discussionReadSchema",
-    ({ establishmentContact, appellationCode, ...rest }) => {
-      const discussionRead: DiscussionReadDto = {
-        ...rest,
-        establishmentContact: {
-          firstName: establishmentContact.firstName,
-          lastName: establishmentContact.lastName,
-          job: establishmentContact.job,
-        },
-        appellation: {
-          appellationCode: appellationCode,
-          appellationLabel: "osef",
-          romeCode: "A2023",
-          romeLabel: "osef",
-        },
-      };
+    const discussionEmail1E1S = new DiscussionBuilder()
+      .withContactMode("EMAIL")
+      .withDiscussionKind("1_ELEVE_1_STAGE")
+      .build();
 
-      expectToEqual(discussionReadSchema.parse(discussionRead), discussionRead);
-    },
-  );
+    const discussionPhoneIF = new DiscussionBuilder()
+      .withContactMode("PHONE")
+      .withDiscussionKind("IF")
+      .build();
+
+    const discussionPhone1E1S = new DiscussionBuilder()
+      .withContactMode("PHONE")
+      .withDiscussionKind("1_ELEVE_1_STAGE")
+      .build();
+
+    const discussionInPersonIF = new DiscussionBuilder()
+      .withContactMode("IN_PERSON")
+      .withDiscussionKind("IF")
+      .build();
+
+    const discussionInPerson1E1S = new DiscussionBuilder()
+      .withContactMode("IN_PERSON")
+      .withDiscussionKind("1_ELEVE_1_STAGE")
+      .build();
+
+    it.each([
+      discussionEmailIF,
+      discussionEmail1E1S,
+      discussionInPerson1E1S,
+      discussionInPersonIF,
+      discussionPhone1E1S,
+      discussionPhoneIF,
+    ])(
+      "Test discussionReadSchema",
+      ({ establishmentContact, appellationCode, ...rest }) => {
+        const discussionRead: DiscussionReadDto = {
+          ...rest,
+          establishmentContact: {
+            firstName: establishmentContact.firstName,
+            lastName: establishmentContact.lastName,
+            job: establishmentContact.job,
+          },
+          appellation: {
+            appellationCode: appellationCode,
+            appellationLabel: "osef",
+            romeCode: "A2023",
+            romeLabel: "osef",
+          },
+        };
+
+        expectToEqual(
+          discussionReadSchema.parse(discussionRead),
+          discussionRead,
+        );
+      },
+    );
+  });
 });
 describe("makeExchangeEmailSchema", () => {
   it("should parse the email", () => {
