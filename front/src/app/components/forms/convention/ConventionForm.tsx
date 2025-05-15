150f6708452be5c98f6fdab3ba4edd687e3fecd8
clement-duport <clement.duport@beta.gouv.fr>
2025-05-15 15:25:33 +0200

    Remove unused convention schema validation and clean up ConventionFormWrapper component

--- front/src/app/components/forms/convention/ConventionForm.tsx
+++ front/src/app/components/forms/convention/ConventionForm.tsx
@@ -624,7 +624,6 @@ export const ConventionForm = ({
                   }}
                   onClick={handleSubmit(onSubmit, (errors) => {
                     validateSteps("doNotClear");
-                    conventionSchema.parse(conventionValues);
                     console.error(conventionValues, errors);
                   })}
                 >
