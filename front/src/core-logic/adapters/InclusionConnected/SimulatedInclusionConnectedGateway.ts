2db54e1c887c251c215cf6b1791cbeba7983e587
Jérôme Burkard <burkardjerome@gmail.com>
2025-05-29 23:55:39 +0200

    fix frontend to align with new UpdateDiscussionStatus usecase

--- front/src/core-logic/adapters/InclusionConnected/SimulatedInclusionConnectedGateway.ts
+++ front/src/core-logic/adapters/InclusionConnected/SimulatedInclusionConnectedGateway.ts
@@ -4,9 +4,9 @@ import {
   type AgencyId,
   DiscussionBuilder,
   type DiscussionReadDto,
-  type DiscussionRejected,
   type InclusionConnectedUser,
   type MarkPartnersErroredConventionAsHandledRequest,
+  type WithDiscussionStatusRejected,
 } from "shared";
 import type { Exchange } from "shared";
 import type {
@@ -80,7 +80,7 @@ export class SimulatedInclusionConnectedGateway
     _payload: {
       jwt: string;
       discussionId: string;
-    } & DiscussionRejected,
+    } & WithDiscussionStatusRejected,
   ): Observable<void> {
     return of(undefined).pipe(delay(this.simulatedLatency));
   }
