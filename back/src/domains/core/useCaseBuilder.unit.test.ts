ac5968335e5dec91556df8793d9d15e5534dec7a
Jérôme Burkard <burkardjerome@gmail.com>
2025-06-02 10:32:31 +0200

    proposition of a useCaseBuilder

---
+++ back/src/domains/core/useCaseBuilder.unit.test.ts
@@ -0,0 +1,36 @@
+import z from "zod";
+import { createInMemoryUow } from "./unit-of-work/adapters/createInMemoryUow";
+import { InMemoryUowPerformer } from "./unit-of-work/adapters/InMemoryUowPerformer";
+import { useCaseBuilder } from "./useCaseBuilder";
+
+describe("useCaseBuilder", () => {
+  it("should be able to define a working usecase", async () => {
+    const uow = createInMemoryUow();
+    const makeAddTruc = useCaseBuilder("AddTruc")
+      .withInput(z.object({ truc: z.string() }))
+      .withOutput<Promise<boolean>>()
+      .withDeps<{ getBidule: () => string }>()
+      .withCurrentUser<{ id: string; email: string }>()
+      .build(async ({ inputParams, currentUser, uow, deps }) => {
+        if (currentUser.email.includes("@example.com"))
+          throw new Error("Bad user");
+        const b = deps.getBidule();
+        const count = await uow.outboxRepository.countAllEvents({
+          status: "in-process",
+        });
+        return b.length >= count && inputParams.truc.length > 0;
+      });
+
+    const addTruc = makeAddTruc({
+      uowPerformer: new InMemoryUowPerformer(uow),
+      deps: { getBidule: () => "bidule" },
+    });
+
+    const result = await addTruc.execute(
+      { truc: "yo" },
+      { id: "123", email: "st@mail.com" },
+    );
+
+    expect(result).toBe(true);
+  });
+});
