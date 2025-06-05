3870247b3d4c3f52ddfa895e7e1e2d3163a02ca0
Enguerran Weiss <enguerranweiss@gmail.com>
2025-06-05 13:55:39 +0200

    added submit buttons on create / edit / admin

--- front/src/app/pages/establishment-dashboard/EstablishmentUsersList.tsx
+++ front/src/app/pages/establishment-dashboard/EstablishmentUsersList.tsx
@@ -66,7 +66,7 @@ export const EstablishmentUsersList = () => {
     <div className="fr-mt-4w">
       <div className={fr.cx("fr-grid-row", "fr-grid-row--middle")}>
         <div>
-          <h3 className={fr.cx("fr-h6")}>Utilisateurs</h3>
+          <h3 className={fr.cx("fr-h6", "fr-mb-0")}>Utilisateurs</h3>
           <UsersWithoutNameHint />
         </div>
         <div className={fr.cx("fr-ml-auto")}>
