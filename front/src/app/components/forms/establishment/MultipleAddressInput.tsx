a2505d95924b6bf58c7772f28e2de1a7cc4a211b
Enguerran Weiss <enguerranweiss@gmail.com>
2025-06-20 15:21:55 +0200

    fix multiple autocompletes

--- front/src/app/components/forms/establishment/MultipleAddressInput.tsx
+++ front/src/app/components/forms/establishment/MultipleAddressInput.tsx
@@ -1,7 +1,12 @@
 import { fr } from "@codegouvfr/react-dsfr";
 import { Button } from "@codegouvfr/react-dsfr/Button";
+import { useDispatch } from "react-redux";
 import type { AddressDto, FormEstablishmentAddress } from "shared";
 import { AddressAutocomplete } from "src/app/components/forms/autocomplete/AddressAutocomplete";
+import {
+  type AddressAutocompleteLocator,
+  geocodingSlice,
+} from "src/core-logic/domain/geocoding/geocoding.slice";
 import { useStyles } from "tss-react/dsfr";
 
 type MultipleAddressInputProps = {
@@ -28,7 +33,7 @@ export const MultipleAddressInput = ({
   hintText,
 }: MultipleAddressInputProps) => {
   const { cx } = useStyles();
-
+  const dispatch = useDispatch();
   return (
     <div
       className={cx(fr.cx("fr-input-group"), "im-multiple-address-input")}
@@ -37,42 +42,50 @@ export const MultipleAddressInput = ({
       <>
         {label && <h2 className={fr.cx("fr-text--lead")}>{label}</h2>}
         {hintText && <p className={fr.cx("fr-hint-text")}>{hintText}</p>}
-        {currentAddresses.map((address, index) => (
-          <div
-            className={fr.cx("fr-grid-row", "fr-grid-row--bottom")}
-            key={address.id}
-          >
-            <div className={fr.cx("fr-col", "fr-mt-2w")}>
-              <AddressAutocomplete
-                multiple
+        {currentAddresses.map((address, index) => {
+          const locator: AddressAutocompleteLocator = `multiple-address-${index}`;
+          return (
+            <div
+              className={fr.cx("fr-grid-row", "fr-grid-row--bottom")}
+              key={address.id}
+            >
+              <div className={fr.cx("fr-col", "fr-mt-2w")}>
+                <AddressAutocomplete
+                  multiple
+                  disabled={disabled}
+                  locator={locator}
+                  label={"Rechercher un lieu *"}
+                  selectProps={{
+                    inputId: `${id}-${index}`,
+                  }}
+                  onAddressSelected={(addressAndPosition) => {
+                    onAddressAdded(addressAndPosition.address, index);
+                  }}
+                  onAddressClear={() => {
+                    onAddressDeleted(index);
+                  }}
+                />
+              </div>
+              <Button
+                type="button"
+                iconId="fr-icon-delete-bin-line"
+                title="Suppression"
                 disabled={disabled}
-                locator={`multiple-address-${index}`}
-                label={"Rechercher un lieu *"}
-                selectProps={{
-                  inputId: `${id}-${index}`,
-                }}
-                onAddressSelected={(addressAndPosition) => {
-                  onAddressAdded(addressAndPosition.address, index);
-                }}
-                onAddressClear={() => {
+                id={`${id}-delete-option-button-${index}`}
+                onClick={() => {
                   onAddressDeleted(index);
+                  dispatch(
+                    geocodingSlice.actions.clearLocatorDataRequested({
+                      locator,
+                      multiple: true,
+                    }),
+                  );
                 }}
               />
             </div>
-            <Button
-              type="button"
-              iconId="fr-icon-delete-bin-line"
-              title="Suppression"
-              disabled={disabled}
-              id={`${id}-delete-option-button-${index}`}
-              onClick={() => {
-                onAddressDeleted(index);
-              }}
-            />
-          </div>
-        ))}
+          );
+        })}
       </>
-
       <Button
         className={fr.cx("fr-my-4v")}
         type="button"
@@ -95,7 +108,6 @@ export const MultipleAddressInput = ({
       >
         Ajouter un lieu d'immersion
       </Button>
-
       {error?.length && (
         <div
           id={`${name}-error-description`}
