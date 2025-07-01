5339506e63124c0792d9df44052bba7a542b37b7
Enguerran Weiss <enguerranweiss@gmail.com>
2025-07-01 16:12:56 +0200

    fix metabaseview circular import

---
+++ front/src/app/contents/statsUrls.ts
@@ -0,0 +1,5 @@
+export const globalStatsUrl =
+  "https://metabase.immersion-facile.beta.gouv.fr/public/dashboard/93afb41e-949d-4677-aab3-95817f81223d";
+
+export const immersionStatsUrl =
+  "https://metabase.immersion-facile.beta.gouv.fr/public/dashboard/531a7757-5e85-4ed7-a958-94f4b493a11a";
