a5d70742bff05469bdec6e76674cd857f77dcd4c
Enguerran Weiss <enguerranweiss@gmail.com>
2025-07-03 14:41:02 +0200

    should_receive_discussion_notifications migration

--- back/src/config/pg/migrations/1751467867402_add-discussion-notification-flag-to-establishment-rights.ts
+++ back/src/config/pg/migrations/1751467867402_add-discussion-notification-flag-to-establishment-rights.ts
@@ -2,7 +2,7 @@
 import type { MigrationBuilder } from "node-pg-migrate";
 
 export async function up(pgm: MigrationBuilder): Promise<void> {
-  pgm.addColumn("establishment_user_rights", {
+  pgm.addColumn("establishments__users", {
     should_receive_discussion_notifications: {
       type: "boolean",
       notNull: true,
@@ -13,7 +13,7 @@ export async function up(pgm: MigrationBuilder): Promise<void> {
 
 export async function down(pgm: MigrationBuilder): Promise<void> {
   pgm.dropColumn(
-    "establishment_user_rights",
+    "establishments__users",
     "should_receive_discussion_notifications",
   );
 }
