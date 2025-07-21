a71475604ad64c2b311673ebae1d7abf6a641361
Jérôme Burkard <burkardjerome@gmail.com>
2025-07-21 13:24:36 +0200

    add some example in useCaseBuilder tests

--- back/src/domains/core/useCaseBuilder.unit.test.ts
+++ back/src/domains/core/useCaseBuilder.unit.test.ts
@@ -1,17 +1,31 @@
 import z from "zod";
 import { createInMemoryUow } from "./unit-of-work/adapters/createInMemoryUow";
 import { InMemoryUowPerformer } from "./unit-of-work/adapters/InMemoryUowPerformer";
+import type { UnitOfWork } from "./unit-of-work/ports/UnitOfWork";
 import { useCaseBuilder } from "./useCaseBuilder";
 
+const isExpectedType = <ExpectedType>(_: ExpectedType) => true;
+
 describe("useCaseBuilder", () => {
-  it("should be able to define a working usecase", async () => {
+  it("should be able to define a working usecase, and force types to be correct", async () => {
     const uow = createInMemoryUow();
+    const inputSchema = z.object({ truc: z.string() });
+    type Input = z.infer<typeof inputSchema>;
+    type Output = boolean;
+    type Deps = { getBidule: () => string };
+    type CurrentUser = { id: string; email: string };
+
     const makeAddTruc = useCaseBuilder("AddTruc")
       .withInput(z.object({ truc: z.string() }))
       .withOutput<boolean>()
       .withDeps<{ getBidule: () => string }>()
       .withCurrentUser<{ id: string; email: string }>()
       .build(async ({ inputParams, currentUser, uow, deps }) => {
+        isExpectedType<Input>(inputParams);
+        isExpectedType<CurrentUser>(currentUser);
+        isExpectedType<Deps>(deps);
+        isExpectedType<UnitOfWork>(uow);
+
         if (currentUser.email.includes("@example.com"))
           throw new Error("Bad user");
         const b = deps.getBidule();
@@ -31,6 +45,28 @@ describe("useCaseBuilder", () => {
       { id: "123", email: "st@mail.com" },
     );
 
+    isExpectedType<Output>(result);
+
     expect(result).toBe(true);
+  });
+
+  it("should infer type from return type of build", async () => {
+    const makeOtherAddTruc = useCaseBuilder("OtherAddTruc").build(
+      async ({ inputParams, currentUser, uow, deps }) => {
+        isExpectedType<void>(inputParams);
+        isExpectedType<void>(currentUser);
+        isExpectedType<void>(deps);
+        isExpectedType<UnitOfWork>(uow);
+        return "hey" as const;
+      },
+    );
+
+    const uow = createInMemoryUow();
+    const otherAddTruc = makeOtherAddTruc({
+      uowPerformer: new InMemoryUowPerformer(uow),
+    });
+
+    const result = await otherAddTruc.execute();
+    isExpectedType<"hey">(result);
   });
 });
