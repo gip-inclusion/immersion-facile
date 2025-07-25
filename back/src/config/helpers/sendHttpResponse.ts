05c93d39ebbfd800eab4b833ddc11fd61f7ca8d2
Benjamin Bohec <bbohec.pro@gmail.com>
2025-07-25 17:22:23 +0200

    remove async try catch pattern on sendHttpResponse
    Signed-off-by: Benjamin Bohec <bbohec.pro@gmail.com>

--- back/src/config/helpers/sendHttpResponse.ts
+++ back/src/config/helpers/sendHttpResponse.ts
@@ -1,16 +1,13 @@
 import type { Request, Response } from "express";
 import { handleHttpJsonResponseError } from "./handleHttpJsonResponseError";
 
-export const sendHttpResponse = async <T>(
+export const sendHttpResponse = <T>(
   request: Request<any, any, any, any, any>,
   response: Response<T>,
   callback: () => Promise<T extends Record<string, never> ? void : T>,
-) => {
-  try {
-    const useCaseResult = await callback();
-    response.json(useCaseResult as T);
-    return;
-  } catch (error: any) {
-    handleHttpJsonResponseError(request, response, error);
-  }
-};
+) =>
+  callback()
+    .then((result) => {
+      response.json(result as T);
+    })
+    .catch((error) => handleHttpJsonResponseError(request, response, error));
