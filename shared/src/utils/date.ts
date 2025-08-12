4c05b023b91b83e39f6bb1f244583bb3e40bbf98
Enguerran Weiss <enguerranweiss@gmail.com>
2025-08-12 12:00:14 +0200

    test zodutils passing

--- shared/src/utils/date.ts
+++ shared/src/utils/date.ts
@@ -22,9 +22,6 @@ export const dateTimeIsoStringSchema: z.Schema<DateTimeIsoString> =
 
 export const dateRegExp = /\d{4}-(0[1-9]|1[012])-(0[1-9]|[12][0-9]|3[01])/;
 
-// HH:MM 24-hour with leading 0
-export const timeHHmmRegExp = /^(0[0-9]|1[0-9]|2[0-3]):[0-5][0-9]$/;
-
 export const toDateUTCString = (date: Date): string => {
   if (Number.isNaN(date.getTime())) {
     throw new Error("Invalid time value");
