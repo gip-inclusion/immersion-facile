41976402451dae14d89521039837190ff4ec94b6
Jérôme Burkard <burkardjerome@gmail.com>
2025-09-09 17:39:01 +0200

    migrate discussion data that cannot be null to some default value

---
+++ back/src/config/pg/migrations/1757431594917_add-missing-past-values-for-discussions.ts
@@ -0,0 +1,29 @@
+import type { MigrationBuilder } from "node-pg-migrate";
+
+export async function up(pgm: MigrationBuilder): Promise<void> {
+  await pgm.db.query(`
+    update discussions
+    set potential_beneficiary_phone = '+33600000000'
+    where potential_beneficiary_phone is null
+  `);
+
+  await pgm.db.query(`
+    update discussions
+    set potential_beneficiary_date_preferences = ''
+    where potential_beneficiary_date_preferences is null
+  `);
+
+  pgm.alterColumn("discussions", "potential_beneficiary_phone", {
+    type: "text",
+    notNull: true,
+  });
+
+  pgm.alterColumn("discussions", "potential_beneficiary_date_preferences", {
+    type: "text",
+    notNull: true,
+  });
+}
+
+export async function down(): Promise<void> {
+  /* nothing to do */
+}
