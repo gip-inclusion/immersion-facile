4221b709d440b416ed0b2f59219d996329348311
celineung <celine.ung.pro@gmail.com>
2025-09-09 10:16:28 +0200

    fix agencyDashboard heading levels

--- front/src/app/components/layout/BackofficeDashboardTabContent.tsx
+++ front/src/app/components/layout/BackofficeDashboardTabContent.tsx
@@ -4,7 +4,7 @@ export const BackofficeDashboardTabContent = (props: HeadingSectionProps) => {
   return (
     <HeadingSection
       {...props}
-      titleAs="h1"
+      titleAs={props.titleAs || "h1"}
       className={props.className ? props.className : "fr-mt-0 fr-mb-0"}
     >
       {props.children}
