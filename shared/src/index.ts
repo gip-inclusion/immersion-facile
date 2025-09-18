9d983175a4f3916dc0dfc0a5bf3c13cd324f90da
clement-duport <clement.duport@beta.gouv.fr>
2025-09-18 10:22:04 +0200

    refactor: reorganize and enhance broadcast feedback types and schemas

--- shared/src/index.ts
+++ shared/src/index.ts
@@ -26,6 +26,7 @@ export * from "./auth/auth.schema";
 export * from "./auth/proConnect/proConnect.dto";
 export * from "./auth/proConnect/proConnect.schema";
 export * from "./Builder";
+export * from "./broadcastFeedback/broadcastFeedback.dto";
 export * from "./business/business";
 export * from "./contactEstablishmentRequest/legacyContactEstablishmentRequest.dto";
 export * from "./contactEstablishmentRequest/legacyContactEstablishmentRequest.schema";
