0e907b99c5142f7f782b60aa79037527cf20acfe
clement-duport <clement.duport@beta.gouv.fr>
2025-09-22 12:22:06 +0200

    add use case and tests for retrieving last broadcast feedback

---
+++ back/src/domains/convention/use-cases/GetLastBroadcastFeedback.ts
@@ -0,0 +1,40 @@
+import {
+  type BroadcastFeedback,
+  type ConnectedUser,
+  type ConventionId,
+  conventionIdSchema,
+  errors,
+} from "shared";
+import { getUserWithRights } from "../../connected-users/helpers/userRights.helper";
+import { useCaseBuilder } from "../../core/useCaseBuilder";
+
+export type GetLastBroadcastFeedback = ReturnType<
+  typeof makeGetLastBroadcastFeedback
+>;
+export const makeGetLastBroadcastFeedback = useCaseBuilder(
+  "GetLastBroadcastFeedback",
+)
+  .withInput<ConventionId>(conventionIdSchema)
+  .withOutput<BroadcastFeedback | null>()
+  .withCurrentUser<ConnectedUser>()
+  .build(async ({ uow, currentUser, inputParams }) => {
+    const convention = await uow.conventionRepository.getById(inputParams);
+    if (!convention)
+      throw errors.convention.notFound({
+        conventionId: inputParams,
+      });
+
+    const userWithRights = await getUserWithRights(uow, currentUser.id);
+    const userAgencyRights = userWithRights.agencyRights.find(
+      (agencyRight) => agencyRight.agency.id === convention.agencyId,
+    );
+    if (!userAgencyRights)
+      throw errors.user.noRightsOnAgency({
+        userId: currentUser.id,
+        agencyId: convention.agencyId,
+      });
+
+    return await uow.broadcastFeedbacksRepository.getLastBroadcastFeedback(
+      inputParams,
+    );
+  });
