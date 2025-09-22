d93517a87bd4725425956fa89c69021c98f4c5ab
Jérôme Burkard <burkardjerome@gmail.com>
2025-09-22 12:57:02 +0200

    add possiblity to have NOT transactionnal use cases in useCaseBuilder

--- back/src/domains/core/useCaseBuilder.ts
+++ back/src/domains/core/useCaseBuilder.ts
@@ -13,27 +13,69 @@ import { extractValue, getSearchParams } from "./useCase.helpers";
 
 const logger = createLogger(__filename);
 
-type UseCaseBuilder<Output, Input, Deps, CurrentUser> = {
+type UseCaseBuilder<
+  Output,
+  Input,
+  Deps,
+  CurrentUser,
+  IsTransactional = true,
+> = {
   withInput: <I>(
     schema: z.ZodType<I, any>,
-  ) => UseCaseBuilder<Output, I, Deps, CurrentUser>;
-  withOutput: <O>() => UseCaseBuilder<O, Input, Deps, CurrentUser>;
-  withDeps: <D>() => UseCaseBuilder<Output, Input, D, CurrentUser>;
-  withCurrentUser: <CU>() => UseCaseBuilder<Output, Input, Deps, CU>;
+  ) => UseCaseBuilder<Output, I, Deps, CurrentUser, IsTransactional>;
+  withOutput: <O>() => UseCaseBuilder<
+    O,
+    Input,
+    Deps,
+    CurrentUser,
+    IsTransactional
+  >;
+  withDeps: <D>() => UseCaseBuilder<
+    Output,
+    Input,
+    D,
+    CurrentUser,
+    IsTransactional
+  >;
+  withCurrentUser: <CU>() => UseCaseBuilder<
+    Output,
+    Input,
+    Deps,
+    CU,
+    IsTransactional
+  >;
+  notTransactional: () => UseCaseBuilder<
+    Output,
+    Input,
+    Deps,
+    CurrentUser,
+    false
+  >;
   build: <
-    Cb extends (args: {
-      inputParams: Input;
-      currentUser: CurrentUser;
-      deps: Deps;
-      uow: UnitOfWork;
-    }) => Promise<Output>,
+    Cb extends IsTransactional extends true
+      ? (args: {
+          inputParams: Input;
+          currentUser: CurrentUser;
+          deps: Deps;
+          uow: UnitOfWork;
+        }) => Promise<Output>
+      : (args: {
+          inputParams: Input;
+          currentUser: CurrentUser;
+          deps: Deps;
+        }) => Promise<Output>,
   >(
     cb: Cb,
   ) => (
-    setupParams: {
-      uowPerformer: UnitOfWorkPerformer;
-      // biome-ignore lint/complexity/noBannedTypes: less wide than Record<string, any>
-    } & (Deps extends void ? {} : { deps: Deps }),
+    setupParams: IsTransactional extends true
+      ? {
+          uowPerformer: UnitOfWorkPerformer;
+          // biome-ignore lint/complexity/noBannedTypes: less wide than Record<string, any>
+        } & (Deps extends void ? {} : { deps: Deps })
+      : // biome-ignore lint/complexity/noBannedTypes: less wide than Record<string, any>
+        Deps extends void
+        ? {}
+        : { deps: Deps },
   ) => {
     useCaseName: string;
     execute: (params: Input, currentUser: CurrentUser) => ReturnType<Cb>;
@@ -45,20 +87,42 @@ export const useCaseBuilder = <
   Input = void,
   Deps = void,
   CurrentUser = void,
+  IsTransactional = true,
 >(
   useCaseName: string,
-  options: { inputSchema: ZodSchemaWithInputMatchingOutput<Input> } = {
+  options: {
+    inputSchema: ZodSchemaWithInputMatchingOutput<Input>;
+    isTransactional?: IsTransactional;
+  } = {
     inputSchema: z.void() as unknown as ZodSchemaWithInputMatchingOutput<Input>,
+    isTransactional: true as IsTransactional,
   },
-): UseCaseBuilder<Output, Input, Deps, CurrentUser> => ({
+): UseCaseBuilder<Output, Input, Deps, CurrentUser, IsTransactional> => ({
   withInput: <I>(inputSchema: ZodSchemaWithInputMatchingOutput<I>) =>
-    useCaseBuilder<Output, I, Deps, CurrentUser>(useCaseName, { inputSchema }),
+    useCaseBuilder<Output, I, Deps, CurrentUser, IsTransactional>(useCaseName, {
+      inputSchema,
+      isTransactional: options.isTransactional,
+    }),
   withOutput: <O>() =>
-    useCaseBuilder<O, Input, Deps, CurrentUser>(useCaseName, options),
+    useCaseBuilder<O, Input, Deps, CurrentUser, IsTransactional>(
+      useCaseName,
+      options,
+    ),
   withDeps: <D>() =>
-    useCaseBuilder<Output, Input, D, CurrentUser>(useCaseName, options),
+    useCaseBuilder<Output, Input, D, CurrentUser, IsTransactional>(
+      useCaseName,
+      options,
+    ),
   withCurrentUser: <CU>() =>
-    useCaseBuilder<Output, Input, Deps, CU>(useCaseName, options),
+    useCaseBuilder<Output, Input, Deps, CU, IsTransactional>(
+      useCaseName,
+      options,
+    ),
+  notTransactional: () =>
+    useCaseBuilder<Output, Input, Deps, CurrentUser, false>(useCaseName, {
+      inputSchema: options.inputSchema,
+      isTransactional: false as false,
+    }),
   build: (cb) => (config) => ({
     useCaseName,
     execute: (async (inputParams: Input, currentUser: CurrentUser) => {
@@ -71,36 +135,65 @@ export const useCaseBuilder = <
         id:
           extractValue("id", inputParams) ?? extractValue("siret", inputParams),
       });
-      const searchParams = getSearchParams(useCaseName, validParams);
 
-      return config.uowPerformer
-        .perform((uow) =>
-          Sentry.startSpan({ name: useCaseName }, async () =>
-            cb({
-              inputParams: validParams,
-              uow,
-              deps: (config as any).deps,
-              currentUser,
-            }),
-          ),
-        )
-        .then((result) => {
-          logger.info({
-            useCaseName,
-            durationInSeconds: calculateDurationInSecondsFrom(startDate),
-            logStatus: "ok",
+      if (options.isTransactional) {
+        const searchParams = getSearchParams(useCaseName, validParams);
+
+        return (config as any).uowPerformer
+          .perform((uow: UnitOfWork) =>
+            Sentry.startSpan({ name: useCaseName }, async () =>
+              cb({
+                inputParams: validParams,
+                uow,
+                deps: (config as any).deps,
+                currentUser,
+              } as any),
+            ),
+          )
+          .then((result: Output) => {
+            logger.info({
+              useCaseName,
+              durationInSeconds: calculateDurationInSecondsFrom(startDate),
+              logStatus: "ok",
+            });
+            return result;
+          })
+          .catch((error: any) => {
+            logger.error({
+              useCaseName,
+              durationInSeconds: calculateDurationInSecondsFrom(startDate),
+              searchParams,
+              message: castError(error).message,
+            });
+            throw error;
           });
-          return result;
-        })
-        .catch((error) => {
-          logger.error({
-            useCaseName,
-            durationInSeconds: calculateDurationInSecondsFrom(startDate),
-            searchParams,
-            message: castError(error).message,
-          });
-          throw error;
+      }
+
+      try {
+        const result = await Sentry.startSpan({ name: useCaseName }, () =>
+          cb({
+            inputParams: validParams,
+            currentUser,
+            deps: (config as any).deps,
+          } as any),
+        );
+
+        const durationInSeconds = calculateDurationInSecondsFrom(startDate);
+        logger.info({
+          useCaseName,
+          durationInSeconds,
         });
+
+        return result;
+      } catch (error: any) {
+        const durationInSeconds = calculateDurationInSecondsFrom(startDate);
+        logger.error({
+          useCaseName,
+          durationInSeconds,
+          message: castError(error)?.message,
+        });
+        throw error;
+      }
     }) as any,
   }),
 });
