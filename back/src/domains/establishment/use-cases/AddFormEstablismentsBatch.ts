ab20991d45f97b61afe9719b9bbd4861f434b00b
Jérôme Burkard <burkardjerome@gmail.com>
2025-09-23 14:13:49 +0200

    move AddFormEstablishmentBatch to useCaseBuilder

--- back/src/domains/establishment/use-cases/AddFormEstablismentsBatch.ts
+++ back/src/domains/establishment/use-cases/AddFormEstablismentsBatch.ts
@@ -3,81 +3,79 @@ import {
   castError,
   type EstablishmentBatchReport,
   executeInSequence,
-  type FormEstablishmentBatchDto,
   formEstablishmentBatchSchema,
   slugify,
 } from "shared";
 import { throwIfNotAdmin } from "../../connected-users/helpers/authorization.helper";
-import { UseCase } from "../../core/UseCase";
 import type { UnitOfWorkPerformer } from "../../core/unit-of-work/ports/UnitOfWorkPerformer";
+import { useCaseBuilder } from "../../core/useCaseBuilder";
 import type { GroupEntity } from "../entities/GroupEntity";
 import type { InsertEstablishmentAggregateFromForm } from "./InsertEstablishmentAggregateFromFormEstablishement";
 
-export class AddFormEstablishmentBatch extends UseCase<
-  FormEstablishmentBatchDto,
-  EstablishmentBatchReport,
-  ConnectedUser
-> {
-  protected inputSchema = formEstablishmentBatchSchema;
+export type AddFormEstablishmentBatch = ReturnType<
+  typeof makeAddFormEstablishmentBatch
+>;
 
-  constructor(
-    private insertEstablishmentAggregateFromForm: InsertEstablishmentAggregateFromForm,
-    private uowPerformer: UnitOfWorkPerformer,
-  ) {
-    super();
-  }
+export const makeAddFormEstablishmentBatch =
+  useCaseBuilder<EstablishmentBatchReport>("AddFormEstablishmentBatch")
+    .notTransactional()
+    .withInput(formEstablishmentBatchSchema)
+    .withOutput<EstablishmentBatchReport>()
+    .withCurrentUser<ConnectedUser>()
+    .withDeps<{
+      insertEstablishmentAggregateFromForm: InsertEstablishmentAggregateFromForm;
+      uowPerformer: UnitOfWorkPerformer;
+    }>()
+    .build(
+      async ({
+        inputParams: { formEstablishments, groupName, description, title },
+        currentUser,
+        deps: { insertEstablishmentAggregateFromForm, uowPerformer },
+      }) => {
+        throwIfNotAdmin(currentUser);
 
-  protected async _execute(
-    {
-      formEstablishments,
-      groupName,
-      description,
-      title,
-    }: FormEstablishmentBatchDto,
-    currentUser: ConnectedUser,
-  ): Promise<EstablishmentBatchReport> {
-    throwIfNotAdmin(currentUser);
-    const group: GroupEntity = {
-      slug: slugify(groupName),
-      name: groupName,
-      sirets: formEstablishments.map(({ siret }) => siret),
-      options: {
-        heroHeader: {
-          title,
-          description,
-        },
-      },
-    };
-    await this.uowPerformer.perform((uow) => uow.groupRepository.save(group));
-
-    const report: EstablishmentBatchReport = {
-      numberOfEstablishmentsProcessed: 0,
-      numberOfSuccess: 0,
-      failures: [],
-    };
-
-    await executeInSequence(formEstablishments, async (formEstablishment) =>
-      this.insertEstablishmentAggregateFromForm
-        .execute(
-          {
-            formEstablishment,
+        const group: GroupEntity = {
+          slug: slugify(groupName),
+          name: groupName,
+          sirets: formEstablishments.map(({ siret }) => siret),
+          options: {
+            heroHeader: {
+              title,
+              description,
+            },
           },
-          currentUser,
-        )
-        .then(() => {
-          report.numberOfSuccess += 1;
-        })
-        .catch((error) => {
-          report.failures.push({
-            siret: formEstablishment.siret,
-            errorMessage: castError(error).message,
-          });
-        })
-        .finally(() => {
-          report.numberOfEstablishmentsProcessed += 1;
-        }),
-    );
+        };
 
-    return report;
-  }
-}
+        await uowPerformer.perform((uow) => uow.groupRepository.save(group));
+
+        const report: EstablishmentBatchReport = {
+          numberOfEstablishmentsProcessed: 0,
+          numberOfSuccess: 0,
+          failures: [],
+        };
+
+        await executeInSequence(formEstablishments, async (formEstablishment) =>
+          insertEstablishmentAggregateFromForm
+            .execute(
+              {
+                formEstablishment,
+              },
+              currentUser,
+            )
+            .then(() => {
+              report.numberOfSuccess += 1;
+            })
+            .catch((error) => {
+              report.failures.push({
+                siret: formEstablishment.siret,
+                errorMessage: castError(error).message,
+              });
+            })
+            .finally(() => {
+              report.numberOfEstablishmentsProcessed += 1;
+            }),
+        );
+
+        return report;
+      },
+    );
