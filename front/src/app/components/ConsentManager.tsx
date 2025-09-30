254715b28ed396871b692efecff3cfc6a1098439
Enguerran Weiss <enguerranweiss@gmail.com>
2025-09-30 16:13:53 +0200

    added subfinalities on cookie consent manager

--- front/src/app/components/ConsentManager.tsx
+++ front/src/app/components/ConsentManager.tsx
@@ -7,11 +7,79 @@ export const { ConsentBannerAndConsentManagement, useConsent } =
         title: "Mesure d'audience",
         description:
           "Nous utilisons des cookies pour mesurer l’audience de notre site et améliorer son contenu.",
+        subFinalities: {
+          matomo: {
+            title: "Matomo",
+            description: (
+              <>
+                <p>
+                  Matomo est un outil de mesure d'audience qui nous permet de
+                  suivre les visites de notre site et d'améliorer son contenu.
+                  Il peut déposer 7 cookies sur votre navigateur.
+                </p>
+                <p>
+                  <a
+                    href="https://matomo.org/faq/general/faq_146/"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    Voir le site officiel
+                  </a>
+                </p>
+              </>
+            ),
+          },
+          metabase: {
+            title: "Metabase",
+            description: (
+              <>
+                <p>
+                  Metabase est un outil de visualisation de données qui nous
+                  permet de suivre les données et produire des tableaux de bord.
+                  Il peut déposer 1 cookie sur votre navigateur.
+                </p>
+                <p>
+                  <a
+                    href="https://www.metabase.com/hosting/subprocessors/"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    Voir le site officiel
+                  </a>
+                  .
+                </p>
+              </>
+            ),
+          },
+        },
       },
       support: {
         title: "Support et assistance utilisateur",
         description:
           "Nous utilisons des cookies pour vous proposer la fonctionnalité de contact par chat avec le support d'Immersion Facilitée.",
+        subFinalities: {
+          crisp: {
+            title: "Crisp",
+            description: (
+              <>
+                <p>
+                  Crisp est un outil de chat qui nous permet de vous proposer la
+                  fonctionnalité de contact par chat avec le support d'Immersion
+                  Facilitée. Il peut déposer 5 cookies sur votre navigateur.
+                </p>
+                <p>
+                  <a
+                    href="https://crisp.chat/fr/privacy/"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    Voir le site officiel
+                  </a>
+                </p>
+              </>
+            ),
+          },
+        },
       },
     }),
     personalDataPolicyLinkProps: {
