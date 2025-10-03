27685495d7ee43f416d7deefa98a6d69bf8141e7
Benjamin Bohec <bbohec.pro@gmail.com>
2025-10-03 10:25:08 +0200

    WIP Stress test PgUowPerformer
    Signed-off-by: Benjamin Bohec <bbohec.pro@gmail.com>

--- back/src/domains/core/unit-of-work/adapters/PgUowPerformer.integration.test.ts
+++ back/src/domains/core/unit-of-work/adapters/PgUowPerformer.integration.test.ts
@@ -1,16 +1,18 @@
-import type { Pool } from "pg";
+import { Pool } from "pg";
 import { expectPromiseToFailWithError, type Notification } from "shared";
+import { v4 as uuid } from "uuid";
+import { AppConfig } from "../../../../config/bootstrap/appConfig";
 import {
   type KyselyDb,
   makeKyselyDb,
 } from "../../../../config/pg/kysely/kyselyUtils";
-import { makeTestPgPool } from "../../../../config/pg/pgPool";
 import {
   type CreateNewEvent,
   makeCreateNewEvent,
 } from "../../events/ports/EventBus";
 import { CustomTimeGateway } from "../../time-gateway/adapters/CustomTimeGateway";
 import { TestUuidGenerator } from "../../uuid-generator/adapters/UuidGeneratorImplementations";
+import type { UuidGenerator } from "../../uuid-generator/ports/UuidGenerator";
 import type { UnitOfWork } from "../ports/UnitOfWork";
 import { createPgUow } from "./createPgUow";
 import { PgUowPerformer } from "./PgUowPerformer";
@@ -19,17 +21,25 @@ describe("PgUowPerformer", () => {
   let pool: Pool;
   let db: KyselyDb;
   let pgUowPerformer: PgUowPerformer;
-
-  const uuidGenerator = new TestUuidGenerator();
-  const createNewEvent = makeCreateNewEvent({
-    uuidGenerator,
-    timeGateway: new CustomTimeGateway(),
-  });
+  let uuidGenerator: TestUuidGenerator;
+  let createNewEvent: CreateNewEvent;
 
   beforeAll(async () => {
-    pool = makeTestPgPool();
+    uuidGenerator = new TestUuidGenerator();
+    const config = AppConfig.createFromEnv();
+    pool = new Pool({
+      connectionString: config.pgImmersionDbUrl,
+      idle_in_transaction_session_timeout: 3_000,
+
+      min: 2,
+      max: 25,
+    });
     db = makeKyselyDb(pool);
     pgUowPerformer = new PgUowPerformer(db, createPgUow);
+    createNewEvent = makeCreateNewEvent({
+      uuidGenerator,
+      timeGateway: new CustomTimeGateway(),
+    });
   });
 
   beforeEach(async () => {
@@ -45,72 +55,103 @@ describe("PgUowPerformer", () => {
     await pool.end();
   });
 
-  it("saves everything when all goes fine", async () => {
-    uuidGenerator.setNextUuid("11111111-1111-1111-1111-111111111111");
+  describe("unit tests", () => {
+    it("saves everything when all goes fine", async () => {
+      uuidGenerator.setNextUuids([...Array(2)].map(() => uuid()));
 
-    await pgUowPerformer.perform(useCaseUnderTest(createNewEvent));
-    await expectLengthOfRepos({
-      notificationLength: 1,
-      outboxLength: 1,
-      db,
+      await pgUowPerformer.perform(
+        useCaseUnderTest(createNewEvent, uuidGenerator),
+      );
+      await expectLengthOfRepos({
+        notificationLength: 1,
+        outboxLength: 1,
+        db,
+      });
+    });
+
+    it("keeps modifications atomic when something goes wrong", async () => {
+      const id = "a failing uuid";
+      uuidGenerator.setNextUuid(id);
+
+      expectPromiseToFailWithError(
+        pgUowPerformer.perform(useCaseUnderTest(createNewEvent, uuidGenerator)),
+        new Error(`invalid input syntax for type uuid: "${id}"`),
+      );
+
+      await expectLengthOfRepos({
+        notificationLength: 0,
+        outboxLength: 0,
+        db,
+      });
     });
   });
 
-  it("keeps modifications atomic when something goes wrong", async () => {
-    const id = "a failing uuid";
-    uuidGenerator.setNextUuid(id);
+  describe("stress test", () => {
+    const numberOfParallelTransactions = 10000;
+    const timeoutInMs = 10_000;
+    it(
+      `try ${numberOfParallelTransactions} parallel transactions through max ${timeoutInMs}ms.`,
+      async () => {
+        uuidGenerator.setNextUuids(
+          [...Array(2 * numberOfParallelTransactions)].map(() => uuid()),
+        );
+        await Promise.all(
+          [...Array(numberOfParallelTransactions)].map(() =>
+            pgUowPerformer.perform(
+              useCaseUnderTest(createNewEvent, uuidGenerator),
+            ),
+          ),
+        );
 
-    expectPromiseToFailWithError(
-      pgUowPerformer.perform(useCaseUnderTest(createNewEvent)),
-      new Error(`invalid input syntax for type uuid: "${id}"`),
-    );
-
-    await expectLengthOfRepos({
-      notificationLength: 0,
-      outboxLength: 0,
-      db,
-    });
-  });
-});
-
-const useCaseUnderTest =
-  (createNewEvent: CreateNewEvent) => async (uow: UnitOfWork) => {
-    const notification: Notification = {
-      id: "11111111-1111-1111-1111-111111111113",
-      createdAt: new Date().toISOString(),
-      kind: "email",
-      templatedContent: {
-        kind: "TEST_EMAIL",
-        params: { input1: "", input2: "", url: "http://" },
-        recipients: ["a@a.com"],
+        await expectLengthOfRepos({
+          notificationLength: numberOfParallelTransactions,
+          outboxLength: numberOfParallelTransactions,
+          db,
+        });
       },
-      followedIds: {},
+      timeoutInMs,
+    );
+  });
+
+  const useCaseUnderTest =
+    (createNewEvent: CreateNewEvent, uuidGenerator: UuidGenerator) =>
+    async (uow: UnitOfWork) => {
+      const notification: Notification = {
+        id: uuidGenerator.new(),
+        createdAt: new Date().toISOString(),
+        kind: "email",
+        templatedContent: {
+          kind: "TEST_EMAIL",
+          params: { input1: "", input2: "", url: "http://" },
+          recipients: ["a@a.com"],
+        },
+        followedIds: {},
+      };
+
+      await uow.notificationRepository.save(notification);
+      await uow.outboxRepository.save(
+        createNewEvent({
+          topic: "NotificationAdded",
+          payload: { id: notification.id, kind: notification.kind },
+        }),
+      );
     };
 
-    await uow.notificationRepository.save(notification);
+  const expectLengthOfRepos = async ({
+    notificationLength,
+    outboxLength,
+    db,
+  }: {
+    notificationLength: number;
+    outboxLength: number;
+    db: KyselyDb;
+  }) => {
+    expect(
+      await db.selectFrom("notifications_email").selectAll().execute(),
+    ).toHaveLength(notificationLength);
 
-    await uow.outboxRepository.save(
-      createNewEvent({
-        topic: "NotificationAdded",
-        payload: { id: notification.id, kind: notification.kind },
-      }),
+    expect(await db.selectFrom("outbox").selectAll().execute()).toHaveLength(
+      outboxLength,
     );
   };
-
-const expectLengthOfRepos = async ({
-  notificationLength,
-  outboxLength,
-  db,
-}: {
-  notificationLength: number;
-  outboxLength: number;
-  db: KyselyDb;
-}) => {
-  expect(
-    await db.selectFrom("notifications_email").selectAll().execute(),
-  ).toHaveLength(notificationLength);
-
-  expect(await db.selectFrom("outbox").selectAll().execute()).toHaveLength(
-    outboxLength,
-  );
-};
+});
