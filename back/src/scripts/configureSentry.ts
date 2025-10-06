276102270a9ab59e1c65f03ccb19ccf2bd55fa2d
Jérôme Burkard <burkardjerome@gmail.com>
2025-10-06 17:47:36 +0200

    add span for http calls in traces

--- back/src/scripts/configureSentry.ts
+++ back/src/scripts/configureSentry.ts
@@ -10,7 +10,11 @@ export const configureSentry = (envType: Environment) => {
   }
   Sentry.init({
     dsn: "https://eb7a121cd835308163ca9966e5c82c98@o4508405260615680.ingest.de.sentry.io/4508999044038736",
-    integrations: [nodeProfilingIntegration()],
+    integrations: [
+      Sentry.httpIntegration(),
+      Sentry.nativeNodeFetchIntegration(),
+      nodeProfilingIntegration(),
+    ],
     environment: envType,
     release: version,
     tracesSampleRate: 0.01,
