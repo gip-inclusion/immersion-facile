03defcf8e5cad2a7f8b9e25f36244c2cbe1e036b
Jérôme Burkard <burkardjerome@gmail.com>
2025-10-27 12:57:31 +0100

    add link to establishment directly from list in user's detail

--- front/src/app/components/establishment/establishments-table/EstablishmentsTablesSection.tsx
+++ front/src/app/components/establishment/establishments-table/EstablishmentsTablesSection.tsx
@@ -3,13 +3,16 @@ import { EstablishmentsRightsTable } from "./EstablishmentsRightsTable";
 
 export const EstablishmentsTablesSection = ({
   withEstablishmentData,
+  isBackofficeAdmin,
 }: {
   withEstablishmentData?: EstablishmentData[];
+  isBackofficeAdmin?: boolean;
 }) => (
   <>
     {withEstablishmentData && withEstablishmentData.length > 0 && (
       <EstablishmentsRightsTable
         withEstablishmentData={withEstablishmentData}
+        isBackofficeAdmin={isBackofficeAdmin}
       />
     )}
   </>
--- front/src/app/components/establishment/establishments-table/establishment-line/EstablishmentLineBusinessName.tsx
+++ front/src/app/components/establishment/establishments-table/establishment-line/EstablishmentLineBusinessName.tsx
@@ -1,11 +1,15 @@
 import { fr } from "@codegouvfr/react-dsfr";
+import Button from "@codegouvfr/react-dsfr/Button";
 import { Tag } from "react-design-system";
-import type { EstablishmentData } from "shared";
+import { domElementIds, type EstablishmentData } from "shared";
+import { routes } from "src/app/routes/routes";
 
 export const EstablishmentLineBusinessName = ({
   data,
+  isBackofficeAdmin,
 }: {
   data: EstablishmentData;
+  isBackofficeAdmin?: boolean;
 }) => (
   <ul key={data.siret} className={fr.cx("fr-raw-list")}>
     <li>
@@ -16,5 +20,19 @@ export const EstablishmentLineBusinessName = ({
       {" "}
       <span className={fr.cx("fr-hint-text")}>{data.siret}</span>
     </li>
+    {isBackofficeAdmin && (
+      <Button
+        priority="tertiary no outline"
+        id={`${domElementIds.profile.adminEstablishmentLink}-${data.siret}`}
+        size="small"
+        linkProps={
+          routes.manageEstablishmentAdmin({
+            siret: data.siret,
+          }).link
+        }
+      >
+        Voir comme admin IF
+      </Button>
+    )}
   </ul>
 );
