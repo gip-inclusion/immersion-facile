93a5d77fd45494b0af55e20b5d9b7c451f21b45b
clement-duport <clement.duport@beta.gouv.fr>
2025-10-28 12:13:46 +0100

    feat:  update search placeholder and organize filter

--- libs/react-design-system/src/immersionFacile/components/rich-table/RichTable.tsx
+++ libs/react-design-system/src/immersionFacile/components/rich-table/RichTable.tsx
@@ -107,7 +107,7 @@ export const RichTable = ({
                 setSearchValue(event.target.value);
               },
             }}
-            className={fr.cx("fr-mb-0", "fr-col-lg-4")}
+            className={fr.cx("fr-mb-0", "fr-col-lg-7")}
           />
           <Button type="submit">Rechercher</Button>
         </form>
