132778c4bc035d518bf40c575f42c3ade45bc8f0
Benjamin Bohec <bbohec.pro@gmail.com>
2025-11-04 18:08:26 +0100

    back PgDiscussionRepository integration
    Signed-off-by: Benjamin Bohec <bbohec.pro@gmail.com>

---
+++ back/src/config/pg/migrations/1761835800000_discussion-update-date-and-discussion-stats.ts
@@ -0,0 +1,93 @@
+/* eslint-disable @typescript-eslint/naming-convention */
+import type { ColumnDefinitions, MigrationBuilder } from "node-pg-migrate";
+
+export const shorthands: ColumnDefinitions | undefined = undefined;
+
+export async function up(pgm: MigrationBuilder): Promise<void> {
+  pgm.addColumn("discussions", {
+    updated_at: {
+      type: "timestamptz",
+      notNull: false,
+    },
+  });
+
+  pgm.sql(`
+    UPDATE discussions
+    SET updated_at = (
+      SELECT MAX(exchanges.sent_at) + interval '3 months'
+      FROM exchanges
+      WHERE exchanges.discussion_id = discussions.id
+    )
+    WHERE discussions.rejection_kind = 'DEPRECATED'
+  `);
+
+  pgm.sql(`
+    UPDATE discussions
+    SET updated_at = (
+      SELECT MAX(exchanges.sent_at)
+      FROM exchanges
+      WHERE exchanges.discussion_id = discussions.id
+    )
+    WHERE discussions.rejection_kind != 'DEPRECATED'
+    OR discussions.rejection_kind IS NULL
+  `);
+
+  pgm.alterColumn("discussions", "updated_at", {
+    notNull: true,
+  });
+  pgm.alterColumn("discussions", "status", {
+    notNull: true,
+    default: null,
+  });
+  pgm.createIndex("discussions", "status");
+  pgm.createIndex("discussions", "updated_at");
+
+  pgm.createTable("discussions__stats", {
+    siret: { type: "char(14)", notNull: true },
+    kind: { type: "text", notNull: true },
+    status: { type: "discussion_status", notNull: true },
+    contact_method: { type: "text", notNull: true },
+    immersion_objective: { type: "immersion_objectives", notNull: false },
+    department_code: { type: "text", notNull: true },
+    candidate_firstname: { type: "text", notNull: true },
+    creation_date: { type: "date", notNull: true },
+    appellation_code: { type: "int", notNull: true },
+    discussions_total: { type: "int", notNull: true },
+    discussions_answered_by_establishment: { type: "int", notNull: true },
+    discussions_with_convention: { type: "int", notNull: true },
+  });
+  pgm.addConstraint(
+    "discussions__stats",
+    "fk_ogr_appellation_public_appellations_data",
+    {
+      foreignKeys: {
+        columns: "appellation_code",
+        references: "public_appellations_data(ogr_appellation)",
+      },
+    },
+  );
+  pgm.addConstraint(
+    "discussions__stats",
+    "unique_siret_kind_status_contact_method_immersion_objective_department_code_candidate_firstname_creation_date_appellation_code",
+    {
+      unique: [
+        "siret",
+        "kind",
+        "status",
+        "contact_method",
+        "immersion_objective",
+        "department_code",
+        "candidate_firstname",
+        "creation_date",
+        "appellation_code",
+      ],
+    },
+  );
+}
+
+export async function down(pgm: MigrationBuilder): Promise<void> {
+  pgm.dropIndex("discussions", "updated_at");
+  pgm.dropIndex("discussions", "status");
+  pgm.dropColumn("discussions", "updated_at");
+  pgm.dropTable("discussions__stats");
+}
