5fa578b904434294ea41916379c36fc417f87576
Benjamin Bohec <bbohec.pro@gmail.com>
2025-11-05 13:24:27 +0100

    back DeleteEvents script
    Signed-off-by: Benjamin Bohec <bbohec.pro@gmail.com>

---
+++ back/src/scripts/triggerDeleteOldEvents.ts
@@ -0,0 +1,38 @@
+import { AppConfig } from "../config/bootstrap/appConfig";
+import { createMakeProductionPgPool } from "../config/pg/pgPool";
+import { makeDeleteEvents } from "../domains/core/events/usecase/DeleteEvents";
+import { RealTimeGateway } from "../domains/core/time-gateway/adapters/RealTimeGateway";
+import { createDbRelatedSystems } from "../domains/core/unit-of-work/adapters/createDbRelatedSystems";
+import { createLogger } from "../utils/logger";
+import { handleCRONScript } from "./handleCRONScript";
+
+const logger = createLogger(__filename);
+const config = AppConfig.createFromEnv();
+
+const deleteOldDiscussionMessages = async () => {
+  const { uowPerformer } = createDbRelatedSystems(
+    config,
+    createMakeProductionPgPool(config),
+  );
+
+  return makeDeleteEvents({
+    uowPerformer,
+    deps: { timeGateway: new RealTimeGateway() },
+  }).execute({
+    limit: 250_000,
+  });
+};
+
+export const triggerDeleteOldDiscussionMessages = ({
+  exitOnFinish,
+}: {
+  exitOnFinish: boolean;
+}) =>
+  handleCRONScript({
+    name: "triggerDeleteOldEvents",
+    config,
+    script: deleteOldDiscussionMessages,
+    handleResults: ({ deletedEvents }) => `${deletedEvents} events deleted.`,
+    logger,
+    exitOnFinish,
+  });
