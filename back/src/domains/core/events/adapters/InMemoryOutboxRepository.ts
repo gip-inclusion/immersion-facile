d96b5e7583f695b4a28e30b171252151d5074a11
Benjamin Bohec <bbohec.pro@gmail.com>
2025-11-05 15:12:31 +0100

    back OutboxRepository
    Signed-off-by: Benjamin Bohec <bbohec.pro@gmail.com>

--- back/src/domains/core/events/adapters/InMemoryOutboxRepository.ts
+++ back/src/domains/core/events/adapters/InMemoryOutboxRepository.ts
@@ -1,12 +1,46 @@
 import { values } from "ramda";
 import { createLogger } from "../../../../utils/logger";
 import type { DomainEvent, EventStatus } from "../events";
-import type { OutboxRepository } from "../ports/OutboxRepository";
+import type {
+  DeleteOldestEventsParams,
+  OutboxRepository,
+} from "../ports/OutboxRepository";
 
 const logger = createLogger(__filename);
 
 export class InMemoryOutboxRepository implements OutboxRepository {
-  constructor(private readonly _events: Record<string, DomainEvent> = {}) {}
+  constructor(private _events: Record<string, DomainEvent> = {}) {}
+  public async deleteOldestEvents({
+    limit,
+    occuredAt,
+  }: DeleteOldestEventsParams): Promise<number> {
+    const { deleted, eventsToKeep } = this.events
+      .sort((a, b) => (a.occurredAt >= b.occurredAt ? 0 : -1))
+      .reduce<{
+        eventsToKeep: Record<string, DomainEvent>;
+        deleted: number;
+      }>(
+        ({ deleted, eventsToKeep }, event) => {
+          const isDeleted = !(
+            deleted === limit ||
+            new Date(event.occurredAt) > occuredAt.to ||
+            (occuredAt.from && new Date(event.occurredAt) < occuredAt.from)
+          );
+
+          return {
+            eventsToKeep: {
+              ...eventsToKeep,
+              ...(isDeleted ? {} : { currentEvent: event }),
+            },
+            deleted: deleted + (isDeleted ? 1 : 0),
+          };
+        },
+        { eventsToKeep: {}, deleted: 0 },
+      );
+
+    this._events = eventsToKeep;
+    return deleted;
+  }
 
   public async countAllEvents({
     status,
@@ -17,6 +51,12 @@ export class InMemoryOutboxRepository implements OutboxRepository {
   }
 
   //test purposes
+  public set events(events: DomainEvent[]) {
+    this._events = events.reduce<Record<string, DomainEvent>>(
+      (acc, value) => ({ ...acc, [value.id]: value }),
+      {},
+    );
+  }
   public get events(): DomainEvent[] {
     return values(this._events);
   }
