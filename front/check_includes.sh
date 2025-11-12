f8ec07f759770ca6ff559c713a7708cf2b244eb5
celineung <celine.ung.pro@gmail.com>
2025-11-12 16:18:47 +0100

    exclude feedback (case insensitive) from check_includes script

--- front/check_includes.sh
+++ front/check_includes.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-files=$(grep --include=\*.{ts,tsx} -rnw 'src' -e 'import .* from .*back/.*' -e 'import .* from .*shared/.*' -e 'import .* from .*react-design-system/.*' -e 'import .* from .*/libs/.*' | grep -v 'import .* from .*feedback.*')
+files=$(grep --include=\*.{ts,tsx} -rnw 'src' -e 'import .* from .*back/.*' -e 'import .* from .*shared/.*' -e 'import .* from .*react-design-system/.*' -e 'import .* from .*/libs/.*' | grep -vi 'import .* from .*feedback.*')
 if [[ $files ]]; then 
   echo -e "\033[0;31mFound includes of back or detailled shared code in front!\033[0m";
   echo $files;
