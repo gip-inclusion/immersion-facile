66df50f68f8ca9e97a211f48a105f43770b3b295
Jérôme Burkard <burkardjerome@gmail.com>
2025-11-18 22:18:17 +0100

    add base establishment models (establishments, locations, offers)

---
+++ bi/models/establishments/establishments.sql
@@ -0,0 +1,41 @@
+{{
+  config(
+    materialized='table',
+    schema='analytics'
+  )
+}}
+
+select
+    siret,
+    name,
+    customized_name,
+    created_at,
+    update_date,
+    naf_code,
+    naf_nomenclature,
+    contact_mode,
+    is_open,
+    source_provider,
+    number_employees,
+    is_commited,
+    fit_for_disabled_workers,
+    website,
+    additional_information,
+    searchable_by_students,
+    searchable_by_job_seekers,
+    max_contacts_per_month,
+    next_availability_date,
+    is_max_discussions_for_period_reached,
+    last_insee_check_date,
+    acquisition_campaign,
+    acquisition_keyword,
+    score,
+    status,
+    status_updated_at,
+    welcome_address_street_number_and_address,
+    welcome_address_postcode,
+    welcome_address_city,
+    welcome_address_department_code,
+    welcome_address_lat,
+    welcome_address_lon
+from {{ source('immersion', 'establishments') }}
