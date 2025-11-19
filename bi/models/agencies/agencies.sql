9a96d6b70f5ff45859447ac6e7d95d0dd5f126e2
Jérôme Burkard <burkardjerome@gmail.com>
2025-11-19 09:07:36 +0100

    add agencies, conventions, and user relationship models

---
+++ bi/models/agencies/agencies.sql
@@ -0,0 +1,39 @@
+{{
+  config(
+    materialized='table',
+    schema='analytics'
+  )
+}}
+
+select
+    a.id,
+    a.name,
+    case
+        when a.kind = 'pole-emploi' then 'france-travail'
+        else a.kind
+    end as kind,
+    a.questionnaire_url,
+    a.email_signature,
+    a.legacy_address,
+    a.position,
+    a.created_at,
+    a.updated_at,
+    a.status,
+    a.agency_siret,
+    a.code_safir,
+    a.logo_url,
+    a.street_number_and_address,
+    a.post_code,
+    a.city,
+    a.department_code,
+    pdr.department_name,
+    pdr.region_name,
+    a.refers_to_agency_id,
+    a.rejection_justification,
+    a.covered_departments,
+    a.acquisition_campaign,
+    a.acquisition_keyword,
+    a.phone_number
+from {{ source('immersion', 'agencies') }} as a
+left join {{ source('immersion', 'public_department_region') }} as pdr
+    on pdr.department_code = a.department_code
