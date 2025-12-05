ebf20d8e490c8193703612154b53453e588fa57e
Enguerran Weiss <enguerranweiss@gmail.com>
2025-12-05 16:28:17 +0100

    separate components for form establishment offers

---
+++ front/src/app/components/forms/establishment/sections/offer/OfferCard.tsx
@@ -0,0 +1,62 @@
+import { fr } from "@codegouvfr/react-dsfr";
+import Badge from "@codegouvfr/react-dsfr/Badge";
+import ButtonsGroup from "@codegouvfr/react-dsfr/ButtonsGroup";
+import { useFormContext } from "react-hook-form";
+import {
+  type FormEstablishmentDto,
+  remoteWorkModeLabels,
+  removeAtIndex,
+} from "shared";
+
+export const OfferCard = ({
+  index,
+  onEditOfferClick,
+}: {
+  index: number;
+  onEditOfferClick: () => void;
+}) => {
+  const { setValue, watch } = useFormContext<FormEstablishmentDto>();
+  const formValues = watch();
+  const { appellationLabel, remoteWorkMode } = formValues.offers[index];
+  return (
+    <div className={fr.cx("fr-col-12", "fr-col-lg-6")}>
+      <article className={fr.cx("fr-card", "fr-px-4w", "fr-py-2w")}>
+        <div className={fr.cx("fr-grid-row", "fr-grid-row--top")}>
+          <h3 className={fr.cx("fr-h6", "fr-col-lg-7", "fr-col-5", "fr-mb-0")}>
+            {appellationLabel}
+          </h3>
+          <Badge className={fr.cx("fr-badge--purple-glycine", "fr-ml-auto")}>
+            {remoteWorkModeLabels[remoteWorkMode].label}
+          </Badge>
+        </div>
+        <p className={fr.cx("fr-mt-2w", "fr-text--sm")}>
+          {remoteWorkModeLabels[remoteWorkMode].description}
+        </p>
+        <ButtonsGroup
+          buttons={[
+            {
+              children: "Modifier",
+              iconPosition: "right",
+              iconId: "fr-icon-edit-line",
+              priority: "tertiary",
+              onClick: onEditOfferClick,
+            },
+            {
+              children: "Supprimer",
+              iconPosition: "right",
+              iconId: "fr-icon-delete-bin-line",
+              priority: "tertiary",
+              onClick: () => {
+                const offers = formValues.offers;
+                const newOffers = removeAtIndex(offers, index);
+                setValue("offers", newOffers);
+              },
+            },
+          ]}
+          className={fr.cx("fr-mt-auto")}
+          inlineLayoutWhen="always"
+        />
+      </article>
+    </div>
+  );
+};
