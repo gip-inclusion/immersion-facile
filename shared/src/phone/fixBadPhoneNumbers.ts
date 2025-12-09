799144e3175f9c3b2481fb0a58253883e54eb778
tpicaud <tom.picaud@hotmail.com>
2025-12-09 15:01:45 +0100

    finalize verify and fix phone number in db

--- shared/src/phone/fixBadPhoneNumbers.ts
+++ shared/src/phone/fixBadPhoneNumbers.ts
@@ -2,67 +2,77 @@ import fs from "fs";
 import { isValidPhoneNumber, parsePhoneNumber } from "libphonenumber-js/max";
 import { getSupportedCountryCodesForCountry } from "../address/address.dto";
 
-interface PhoneInDB {
-	phone_number: string;
-	phone_label_in_table: string;
-	source_table: string;
-	related_id: string;
-}
+type PhoneInDB = {
+  phone_number: string;
+  phone_label_in_table: string;
+  source_table: string;
+  related_id: string;
+};
 
-export const verifyAndFixPhoneNumberCountryCodeFromJsonFile = (
-	jsonFileUri: string,
+export const verifyAndFixPhoneInDBFromJsonFile = (
+  jsonFileUri: string,
 ): {
-	correctPhonesInDB: PhoneInDB[];
-	fixedPhonesInDB: { phoneInDB: PhoneInDB; fixedPhoneNumber: string }[];
-	unfixablePhonesInDB: PhoneInDB[];
+  correctPhonesInDB: PhoneInDB[];
+  fixedPhonesInDB: { phoneInDB: PhoneInDB; fixedPhoneNumber: string }[];
+  unfixablePhonesInDB: PhoneInDB[];
 } => {
-	const phonesInDB = getPhonesInDBFromJsonFile(jsonFileUri);
-
-	const correctPhonesInDB: PhoneInDB[] = [];
-	const unfixablePhonesInDB: PhoneInDB[] = [];
-	const fixedPhonesInDB: { phoneInDB: PhoneInDB; fixedPhoneNumber: string }[] =
-		[];
-	phonesInDB.map((phone) => {
-		if (isValidPhoneNumber(phone.phone_number)) {
-			correctPhonesInDB.push(phone);
-			return;
-		}
-
-		const fixedPhoneNumber = fixPhoneNumberCountryCode(phone.phone_number);
-		if (!fixedPhoneNumber) {
-			unfixablePhonesInDB.push(phone);
-			return;
-		}
-
-		fixedPhonesInDB.push({ phoneInDB: phone, fixedPhoneNumber });
-	});
-	return { correctPhonesInDB, fixedPhonesInDB, unfixablePhonesInDB };
+  const fileContent = fs.readFileSync(jsonFileUri, "utf8");
+  const phonesInDB: PhoneInDB[] = JSON.parse(fileContent);
+  return verifyAndFixPhonesInDB(phonesInDB);
 };
 
-const fixPhoneNumberCountryCode = (phoneNumber: string): string | undefined => {
-	const phoneNumberWithoutCountryCode = `0${parsePhoneNumber(phoneNumber).nationalNumber.toString()}`;
+export const verifyAndFixPhonesInDB = (
+  phonesInDB: PhoneInDB[],
+): {
+  correctPhonesInDB: PhoneInDB[];
+  fixedPhonesInDB: { phoneInDB: PhoneInDB; fixedPhoneNumber: string }[];
+  unfixablePhonesInDB: PhoneInDB[];
+} => {
+  const correctPhonesInDB: PhoneInDB[] = [];
+  const unfixablePhonesInDB: PhoneInDB[] = [];
+  const fixedPhonesInDB: { phoneInDB: PhoneInDB; fixedPhoneNumber: string }[] =
+    [];
 
-	const newCountryCode = getSupportedCountryCodesForCountry("FR").find(
-		(countryCode) => {
-			console.log(countryCode);
-			return isValidPhoneNumber(phoneNumberWithoutCountryCode, countryCode);
-		},
-	);
+  phonesInDB.map((phone) => {
+    if (isValidPhoneNumber(phone.phone_number)) {
+      correctPhonesInDB.push(phone);
+      return;
+    }
 
-	if (!newCountryCode) {
-		return undefined;
-	}
+    const fixedPhoneNumber = fixPhoneNumberCountryCode(phone.phone_number);
+    if (fixedPhoneNumber) {
+      fixedPhonesInDB.push({ phoneInDB: phone, fixedPhoneNumber });
+      return;
+    }
 
-	const fixedPhoneNumber = parsePhoneNumber(
-		phoneNumberWithoutCountryCode,
-		newCountryCode,
-	).format("E.164");
-
-	return fixedPhoneNumber;
+    unfixablePhonesInDB.push(phone);
+  });
+  return { correctPhonesInDB, fixedPhonesInDB, unfixablePhonesInDB };
 };
 
-const getPhonesInDBFromJsonFile = (jsonFileUri: string): PhoneInDB[] => {
-	const fileContent = fs.readFileSync(jsonFileUri, "utf8");
-	const data: PhoneInDB[] = JSON.parse(fileContent);
-	return data;
+export const fixPhoneNumberCountryCode = (
+  phoneNumber: string,
+): string | undefined => {
+  const phoneNumberWithoutCountryCode = `0${parsePhoneNumber(phoneNumber).nationalNumber.toString()}`;
+
+  const newCountryCode = getSupportedCountryCodesForCountry("FR").find(
+    (countryCode) => {
+      return isValidPhoneNumber(phoneNumberWithoutCountryCode, countryCode);
+    },
+  );
+
+  if (!newCountryCode) {
+    return undefined;
+  }
+
+  const fixedPhoneNumber = parsePhoneNumber(
+    phoneNumberWithoutCountryCode,
+    newCountryCode,
+  ).format("E.164");
+
+  if (!isValidPhoneNumber(fixedPhoneNumber)) {
+    return undefined;
+  }
+
+  return fixedPhoneNumber;
 };
