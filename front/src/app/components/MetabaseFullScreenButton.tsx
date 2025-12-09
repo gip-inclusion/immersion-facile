6a053024fc273c479c0c51ad3340bc1161fee5e4
Jérôme Burkard <burkardjerome@gmail.com>
2025-12-09 11:44:01 +0100

    change wording to open conventions old metabase

--- front/src/app/components/MetabaseFullScreenButton.tsx
+++ front/src/app/components/MetabaseFullScreenButton.tsx
@@ -1,24 +1,47 @@
 import { fr } from "@codegouvfr/react-dsfr";
 import Button from "@codegouvfr/react-dsfr/Button";
+import Tooltip from "@codegouvfr/react-dsfr/Tooltip";
 import type { AbsoluteUrl } from "shared";
 
 export const MetabaseFullScreenButton = ({
   url,
+  tooltipText,
   label = "Ouvrir en plein écran",
 }: {
   url: AbsoluteUrl;
   label?: string;
-}) => (
-  <Button
-    linkProps={{
-      target: "_blank",
-      rel: "noreferrer",
-      href: url,
-    }}
-    size="small"
-    priority="tertiary"
-    className={fr.cx("fr-ml-auto")}
-  >
-    {label}
-  </Button>
-);
+  tooltipText?: string;
+}) => {
+  if (tooltipText)
+    return (
+      <Tooltip title={tooltipText}>
+        <Button
+          linkProps={{
+            target: "_blank",
+            rel: "noreferrer",
+            href: url,
+          }}
+          size="small"
+          priority="tertiary"
+          className={fr.cx("fr-ml-auto")}
+        >
+          {label}
+        </Button>
+      </Tooltip>
+    );
+
+  return (
+    <Button
+      linkProps={{
+        target: "_blank",
+        rel: "noreferrer",
+        href: url,
+      }}
+      size="small"
+      priority="tertiary"
+      className={fr.cx("fr-ml-auto")}
+    >
+      {label}
+    </Button>
+  );
+};
