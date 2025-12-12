aa415017a8f356f3964fa99bf107f6dcbd687043
Benjamin Bohec <bbohec.pro@gmail.com>
2025-12-12 16:19:11 +0100

    back core OfferEntity with RemoteWorkMode
    Signed-off-by: Benjamin Bohec <bbohec.pro@gmail.com>

--- back/src/domains/establishment/entities/OfferEntity.ts
+++ back/src/domains/establishment/entities/OfferEntity.ts
@@ -1,8 +1,9 @@
-import type { RomeCode } from "shared";
+import type { RemoteWorkMode, RomeCode } from "shared";
 
 export type OfferEntity = {
   appellationCode: string;
   appellationLabel: string;
+  remoteWorkMode: RemoteWorkMode;
   createdAt: Date;
   romeCode: RomeCode;
   romeLabel: string;
