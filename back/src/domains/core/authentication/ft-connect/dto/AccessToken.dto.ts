a2b7b9423fae97406b73aa386b57641818ec4d15
celineung <celine.ung.pro@gmail.com>
2025-12-18 17:39:07 +0100

    remove unecessary comment

--- back/src/domains/core/authentication/ft-connect/dto/AccessToken.dto.ts
+++ back/src/domains/core/authentication/ft-connect/dto/AccessToken.dto.ts
@@ -3,5 +3,5 @@ import type { IdToken } from "shared";
 export type AccessTokenDto = {
   value: string;
   expiresIn: number;
-  idToken: IdToken; // needed for FTConnect logout
+  idToken: IdToken;
 };
