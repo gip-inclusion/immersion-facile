e246e5c28f3e88e554cfbc2e94454562ba45dbee
clement-duport <clement.duport@beta.gouv.fr>
2025-12-19 15:35:49 +0100

    fix double header

--- front/src/app/pages/convention/ConventionManageConnectedUserPage.tsx
+++ front/src/app/pages/convention/ConventionManageConnectedUserPage.tsx
@@ -1,9 +1,7 @@
 import { Alert } from "@codegouvfr/react-dsfr/Alert";
 
-import { MainWrapper } from "react-design-system";
 import { ConventionManageContent } from "src/app/components/admin/conventions/ConventionManageContent";
 import { WithFeedbackReplacer } from "src/app/components/feedback/WithFeedbackReplacer";
-import { HeaderFooterLayout } from "src/app/components/layout/HeaderFooterLayout";
 import { useAppSelector } from "src/app/hooks/reduxHooks";
 import type { routes } from "src/app/routes/routes";
 import { authSelectors } from "src/core-logic/domain/auth/auth.selectors";
@@ -20,26 +18,24 @@ export const ConventionManageConnectedUserPage = ({
   const connectedUserJwt = useAppSelector(authSelectors.connectedUserJwt);
 
   return (
-    <HeaderFooterLayout>
-      <MainWrapper layout="default" vSpacing={8}>
-        {connectedUserJwt ? (
-          <WithFeedbackReplacer topic="transfer-convention-to-agency">
-            <ConventionManageContent
-              jwtParams={{
-                jwt: connectedUserJwt,
-                kind: "connected user",
-              }}
-              conventionId={conventionId}
-            />
-          </WithFeedbackReplacer>
-        ) : (
-          <Alert
-            severity="error"
-            title="Non autorisé"
-            description="Cette page est reservée aux utilisteurs connectés avec ProConnect, et dont l'agence est responsable de cette convention."
+    <>
+      {connectedUserJwt ? (
+        <WithFeedbackReplacer topic="transfer-convention-to-agency">
+          <ConventionManageContent
+            jwtParams={{
+              jwt: connectedUserJwt,
+              kind: "connected user",
+            }}
+            conventionId={conventionId}
           />
-        )}
-      </MainWrapper>
-    </HeaderFooterLayout>
+        </WithFeedbackReplacer>
+      ) : (
+        <Alert
+          severity="error"
+          title="Non autorisé"
+          description="Cette page est reservée aux utilisteurs connectés avec ProConnect, et dont l'agence est responsable de cette convention."
+        />
+      )}
+    </>
   );
 };
