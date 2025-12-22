6e9e752f009abcb940b1ebf544f006c2d12330f9
celineung <celine.ung.pro@gmail.com>
2025-12-22 11:00:46 +0100

    update ShareConventionByEmailDto

--- front/src/app/components/forms/convention/ShareForm.tsx
+++ front/src/app/components/forms/convention/ShareForm.tsx
@@ -1,13 +1,15 @@
+import { fr } from "@codegouvfr/react-dsfr";
 import { Button } from "@codegouvfr/react-dsfr/Button";
 import { Input } from "@codegouvfr/react-dsfr/Input";
+import ToggleSwitch from "@codegouvfr/react-dsfr/ToggleSwitch";
 import { zodResolver } from "@hookform/resolvers/zod";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useForm } from "react-hook-form";
 import {
   domElementIds,
   type InternshipKind,
-  type ShareLinkByEmailDto,
-  shareLinkByEmailSchema,
+  type ShareConventionByEmailDto,
+  shareConventionByEmailSchema,
 } from "shared";
 import { outOfReduxDependencies } from "src/config/dependencies";
 
@@ -16,31 +18,16 @@ type ShareFormProps = {
   onError: () => void;
   conventionFormData: {
     internshipKind: InternshipKind;
-    establishmentRepresentativeEmail: string;
-    firstName: string;
-    lastName: string;
   };
 };
 
 const makeInitialValues = ({
-  firstName,
-  lastName,
-  establishmentRepresentativeEmail,
-  link,
   internshipKind,
 }: {
-  firstName: string;
-  lastName: string;
-  establishmentRepresentativeEmail: string;
-  link: string;
   internshipKind: InternshipKind;
-}): Required<ShareLinkByEmailDto> => ({
+}): ShareConventionByEmailDto => ({
   internshipKind,
-  email: establishmentRepresentativeEmail,
-  conventionLink: link,
-  details: `${firstName || "Prénom"} ${
-    lastName || "Nom"
-  } vous invite à prendre connaissance de cette demande de convention d’immersion déjà partiellement remplie afin que vous la complétiez.  Merci !`,
+  senderEmail: "",
 });
 
 export const ShareForm = ({
@@ -48,64 +35,75 @@ export const ShareForm = ({
   onSuccess,
   onError,
 }: ShareFormProps) => {
-  const onSubmit = async (values: {
-    email: string;
-    details: string;
-    internshipKind: InternshipKind;
-  }) => {
+  const [isOnlyForSelf, setIsOnlyForSelf] = useState(false);
+  const onSubmit = async (values: ShareConventionByEmailDto) => {
     const result =
       await outOfReduxDependencies.conventionGateway.shareConventionLinkByEmail(
-        {
-          ...values,
-          conventionLink: window.location.href,
-        },
+        values,
       );
     result ? onSuccess() : onError();
   };
-  const methods = useForm<ShareLinkByEmailDto>({
+  const methods = useForm<ShareConventionByEmailDto>({
     mode: "onTouched",
-    defaultValues: makeInitialValues({
-      ...conventionFormData,
-      link: window.location.href,
-    }),
-    resolver: zodResolver(shareLinkByEmailSchema),
+    defaultValues: makeInitialValues(conventionFormData),
+    resolver: zodResolver(shareConventionByEmailSchema),
   });
-  const { register, handleSubmit, formState, reset } = methods;
+  const { register, handleSubmit, formState, reset, setValue } = methods;
 
   useEffect(() => {
-    reset(
-      makeInitialValues({
-        ...conventionFormData,
-        link: window.location.href,
-      }),
-    );
+    reset(makeInitialValues(conventionFormData));
   }, [conventionFormData, reset]);
+
   return (
     <form
       onSubmit={handleSubmit(onSubmit)}
       id={domElementIds.conventionImmersionRoute.shareForm}
     >
-      <input type="hidden" {...register("conventionLink")} />
+      <p className={fr.cx("fr-text--xs")}>
+        Tous les champs marqués d'une astérisque (*) sont obligatoires.
+      </p>
       <Input
-        label="Adresse mail à qui partager la demande"
+        label="Votre adresse email *"
         nativeInputProps={{
-          ...register("email"),
+          ...register("senderEmail"),
           type: "email",
           placeholder: "nom@exemple.com",
         }}
-        state={formState.errors.email ? "error" : "default"}
-        stateRelatedMessage={formState.errors.email?.message}
+        state={formState.errors.senderEmail ? "error" : "default"}
+        stateRelatedMessage={formState.errors.senderEmail?.message}
+      />
+      <ToggleSwitch
+        label="Je souhaite uniquement enregistrer un brouillon pour moi"
+        checked={isOnlyForSelf}
+        onChange={(checked) => {
+          setValue("recipientEmail", "");
+          setValue("details", "");
+          setIsOnlyForSelf(checked);
+        }}
       />
       <Input
-        label="Votre message (pour expliquer ce qui reste à compléter)"
+        className={fr.cx("fr-mt-2w")}
+        label={`Adresse email du destinataire ${isOnlyForSelf ? "(facultatif)" : "*"}`}
+        nativeInputProps={{
+          ...register("recipientEmail"),
+          type: "email",
+          placeholder: "nom@exemple.com",
+        }}
+        state={formState.errors.recipientEmail ? "error" : "default"}
+        stateRelatedMessage={formState.errors.recipientEmail?.message}
+        disabled={isOnlyForSelf}
+      />
+      <Input
+        label="Votre message (facultatif)"
         nativeTextAreaProps={{ ...register("details") }}
         textArea
         state={formState.errors.details ? "error" : "default"}
         stateRelatedMessage={formState.errors.details?.message}
+        disabled={isOnlyForSelf}
       />
       <Button
         type="submit"
-        title="Envoyer"
+        title="Envoyer le brouillon"
         disabled={!formState.isValid}
         id={domElementIds.conventionImmersionRoute.shareFormSubmitButton}
       >
