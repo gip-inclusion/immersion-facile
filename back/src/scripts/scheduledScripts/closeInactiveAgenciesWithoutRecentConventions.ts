af77207912419ce6fd8a0a011f1735efd21e5d8f
clement-duport <clement.duport@beta.gouv.fr>
2025-12-23 13:47:07 +0100

    Add cron job and script to close inactive agencies without recent conventions

---
+++ back/src/scripts/scheduledScripts/closeInactiveAgenciesWithoutRecentConventions.ts
@@ -0,0 +1,52 @@
+import { AppConfig } from "../../config/bootstrap/appConfig";
+import { createMakeProductionPgPool } from "../../config/pg/pgPool";
+import { makeCloseInactiveAgenciesWithoutRecentConventions } from "../../domains/agency/use-cases/CloseInactiveAgenciesWithoutRecentConventions";
+import { makeSaveNotificationsBatchAndRelatedEvent } from "../../domains/core/notifications/helpers/Notification";
+import { CustomTimeGateway } from "../../domains/core/time-gateway/adapters/CustomTimeGateway";
+import { createDbRelatedSystems } from "../../domains/core/unit-of-work/adapters/createDbRelatedSystems";
+import { UuidV4Generator } from "../../domains/core/uuid-generator/adapters/UuidGeneratorImplementations";
+import { createLogger } from "../../utils/logger";
+import { handleCRONScript } from "../handleCRONScript";
+
+const logger = createLogger(__filename);
+const config = AppConfig.createFromEnv();
+export const numberOfMonthsWithoutConvention = 6;
+
+const closeInactiveAgenciesWithoutRecentConventionsScript = async () => {
+  const { uowPerformer } = createDbRelatedSystems(
+    config,
+    createMakeProductionPgPool(config),
+  );
+
+  const closeInactiveAgenciesWithoutRecentConventions =
+    makeCloseInactiveAgenciesWithoutRecentConventions({
+      uowPerformer,
+      deps: {
+        saveNotificationsBatchAndRelatedEvent:
+          makeSaveNotificationsBatchAndRelatedEvent(
+            new UuidV4Generator(),
+            new CustomTimeGateway(),
+          ),
+      },
+    });
+
+  const result = await closeInactiveAgenciesWithoutRecentConventions.execute({
+    numberOfMonthsWithoutConvention,
+  });
+  return result;
+};
+
+export const triggerCloseInactiveAgenciesWithoutRecentConventions = ({
+  exitOnFinish,
+}: {
+  exitOnFinish: boolean;
+}) =>
+  handleCRONScript({
+    name: "triggerCloseInactiveAgenciesWithoutRecentConventions",
+    config,
+    script: closeInactiveAgenciesWithoutRecentConventionsScript,
+    handleResults: ({ numberOfAgenciesClosed }) =>
+      `${numberOfAgenciesClosed} agencies were closed , because they had no conventions validated or to be validated since the past 6 months`,
+    logger,
+    exitOnFinish,
+  });
