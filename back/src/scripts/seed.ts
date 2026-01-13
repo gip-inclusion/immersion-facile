90e009138dfcef6c52a88b138a94ffebe4d539ac
celineung <celine.ung.pro@gmail.com>
2026-01-13 12:14:59 +0100

    add conventionDraft in seed

--- back/src/scripts/seed.ts
+++ back/src/scripts/seed.ts
@@ -5,6 +5,7 @@ import {
 } from "../config/bootstrap/createAppDependencies";
 import { type KyselyDb, makeKyselyDb } from "../config/pg/kysely/kyselyUtils";
 import { agencySeed } from "./seed/agencySeed";
+import { conventionDraftSeed } from "./seed/conventionDraftSeed";
 import { conventionSeed } from "./seed/conventionSeed";
 import { establishmentSeed } from "./seed/establishmentSeed";
 import { featureFlagsSeed } from "./seed/featureFlagSeed";
@@ -21,6 +22,7 @@ const executeSeedTasks = async (db: KyselyDb, deps: AppDependencies) => {
     const agencyIds = await agencySeed(uow);
     await establishmentSeed(uow);
     await conventionSeed(uow, agencyIds);
+    await conventionDraftSeed(uow);
   });
 
   // biome-ignore lint/suspicious/noConsole: <explanation>
@@ -88,4 +90,4 @@ seed()
   });
 
 // un lien rapide pour pas remplir le formulaire à la main en local (avec le siret france merguez, et l'agence pole emploi paris défini dans cette seed)
-// http://localhost:3000/demande-immersion?email=john.doe%40mail.mo&firstName=John&lastName=Doe&phone=0202020202&financiaryHelp=&emergencyContact=&emergencyContactPhone=&isRqth=false&birthdate=2000-10-10&agencyDepartment=75&siret=34493368400021&businessName=FRANCE%20MERGUEZ%20DISTRIBUTION&businessAdvantages=&etFirstName=Bob&etLastName=Le%20tuteur&etJob=Tuteur&etPhone=0303030303&etEmail=bob.letuteur%40mail.com&erFirstName=Bob&erLastName=Le%20tuteur&erPhone=0303030303&erEmail=bob.letuteur%40mail.com&immersionAddress=ZI%20VILLETANEUSE%206%20RUE%20RAYMOND%20BROSSE%2093430%20VILLETANEUSE&agencyId=aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaaa&immersionObjective=Confirmer%20un%20projet%20professionnel&immersionActivities=Charcuttage%20de%20comp%C3%AAt.&immersionSkills=&sanitaryPreventionDescription=&workConditions=&sanitaryPrevention=false&individualProtection=false&dateStart=2023-06-09&dateEnd=2023-06-10T00%3A00%3A00.000Z&schedule=%7B%22totalHours%22%3A6%2C%22workedDays%22%3A2%2C%22isSimple%22%3Atrue%2C%22selectedIndex%22%3A0%2C%22complexSchedule%22%3A%5B%7B%22date%22%3A%222023-06-09T00%3A00%3A00.000Z%22%2C%22timePeriods%22%3A%5B%7B%22start%22%3A%2209%3A00%22%2C%22end%22%3A%2212%3A00%22%7D%5D%7D%2C%7B%22date%22%3A%222023-06-10T00%3A00%3A00.000Z%22%2C%22timePeriods%22%3A%5B%7B%22start%22%3A%2209%3A00%22%2C%22end%22%3A%2212%3A00%22%7D%5D%7D%5D%7D&immersionAppellation=%7B%22romeCode%22%3A%22D1103%22%2C%22romeLabel%22%3A%22Charcuterie%20-%20traiteur%22%2C%22appellationCode%22%3A%2211741%22%2C%22appellationLabel%22%3A%22Charcutier%20%2F%20Charcuti%C3%A8re%22%7D
+// http://localhost:3000/demande-immersion?conventionDraftId=11111111-1111-4111-9111-111111111111
