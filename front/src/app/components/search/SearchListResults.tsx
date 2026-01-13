f593e97ce616efdc9082dfe7dee7dd86408fab4c
Enguerran Weiss <enguerranweiss@gmail.com>
2026-01-13 16:56:16 +0100

    adjust search offers cards and search result page to fit to remote work modes

--- front/src/app/components/search/SearchListResults.tsx
+++ front/src/app/components/search/SearchListResults.tsx
@@ -12,6 +12,7 @@ import {
 import {
   type AppellationCode,
   domElementIds,
+  type ExternalSearchResultDto,
   hasSearchGeoParams,
   isSuperEstablishment,
   type SearchResultDto,
@@ -115,7 +116,7 @@ export const SearchListResults = ({
                     key={`${searchResult.siret}-${searchResult.rome}-${searchResult.locationId}`}
                   >
                     <SearchResult
-                      establishment={searchResult}
+                      searchResult={searchResult}
                       illustration={
                         <SearchResultIllustration
                           illustration={
@@ -242,7 +243,11 @@ export const SearchListResults = ({
 
 const makeOfferLink = (
   route: SearchRoute,
-  { siret, locationId, voluntaryToImmersion }: SearchResultDto,
+  {
+    siret,
+    locationId,
+    voluntaryToImmersion,
+  }: SearchResultDto | ExternalSearchResultDto,
   appellationCode?: AppellationCode,
 ): Link => {
   const definedAppellationCode: AppellationCode = appellationCode ?? "";
