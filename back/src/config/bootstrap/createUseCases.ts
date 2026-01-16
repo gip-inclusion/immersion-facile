4d94f5c3798cc7cd2ac375bb706de990196a31bc
Benjamin Bohec <bbohec.pro@gmail.com>
2026-01-16 14:46:16 +0100

    back - remove unnecessary DeliverRenewedMagicLink usecase + event and add logic in RenewExpiredJwt
    Signed-off-by: Benjamin Bohec <bbohec.pro@gmail.com>

--- back/src/config/bootstrap/createUseCases.ts
+++ back/src/config/bootstrap/createUseCases.ts
@@ -45,7 +45,6 @@ import { GetConventionForApiConsumer } from "../../domains/convention/use-cases/
 import { makeGetConventionsForAgencyUser } from "../../domains/convention/use-cases/GetConventionsForAgencyUser";
 import { GetConventionsForApiConsumer } from "../../domains/convention/use-cases/GetConventionsForApiConsumer";
 import { makeGetLastBroadcastFeedback } from "../../domains/convention/use-cases/GetLastBroadcastFeedback";
-import { DeliverRenewedMagicLink } from "../../domains/convention/use-cases/notifications/DeliverRenewedMagicLink";
 import { NotifyAgencyDelegationContact } from "../../domains/convention/use-cases/notifications/NotifyAgencyDelegationContact";
 import { NotifyAgencyThatAssessmentIsCreated } from "../../domains/convention/use-cases/notifications/NotifyAgencyThatAssessmentIsCreated";
 import { NotifyAllActorsOfFinalConventionValidation } from "../../domains/convention/use-cases/notifications/NotifyAllActorsOfFinalConventionValidation";
@@ -323,14 +322,14 @@ export const createUseCases = ({
         createNewEvent,
         gateways.timeGateway,
       ),
-      renewExpiredJwt: new RenewExpiredJwt(
+      renewExpiredJwt: new RenewExpiredJwt({
         uowPerformer,
-        createNewEvent,
-        generateConventionMagicLinkUrl,
         config,
-        gateways.timeGateway,
-        gateways.shortLinkGenerator,
-      ),
+        timeGateway: gateways.timeGateway,
+        shortLinkIdGeneratorGateway: gateways.shortLinkGenerator,
+        makeGenerateConventionMagicLinkUrl: generateConventionMagicLinkUrl,
+        saveNotificationAndRelatedEvent,
+      }),
       renewConvention: new RenewConvention(uowPerformer, addConvention),
       notifyConventionReminder: new NotifyConventionReminder(
         uowPerformer,
@@ -489,11 +488,6 @@ export const createUseCases = ({
           uowPerformer,
           saveNotificationAndRelatedEvent,
         ),
-      deliverRenewedMagicLink: new DeliverRenewedMagicLink(
-        uowPerformer,
-        saveNotificationAndRelatedEvent,
-      ),
-
       notifyFranceTravailUserAdvisorOnConventionFullySigned:
         new NotifyFranceTravailUserAdvisorOnConventionFullySigned(
           uowPerformer,
