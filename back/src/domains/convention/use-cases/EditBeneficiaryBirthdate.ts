7803d9a8d681d2154b19e25355be20be0bacf553
clement-duport <clement.duport@beta.gouv.fr>
2026-01-29 13:17:51 +0100

    Add edit beneficiary birthdate functionality, including router integration, use case implementation, and tests

---
+++ back/src/domains/convention/use-cases/EditBeneficiaryBirthdate.ts
@@ -0,0 +1,85 @@
+import {
+  type ConnectedUser,
+  type ConventionDto,
+  type EditBeneficiaryBirthdateRequestDto,
+  editBeneficiaryBirthdateRequestSchema,
+  errors,
+} from "shared";
+import { throwErrorIfConventionStatusNotAllowed } from "../../../utils/convention";
+import { throwIfNotAdmin } from "../../connected-users/helpers/authorization.helper";
+import type { CreateNewEvent } from "../../core/events/ports/EventBus";
+import { useCaseBuilder } from "../../core/useCaseBuilder";
+
+export type EditBeneficiaryBirthdate = ReturnType<
+  typeof makeEditBeneficiaryBirthdate
+>;
+
+export const makeEditBeneficiaryBirthdate = useCaseBuilder(
+  "EditBeneficiaryBirthdate",
+)
+  .withInput<EditBeneficiaryBirthdateRequestDto>(
+    editBeneficiaryBirthdateRequestSchema,
+  )
+  .withOutput<void>()
+  .withCurrentUser<ConnectedUser>()
+  .withDeps<{
+    createNewEvent: CreateNewEvent;
+  }>()
+  .build(async ({ inputParams, uow, deps, currentUser }) => {
+    throwIfNotAdmin(currentUser);
+    const convention = await uow.conventionRepository.getById(
+      inputParams.conventionId,
+    );
+
+    if (!convention)
+      throw errors.convention.notFound({
+        conventionId: inputParams.conventionId,
+      });
+
+    throwErrorIfConventionStatusNotAllowed(
+      convention.status,
+      ["ACCEPTED_BY_VALIDATOR"],
+      errors.convention.editBeneficiaryBirthdateNotAllowedForStatus({
+        status: convention.status,
+      }),
+    );
+
+    const updatedConvention: ConventionDto =
+      convention.internshipKind === "mini-stage-cci"
+        ? {
+            ...convention,
+            signatories: {
+              ...convention.signatories,
+              beneficiary: {
+                ...convention.signatories.beneficiary,
+                birthdate: inputParams.updatedBeneficiaryBirthDate,
+              },
+            },
+          }
+        : {
+            ...convention,
+            signatories: {
+              ...convention.signatories,
+              beneficiary: {
+                ...convention.signatories.beneficiary,
+                birthdate: inputParams.updatedBeneficiaryBirthDate,
+              },
+            },
+          };
+
+    await Promise.all([
+      uow.conventionRepository.update(updatedConvention),
+      uow.outboxRepository.save(
+        deps.createNewEvent({
+          topic: "ConventionBeneficiaryBirthdateEdited",
+          payload: {
+            convention: updatedConvention,
+            triggeredBy: {
+              kind: "connected-user",
+              userId: currentUser.id,
+            },
+          },
+        }),
+      ),
+    ]);
+  });
