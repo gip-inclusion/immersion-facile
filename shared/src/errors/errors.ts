7d0e189e4e4c9440f8f4905b79c68f900620b87c
tpicaud <tom.picaud@hotmail.com>
2026-01-29 11:16:48 +0100

    add not found phone error

--- shared/src/errors/errors.ts
+++ shared/src/errors/errors.ts
@@ -1100,4 +1100,7 @@ export const errors = {
     notFromIFDomain: (url: string) =>
       new BadRequestError(`L'url ${url} ne provient pas d'Immersion Facilitée`),
   },
+  phoneNumber: {
+    notFound: () => new NotFoundError("Numéro de téléphone introuvable"),
+  },
 };
