df485dac98e2828fb1e37c427a326272c6be2758
Enguerran Weiss <enguerranweiss@gmail.com>
2026-02-03 10:47:54 +0100

    delete: matomo tag manager container

--- front/src/app/App.tsx
+++ front/src/app/App.tsx
@@ -5,7 +5,6 @@ import { useDispatch } from "react-redux";
 import { ConsentManager, useConsent } from "src/app/components/ConsentManager";
 import { useSetAcquisitionParams } from "src/app/hooks/acquisition.hooks";
 import { useFetchFeatureFlags } from "src/app/hooks/useFeatureFlags";
-import { MatomoTagManager } from "src/app/MatomoTagManager";
 import { ErrorPage } from "src/app/pages/error/ErrorPage";
 import { useRoute } from "src/app/routes/routes";
 import { ENV } from "src/config/environmentVariables";
@@ -25,17 +24,6 @@ export const App = () => {
   const currentRoute = useRoute();
   const dispatch = useDispatch();
   const consent = useConsent();
-  const matomoUrls: Record<typeof ENV.envType, string> = {
-    local:
-      "https://matomo.inclusion.beta.gouv.fr/js/container_gXlljpZ7_dev_d2e47aeaf37d823506115b8a.js",
-    dev: "https://matomo.inclusion.beta.gouv.fr/js/container_gXlljpZ7_dev_d2e47aeaf37d823506115b8a.js",
-    pentest:
-      "https://matomo.inclusion.beta.gouv.fr/js/container_gXlljpZ7_pentest_a8b07fda200254c3602d860a.js",
-    staging:
-      "https://matomo.inclusion.beta.gouv.fr/js/container_gXlljpZ7_staging_60e68dc0e23bda19899cf43d.js",
-    production:
-      "https://matomo.inclusion.beta.gouv.fr/js/container_gXlljpZ7.js",
-  };
   useSetAcquisitionParams();
   return (
     <ErrorBoundary
@@ -53,7 +41,6 @@ export const App = () => {
           userConsent={!!consent?.finalityConsent?.support}
         />
       )}
-      <MatomoTagManager containerUrl={matomoUrls[ENV.envType]} />
     </ErrorBoundary>
   );
 };
