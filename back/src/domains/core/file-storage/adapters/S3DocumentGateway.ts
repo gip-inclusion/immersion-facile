30ef16018e0257c7249352c81ae22f417c1a0629
Benjamin Bohec <bbohec.pro@gmail.com>
2026-02-17 16:08:13 +0100

    replace aws sdk client v2 by aws4fetch
    Signed-off-by: Benjamin Bohec <bbohec.pro@gmail.com>

--- back/src/domains/core/file-storage/adapters/S3DocumentGateway.ts
+++ back/src/domains/core/file-storage/adapters/S3DocumentGateway.ts
@@ -1,5 +1,10 @@
-import * as AWS from "aws-sdk";
-import type { AbsoluteUrl, StoredFileId } from "shared";
+import { AwsClient } from "aws4fetch";
+import {
+  type AbsoluteUrl,
+  absoluteUrlSchema,
+  errors,
+  type StoredFileId,
+} from "shared";
 import { createLogger } from "../../../../utils/logger";
 import type { StoredFile } from "../entity/StoredFile";
 import type { DocumentGateway } from "../port/DocumentGateway";
@@ -15,72 +20,63 @@ export interface S3Params {
 
 export class S3DocumentGateway implements DocumentGateway {
   readonly #bucketName: string;
-
   readonly #endpoint: string;
-
-  readonly #s3: AWS.S3;
+  readonly #client: AwsClient;
 
   constructor(params: S3Params) {
-    AWS.config.update({
-      accessKeyId: params.accessKeyId,
-      secretAccessKey: params.secretAccessKey,
-    });
     this.#bucketName = params.bucketName;
     this.#endpoint = params.endPoint;
-    this.#s3 = new AWS.S3({ endpoint: params.endPoint });
+    this.#client = new AwsClient({
+      accessKeyId: params.accessKeyId,
+      secretAccessKey: params.secretAccessKey,
+      service: "s3",
+    });
   }
 
   public async getUrl(fileId: StoredFileId): Promise<AbsoluteUrl | undefined> {
-    return new Promise((resolve) => {
-      this.#s3.getObject(
-        {
-          Bucket: this.#bucketName,
-          Key: fileId,
-        },
-        (err, data) => {
-          if (err) {
-            logger.error({ error: err });
-            resolve(undefined);
-          }
-          if (!data) {
-            resolve(undefined);
-          }
-          resolve(`https://${this.#bucketName}.${this.#endpoint}/${fileId}`);
-        },
-      );
+    return this.awsSignedFetch(fileId, "HEAD").then(async (response) => {
+      if (response.status === 404) return undefined;
+      if (response.status === 200) return absoluteUrlSchema.parse(response.url);
+      throw await errors.fetch.errorResponse(response);
     });
   }
 
   public async save(file: StoredFile): Promise<void> {
-    return new Promise((resolve, reject) => {
-      this.#s3.putObject(
-        {
-          Key: file.id,
-          Bucket: this.#bucketName,
-          Body: file.buffer,
-          ACL: "public-read",
-          ContentType: file.mimetype,
-        },
-        (err) => {
-          if (err) {
-            return reject(err);
-          }
-
-          logger.info({
-            message: `File uploaded successfully in bucket ${this.#bucketName}, file id : ${file.id}, file name: ${file.name}`,
-          });
-          return resolve();
-        },
-      );
+    return this.awsSignedFetch(file.id, "PUT", file.buffer, {
+      "content-type": file.mimetype,
+      "x-amz-acl": "public-read",
+    }).then(async (response) => {
+      if (response.status === 200) {
+        logger.info({
+          message: `File uploaded successfully in bucket ${this.#bucketName}, file id : ${file.id}, file name: ${file.name}`,
+        });
+        return;
+      }
+      throw await errors.fetch.errorResponse(response);
     });
   }
 
   //For reset testing
   public async delete(id: StoredFileId): Promise<void> {
-    return new Promise((resolve, reject) => {
-      this.#s3.deleteObject({ Bucket: this.#bucketName, Key: id }, (err) =>
-        err ? reject(err) : resolve(),
-      );
+    return this.awsSignedFetch(id, "DELETE").then(async (response) => {
+      if (response.status === 204) return;
+      throw await errors.fetch.errorResponse(response);
     });
+  }
+
+  private async awsSignedFetch(
+    fileId: StoredFileId,
+    method: "HEAD" | "PUT" | "DELETE",
+    body?: Buffer,
+    headers?: Record<string, string>,
+  ): Promise<Response> {
+    return this.#client.fetch(
+      `https://${this.#bucketName}.${this.#endpoint}/${fileId}`,
+      {
+        body,
+        method,
+        headers,
+      },
+    );
   }
 }
