90a273d6b7ad26c68f9f642f3ae23f852dfcdf04
Benjamin Bohec <bbohec.pro@gmail.com>
2026-02-17 12:07:18 +0100

    replace babel jest by swc jest
    Signed-off-by: Benjamin Bohec <bbohec.pro@gmail.com>

--- front/jest.config.js
+++ front/jest.config.js
@@ -1,5 +1,9 @@
 module.exports = {
   testEnvironment: "node",
+  transform: {
+    "^.+\\.(t|j)sx?$": ["@swc/jest"],
+  },
+  moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json"],
   moduleNameMapper: {
     "shared/(.*)": "<rootDir>/../shared/$1",
     "src/(.*)": "<rootDir>/src/$1",
--- shared/jest.config.js
+++ shared/jest.config.js
@@ -2,4 +2,8 @@ process.env.TZ = "Europe/Paris";
 
 module.exports = {
   testEnvironment: "node",
+  transform: {
+    "^.+\\.(t|j)sx?$": ["@swc/jest"],
+  },
+  moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json"],
 };
