b4f49f68290f296cd0e7f96e323531fd3072817b
celineung <celine.ung.pro@gmail.com>
2026-02-18 17:43:41 +0100

    add InfoSection component with styles and Storybook stories

---
+++ libs/react-design-system/src/immersionFacile/components/info-section/InfoSection.styles.ts
@@ -0,0 +1,5 @@
+import "./InfoSection.scss";
+
+export default {
+  root: "im-info-section",
+};
---
+++ libs/react-design-system/src/immersionFacile/components/info-section/index.ts
@@ -0,0 +1,5 @@
+export * from "./InfoSection";
+
+import { storybookPrefix } from "../../storyPrefixes";
+
+export const infoSectionPrefix = storybookPrefix("InfoSection");
