b34d72b89c0cae5c14aed3b67461d8c8e7680e3a
Benjamin Bohec <bbohec.pro@gmail.com>
2026-02-20 15:49:34 +0100

    SMS
    Signed-off-by: Benjamin Bohec <bbohec.pro@gmail.com>

--- shared/src/sms/sms.schema.ts
+++ shared/src/sms/sms.schema.ts
@@ -2,12 +2,18 @@ import parseMobile from "libphonenumber-js/mobile";
 import { z } from "zod";
 import type { PhoneNumber } from "../phone/phone.dto";
 import { phoneNumberSchema } from "../phone/phone.schema";
+import { makeHardenedStringSchema } from "../utils/string.schema";
 import type { ZodSchemaWithInputMatchingOutput } from "../zodUtils";
-import type { TemplatedSms } from "./smsTemplateByName";
+import type { SmsTemplateKind, TemplatedSms } from "./smsTemplateByName";
+
+const MAX_INTERNATIONAL_PHONE_DIGIT_SIZE = 15;
 
 //Mobile number to send SMS with the country code - Limited on this gateway to allow only french mobile phones
 export const smsRecipientPhoneSchema: ZodSchemaWithInputMatchingOutput<PhoneNumber> =
-  z.string().superRefine((phone, ctx) => {
+  // TODO : pour review > schema original z.string() > on veut autoriser les données vide avant le superRefine?
+  makeHardenedStringSchema({
+    max: MAX_INTERNATIONAL_PHONE_DIGIT_SIZE + 5,
+  }).superRefine((phone, ctx) => {
     if (
       phoneNumberSchema.safeParse(phone).success &&
       parseMobile(phone)?.getType() !== "MOBILE"
@@ -18,8 +24,11 @@ export const smsRecipientPhoneSchema: ZodSchemaWithInputMatchingOutput<PhoneNumb
       });
   });
 
+const smsTemplateKindSchema: ZodSchemaWithInputMatchingOutput<SmsTemplateKind> =
+  z.literal(["ReminderForSignatories", "ReminderForAssessment", "HelloWorld"]);
+
 export const templatedSmsSchema = z.object({
-  kind: z.string(),
+  kind: smsTemplateKindSchema,
   recipientPhone: smsRecipientPhoneSchema,
   params: z.any(),
 }) as ZodSchemaWithInputMatchingOutput<TemplatedSms>;
