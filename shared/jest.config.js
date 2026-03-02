e56a05b7fbe7fc3602339654f08c5787c69c9ede
Benjamin Bohec <bbohec.pro@gmail.com>
2026-03-02 18:09:37 +0100

    install isomorphic-dompurify & htmlparser2 & rework swc/jest config for allowing mjs/mts modules

--- shared/jest.config.js
+++ shared/jest.config.js
@@ -3,7 +3,10 @@ process.env.TZ = "Europe/Paris";
 module.exports = {
   testEnvironment: "node",
   transform: {
-    "^.+\\.(t|j)sx?$": ["@swc/jest"],
+    "^.+\\.(m?[jt]sx?)$": ["@swc/jest"],
   },
-  moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json"],
+  transformIgnorePatterns: [
+    "node_modules\\/(?!(.pnpm|@|isomorphic-dompurify|parse5))",
+  ],
+  moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "mjs"],
 };
