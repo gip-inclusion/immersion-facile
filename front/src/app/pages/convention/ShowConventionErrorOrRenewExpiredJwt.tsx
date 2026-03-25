54b5e03d6d4e78d156661946ce67a20b05ac9815
Benjamin Bohec <bbohec.pro@gmail.com>
2026-03-25 16:02:05 +0100

    originalUrl required for RenewExpiredJwtButton in ShowConventionErrorOrRenewExpiredJwt
    Signed-off-by: Benjamin Bohec <bbohec.pro@gmail.com>

--- front/src/app/pages/convention/ShowConventionErrorOrRenewExpiredJwt.tsx
+++ front/src/app/pages/convention/ShowConventionErrorOrRenewExpiredJwt.tsx
@@ -24,6 +24,7 @@ export const ShowConventionErrorOrRenewExpiredJwt = ({
         key={RenewExpiredJwtButton.name}
         expiredJwt={jwt}
         feedbackTopic={feedbackTopic}
+        originalUrl={window.location.href}
       />
     ),
   });
