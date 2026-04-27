7fa53be6dd383baba2c1339a2e6d5b563b2f7832
Benjamin Bohec <bbohec.pro@gmail.com>
2026-04-27 12:17:12 +0200

    fix unsupported dates on DB
    Signed-off-by: Benjamin Bohec <bbohec.pro@gmail.com>

---
+++ back/src/config/pg/migrations/1776855375794_fix-unsupported-dates.ts
@@ -0,0 +1,71 @@
+import type { MigrationBuilder } from "node-pg-migrate";
+
+export async function up(pgm: MigrationBuilder): Promise<void> {
+  const minDateString = "1900-01-01";
+  const minDateIsoString = "1900-01-01 00:00:00+00";
+  const maxDateString = "2031-01-01";
+  const maxDateIsoString = "2031-01-01 00:00:00+00";
+
+  pgm.sql(`
+    UPDATE actors
+    SET extra_fields = jsonb_set(extra_fields, '{birthdate}', 
+      CASE 
+        WHEN (extra_fields->>'birthdate')::date < '${minDateString}' THEN '"${minDateString}"'::jsonb
+        WHEN (extra_fields->>'birthdate')::date > '${maxDateString}' THEN '"${maxDateString}"'::jsonb
+      END
+    )
+    WHERE extra_fields ? 'birthdate'
+    AND (
+      (extra_fields->>'birthdate')::date < '${minDateString}' 
+      OR 
+      (extra_fields->>'birthdate')::date > '${maxDateString}'
+    )
+  `);
+
+  pgm.sql(`
+    UPDATE establishments
+    SET next_availability_date = 
+      CASE 
+        WHEN next_availability_date < '${minDateString}' THEN '${minDateIsoString}'::timestamptz
+        WHEN next_availability_date > '${maxDateString}' THEN '${maxDateIsoString}'::timestamptz
+      END
+    WHERE next_availability_date IS NOT NULL 
+    AND (
+      next_availability_date < '${minDateString}' 
+      OR 
+      next_availability_date > '${maxDateString}'
+    )
+  `);
+
+  pgm.sql(`
+    UPDATE immersion_assessments
+    SET last_day_of_presence = 
+      CASE 
+        WHEN last_day_of_presence < '${minDateString}' THEN '${minDateIsoString}'::timestamptz
+        WHEN last_day_of_presence > '${maxDateString}' THEN '${maxDateIsoString}'::timestamptz
+      END
+    WHERE last_day_of_presence IS NOT NULL 
+    AND (
+      last_day_of_presence < '${minDateString}'
+      OR 
+      last_day_of_presence > '${maxDateString}'
+    );
+  `);
+
+  pgm.sql(`
+    UPDATE immersion_assessments
+    SET contract_start_date = 
+      CASE 
+        WHEN contract_start_date < '${minDateString}' THEN '${minDateIsoString}'::timestamptz
+        WHEN contract_start_date > '${maxDateString}' THEN '${maxDateIsoString}'::timestamptz
+      END
+    WHERE contract_start_date IS NOT NULL 
+    AND (
+      contract_start_date < '${minDateString}'
+      OR
+      contract_start_date > '${maxDateString}'
+    );
+  `);
+}
+
+export async function down(pgm: MigrationBuilder): Promise<void> {}
