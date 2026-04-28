52fe98d6deda8d1f7d72fe1eb8964bcc79cce6df
Enguerran Weiss <enguerranweiss@gmail.com>
2026-04-28 13:03:19 +0200

    fix: more simple internalOfferSchema

--- shared/src/remoteWorkMode/remoteWorkMode.schema.ts
+++ shared/src/remoteWorkMode/remoteWorkMode.schema.ts
@@ -3,18 +3,21 @@ import type { ZodSchemaWithInputMatchingOutput } from "../zodUtils";
 import {
   type RemoteWorkMode,
   remoteWorkModes,
-  type WithOptionnalRemoteWorkModes,
+  type WithOptionalRemoteWorkModes,
   type WithRemoteWorkMode,
 } from "./remoteWorkMode.dto";
 
 export const remoteWorkModeSchema: ZodSchemaWithInputMatchingOutput<RemoteWorkMode> =
   z.enum(remoteWorkModes, "Veuillez choisir une modalité");
-export const withRemoteWorkModeSchema: ZodSchemaWithInputMatchingOutput<WithRemoteWorkMode> =
-  z.object({
-    remoteWorkMode: remoteWorkModeSchema,
-  });
 
-export const withOptionnalRemoteWorkModesSchema: ZodSchemaWithInputMatchingOutput<WithOptionnalRemoteWorkModes> =
+export const remoteWorkModeShape = {
+  remoteWorkMode: remoteWorkModeSchema,
+};
+
+export const withRemoteWorkModeSchema: ZodSchemaWithInputMatchingOutput<WithRemoteWorkMode> =
+  z.object(remoteWorkModeShape);
+
+export const withOptionnalRemoteWorkModesSchema: ZodSchemaWithInputMatchingOutput<WithOptionalRemoteWorkModes> =
   z.object({
     remoteWorkModes: z.array(remoteWorkModeSchema).optional(),
   });
