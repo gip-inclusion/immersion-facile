71b0580a9eba2ec3d9d65d7bea162994c365d924
tpicaud <tom.picaud@hotmail.com>
2026-04-30 13:25:20 +0200

    add badge in profile

--- front/src/app/components/establishment/establishments-table/establishment-line/EstablishmentLineBusinessName.tsx
+++ front/src/app/components/establishment/establishments-table/establishment-line/EstablishmentLineBusinessName.tsx
@@ -1,19 +1,33 @@
 import { fr } from "@codegouvfr/react-dsfr";
+import Badge from "@codegouvfr/react-dsfr/Badge";
 import Button from "@codegouvfr/react-dsfr/Button";
 import { Tag } from "react-design-system";
-import { domElementIds, type UserEstablishmentRightDetails } from "shared";
+import {
+  domElementIds,
+  type UserEstablishmentRightDetailsWithAcceptedStatus,
+  type UserEstablishmentRightDetailsWithPendingStatus,
+} from "shared";
 import { routes } from "src/app/routes/routes";
 
 export const EstablishmentLineBusinessName = ({
   data,
+  isEstablishmentBanned,
   isBackofficeAdmin,
 }: {
-  data: UserEstablishmentRightDetails;
+  data:
+    | UserEstablishmentRightDetailsWithAcceptedStatus
+    | UserEstablishmentRightDetailsWithPendingStatus;
+  isEstablishmentBanned: boolean;
   isBackofficeAdmin?: boolean;
 }) => (
   <ul key={data.siret} className={fr.cx("fr-raw-list")}>
     <li>
       <Tag theme={"etablissement"} />
+      {isEstablishmentBanned && (
+        <Badge severity="error" small>
+          ENTREPRISE BANNIE
+        </Badge>
+      )}
     </li>
     <li> {data.businessName}</li>
     <li>
