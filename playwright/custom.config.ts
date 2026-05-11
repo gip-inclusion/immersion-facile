f8109224aaa3c957048a937f8ef90454b426adcf
Enguerran Weiss <enguerranweiss@gmail.com>
2026-05-11 15:34:58 +0200

    load .env in playwright config, playwright using webservers

--- playwright/custom.config.ts
+++ playwright/custom.config.ts
@@ -1,6 +1,13 @@
+import { resolve } from "node:path";
+import dotEnv from "dotenv";
 import { makeThrowIfNotDefinedOrDefault } from "shared";
 
-const throwIfNotDefinedOrDefault = makeThrowIfNotDefinedOrDefault(process.env);
+const playwrightEnv: Record<string, string> =
+  dotEnv.config({ path: resolve(__dirname, ".env"), processEnv: {} }).parsed ??
+  {};
+
+const throwIfNotDefinedOrDefault =
+  makeThrowIfNotDefinedOrDefault(playwrightEnv);
 export const testConfig = {
   timeForDebounce: 600, // debounce time value * 2 for safety
   timeForEventCrawler: 2000, // event crawler time interval + 1s for safety
