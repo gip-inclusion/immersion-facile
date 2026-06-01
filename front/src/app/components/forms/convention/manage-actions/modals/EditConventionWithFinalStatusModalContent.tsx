2fcc6e4629e9e319f59418cd6edd72558182c3bc
clement-duport <clement.duport@beta.gouv.fr>
2026-06-01 15:45:19 +0200

    remove disabled field in modification modal for convention with final status

--- front/src/app/components/forms/convention/manage-actions/modals/EditConventionWithFinalStatusModalContent.tsx
+++ front/src/app/components/forms/convention/manage-actions/modals/EditConventionWithFinalStatusModalContent.tsx
@@ -4,12 +4,10 @@ import { zodResolver } from "@hookform/resolvers/zod";
 import { type SubmitHandler, useForm } from "react-hook-form";
 import {
   type ConventionDto,
-  convertLocaleDateToUtcTimezoneDate,
   domElementIds,
   type EditConventionWithFinalStatusRequestDto,
   editConventionWithFinalStatusRequestSchema,
   toDateUTCString,
-  toDisplayedDate,
 } from "shared";
 import { makeFieldError } from "src/app/hooks/formContents.hooks";
 
@@ -23,9 +21,6 @@ export const EditConventionWithFinalStatusModalContent = ({
   closeModal: () => void;
 }) => {
   const beneficiary = convention.signatories.beneficiary;
-  const currentDateDisplay = toDisplayedDate({
-    date: convertLocaleDateToUtcTimezoneDate(new Date(beneficiary.birthdate)),
-  });
 
   const { register, handleSubmit, formState } =
     useForm<EditConventionWithFinalStatusRequestDto>({
@@ -33,9 +28,9 @@ export const EditConventionWithFinalStatusModalContent = ({
       mode: "onTouched",
       defaultValues: {
         conventionId: convention.id,
-        updatedBeneficiaryBirthDate: undefined,
-        firstname: undefined,
-        lastname: undefined,
+        updatedBeneficiaryBirthDate: beneficiary.birthdate,
+        firstname: beneficiary.firstName,
+        lastname: beneficiary.lastName,
       },
     });
 
@@ -57,88 +52,43 @@ export const EditConventionWithFinalStatusModalContent = ({
       </p>
 
       <div className={fr.cx("fr-card", "fr-p-2w", "fr-mb-3w")}>
-        <h3 className={fr.cx("fr-h6", "fr-mb-2w")}>Date de naissance</h3>
-        <div className={fr.cx("fr-grid-row", "fr-grid-row--gutters")}>
-          <div className={fr.cx("fr-col-12", "fr-col-md-6")}>
-            <Input
-              label="Date de naissance actuelle"
-              nativeInputProps={{
-                value: currentDateDisplay,
-                readOnly: true,
-              }}
-              disabled={true}
-            />
-          </div>
-          <div className={fr.cx("fr-col-12", "fr-col-md-6")}>
-            <Input
-              label="Nouvelle date de naissance"
-              nativeInputProps={{
-                ...register("updatedBeneficiaryBirthDate", {
-                  setValueAs: (value) => (value ? value : undefined),
-                }),
-                id: domElementIds.manageConvention
-                  .editConventionWithFinalStatusModalNewDateInput,
-                type: "date",
-                max: toDateUTCString(new Date()),
-              }}
-              {...getFieldError("updatedBeneficiaryBirthDate")}
-            />
-          </div>
-        </div>
+        <h3 className={fr.cx("fr-h6", "fr-mb-2w")}>Personne en immersion</h3>
+        <Input
+          label="Date de naissance"
+          nativeInputProps={{
+            ...register("updatedBeneficiaryBirthDate", {
+              setValueAs: (value) => (value ? value : undefined),
+            }),
+            id: domElementIds.manageConvention
+              .editConventionWithFinalStatusModalNewDateInput,
+            type: "date",
+            max: toDateUTCString(new Date()),
+          }}
+          {...getFieldError("updatedBeneficiaryBirthDate")}
+        />
+
+        <Input
+          label="Prénom"
+          nativeInputProps={{
+            ...register("firstname"),
+            id: domElementIds.manageConvention
+              .editConventionWithFinalStatusModalNewFirstNameInput,
+          }}
+          {...getFieldError("firstname")}
+        />
+        <Input
+          label="Nom de famille"
+          nativeInputProps={{
+            ...register("lastname"),
+            id: domElementIds.manageConvention
+              .editConventionWithFinalStatusModalNewLastNameInput,
+          }}
+          {...getFieldError("lastname")}
+        />
       </div>
 
       <div className={fr.cx("fr-card", "fr-p-2w")}>
-        <h3 className={fr.cx("fr-h6", "fr-mb-2w")}>
-          Prénom ou nom de la personne en immersion
-        </h3>
-        <div
-          className={fr.cx("fr-grid-row", "fr-grid-row--gutters", "fr-mb-2w")}
-        >
-          <div className={fr.cx("fr-col-12", "fr-col-md-6")}>
-            <Input
-              label="Prénom actuel du candidat"
-              nativeInputProps={{
-                value: beneficiary.firstName,
-                readOnly: true,
-              }}
-              disabled={true}
-            />
-          </div>
-          <div className={fr.cx("fr-col-12", "fr-col-md-6")}>
-            <Input
-              label="Nouveau prénom du candidat"
-              nativeInputProps={{
-                ...register("firstname"),
-                id: domElementIds.manageConvention
-                  .editConventionWithFinalStatusModalNewFirstNameInput,
-              }}
-              {...getFieldError("firstname")}
-            />
-          </div>
-        </div>
-        <div className={fr.cx("fr-grid-row", "fr-grid-row--gutters")}>
-          <div className={fr.cx("fr-col-12", "fr-col-md-6")}>
-            <Input
-              label="Nom de famille actuel du candidat"
-              nativeInputProps={{
-                value: beneficiary.lastName,
-                readOnly: true,
-              }}
-              disabled={true}
-            />
-          </div>
-          <div className={fr.cx("fr-col-12", "fr-col-md-6")}>
-            <Input
-              label="Nouveau nom de famille du candidat"
-              nativeInputProps={{
-                ...register("lastname"),
-                id: domElementIds.manageConvention
-                  .editConventionWithFinalStatusModalNewLastNameInput,
-              }}
-              {...getFieldError("lastname")}
-            />
-          </div>
-        </div>
+        <h3 className={fr.cx("fr-h6", "fr-mb-2w")}>Tuteur de l'immersion</h3>
       </div>
     </form>
   );
