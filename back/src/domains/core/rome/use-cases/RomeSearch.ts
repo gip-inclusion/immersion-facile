179d356041ef6d6e1390c61846df298d269bbf58
Benjamin Bohec <bbohec.pro@gmail.com>
2026-06-15 15:46:06 +0200

    RomeSearch with usecase builder
    Signed-off-by: Benjamin Bohec <bbohec.pro@gmail.com>

--- back/src/domains/core/rome/use-cases/RomeSearch.ts
+++ back/src/domains/core/rome/use-cases/RomeSearch.ts
@@ -3,18 +3,15 @@ import {
   type RomeDto,
   zStringMinLength1Max1024,
 } from "shared";
-import { TransactionalUseCase } from "../../UseCase";
-import type { UnitOfWork } from "../../unit-of-work/ports/UnitOfWork";
+import { useCaseBuilder } from "../../useCaseBuilder";
 
-export class RomeSearch extends TransactionalUseCase<string, RomeDto[]> {
-  protected inputSchema = zStringMinLength1Max1024;
+export type RomeSearch = ReturnType<typeof makeRomeSearch>;
 
-  public async _execute(
-    searchText: string,
-    uow: UnitOfWork,
-  ): Promise<RomeDto[]> {
-    if (searchText.length < ROME_AND_APPELLATION_MIN_SEARCH_TEXT_LENGTH)
-      return [];
-    return uow.romeRepository.searchRome(searchText);
-  }
-}
+export const makeRomeSearch = useCaseBuilder("RomeSearch")
+  .withInput(zStringMinLength1Max1024)
+  .withOutput<RomeDto[]>()
+  .build(async ({ inputParams, uow }) =>
+    inputParams.length < ROME_AND_APPELLATION_MIN_SEARCH_TEXT_LENGTH
+      ? []
+      : uow.romeRepository.searchRome(inputParams),
+  );
