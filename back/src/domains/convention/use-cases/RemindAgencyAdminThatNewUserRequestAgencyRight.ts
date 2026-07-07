965f4bda0de17b3280f12ff4cf71159b4e98ec17
Benjamin Bohec <bbohec.pro@gmail.com>
2026-07-07 12:59:10 +0200

    remind agency rights to review - refactor usecase without changing behavior
    Signed-off-by: Benjamin Bohec <bbohec.pro@gmail.com>

--- back/src/domains/convention/use-cases/RemindAgencyAdminThatNewUserRequestAgencyRight.ts
+++ back/src/domains/convention/use-cases/RemindAgencyAdminThatNewUserRequestAgencyRight.ts
@@ -1,27 +1,17 @@
 import { toPairs, uniq } from "ramda";
-import { type AbsoluteUrl, isTruthy, type UserId } from "shared";
+import { type AbsoluteUrl, isTruthy } from "shared";
 import { z } from "zod";
+import type { AgencyWithNumberOfUsersToReview } from "../../agency/ports/AgencyRepository";
 import type {
   NotificationContentAndFollowedIds,
   SaveNotificationsBatchAndRelatedEvent,
 } from "../../core/notifications/helpers/Notification";
+import type { UnitOfWork } from "../../core/unit-of-work/ports/UnitOfWork";
 import { useCaseBuilder } from "../../core/useCaseBuilder";
 
-export type AgencyNameWithUsersToReview = {
-  agencyName: string;
-  numberOfUsersToReview: number;
-};
-
-type AgencyAdminWithAgencies = {
-  userId: UserId;
-  firstName: string;
-  lastName: string;
-  email: string;
-  agencies: AgencyNameWithUsersToReview[];
-};
-
-export type RemindAgencyAdminThatNewUserRequestAgencyRightResult = {
-  remindersSent: number;
+type Deps = {
+  saveNotificationsBatchAndRelatedEvent: SaveNotificationsBatchAndRelatedEvent;
+  immersionBaseUrl: AbsoluteUrl;
 };
 
 export type RemindAgencyAdminThatNewUserRequestAgencyRight = ReturnType<
@@ -31,16 +21,45 @@ export type RemindAgencyAdminThatNewUserRequestAgencyRight = ReturnType<
 export const makeRemindAgencyAdminThatNewUserRequestAgencyRight =
   useCaseBuilder("RemindAgencyAdminThatNewUserRequestAgencyRight")
     .withInput(z.void())
-    .withOutput<RemindAgencyAdminThatNewUserRequestAgencyRightResult>()
-    .withDeps<{
-      saveNotificationsBatchAndRelatedEvent: SaveNotificationsBatchAndRelatedEvent;
-      immersionBaseUrl: AbsoluteUrl;
+    .withOutput<{
+      remindersSent: number;
     }>()
-    .build(async ({ uow, deps }) => {
-      const agenciesWithNumberOfUsersToReview =
-        await uow.agencyRepository.getAllAgenciesWithUsersToReview();
+    .withDeps<Deps>()
+    .build(
+      async ({
+        uow,
+        deps: { immersionBaseUrl, saveNotificationsBatchAndRelatedEvent },
+      }) =>
+        uow.agencyRepository
+          .getAllAgenciesWithUsersToReview()
+          .then((agenciesWithNumberOfUsersToReview) =>
+            makeNotifications({
+              uow,
+              immersionBaseUrl,
+              agenciesWithNumberOfUsersToReview,
+            }),
+          )
+          .then((notifications) =>
+            saveNotificationsBatchAndRelatedEvent(uow, notifications).then(
+              () => ({
+                remindersSent: notifications.length,
+              }),
+            ),
+          ),
+    );
 
-      const agencyAdminUserIds: UserId[] = uniq(
+const makeNotifications = async ({
+  uow,
+  immersionBaseUrl,
+  agenciesWithNumberOfUsersToReview,
+}: {
+  uow: UnitOfWork;
+  immersionBaseUrl: AbsoluteUrl;
+  agenciesWithNumberOfUsersToReview: AgencyWithNumberOfUsersToReview[];
+}): Promise<NotificationContentAndFollowedIds[]> =>
+  uow.userRepository
+    .getByIds(
+      uniq(
         agenciesWithNumberOfUsersToReview.flatMap(({ agency }) =>
           toPairs(agency.usersRights)
             .filter(([_, userRight]) =>
@@ -49,60 +68,30 @@ export const makeRemindAgencyAdminThatNewUserRequestAgencyRight =
             .map(([userId]) => userId)
             .filter(isTruthy),
         ),
-      );
-
-      const adminUsers = await uow.userRepository.getByIds(agencyAdminUserIds);
-
-      const agenciesRelatedToAdmin: AgencyAdminWithAgencies[] = adminUsers.map(
-        (admin) => {
-          const agencies: AgencyNameWithUsersToReview[] =
-            agenciesWithNumberOfUsersToReview
-              .filter(({ agency }) =>
-                toPairs(agency.usersRights).some(
-                  ([userId, userRight]) =>
-                    userId === admin.id &&
-                    userRight?.roles.includes("agency-admin"),
-                ),
+      ),
+    )
+    .then((uniqAdminUsers) =>
+      uniqAdminUsers.map(({ firstName, email, lastName, id: userId }) => ({
+        kind: "email",
+        templatedContent: {
+          kind: "AGENCY_ADMIN_NEW_USERS_TO_REVIEW_NOTIFICATION",
+          recipients: [email],
+          params: {
+            firstName,
+            lastName,
+            immersionBaseUrl,
+            agencies: agenciesWithNumberOfUsersToReview
+              .filter(({ agency: { usersRights } }) =>
+                usersRights[userId]?.roles.includes("agency-admin"),
               )
-              .map(({ agency, numberOfUsersToReview }) => ({
-                agencyName: agency.name,
+              .map(({ agency: { name }, numberOfUsersToReview }) => ({
+                agencyName: name,
                 numberOfUsersToReview,
-              }));
-
-          return {
-            userId: admin.id,
-            firstName: admin.firstName,
-            lastName: admin.lastName,
-            email: admin.email,
-            agencies,
-          };
-        },
-      );
-
-      const notifications: NotificationContentAndFollowedIds[] =
-        agenciesRelatedToAdmin.map((admin) => ({
-          kind: "email",
-          templatedContent: {
-            kind: "AGENCY_ADMIN_NEW_USERS_TO_REVIEW_NOTIFICATION",
-            recipients: [admin.email],
-            params: {
-              firstName: admin.firstName,
-              lastName: admin.lastName,
-              immersionBaseUrl: deps.immersionBaseUrl,
-              agencies: admin.agencies.map((agency) => ({
-                agencyName: agency.agencyName,
-                numberOfUsersToReview: agency.numberOfUsersToReview,
               })),
-            },
           },
-          followedIds: {
-            userId: admin.userId,
-          },
-        }));
-
-      return await deps
-        .saveNotificationsBatchAndRelatedEvent(uow, notifications)
-        .then(() => ({
-          remindersSent: notifications.length,
-        }));
-    });
+        },
+        followedIds: {
+          userId,
+        },
+      })),
+    );
