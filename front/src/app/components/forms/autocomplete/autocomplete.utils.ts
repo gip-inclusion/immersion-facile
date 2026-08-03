e5d00648a6575bb2acfc950c50b42b554ea5bbc7
tpicaud <tom.picaud@hotmail.com>
2026-08-03 17:27:36 +0200

    extract getAutocompleteValue to utils

--- front/src/app/components/forms/autocomplete/autocomplete.utils.ts
+++ front/src/app/components/forms/autocomplete/autocomplete.utils.ts
@@ -4,3 +4,19 @@ import type { PropsValue } from "react-select";
 export const isSingleOption = <T>(
   option: PropsValue<OptionType<T>>,
 ): option is OptionType<T> => option !== null && !Array.isArray(option);
+
+export const getAutocompleteValue = <T>(
+  getLabel: (value: T) => string,
+  value: T | null | undefined,
+  defaultValue: PropsValue<OptionType<T>> | undefined,
+  searchTerm: string | undefined,
+): OptionType<T> | null => {
+  if (value)
+    return {
+      label: getLabel(value),
+      value,
+    };
+  if (searchTerm === "" && defaultValue === undefined) return null;
+  if (defaultValue && isSingleOption(defaultValue)) return defaultValue;
+  return null;
+};
